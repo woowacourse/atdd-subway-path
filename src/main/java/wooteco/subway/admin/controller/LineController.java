f4466f331ff5c7eb7050f40a3a53143a94460392
toneyparky <qkrtmddhks95@gmail.com>
2020-05-12 16:03:43 +0900

    feat: Mock 서버와 응답 DTO 구현
    LineController: showLinesDetail 메서드 생성
    WholeSubwayResponse: 필드 생성 및 구현

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -3,14 +3,12 @@ package wooteco.subway.admin.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import wooteco.subway.admin.domain.Line;
-import wooteco.subway.admin.dto.LineDetailResponse;
-import wooteco.subway.admin.dto.LineRequest;
-import wooteco.subway.admin.dto.LineResponse;
-import wooteco.subway.admin.dto.LineStationCreateRequest;
+import wooteco.subway.admin.domain.Station;
+import wooteco.subway.admin.dto.*;
 import wooteco.subway.admin.service.LineService;
 
 import java.net.URI;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class LineController {
@@ -30,8 +28,21 @@ public class LineController {
     }
 
     @GetMapping("/lines")
-    public ResponseEntity<List<LineResponse>> showLine() {
+    public ResponseEntity<List<LineResponse>> showLines() {
         return ResponseEntity.ok().body(LineResponse.listOf(lineService.showLines()));
+    }
+
+    @GetMapping("/lines/detail")
+    public ResponseEntity showLinesdetail() {
+        return ResponseEntity.ok().body(WholeSubwayResponse.of(Arrays.asList(createMockResponse(), createMockResponse())));
+    }
+
+    private LineDetailResponse createMockResponse() {
+        List<Station> stations = new ArrayList<>();
+        stations.add(new Station());
+        stations.add(new Station());
+        stations.add(new Station());
+        return LineDetailResponse.of(new Line(), stations);
     }
 
     @GetMapping("/lines/{id}")
