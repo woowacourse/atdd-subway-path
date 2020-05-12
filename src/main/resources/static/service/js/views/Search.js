bb03dd989779d1455820fb7d7b811ac42e5e7dc3
moonyoungCHAE <xpf_fl@naver.com>
2020-05-12 17:27:26 +0900

    feat: front 추가

--- src/main/resources/static/service/js/views/Search.js
+++ src/main/resources/static/service/js/views/Search.js
@@ -1,28 +1,50 @@
-import { EVENT_TYPE } from '../../utils/constants.js'
+import {ERROR_MESSAGE, EVENT_TYPE, PATH_TYPE} from '../../utils/constants.js'
+import api from '../../api/index.js'
+import {searchResultTemplate} from '../../utils/templates.js'
 
 function Search() {
-  const $departureStationName = document.querySelector('#departure-station-name')
-  const $arrivalStationName = document.querySelector('#arrival-station-name')
-  const $searchButton = document.querySelector('#search-button')
-  const $searchResultContainer = document.querySelector('#search-result-container')
-  const $favoriteButton = document.querySelector('#favorite-button')
+    const $departureStationName = document.querySelector('#departure-station-name')
+    const $arrivalStationName = document.querySelector('#arrival-station-name')
+    const $searchButton = document.querySelector('#search-button')
+    const $searchResultContainer = document.querySelector('#search-result-container')
+    const $favoriteButton = document.querySelector('#favorite-button')
+    const $searchResult = document.querySelector('#search-result')
+    const $shortestDistanceTab = document.querySelector('#shortest-distance-tab')
+    const $minimumTimeTab = document.querySelector('#minimum-time-tab')
 
-  const showSearchResult = () => {
-    const isHidden = $searchResultContainer.classList.contains('hidden')
-    if (isHidden) {
-      $searchResultContainer.classList.remove('hidden')
+    const showSearchResult = data => {
+        const isHidden = $searchResultContainer.classList.contains('hidden')
+        if (isHidden) {
+            $searchResultContainer.classList.remove('hidden')
+        }
+        $searchResult.innerHTML = searchResultTemplate(data)
     }
-  }
 
-  const onSearch = event => {
-    event.preventDefault()
-    const searchInput = {
-      source: $departureStationName.value,
-      target: $arrivalStationName.value
+    const onSearchShortestDistance = event => {
+        event.preventDefault()
+        $shortestDistanceTab.classList.add('active-tab')
+        $minimumTimeTab.classList.remove('active-tab')
+        getSearchResult(PATH_TYPE.DISTANCE)
     }
-    console.log(searchInput)
-    showSearchResult(searchInput)
-  }
+
+    const onSearchMinimumTime = event => {
+        event.preventDefault()
+        $minimumTimeTab.classList.add('active-tab')
+        $shortestDistanceTab.classList.remove('active-tab')
+        getSearchResult(PATH_TYPE.DURATION)
+    }
+
+    const getSearchResult = pathType => {
+        const searchInput = {
+            source: $departureStationName.value,
+            target: $arrivalStationName.value,
+            type: pathType
+        }
+        api.path
+            .find(searchInput)
+            .then(data => showSearchResult(data))
+            .catch(error => alert(ERROR_MESSAGE.COMMON))
+    }
 
   const onToggleFavorite = event => {
     event.preventDefault()
@@ -31,26 +53,30 @@ function Search() {
 
     if (isFavorite) {
       classList.add('mdi-star-outline')
-      classList.add('text-gray-600')
-      classList.remove('mdi-star')
+        classList.add('text-gray-600')
+        classList.add('bg-yellow-500')
+        classList.remove('mdi-star')
       classList.remove('text-yellow-500')
     } else {
       classList.remove('mdi-star-outline')
-      classList.remove('text-gray-600')
-      classList.add('mdi-star')
+        classList.remove('text-gray-600')
+        classList.remove('bg-yellow-500')
+        classList.add('mdi-star')
       classList.add('text-yellow-500')
     }
   }
 
   const initEventListener = () => {
-    $favoriteButton.addEventListener(EVENT_TYPE.CLICK, onToggleFavorite)
-    $searchButton.addEventListener(EVENT_TYPE.CLICK, onSearch)
+      $favoriteButton.addEventListener(EVENT_TYPE.CLICK, onToggleFavorite)
+      $searchButton.addEventListener(EVENT_TYPE.CLICK, onSearchShortestDistance)
+      $shortestDistanceTab.addEventListener(EVENT_TYPE.CLICK, onSearchShortestDistance)
+      $minimumTimeTab.addEventListener(EVENT_TYPE.CLICK, onSearchMinimumTime)
   }
 
-  this.init = () => {
-    initEventListener()
-  }
+    this.init = () => {
+        initEventListener()
+    }
 }
 
-const login = new Search()
-login.init()
+const search = new Search()
+search.init()
