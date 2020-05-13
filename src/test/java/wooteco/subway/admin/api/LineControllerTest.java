2b785cc5fcbae9084709dfca96f753b29d415edd
Seyun <ksy90101@gmail.com>
2020-05-13 13:41:30 +0900

    feat : Etag 적용

--- src/test/java/wooteco/subway/admin/api/LineControllerTest.java
+++ src/test/java/wooteco/subway/admin/api/LineControllerTest.java
@@ -1,5 +1,6 @@
 package wooteco.subway.admin.api;
 
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -33,11 +34,12 @@ public class LineControllerTest {
 
     @Test
     void ETag() throws Exception {
-        // WholeSubwayResponse response = WholeSubwayResponse.of(Arrays.asList(createMockResponse(), createMockResponse()));
-        // given(lineService.wholeLines()).willReturn(response);
+        List<LineDetailResponse> response = Arrays.asList(createMockResponse(), createMockResponse());
+
+        given(lineService.wholeLines()).willReturn(response);
 
         // TODO: 전체 지하철 노선도 정보를 조회하는 URI 입력하기
-        String uri = "";
+        String uri = "/api/lines";
 
         MvcResult mvcResult = mockMvc.perform(get(uri))
                 .andDo(print())
