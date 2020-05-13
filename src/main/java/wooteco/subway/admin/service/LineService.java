8316cfdac03699eafb4b396832359f25a47004e6
kuenhwi <kueniyam@naver.com>
2020-05-13 13:09:09 +0900

    test: 지하철 노선도 전체 조회 인수 테스트
     - 추가적으로 전체 파일 포맷팅

--- src/main/java/wooteco/subway/admin/service/LineService.java
+++ src/main/java/wooteco/subway/admin/service/LineService.java
@@ -1,6 +1,10 @@
 package wooteco.subway.admin.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+
 import wooteco.subway.admin.domain.Line;
 import wooteco.subway.admin.domain.LineStation;
 import wooteco.subway.admin.domain.Station;
@@ -11,58 +15,60 @@ import wooteco.subway.admin.dto.WholeSubwayResponse;
 import wooteco.subway.admin.repository.LineRepository;
 import wooteco.subway.admin.repository.StationRepository;
 
-import java.util.List;
-
 @Service
 public class LineService {
-    private LineRepository lineRepository;
-    private StationRepository stationRepository;
+	private LineRepository lineRepository;
+	private StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
-        this.lineRepository = lineRepository;
-        this.stationRepository = stationRepository;
-    }
+	public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+		this.lineRepository = lineRepository;
+		this.stationRepository = stationRepository;
+	}
 
-    public Line save(Line line) {
-        return lineRepository.save(line);
-    }
+	public Line save(Line line) {
+		return lineRepository.save(line);
+	}
 
-    public List<Line> showLines() {
-        return lineRepository.findAll();
-    }
+	public List<Line> showLines() {
+		return lineRepository.findAll();
+	}
 
-    public void updateLine(Long id, LineRequest request) {
-        Line persistLine = lineRepository.findById(id).orElseThrow(RuntimeException::new);
-        persistLine.update(request.toLine());
-        lineRepository.save(persistLine);
-    }
+	public void updateLine(Long id, LineRequest request) {
+		Line persistLine = lineRepository.findById(id).orElseThrow(RuntimeException::new);
+		persistLine.update(request.toLine());
+		lineRepository.save(persistLine);
+	}
 
-    public void deleteLineById(Long id) {
-        lineRepository.deleteById(id);
-    }
+	public void deleteLineById(Long id) {
+		lineRepository.deleteById(id);
+	}
 
-    public void addLineStation(Long id, LineStationCreateRequest request) {
-        Line line = lineRepository.findById(id).orElseThrow(RuntimeException::new);
-        LineStation lineStation = new LineStation(request.getPreStationId(), request.getStationId(), request.getDistance(), request.getDuration());
-        line.addLineStation(lineStation);
+	public void addLineStation(Long id, LineStationCreateRequest request) {
+		Line line = lineRepository.findById(id).orElseThrow(RuntimeException::new);
+		LineStation lineStation = new LineStation(request.getPreStationId(), request.getStationId(),
+			request.getDistance(), request.getDuration());
+		line.addLineStation(lineStation);
 
-        lineRepository.save(line);
-    }
+		lineRepository.save(line);
+	}
 
-    public void removeLineStation(Long lineId, Long stationId) {
-        Line line = lineRepository.findById(lineId).orElseThrow(RuntimeException::new);
-        line.removeLineStationById(stationId);
-        lineRepository.save(line);
-    }
+	public void removeLineStation(Long lineId, Long stationId) {
+		Line line = lineRepository.findById(lineId).orElseThrow(RuntimeException::new);
+		line.removeLineStationById(stationId);
+		lineRepository.save(line);
+	}
 
-    public LineDetailResponse findLineWithStationsById(Long id) {
-        Line line = lineRepository.findById(id).orElseThrow(RuntimeException::new);
-        List<Station> stations = stationRepository.findAllById(line.getLineStationsId());
-        return LineDetailResponse.of(line, stations);
-    }
+	public LineDetailResponse findLineWithStationsById(Long id) {
+		Line line = lineRepository.findById(id)
+			.orElseThrow(RuntimeException::new);
+		List<Station> stations = stationRepository.findAllById(line.getLineStationsId());
+		return LineDetailResponse.of(line, stations);
+	}
 
-    // TODO: 구현하세요 :)
-    public WholeSubwayResponse wholeLines() {
-        return null;
-    }
+	public WholeSubwayResponse wholeLines() {
+		List<Line> lines = lineRepository.findAll();
+		return lines.stream()
+			.map(line -> findLineWithStationsById(line.getId()))
+			.collect(Collectors.collectingAndThen(Collectors.toList(), WholeSubwayResponse::of));
+	}
 }
--- src/test/java/wooteco/subway/admin/acceptance/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/admin/acceptance/LineAcceptanceTest.java
@@ -1,50 +1,78 @@
 package wooteco.subway.admin.acceptance;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import wooteco.subway.admin.dto.LineDetailResponse;
-import wooteco.subway.admin.dto.LineResponse;
+import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import wooteco.subway.admin.dto.LineDetailResponse;
+import wooteco.subway.admin.dto.LineResponse;
+import wooteco.subway.admin.dto.StationResponse;
 
 public class LineAcceptanceTest extends AcceptanceTest {
-    @DisplayName("지하철 노선을 관리한다")
-    @Test
-    void manageLine() {
-        // when
-        createLine(LINE_NAME_SINBUNDANG);
-        createLine(LINE_NAME_BUNDANG);
-        createLine(LINE_NAME_2);
-        createLine(LINE_NAME_3);
-        // then
-        List<LineResponse> lines = getLines();
-        assertThat(lines.size()).isEqualTo(4);
+	@DisplayName("지하철 노선을 관리한다")
+	@Test
+	void manageLine() {
+		// when
+		createLine(LINE_NAME_SINBUNDANG);
+		createLine(LINE_NAME_BUNDANG);
+		createLine(LINE_NAME_2);
+		createLine(LINE_NAME_3);
+		// then
+		List<LineResponse> lines = getLines();
+		assertThat(lines.size()).isEqualTo(4);
 
-        // when
-        LineDetailResponse line = getLine(lines.get(0).getId());
-        // then
-        assertThat(line.getId()).isNotNull();
-        assertThat(line.getName()).isNotNull();
-        assertThat(line.getStartTime()).isNotNull();
-        assertThat(line.getEndTime()).isNotNull();
-        assertThat(line.getIntervalTime()).isNotNull();
+		// when
+		LineDetailResponse line = getLine(lines.get(0).getId());
+		// then
+		assertThat(line.getId()).isNotNull();
+		assertThat(line.getName()).isNotNull();
+		assertThat(line.getStartTime()).isNotNull();
+		assertThat(line.getEndTime()).isNotNull();
+		assertThat(line.getIntervalTime()).isNotNull();
 
-        // when
-        LocalTime startTime = LocalTime.of(8, 00);
-        LocalTime endTime = LocalTime.of(22, 00);
-        updateLine(line.getId(), startTime, endTime);
-        //then
-        LineDetailResponse updatedLine = getLine(line.getId());
-        assertThat(updatedLine.getStartTime()).isEqualTo(startTime);
-        assertThat(updatedLine.getEndTime()).isEqualTo(endTime);
+		// when
+		LocalTime startTime = LocalTime.of(8, 00);
+		LocalTime endTime = LocalTime.of(22, 00);
+		updateLine(line.getId(), startTime, endTime);
+		//then
+		LineDetailResponse updatedLine = getLine(line.getId());
+		assertThat(updatedLine.getStartTime()).isEqualTo(startTime);
+		assertThat(updatedLine.getEndTime()).isEqualTo(endTime);
 
-        // when
-        deleteLine(line.getId());
-        // then
-        List<LineResponse> linesAfterDelete = getLines();
-        assertThat(linesAfterDelete.size()).isEqualTo(3);
-    }
+		// when
+		deleteLine(line.getId());
+		// then
+		List<LineResponse> linesAfterDelete = getLines();
+		assertThat(linesAfterDelete.size()).isEqualTo(3);
+	}
+
+	@DisplayName("지하철 노선도 전체 정보 조회")
+	@Test
+	void wholeSubway() {
+		LineResponse lineTwoResponse = createLine(LINE_NAME_2);
+		StationResponse stationKangnamResponse = createStation(STATION_NAME_KANGNAM);
+		StationResponse stationYeoksamResponse = createStation(STATION_NAME_YEOKSAM);
+		StationResponse stationSamsungResponse = createStation(STATION_NAME_SAMSUNG);
+
+		addLineStation(lineTwoResponse.getId(), null, stationKangnamResponse.getId());
+		addLineStation(lineTwoResponse.getId(), stationKangnamResponse.getId(), stationYeoksamResponse.getId());
+		addLineStation(lineTwoResponse.getId(), stationYeoksamResponse.getId(), stationSamsungResponse.getId());
+
+		LineResponse lineSinbundangResponse = createLine(LINE_NAME_SINBUNDANG);
+		StationResponse stationYangjaeResponse = createStation(STATION_NAME_YANGJAE);
+		StationResponse stationYangjaeForestResponse = createStation(STATION_NAME_YANGJAE_FOREST);
+		addLineStation(lineSinbundangResponse.getId(), null, stationKangnamResponse.getId());
+		addLineStation(lineSinbundangResponse.getId(), stationKangnamResponse.getId(), stationYangjaeResponse.getId());
+		addLineStation(lineSinbundangResponse.getId(), stationYangjaeResponse.getId(),
+			stationYangjaeForestResponse.getId());
+
+		List<LineDetailResponse> lineDetailResponses = retrieveWholeSubway().getLineDetailResponses();
+		assertThat(lineDetailResponses.size()).isEqualTo(2);
+		assertThat(lineDetailResponses.get(0).getStations().size()).isEqualTo(3);
+		assertThat(lineDetailResponses.get(1).getStations().size()).isEqualTo(3);
+	}
 }
