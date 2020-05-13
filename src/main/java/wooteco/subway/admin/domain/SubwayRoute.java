36f4e9fa12e7b720a49e39c459a229215486d5b2
kouz <lastpuzly@gmail.com>
2020-05-13 20:20:10 +0900

    feat: SubwayRoute 구현

---
+++ src/main/java/wooteco/subway/admin/domain/SubwayRoute.java
@@ -0,0 +1,31 @@
+package wooteco.subway.admin.domain;
+
+import java.util.List;
+
+import org.jgrapht.GraphPath;
+
+public class SubwayRoute {
+    private final GraphPath<Long, RouteEdge> path;
+
+    public SubwayRoute(GraphPath<Long, RouteEdge> path) {
+        this.path = path;
+    }
+
+    public List<Long> getShortestPath() {
+        return path.getVertexList();
+    }
+
+    public int calculateTotalDistance() {
+        return path.getEdgeList()
+            .stream()
+            .mapToInt(RouteEdge::getDistance)
+            .sum();
+    }
+
+    public int calculateTotalDuration() {
+        return path.getEdgeList()
+            .stream()
+            .mapToInt(RouteEdge::getDuration)
+            .sum();
+    }
+}
