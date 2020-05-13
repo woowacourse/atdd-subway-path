28264192f6ea84ee2696965f40448f112471b53b
toneyparky <qkrtmddhks95@gmail.com>
2020-05-13 16:28:19 +0900

    feat: 인수 조건을 바탕으로 시나리오 작성

---
+++ src/test/java/wooteco/subway/admin/acceptance/PathAcceptanceTest.java
@@ -0,0 +1,9 @@
+package wooteco.subway.admin.acceptance;
+
+public class PathAcceptanceTest {
+//	Feature: 최단거리 경로를 구한다.
+//
+//		Given 지하철 노선에 지하철역이 추가되어있다.
+//		When A역부터 B역까지 최단거리 경로를 구하고 싶다.
+//		Then A역부터 B역까지 최단거리 경로가 구해졌다.
+}
