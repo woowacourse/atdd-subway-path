afdf0e117598e90fb4f7e5ffac4875e655089d29
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-05-13 15:35:53 +0900

    test: 전체 지하철 정보 조회 인수테스트 작성

---
+++ src/test/java/wooteco/subway/admin/acceptance/WholeSubwayLineAcceptanceTest.java
@@ -0,0 +1,45 @@
+package wooteco.subway.admin.acceptance;
+
+import org.junit.jupiter.api.Test;
+import wooteco.subway.admin.dto.LineDetailResponse;
+import wooteco.subway.admin.dto.LineResponse;
+import wooteco.subway.admin.dto.StationResponse;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WholeSubwayLineAcceptanceTest extends AcceptanceTest {
+
+    //    Feature: 전체 지하철 노선도 정보 조회
+//    Scenario: 지하철 노선도 정보 조회를 한다.
+    @Test
+    void wholeSubway() {
+
+        //given
+        LineResponse lineResponse1 = createLine("2호선");
+        StationResponse stationResponse1 = createStation("강남역");
+        StationResponse stationResponse2 = createStation("역삼역");
+        StationResponse stationResponse3 = createStation("삼성역");
+        addLineStation(lineResponse1.getId(), null, stationResponse1.getId());
+        addLineStation(lineResponse1.getId(), stationResponse1.getId(), stationResponse2.getId());
+        addLineStation(lineResponse1.getId(), stationResponse2.getId(), stationResponse3.getId());
+        //and
+        LineResponse lineResponse2 = createLine("2호선");
+        StationResponse stationResponse4 = createStation("강남역");
+        StationResponse stationResponse5 = createStation("양재역");
+        StationResponse stationResponse6 = createStation("양재시민의숲역");
+        addLineStation(lineResponse2.getId(), null, stationResponse4.getId());
+        addLineStation(lineResponse2.getId(), stationResponse4.getId(), stationResponse5.getId());
+        addLineStation(lineResponse2.getId(), stationResponse5.getId(), stationResponse6.getId());
+
+        //when
+        List<LineDetailResponse> responses = retrieveWholeSubway().getLineDetailResponses();
+
+        //then
+        assertThat(responses.size()).isEqualTo(2);
+        assertThat(responses.get(0).getStations().size()).isEqualTo(3);
+        assertThat(responses.get(1).getStations().size()).isEqualTo(3);
+
+    }
+}
