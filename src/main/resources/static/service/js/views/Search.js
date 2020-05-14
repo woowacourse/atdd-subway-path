c9799c3c7a6f173bc6b4d8843b19e36c72f75c9f
kouz <lastpuzly@gmail.com>
2020-05-14 15:20:48 +0900

    feat: 지하철 경로 검색 - front 구현

--- src/main/resources/static/service/js/views/Search.js
+++ src/main/resources/static/service/js/views/Search.js
@@ -1,5 +1,6 @@
 import { EVENT_TYPE } from '../../utils/constants.js'
-import api from '../../api/index.js';
+import api from '../../api/index.js'
+import { searchResultTemplate } from '../../utils/templates.js'
 
 function Search() {
   const $departureStationName = document.querySelector('#departure-station-name')
@@ -7,26 +8,32 @@ function Search() {
   const $searchButton = document.querySelector('#search-button')
   const $searchResultContainer = document.querySelector('#search-result-container')
   const $favoriteButton = document.querySelector('#favorite-button')
+  const $searchResult = document.querySelector('#search-result')
+  const $shortestDistanceTab = document.querySelector('#shortest-distance-tab')
+  const $minimumTimeTab = document.querySelector('#minimum-time-tab')
 
-  const showSearchResult = () => {
+  const showSearchResult = data => {
     const isHidden = $searchResultContainer.classList.contains('hidden')
     if (isHidden) {
       $searchResultContainer.classList.remove('hidden')
     }
+    $searchResult.innerHTML = searchResultTemplate(data)
   }
 
-  const onSearch = async event => {
+  const onSearchShortestDuration = event => {
     event.preventDefault()
+    $minimumTimeTab.classList.add('active-tab')
+    $shortestDistanceTab.classList.remove('active-tab')
+    const results = JSON.parse(localStorage.getItem("results"));
+    showSearchResult(results.response.duration)
+  }
 
-    const searchInput = {
-      source: $departureStationName.value,
-      target: $arrivalStationName.value
-    }
-
-    const response = await api.path.find(searchInput);
-    console.log(response);
-    console.log(searchInput)
-    showSearchResult(searchInput)
+  const onSearchShortestDistance = event => {
+    event.preventDefault()
+    $shortestDistanceTab.classList.add('active-tab')
+    $minimumTimeTab.classList.remove('active-tab')
+    const results = JSON.parse(localStorage.getItem("results"));
+    showSearchResult(results.response.distance)
   }
 
   const onToggleFavorite = event => {
@@ -37,19 +44,34 @@ function Search() {
     if (isFavorite) {
       classList.add('mdi-star-outline')
       classList.add('text-gray-600')
+      classList.add('bg-yellow-500')
       classList.remove('mdi-star')
       classList.remove('text-yellow-500')
     } else {
       classList.remove('mdi-star-outline')
       classList.remove('text-gray-600')
+      classList.remove('bg-yellow-500')
       classList.add('mdi-star')
       classList.add('text-yellow-500')
     }
   }
 
+  const onSearch = async (event) => {
+    event.preventDefault();
+    const searchInput = {
+      source: $departureStationName.value,
+      target: $arrivalStationName.value,
+    }
+    const responses = await api.path.find(searchInput);
+    localStorage.setItem("results", JSON.stringify(responses));
+    onSearchShortestDistance(event);
+  }
+
   const initEventListener = () => {
     $favoriteButton.addEventListener(EVENT_TYPE.CLICK, onToggleFavorite)
     $searchButton.addEventListener(EVENT_TYPE.CLICK, onSearch)
+    $shortestDistanceTab.addEventListener(EVENT_TYPE.CLICK, onSearchShortestDistance)
+    $minimumTimeTab.addEventListener(EVENT_TYPE.CLICK, onSearchShortestDuration)
   }
 
   this.init = () => {
@@ -57,5 +79,5 @@ function Search() {
   }
 }
 
-const login = new Search()
-login.init()
+const search = new Search()
+search.init()
