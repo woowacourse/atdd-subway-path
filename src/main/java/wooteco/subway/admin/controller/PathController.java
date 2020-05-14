bd2b71849be90e05a6280fc6f9cce479cae98123
Andy H. Jung <commaniakr@gmail.com>
2020-05-14 16:35:28 +0900

    feat: 경로 조회에 빈 값 혹은 null 입력시 예외 처리

--- src/main/java/wooteco/subway/admin/controller/PathController.java
+++ src/main/java/wooteco/subway/admin/controller/PathController.java
@@ -1,6 +1,7 @@
 package wooteco.subway.admin.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,11 @@ import wooteco.subway.admin.dto.PathResponse;
 import wooteco.subway.admin.service.LineService;
 import wooteco.subway.admin.service.PathService;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @RestController
+@Validated
 public class PathController {
     private final LineService lineService;
     private final PathService pathService;
@@ -19,9 +24,9 @@ public class PathController {
     }
 
     @GetMapping("/paths")
-    public ResponseEntity<PathResponse> findPath(@RequestParam String source,
-                                                 @RequestParam String target,
-                                                 @RequestParam String type) {
+    public ResponseEntity<PathResponse> findPath(@RequestParam @NotNull @NotBlank(message = "시작역이 없습니다!") String source,
+                                                 @RequestParam @NotNull @NotBlank(message = "끝역이 없습니다!") String target,
+                                                 @RequestParam @NotNull @NotBlank(message = "검색 기준을 선택해주세요") String type) {
         return ResponseEntity.ok()
                 .body(pathService.calculatePath(source, target, type));
     }
