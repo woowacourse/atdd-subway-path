4ef1311841dda8f2e989fc9c050ef5621a95b34c
Tak Hyungmin <milkho11@naver.com>
2020-05-14 17:01:43 +0900

    feat: 출발역과 도착역이 같은 경우
    ErrorResponse 공통된 에러 포맷을 반환하기 위해 생성

--- src/main/java/wooteco/subway/admin/controller/PathController.java
+++ src/main/java/wooteco/subway/admin/controller/PathController.java
@@ -2,15 +2,19 @@ package wooteco.subway.admin.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.admin.dto.PathResponse;
+import wooteco.subway.admin.dto.error.ErrorResponse;
+import wooteco.subway.admin.exception.OverlappedStationException;
 import wooteco.subway.admin.service.LineService;
 import wooteco.subway.admin.service.PathService;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestController
 @Validated
@@ -24,10 +28,25 @@ public class PathController {
     }
 
     @GetMapping("/paths")
-    public ResponseEntity<PathResponse> findPath(@RequestParam @NotNull @NotBlank(message = "시작역이 없습니다!") String source,
-                                                 @RequestParam @NotNull @NotBlank(message = "끝역이 없습니다!") String target,
-                                                 @RequestParam @NotNull @NotBlank(message = "검색 기준을 선택해주세요") String type) {
+    public ResponseEntity<PathResponse> findPath(@RequestParam @NotBlank(message = "시작역이 없습니다!") String source,
+                                                 @RequestParam @NotBlank(message = "끝역이 없습니다!") String target,
+                                                 @RequestParam @NotBlank(message = "검색 기준을 선택해주세요") String type) {
+        validateOverlappedStation(source, target);
         return ResponseEntity.ok()
                 .body(pathService.calculatePath(source, target, type));
+    }
+
+    private void validateOverlappedStation(String source, String target) {
+        if (source.equals(target)) {
+            throw new OverlappedStationException();
+        }
+    }
+
+    @ExceptionHandler(OverlappedStationException.class)
+    public ResponseEntity<ErrorResponse> overlappedStationExceptionHandler(OverlappedStationException exception) {
+        final ErrorResponse response = ErrorResponse.of(BAD_REQUEST.value(), exception.getMessage());
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(response);
     }
 }
