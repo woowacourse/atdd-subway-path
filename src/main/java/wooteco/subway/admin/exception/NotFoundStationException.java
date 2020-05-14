16c323cc03d7d0121e4a5beb319456613e9583a0
ordinCode <ysh1346@naver.com>
2020-05-14 22:02:16 +0900

    style

--- src/main/java/wooteco/subway/admin/exception/NotFoundStationException.java
+++ src/main/java/wooteco/subway/admin/exception/NotFoundStationException.java
@@ -1,7 +1,6 @@
 package wooteco.subway.admin.exception;
 
 public class NotFoundStationException extends IllegalArgumentException {
-
     public static final String ERROR_MESSAGE = "존재하지 않는 역입니다 [%s]";
 
     public NotFoundStationException(String name) {
--- src/test/java/wooteco/subway/admin/acceptance/PageAcceptanceTest.java
+++ src/test/java/wooteco/subway/admin/acceptance/PageAcceptanceTest.java
@@ -4,19 +4,10 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql("/truncate.sql")
 public class PageAcceptanceTest extends AcceptanceTest{
     @LocalServerPort
     int port;
