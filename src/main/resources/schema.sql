3294a49e2879cffa5a06b180ad5346b937edef87
toneyparky <qkrtmddhks95@gmail.com>
2020-05-14 16:49:13 +0900

    chore: 디폴트 데이터 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -32,22 +32,53 @@ create table if not exists LINE_STATION
 );
 
 -- 테스트시 주석 처리가 필요합니다.
--- INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('1호선', '03:00', '19:00', 15, 'bg-blue-800');
--- INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('2호선', '05:00', '23:00', 5, 'bg-green-500');
--- INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('4호선', '05:00', '23:00', 10, 'bg-blue-300');
--- INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('5호선', '05:00', '23:00', 10, 'bg-purple-500');
--- INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('9호선', '05:00', '23:00', 9, 'bg-yellow-500');
---
--- INSERT INTO STATION (name) VALUES('구로');
--- INSERT INTO STATION (name) VALUES('신도림');
--- INSERT INTO STATION (name) VALUES('신길');
--- INSERT INTO STATION (name) VALUES('용산');
--- INSERT INTO STATION (name) VALUES('서울역');
--- INSERT INTO STATION (name) VALUES('충정로');
--- INSERT INTO STATION (name) VALUES('당산');
--- INSERT INTO STATION (name) VALUES('영등포구청');
--- INSERT INTO STATION (name) VALUES('대림');
--- INSERT INTO STATION (name) VALUES('여의도');
--- INSERT INTO STATION (name) VALUES('동작');
--- INSERT INTO STATION (name) VALUES('삼각지');
--- INSERT INTO STATION (name) VALUES('시청');
+INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('1호선', '03:00', '19:00', 15, 'bg-blue-800');
+INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('2호선', '05:00', '23:00', 5, 'bg-green-500');
+INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('4호선', '05:00', '23:00', 10, 'bg-blue-300');
+INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('5호선', '05:00', '23:00', 10, 'bg-purple-500');
+INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('9호선', '05:00', '23:00', 9, 'bg-yellow-500');
+
+INSERT INTO STATION (name)
+VALUES
+('구로'),
+('신도림'),
+('신길'),
+('용산'),
+('서울역'),
+('충정로'),
+('당산'),
+('영등포구청'),
+('대림'),
+('여의도'),
+('동작'),
+('삼각지'),
+('시청');
+
+INSERT INTO LINE_STATION (line, line_key, pre_station_id, station_id, distance, duration)
+VALUES
+(1, 0, null, 1, 10, 10),
+(1, 1, 1, 2, 10, 10),
+(1, 2, 2, 3, 10, 10),
+(1, 3, 3, 4, 10, 10),
+(1, 4, 4, 5, 10, 10),
+(1, 5, 5, 13, 10, 10),
+
+(2, 0, null, 13, 10, 10),
+(2, 1, 13, 6, 10, 10),
+(2, 2, 6, 7, 10, 10),
+(2, 3, 7, 8, 10, 10),
+(2, 4, 8, 2, 10, 10),
+(2, 5, 2, 9, 10, 10),
+
+(3, 0, null, 11, 10, 10),
+(3, 1, 11, 12, 10, 10),
+(3, 2, 12, 5, 10, 10),
+
+(4, 0, null, 8, 10, 10),
+(4, 1, 8, 3, 10, 10),
+(4, 2, 3, 10, 10, 10),
+(4, 3, 10, 6, 10, 10),
+
+(5, 0, null, 7, 10, 10),
+(5, 1, 7, 10, 10, 10),
+(5, 2, 10, 11, 10, 10);
