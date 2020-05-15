b4ba88f9179fc1a1d43cef24472951499a6fc598
Andy H. Jung <commaniakr@gmail.com>
2020-05-15 15:12:52 +0900

    feat: 노선도 fixture 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -30,29 +30,30 @@ create table if not exists LINE_STATION
     updated_at datetime
 );
 
-INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('1호선', '03:00', '19:00', 15, 'bg-blue-600');
-INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('2호선', '05:00', '23:00', 10, 'bg-green-500');
+    INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('2호선', '03:00', '19:00', 15, 'bg-blue-600');
+    INSERT INTO LINE (name, start_time, end_time, interval_time, color) VALUES('8호선', '05:00', '23:00', 10, 'bg-green-500');
 
-INSERT INTO STATION (name) VALUES('구로');
-INSERT INTO STATION (name) VALUES('신도림');
-INSERT INTO STATION (name) VALUES('영등포');
-INSERT INTO STATION (name) VALUES('신길');
+    INSERT INTO STATION (name) VALUES('잠실');
+    INSERT INTO STATION (name) VALUES('잠실새내');
+    INSERT INTO STATION (name) VALUES('종합운동장');
+    INSERT INTO STATION (name) VALUES('삼전');
+    INSERT INTO STATION (name) VALUES('석촌고분');
+    INSERT INTO STATION (name) VALUES('석촌');
 
-INSERT INTO STATION (name) VALUES('문래');
-INSERT INTO STATION (name) VALUES('대림');
+    -- null <-> 잠실
+    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 1, null, 0, 0);
+    -- 잠실 <-> 잠실새내
+    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 2, 1, 1, 20);
+    -- 잠실새내 <-> 종합운동장
+    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 3, 2, 1, 30);
 
--- null <-> 구로
-INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 1, null, 10, 10);
--- 구로 <-> 신도림
-INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 2, 1, 10, 10);
--- 신도림 <-> 영등포
-INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 3, 2, 10, 10);
--- 영등포 <-> 신길
-INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 4, 3, 10, 10);
-
--- null <-> 문래
-INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 5, null, 10, 10);
--- 문래 <-> 신도림
-INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 2, 5, 10, 10);
--- 신도림 <-> 대림
-INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 6, 2, 10, 10);
+    -- null <-> 종합운동장
+    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 3, null, 0, 0);
+    -- 종합운동장 <-> 삼전
+    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 4, 3, 20, 1);
+    -- 삼전 <-> 석촌고분
+    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 5, 4, 30, 1);
+    -- 석촌고분 <-> 석촌
+    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 6, 5, 40, 1);
+    -- 석촌 <-> 잠실
+    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 1, 6, 50, 1);
