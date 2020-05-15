90fedcae2c375deca5368ad2f31bd6da4539a4e1
Tak Hyungmin <milkho11@naver.com>
2020-05-15 17:07:44 +0900

    refactor: SQL 더미 데이터 삭제

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -40,20 +40,20 @@ create table if not exists LINE_STATION
     INSERT INTO STATION (name) VALUES('석촌고분');
     INSERT INTO STATION (name) VALUES('석촌');
 
-    -- null <-> 잠실
-    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 1, null, 0, 0);
-    -- 잠실 <-> 잠실새내
-    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 2, 1, 1, 20);
-    -- 잠실새내 <-> 종합운동장
-    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 3, 2, 1, 30);
-
-    -- null <-> 종합운동장
-    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 3, null, 0, 0);
-    -- 종합운동장 <-> 삼전
-    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 4, 3, 20, 1);
-    -- 삼전 <-> 석촌고분
-    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 5, 4, 30, 1);
-    -- 석촌고분 <-> 석촌
-    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 6, 5, 40, 1);
-    -- 석촌 <-> 잠실
-    INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 1, 6, 50, 1);
+--     -- null <-> 잠실
+--     INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 1, null, 0, 0);
+--     -- 잠실 <-> 잠실새내
+--     INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 2, 1, 1, 20);
+--     -- 잠실새내 <-> 종합운동장
+--     INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (1, 3, 2, 1, 30);
+--
+--     -- null <-> 종합운동장
+--     INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 3, null, 0, 0);
+--     -- 종합운동장 <-> 삼전
+--     INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 4, 3, 20, 1);
+--     -- 삼전 <-> 석촌고분
+--     INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 5, 4, 30, 1);
+--     -- 석촌고분 <-> 석촌
+--     INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 6, 5, 40, 1);
+--     -- 석촌 <-> 잠실
+--     INSERT INTO LINE_STATION (line, station_id, pre_station_id, distance, duration) VALUES (2, 1, 6, 50, 1);
