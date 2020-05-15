e035eda5b6a6f58d69d25b45c96f9e305d6a429a
chanin <pci2676@gmail.com>
2020-05-15 16:23:26 +0900

    refactor : StationController에서 StationRepository 의존 제거

--- src/main/java/wooteco/subway/admin/config/SubwayDefaultDataConfiguration.java
+++ src/main/java/wooteco/subway/admin/config/SubwayDefaultDataConfiguration.java
@@ -14,7 +14,6 @@ import java.time.LocalTime;
 
 @Configuration
 public class SubwayDefaultDataConfiguration {
-
     @Profile("local")
     @Configuration
     private class LocalDataApplicationRunner implements ApplicationRunner {
@@ -29,15 +28,15 @@ public class SubwayDefaultDataConfiguration {
 
         @Override
         public void run(final ApplicationArguments args) throws Exception {
-            Station station1 = stationRepository.save(new Station("터틀"));
-            Station station2 = stationRepository.save(new Station("비밥"));
-            Station station3 = stationRepository.save(new Station("포비"));
+            Station station1 = stationRepository.save(new Station("중랑"));
+            Station station2 = stationRepository.save(new Station("청량리"));
+            Station station3 = stationRepository.save(new Station("왕십리"));
 
-            Line lineNumber1 = new Line("1호선", LocalTime.of(05, 10), LocalTime.of(22, 10), 10);
+            Line lineNumber1 = new Line("경의중앙선", LocalTime.of(05, 10), LocalTime.of(22, 10), 10);
             lineNumber1.addEdge(new Edge(null, station1.getId(), 10, 10));
             lineNumber1.addEdge(new Edge(station1.getId(), station2.getId(), 1, 10));
             lineNumber1.addEdge(new Edge(station2.getId(), station3.getId(), 10, 10));
-            Line lineNumber2 = new Line("2호선", LocalTime.of(05, 20), LocalTime.of(23, 30), 15);
+            Line lineNumber2 = new Line("분당선", LocalTime.of(05, 20), LocalTime.of(23, 30), 15);
             lineNumber2.addEdge(new Edge(null, station2.getId(), 10, 10));
             lineNumber2.addEdge(new Edge(station2.getId(), station3.getId(), 1, 10));
 
--- src/main/java/wooteco/subway/admin/controller/PageController.java
+++ src/main/java/wooteco/subway/admin/controller/PageController.java
@@ -2,41 +2,32 @@ package wooteco.subway.admin.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import wooteco.subway.admin.repository.StationRepository;
 
 @Controller
 public class PageController {
-    private StationRepository stationRepository;
-
-    public PageController(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
-    }
-
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String index() {
         return "admin/index";
     }
 
     @GetMapping(value = "/stations", produces = MediaType.TEXT_HTML_VALUE)
-    public String stationPage(Model model) {
-        model.addAttribute("stations", stationRepository.findAll());
+    public String stationPage() {
         return "admin/admin-station";
     }
 
     @GetMapping(value = "/edges", produces = MediaType.TEXT_HTML_VALUE)
-    public String edgePage(Model model) {
+    public String edgePage() {
         return "admin/admin-edge";
     }
 
     @GetMapping(value = "/lines/map", produces = MediaType.TEXT_HTML_VALUE)
-    public String mapPage(Model model) {
+    public String mapPage() {
         return "service/map";
     }
 
     @GetMapping(value = "/search", produces = MediaType.TEXT_HTML_VALUE)
-    public String searchPage(Model model) {
+    public String searchPage() {
         return "service/search";
     }
 }
