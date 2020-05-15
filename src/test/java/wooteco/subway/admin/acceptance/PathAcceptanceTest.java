5ee22006565c47955dffe081f12093bc4922bcfd
Tak Hyungmin <milkho11@naver.com>
2020-05-15 13:53:01 +0900

    feat: 최소 시간 기준 경로 찾기 인수 테스트 작성

--- src/test/java/wooteco/subway/admin/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/admin/acceptance/PathAcceptanceTest.java
@@ -11,26 +11,72 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Sql("/truncate.sql")
 public class PathAcceptanceTest extends AcceptanceTest {
-    @DisplayName("지하철 경로를 조회한다.")
+
+    private static final String JAMSIL_STATION_NAME = "잠실역";
+    private static final String JAMSIL_SAENAE_STATION_NAME = "잠실새내역";
+    private static final String OLYMPIC_STATION_NAME = "종합운동장역";
+    private static final String SAMJEON_STATION_NAME = "삼전역";
+    private static final String SEOKCHON_TOMB_STATION_NAME = "석촌고분역";
+    private static final String SEOKCHON_STATION_NAME = "석촌역";
+    private static final String SECOND_LINE_NAME = "2호선";
+    private static final String EIGHT_LINE_NAME = "8호선";
+
+    private LineResponse secondLineResponse;
+    private StationResponse jamsilStationResponse;
+    private StationResponse jamsilSaenaeResponse;
+    private StationResponse olympicStationResponse;
+    private LineResponse eighthLineResponse;
+    private StationResponse samjeonStationResponse;
+    private StationResponse seokchonTombStationResponse;
+    private StationResponse seokchonStationResponse;
+
+    private void initialize() {
+        LineResponse secondLineResponse = createLine(SECOND_LINE_NAME);
+        StationResponse jamsilStationResponse = createStation(JAMSIL_STATION_NAME);
+        StationResponse jamsilSaenaeResponse = createStation(JAMSIL_SAENAE_STATION_NAME);
+        StationResponse olympicStationResponse = createStation(OLYMPIC_STATION_NAME);
+        addLineStation(secondLineResponse.getId(), null, jamsilStationResponse.getId(), 0, 0);
+        addLineStation(secondLineResponse.getId(), jamsilStationResponse.getId(), jamsilSaenaeResponse.getId(), 1, 20);
+        addLineStation(secondLineResponse.getId(), jamsilSaenaeResponse.getId(), olympicStationResponse.getId(), 1, 30);
+
+        LineResponse eighthLineResponse = createLine(EIGHT_LINE_NAME);
+        StationResponse samjeonStationResponse = createStation(SAMJEON_STATION_NAME);
+        StationResponse seokchonTombStationResponse = createStation(SEOKCHON_TOMB_STATION_NAME);
+        StationResponse seokchonStationResponse = createStation(SEOKCHON_STATION_NAME);
+        addLineStation(eighthLineResponse.getId(), null, olympicStationResponse.getId(), 0, 0);
+        addLineStation(eighthLineResponse.getId(), olympicStationResponse.getId(), samjeonStationResponse.getId(), 20, 1);
+        addLineStation(eighthLineResponse.getId(), samjeonStationResponse.getId(), seokchonTombStationResponse.getId(), 30, 1);
+        addLineStation(eighthLineResponse.getId(), seokchonTombStationResponse.getId(), seokchonStationResponse.getId(), 40, 1);
+        addLineStation(eighthLineResponse.getId(), seokchonStationResponse.getId(), jamsilStationResponse.getId(), 50, 1);
+    }
+
+    @DisplayName("최소 거리 지하철 경로를 조회한다.")
     @Test
-    void findPaths() {
+    void findPathsByDistance() {
+        initialize();
 
-        StationResponse stationResponse1 = createStation(STATION_NAME_KANGNAM);
-        StationResponse stationResponse2 = createStation(STATION_NAME_YEOKSAM);
-        StationResponse stationResponse3 = createStation(STATION_NAME_SEOLLEUNG);
+        PathResponse response = getPath(JAMSIL_STATION_NAME, OLYMPIC_STATION_NAME, "DISTANCE");
 
-        LineResponse lineResponse = createLine("2호선");
+        assertThat(response.getStations().get(0).getName()).isEqualTo(JAMSIL_STATION_NAME);
+        assertThat(response.getStations().get(1).getName()).isEqualTo(JAMSIL_SAENAE_STATION_NAME);
+        assertThat(response.getStations().get(2).getName()).isEqualTo(OLYMPIC_STATION_NAME);
+        assertThat(response.getDistance()).isEqualTo(2);
+        assertThat(response.getDuration()).isEqualTo(50);
+    }
 
-        addLineStation(lineResponse.getId(), null, stationResponse1.getId());
-        addLineStation(lineResponse.getId(), stationResponse1.getId(), stationResponse2.getId());
-        addLineStation(lineResponse.getId(), stationResponse2.getId(), stationResponse3.getId());
+    @DisplayName("최소 시간 지하철 경로를 조회한다.")
+    @Test
+    void findPathsByDuration() {
+        initialize();
 
-        PathResponse response = getPath(STATION_NAME_KANGNAM, STATION_NAME_SEOLLEUNG, "DISTANCE");
+        PathResponse response = getPath(JAMSIL_STATION_NAME, OLYMPIC_STATION_NAME, "DURATION");
 
-        assertThat(response.getStations().get(0).getName()).isEqualTo(STATION_NAME_KANGNAM);
-        assertThat(response.getStations().get(1).getName()).isEqualTo(STATION_NAME_YEOKSAM);
-        assertThat(response.getStations().get(2).getName()).isEqualTo(STATION_NAME_SEOLLEUNG);
-        assertThat(response.getDistance()).isEqualTo(20);
-        assertThat(response.getDuration()).isEqualTo(20);
+        assertThat(response.getStations().get(0).getName()).isEqualTo(JAMSIL_STATION_NAME);
+        assertThat(response.getStations().get(1).getName()).isEqualTo(SEOKCHON_STATION_NAME);
+        assertThat(response.getStations().get(2).getName()).isEqualTo(SEOKCHON_TOMB_STATION_NAME);
+        assertThat(response.getStations().get(3).getName()).isEqualTo(SAMJEON_STATION_NAME);
+        assertThat(response.getStations().get(4).getName()).isEqualTo(OLYMPIC_STATION_NAME);
+        assertThat(response.getDistance()).isEqualTo(140);
+        assertThat(response.getDuration()).isEqualTo(4);
     }
 }
