5d3df84846e2f2d1060b7576b72f34853f7e6a32
hamliet <hamliet@hanyang.ac.kr>
2020-05-15 14:15:54 +0900

    refactor : PathService 리팩토링

--- src/main/java/wooteco/subway/admin/service/PathService.java
+++ src/main/java/wooteco/subway/admin/service/PathService.java
@@ -13,7 +13,6 @@ import wooteco.subway.admin.repository.LineRepository;
 import wooteco.subway.admin.repository.LineStationRepository;
 import wooteco.subway.admin.repository.StationRepository;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -32,63 +31,75 @@ public class PathService {
 
     public SearchPathResponse searchPath(String startStationName, String targetStationName, String type) {
         WeightedMultigraph<Station, DefaultWeightedEdge> graph = makeGraph(type);
-        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-
-        Station startStation = stationRepository.findByName(startStationName)
-                .orElseThrow(() -> new IllegalArgumentException("역이 존재하지 않습니다."));
-        Station targetStation = stationRepository.findByName(targetStationName)
-                .orElseThrow(() -> new IllegalArgumentException("역이 존재하지 않습니다."));
-
-        GraphPath path = dijkstraShortestPath.getPath(startStation, targetStation);
-        if (path == null) {
-            throw new IllegalArgumentException("두 역이 연결되어있지 않습니다.");
-        }
-
-        List<Station> shortestPath = path.getVertexList();
-        if (shortestPath.size() == 1) {
-            throw new IllegalArgumentException("시작역과 도착역이 같습니다.");
-        }
-
+        List<Station> shortestPath = findShortestPath(graph, startStationName, targetStationName);
         int durationSum = calculateValueSum(shortestPath, "duration");
         int distanceSum = calculateValueSum(shortestPath, "distance");
-        List<String> stationNames = shortestPath.stream()
-                .map(o -> o.getName())
-                .collect(Collectors.toList());
+        List<String> stationNames = toStationNames(shortestPath);
 
         return new SearchPathResponse(durationSum, distanceSum, stationNames);
     }
 
     private WeightedMultigraph<Station, DefaultWeightedEdge> makeGraph(String type) {
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
         List<Line> lines = lineRepository.findAll();
         for (Line line : lines) {
-            Set<LineStation> stations = line.getStations();
-
-            Iterator<LineStation> lineStationIterator = stations.iterator();
-            while (lineStationIterator.hasNext()) {
-                LineStation lineStation = lineStationIterator.next();
-                Station station = stationRepository.findById(lineStation.getStationId())
-                        .orElseThrow(() -> new IllegalArgumentException("역이 존재하지 않습니다."));
-                graph.addVertex(station);
-
-                if (lineStation.getPreStationId() == null) {
-                    continue;
-                }
-
-                Station preStation = stationRepository.findById(lineStation.getPreStationId())
-                        .orElseThrow(() -> new IllegalArgumentException("이전역이 존재하지 않습니다."));
-                graph.addVertex(preStation);
-
-                if (type.equals("duration")) {
-                    graph.setEdgeWeight(graph.addEdge(station, preStation), lineStation.getDuration());
-                    graph.setEdgeWeight(graph.addEdge(preStation, station), lineStation.getDuration());
-                    continue;
-                }
-                graph.setEdgeWeight(graph.addEdge(preStation, station), lineStation.getDistance());
-                graph.setEdgeWeight(graph.addEdge(station, preStation), lineStation.getDistance());
-            }
+            makeRelation(graph, line, type);
         }
         return graph;
+    }
+
+    private void makeRelation(WeightedMultigraph<Station, DefaultWeightedEdge> graph, Line line, String type) {
+        for (LineStation lineStation : line.getStations()) {
+            if (lineStation.getPreStationId() == null) {
+                continue;
+            }
+            Station station = findStationById(lineStation.getStationId());
+            Station preStation = findStationById(lineStation.getPreStationId());
+            graph.addVertex(station);
+            graph.addVertex(preStation);
+            graph.setEdgeWeight(graph.addEdge(preStation, station), edgeValue(type, lineStation));
+        }
+    }
+
+    private Station findStationById(Long preStationId) {
+        return stationRepository.findById(preStationId)
+                .orElseThrow(() -> new IllegalArgumentException("이전역이 존재하지 않습니다."));
+    }
+
+    private List<Station> findShortestPath(WeightedMultigraph<Station, DefaultWeightedEdge> graph, String startStationName, String targetStationName) {
+        DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+        Station startStation = findStationByName(startStationName);
+        Station targetStation = findStationByName(targetStationName);
+
+        GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(startStation, targetStation);
+        validatePath(path);
+        List<Station> shortestPath = path.getVertexList();
+        validateShortestPath(shortestPath);
+
+        return shortestPath;
+    }
+
+    private Station findStationByName(String startStationName) {
+        return stationRepository.findByName(startStationName)
+                .orElseThrow(() -> new IllegalArgumentException("역이 존재하지 않습니다."));
+    }
+
+    private void validatePath(GraphPath<Station, DefaultWeightedEdge> path) {
+        if (path == null) {
+            throw new IllegalArgumentException("두 역이 연결되어있지 않습니다.");
+        }
+    }
+
+    private void validateShortestPath(List<Station> shortestPath) {
+        if (shortestPath.size() == 1) {
+            throw new IllegalArgumentException("시작역과 도착역이 같습니다.");
+        }
+    }
+
+    private List<String> toStationNames(List<Station> shortestPath) {
+        return shortestPath.stream()
+                .map(Station::getName)
+                .collect(Collectors.toList());
     }
 
     private int calculateValueSum(List<Station> shortestPath, String type) {
@@ -96,21 +107,34 @@ public class PathService {
         for (int i = 1; i < shortestPath.size(); i++) {
             Station preStation = shortestPath.get(i - 1);
             Station station = shortestPath.get(i);
-            int value = 0;
-
-            //todo: exception message add
-            if (type.equals("duration")) {
-                value = lineStationRepository.findById(preStation.getId(), station.getId())
-                        .orElseThrow(IllegalArgumentException::new)
-                        .getDuration();
-            }
-            if (type.equals("distance")) {
-                value = lineStationRepository.findById(preStation.getId(), station.getId())
-                        .orElseThrow(IllegalArgumentException::new)
-                        .getDistance();
-            }
-            valueSum += value;
+            valueSum += edgeValue(type, preStation, station);
         }
         return valueSum;
+    }
+
+    private int edgeValue(String type, Station preStation, Station station) {
+        LineStation lineStation = findLineStationByIds(preStation, station);
+        if (type.equals("duration")) {
+            return lineStation.getDuration();
+        }
+        if (type.equals("distance")) {
+            return lineStation.getDistance();
+        }
+        return 0;
+    }
+
+    private int edgeValue(String type, LineStation lineStation) {
+        if (type.equals("duration")) {
+            return lineStation.getDuration();
+        }
+        if (type.equals("distance")) {
+            return lineStation.getDistance();
+        }
+        return 0;
+    }
+
+    private LineStation findLineStationByIds(Station preStation, Station station) {
+        return lineStationRepository.findById(preStation.getId(), station.getId())
+                .orElseThrow(()->new IllegalArgumentException("연결되지 않았습니다."));
     }
 }
