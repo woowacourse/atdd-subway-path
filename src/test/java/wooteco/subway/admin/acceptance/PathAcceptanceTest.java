fb08a4c07a504baee60097cba6758da21534b9d5
toneyparky <qkrtmddhks95@gmail.com>
2020-05-17 12:52:05 +0900

    refactor: 테스트 중복을 BeforeEach로 제거

--- src/test/java/wooteco/subway/admin/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/admin/acceptance/PathAcceptanceTest.java
@@ -1,5 +1,6 @@
 package wooteco.subway.admin.acceptance;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
@@ -14,11 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Sql("/truncate.sql")
 public class PathAcceptanceTest extends AcceptanceTest {
 
-	@DisplayName("최단거리 경로를 조회")
-	@Test
-	void getShortestPathByDistance() {
-		//	Feature: 최단거리 경로를 구한다.
-
+	@BeforeEach
+	void addData() {
 		//		Given 지하철 노선들이 추가되어있다.
 		createLine("1호선");
 		createLine("2호선");
@@ -68,6 +66,12 @@ public class PathAcceptanceTest extends AcceptanceTest {
 		addLineStation(5L, null, 7L, 30, 10);
 		addLineStation(5L, 7L, 10L, 30, 10);
 		addLineStation(5L, 10L, 11L, 30, 10);
+	}
+
+	@DisplayName("최단거리 경로를 조회")
+	@Test
+	void getShortestPathByDistance() {
+		//	Feature: 최단거리 경로를 구한다.
 
 		//		When 시청역부터 신도림역까지 최단거리 경로를 구하고 싶다.
 		ShortestPathResponse shortestPath = findShortestDistancePath("시청", "신도림", "distance");
@@ -92,49 +96,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
 	@Test
 	void getShortestPathByDuration() {
 		// Feature: 최단시간 경로를 구한다.
-		//    Given 지하철 노선들이 추가되어있다.
-		createLine("1호선");
-		createLine("2호선");
-		createLine("4호선");
-		createLine("5호선");
-		createLine("9호선");
-		//    And 지하철 역들이 추가되어있다.
-		createStation("구로");
-		createStation("신도림");
-		createStation("신길");
-		createStation("용산");
-		createStation("서울역");
-		createStation("충정로");
-		createStation("당산");
-		createStation("영등포구청");
-		createStation("대림");
-		createStation("여의도");
-		createStation("동작");
-		createStation("삼각지");
-		createStation("시청");
-		//    And    지하철 노선들에 지하철역들이 추가되어있다.
-		addLineStation(1L, null, 1L, 10, 10);
-		addLineStation(1L, 1L, 2L, 10, 10);
-		addLineStation(1L, 2L, 3L, 10, 20);
-		addLineStation(1L, 3L, 4L, 20, 20);
-		addLineStation(1L, 4L, 5L, 10, 10);
-		addLineStation(1L, 5L, 13L, 10, 10);
-		addLineStation(2L, null, 13L, 10, 10);
-		addLineStation(2L, 13L, 6L, 10, 10);
-		addLineStation(2L, 6L, 7L, 10, 50);
-		addLineStation(2L, 7L, 8L, 10, 10);
-		addLineStation(2L, 8L, 2L, 10, 10);
-		addLineStation(2L, 2L, 9L, 10, 10);
-		addLineStation(3L, null, 11L, 10, 10);
-		addLineStation(3L, 11L, 12L, 10, 10);
-		addLineStation(3L, 12L, 5L, 10, 10);
-		addLineStation(4L, null, 8L, 10, 10);
-		addLineStation(4L, 8L, 3L, 20, 10);
-		addLineStation(4L, 3L, 10L, 20, 10);
-		addLineStation(4L, 10L, 6L, 20, 10);
-		addLineStation(5L, null, 7L, 30, 10);
-		addLineStation(5L, 7L, 10L, 30, 10);
-		addLineStation(5L, 10L, 11L, 30, 10);
+
 		//    When 시청역부터 신도림역까지 최단시간 경로를 구하고 싶다.
 		ShortestPathResponse shortestPath = findShortestDistancePath("시청", "신도림", "duration");
 		//    Then 시청역부터 신도림역까지 최단시간 경로가 구해졌다.
