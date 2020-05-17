9b564156690f19fef069e1d21708a2f379ab3190
Seyun <ksy90101@gmail.com>
2020-05-17 19:51:48 +0900

    refactor : PathValidator Component 어노테이션 제거 및 static 처리

--- src/main/java/wooteco/subway/admin/controller/validator/PathValidator.java
+++ src/main/java/wooteco/subway/admin/controller/validator/PathValidator.java
@@ -6,9 +6,8 @@ import org.springframework.stereotype.Component;
 
 import wooteco.subway.admin.dto.PathRequest;
 
-@Component
 public class PathValidator {
-    public void valid(PathRequest pathRequest){
+    public static void valid(PathRequest pathRequest){
         if(Objects.equals(pathRequest.getSource(), pathRequest.getTarget())){
             throw new IllegalArgumentException("출발역과 도착역이 동일합니다.");
         }
