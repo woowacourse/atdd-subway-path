158a43f853f0a24afd32189b1e4748133201c6fd
moon <mjj920930@gmail.com>
2020-05-17 20:44:01 +0900

    refactor: admin과 serivce페이지 구분을 보다 명확히 하기 위해 라우터 uri를 수정

--- src/main/java/wooteco/subway/admin/controller/PageController.java
+++ src/main/java/wooteco/subway/admin/controller/PageController.java
@@ -17,32 +17,32 @@ public class PageController {
 		this.stationRepository = stationRepository;
 	}
 
-	@GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "/service", produces = MediaType.TEXT_HTML_VALUE)
 	public String index() {
 		return "service/index";
 	}
 
-    @GetMapping(value = "/join", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "service/join", produces = MediaType.TEXT_HTML_VALUE)
     public String join() {
         return "service/join";
     }
 
-    @GetMapping(value = "/mypage", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "service/mypage", produces = MediaType.TEXT_HTML_VALUE)
     public String mypage() {
         return "service/mypage";
     }
 
-    @GetMapping(value = "/mypage-edit", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "service/mypage-edit", produces = MediaType.TEXT_HTML_VALUE)
     public String mypageEdit() {
         return "service/mypage-edit";
     }
 
-    @GetMapping(value = "/map", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "service/map", produces = MediaType.TEXT_HTML_VALUE)
     public String map() {
         return "service/map";
     }
 
-    @GetMapping(value = "/search", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "service/search", produces = MediaType.TEXT_HTML_VALUE)
     public String search() {
         return "service/search";
     }
@@ -52,19 +52,19 @@ public class PageController {
 		return "admin/index";
 	}
 
-	@GetMapping(value = "/stations", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "admin/stations", produces = MediaType.TEXT_HTML_VALUE)
 	public String stationPage(Model model) {
 		model.addAttribute("stations", stationRepository.findAll());
 		return "admin/admin-station";
 	}
 
-	@GetMapping(value = "/lines", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "admin/lines", produces = MediaType.TEXT_HTML_VALUE)
 	public String linePage(Model model) {
 		model.addAttribute("lines", lineService.showLines());
 		return "admin/admin-line";
 	}
 
-	@GetMapping(value = "/edges", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "admin/edges", produces = MediaType.TEXT_HTML_VALUE)
 	public String edgePage(Model model) {
 		return "admin/admin-edge";
 	}
