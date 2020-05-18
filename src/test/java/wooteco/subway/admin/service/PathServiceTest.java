67edd1a386f4d5ae62a0907f7ece1d6426dc0a4f
yeonnseok <ljinsk3@gmail.com>
2020-05-18 11:49:43 +0900

    refactor: 예외 케이스 테스트 수정 및 추가

--- src/test/java/wooteco/subway/admin/service/PathServiceTest.java
+++ src/test/java/wooteco/subway/admin/service/PathServiceTest.java
@@ -10,6 +10,7 @@ import wooteco.subway.admin.domain.LineStation;
 import wooteco.subway.admin.domain.SearchType;
 import wooteco.subway.admin.domain.Station;
 import wooteco.subway.admin.dto.PathResponse;
+import wooteco.subway.admin.exceptions.DuplicatedStationNamesException;
 import wooteco.subway.admin.exceptions.NotExistStationException;
 import wooteco.subway.admin.repository.LineRepository;
 import wooteco.subway.admin.repository.StationRepository;
@@ -21,6 +22,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,23 +40,43 @@ public class PathServiceTest {
 	void setUp() {
 		pathService = new PathService(lineRepository, stationRepository);
 		stations = Arrays.asList(new Station(1L, "강남역"),
-		                         new Station(2L, "역삼역"),
-		                         new Station(3L, "선릉역"),
-		                         new Station(4L, "잠실역"));
+				new Station(2L, "역삼역"),
+				new Station(3L, "선릉역"),
+				new Station(4L, "잠실역"));
 		lineStations = Arrays.asList(new LineStation(null, 1L, 10, 5),
-		                             new LineStation(1L, 2L, 10, 5),
-		                             new LineStation(2L, 3L, 10, 5),
-		                             new LineStation(3L, 4L, 10, 5));
+				new LineStation(1L, 2L, 10, 5),
+				new LineStation(2L, 3L, 10, 5),
+				new LineStation(3L, 4L, 10, 5));
 	}
 
-	@DisplayName("출발역과 도착역이 존재하는 역인지 확인")
+	@DisplayName("출발역이 존재 하지 않은 역인 경우 예외 발생")
 	@Test
-	void existStations() {
-		when(stationRepository.notExistsByName(any())).thenReturn(true);
+	void existSourceStations() {
+		when(stationRepository.notExistsByName(eq("사당역"))).thenReturn(true);
+		when(stationRepository.notExistsByName(eq("역삼역"))).thenReturn(false);
 
 		assertThatThrownBy(() -> {
-			pathService.searchPath("source", "target", SearchType.DISTANCE);
+			pathService.searchPath("사당역", "역삼역", SearchType.DISTANCE);
 		}).isInstanceOf(NotExistStationException.class);
+	}
+
+	@DisplayName("도착역이 존재 하지 않은 역인 경우 예외 발생")
+	@Test
+	void existTargetStations() {
+		when(stationRepository.notExistsByName(eq("강남역"))).thenReturn(false);
+		when(stationRepository.notExistsByName(eq("의정부역"))).thenReturn(true);
+
+		assertThatThrownBy(() -> {
+			pathService.searchPath("강남역", "의정부역", SearchType.DISTANCE);
+		}).isInstanceOf(NotExistStationException.class);
+	}
+
+	@DisplayName("출발역과 도착역이 같은 경우 예외 발생")
+	@Test
+	void duplicatedStations() {
+		assertThatThrownBy(() -> {
+			pathService.searchPath("source", "source", SearchType.DISTANCE);
+		}).isInstanceOf(DuplicatedStationNamesException.class);
 	}
 
 	@DisplayName("최단 거리를 기준으로 경로 조회")
