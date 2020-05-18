a79ecabfe2a420317d47d2603e9419dc6dd06aba
moonyoungCHAE <xpf_fl@naver.com>
2020-05-18 15:41:16 +0900

    refactor: Station Database Exception 처리 추가 및 Station Service 생성

--- src/main/java/wooteco/subway/admin/controller/StationController.java
+++ src/main/java/wooteco/subway/admin/controller/StationController.java
@@ -5,23 +5,23 @@ import org.springframework.web.bind.annotation.*;
 import wooteco.subway.admin.domain.Station;
 import wooteco.subway.admin.dto.StationCreateRequest;
 import wooteco.subway.admin.dto.StationResponse;
-import wooteco.subway.admin.repository.StationRepository;
+import wooteco.subway.admin.service.StationService;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
 public class StationController {
-    private final StationRepository stationRepository;
+    private final StationService stationService;
 
-    public StationController(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
+    public StationController(StationService stationService) {
+        this.stationService = stationService;
     }
 
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationCreateRequest view) {
         Station station = view.toStation();
-        Station persistStation = stationRepository.save(station);
+        Station persistStation = stationService.save(station);
         return ResponseEntity
                 .created(URI.create("/stations/" + persistStation.getId()))
                 .body(StationResponse.of(persistStation));
@@ -29,14 +29,14 @@ public class StationController {
 
     @GetMapping("/stations")
     public ResponseEntity<List<StationResponse>> showStations() {
-        List<StationResponse> response = StationResponse.listOf(stationRepository.findAll());
+        List<StationResponse> response = StationResponse.listOf(stationService.findAll());
         return ResponseEntity.ok()
                 .body(response);
     }
 
     @DeleteMapping("/stations/{id}")
-    public ResponseEntity deleteStation(@PathVariable Long id) {
-        stationRepository.deleteById(id);
+    public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
+        stationService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
--- src/test/java/wooteco/subway/admin/repository/StationRepositoryTest.java
+++ src/test/java/wooteco/subway/admin/repository/StationRepositoryTest.java
@@ -1,11 +1,18 @@
 package wooteco.subway.admin.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.relational.core.conversion.DbActionExecutionException;
 import wooteco.subway.admin.domain.Station;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJdbcTest
@@ -19,5 +26,33 @@ public class StationRepositoryTest {
         stationRepository.save(new Station(stationName));
 
         assertThrows(DbActionExecutionException.class, () -> stationRepository.save(new Station(stationName)));
+    }
+
+    @Test
+    void findAllNameById() {
+        Station station1 = stationRepository.save(new Station("강남역"));
+        Station station2 = stationRepository.save(new Station("역삼역"));
+
+        List<String> result = stationRepository.findAllNameById(Arrays.asList(station1.getId(), station2.getId()));
+        Assertions.assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void saveDuplicatedLine() {
+        Station station1 = new Station("잠실역");
+        Station station2 = new Station("잠실역");
+
+        stationRepository.save(station1);
+        assertThatThrownBy(() -> stationRepository.save(station2))
+                .isInstanceOf(DbActionExecutionException.class)
+                .hasCauseInstanceOf(DuplicateKeyException.class);
+    }
+
+    @Test
+    void saveNullLine() {
+        Station station = new Station(null);
+        assertThatThrownBy(() -> stationRepository.save(station))
+                .isInstanceOf(DbActionExecutionException.class)
+                .hasCauseInstanceOf(DataIntegrityViolationException.class);
     }
 }
