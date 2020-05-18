8bc0b2f07daba5dffe992ed4b7ac3f9e73cb78e0
icyMojito <dev.icymojito@gmail.com>
2020-05-19 01:49:33 +0900

    refactor: SearchType을 찾는 of 메서드에 Enum 기본 메서드인 valueOf 적용

--- src/main/java/wooteco/subway/admin/domain/SearchType.java
+++ src/main/java/wooteco/subway/admin/domain/SearchType.java
@@ -1,18 +1,11 @@
 package wooteco.subway.admin.domain;
 
-import java.util.Arrays;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-
 public enum SearchType {
     DISTANCE,
     DURATION;
 
     public static SearchType of(final String typeName) {
-        return Arrays.stream(values())
-                .filter(it -> Objects.equals(it.name(), typeName.toUpperCase()))
-                .findFirst()
-                .orElseThrow(NoSuchElementException::new);
+        return valueOf(typeName.toUpperCase());
     }
 
     public boolean isDistance() {
