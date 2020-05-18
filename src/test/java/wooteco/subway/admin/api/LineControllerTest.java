0c00bbe5662faecd6f34c83b419a89ed3ff3cef9
junk-sound <wom2277@naver.com>
2020-05-19 01:59:12 +0900

    refac: 불필요한 todo 지우기

--- src/test/java/wooteco/subway/admin/api/LineControllerTest.java
+++ src/test/java/wooteco/subway/admin/api/LineControllerTest.java
@@ -38,7 +38,6 @@ public class LineControllerTest {
         WholeSubwayResponse response = WholeSubwayResponse.of(Arrays.asList(createMockResponse(), createMockResponse()));
         given(lineService.wholeLines()).willReturn(response);
 
-        // TODO: 전체 지하철 노선도 정보를 조회하는 URI 입력하기
         String uri = "/lines";
 
 
