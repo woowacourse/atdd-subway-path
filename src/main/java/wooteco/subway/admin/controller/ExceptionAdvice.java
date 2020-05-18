ea71dccd8b5ae9d0847b745014797b691cba4a1f
yeonnseok <ljinsk3@gmail.com>
2020-05-18 10:23:45 +0900

    refactor: ControlerAdvice -> RestControllerAdvice로 변경

--- src/main/java/wooteco/subway/admin/controller/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/admin/controller/ExceptionAdvice.java
@@ -1,33 +1,29 @@
 package wooteco.subway.admin.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.admin.dto.ExceptionResponse;
 import wooteco.subway.admin.exceptions.DuplicatedStationNamesException;
 import wooteco.subway.admin.exceptions.NotExistStationException;
 import wooteco.subway.admin.exceptions.UnconnectedStationsException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionAdvice {
 
-	@ResponseBody
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(NotExistStationException.class)
 	public ExceptionResponse notExistStation(NotExistStationException e) {
 		return new ExceptionResponse("NOT_EXIST_STATION", e.getMessage());
 	}
 
-	@ResponseBody
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(DuplicatedStationNamesException.class)
 	public ExceptionResponse duplicatedStation(DuplicatedStationNamesException e) {
 		return new ExceptionResponse("DUPLICATED_STATION", e.getMessage());
 	}
 
-	@ResponseBody
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(UnconnectedStationsException.class)
 	public ExceptionResponse unconnectedStation(UnconnectedStationsException e) {
