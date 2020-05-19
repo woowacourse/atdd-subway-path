0d549ef1265863dc3226037a6a5753f057f2fc8e
moon <mjj920930@gmail.com>
2020-05-19 17:41:39 +0900

    fix: PageController의 URI에 '/' 누락된 것 수정

--- src/main/java/wooteco/subway/admin/controller/PageController.java
+++ src/main/java/wooteco/subway/admin/controller/PageController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import wooteco.subway.admin.repository.StationRepository;
 import wooteco.subway.admin.service.LineService;
 
@@ -22,27 +23,27 @@ public class PageController {
 		return "service/index";
 	}
 
-    @GetMapping(value = "service/join", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "/service/join", produces = MediaType.TEXT_HTML_VALUE)
     public String join() {
         return "service/join";
     }
 
-    @GetMapping(value = "service/mypage", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "/service/mypage", produces = MediaType.TEXT_HTML_VALUE)
     public String mypage() {
         return "service/mypage";
     }
 
-    @GetMapping(value = "service/mypage-edit", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "/service/mypage-edit", produces = MediaType.TEXT_HTML_VALUE)
     public String mypageEdit() {
         return "service/mypage-edit";
     }
 
-    @GetMapping(value = "service/map", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "/service/map", produces = MediaType.TEXT_HTML_VALUE)
     public String map() {
         return "service/map";
     }
 
-    @GetMapping(value = "service/search", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "/service/search", produces = MediaType.TEXT_HTML_VALUE)
     public String search() {
         return "service/search";
     }
@@ -52,13 +53,13 @@ public class PageController {
 		return "admin/index";
 	}
 
-	@GetMapping(value = "admin/stations", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "/admin/stations", produces = MediaType.TEXT_HTML_VALUE)
 	public String stationPage(Model model) {
 		model.addAttribute("stations", stationRepository.findAll());
 		return "admin/admin-station";
 	}
 
-	@GetMapping(value = "admin/lines", produces = MediaType.TEXT_HTML_VALUE)
+	@GetMapping(value = "/admin/lines", produces = MediaType.TEXT_HTML_VALUE)
 	public String linePage(Model model) {
 		model.addAttribute("lines", lineService.showLines());
 		return "admin/admin-line";
