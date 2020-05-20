3e48b70a15b7c138f986f15958f91c7d25e99d57
ParkDooWon <50234855+ParkDooWon@users.noreply.github.com>
2020-05-20 22:29:25 +0900

    refactor : EdgeWeightType의 of 메서드의 불필요한 로직 제거

--- src/main/java/wooteco/subway/admin/domain/EdgeWeightType.java
+++ src/main/java/wooteco/subway/admin/domain/EdgeWeightType.java
@@ -1,15 +1,10 @@
 package wooteco.subway.admin.domain;
 
-import java.util.Arrays;
-
 public enum EdgeWeightType {
 	DISTANCE,
 	DURATION;
 
 	public static EdgeWeightType of(String type) {
-		return Arrays.stream(values())
-			.filter(value -> value.name().equals(type.toUpperCase()))
-			.findFirst()
-			.orElseThrow(() -> new IllegalArgumentException("잘못된 EdgeWeight"));
+		return EdgeWeightType.valueOf(type);
 	}
 }
