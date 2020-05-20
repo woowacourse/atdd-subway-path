86a753fc5f7e3b9f55308c3b45a2ae4db40e89f9
kouz <lastpuzly@gmail.com>
2020-05-20 21:43:01 +0900

    refactor: LineStationCreateRequest.toLineStation() 구현

--- src/main/java/wooteco/subway/admin/service/LineService.java
+++ src/main/java/wooteco/subway/admin/service/LineService.java
@@ -57,8 +57,7 @@ public class LineService {
 
     public void addLineStation(Long id, LineStationCreateRequest request) {
         Line line = lineRepository.findById(id).orElseThrow(RuntimeException::new);
-        LineStation lineStation = new LineStation(request.getPreStationId(), request.getStationId(),
-            request.getDistance(), request.getDuration());
+        LineStation lineStation = request.toLineStation();
         line.addLineStation(lineStation);
 
         lineRepository.save(line);
