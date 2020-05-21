b397fa1293d61c0ba4521e7a75e2e314f0b2b524
ParkDooWon <50234855+ParkDooWon@users.noreply.github.com>
2020-05-21 22:21:57 +0900

    refactor : 각 상황에 맞는 에러를 잡아서 해당하는 에러메세지를 출력하도록 수정

--- src/main/java/wooteco/subway/admin/controller/ExceptionController.java
+++ src/main/java/wooteco/subway/admin/controller/ExceptionController.java
@@ -4,10 +4,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import wooteco.subway.admin.exception.InaccessibleStationException;
+import wooteco.subway.admin.exception.NonExistentDataException;
+
 @ControllerAdvice
 public class ExceptionController {
+	@ExceptionHandler(InaccessibleStationException.class)
+	public ResponseEntity<String> handleInaccessibleStationException() {
+		return ResponseEntity.badRequest().body("갈 수 없는 역입니다. 다시 입력해주세요.");
+	}
+
+	@ExceptionHandler(NonExistentDataException.class)
+	public ResponseEntity<String> handleNonExistentDataException() {
+		return ResponseEntity.badRequest().body("존재하지 않는 데이터입니다.");
+	}
+
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<String> handleException(Exception e) {
-		return ResponseEntity.badRequest().body(e.getMessage());
+	public ResponseEntity<String> handleException() {
+		return ResponseEntity.badRequest().body("시스템 에러");
 	}
 }
