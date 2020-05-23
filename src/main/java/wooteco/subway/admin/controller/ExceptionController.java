de24c0f9b7f24a49c72b1f0c706ac0719df4a1ac
giantim <giantim132@gmail.com>
2020-05-23 10:51:02 +0900

    refactor: 리뷰 반영
    * ExceptionController 예기치 못한 서버 에러 핸들링 / 404 not found -> 400 bad request 로 변경

--- src/main/java/wooteco/subway/admin/controller/ExceptionController.java
+++ src/main/java/wooteco/subway/admin/controller/ExceptionController.java
@@ -10,14 +10,23 @@ import wooteco.subway.admin.exception.*;
 
 @ControllerAdvice
 public class ExceptionController {
+    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "예기치 못한 에러가 발생했습니다. 관리자에게 문의주세요.";
+
     @ExceptionHandler(value = {LineNotFoundException.class, LineStationNotFoundException.class,
             StationNotFoundException.class, SameDepatureArrivalStationException.class})
     public ResponseEntity<ErrorResponse> handleLineNotFoundException(RuntimeException exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(VerticesNotConnectedException.class)
     public ResponseEntity<ErrorResponse> handleVerticesNotConnectedException(VerticesNotConnectedException exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleInternalServerError(Exception exception) {
+        System.out.println(exception.getMessage());
+        return new ResponseEntity<>(new ErrorResponse(INTERNAL_SERVER_ERROR_MESSAGE),
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
