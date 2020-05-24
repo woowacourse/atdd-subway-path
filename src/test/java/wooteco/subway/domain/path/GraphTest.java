dd95ae201ebc429f6e87a4347fda1fe8cd042c4b
soring <jnsorn@gmail.com>
2020-05-24 23:27:10 +0900

    test : graphTest에 정상적인 경우의 test 추가

--- src/test/java/wooteco/subway/domain/path/GraphTest.java
+++ src/test/java/wooteco/subway/domain/path/GraphTest.java
@@ -1,5 +1,6 @@
 package wooteco.subway.domain.path;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalTime;
@@ -21,9 +22,9 @@ class GraphTest {
 	private static final String STATION_NAME3 = "선릉역";
 	private static final String STATION_NAME4 = "삼성역";
 
-	private Line line;
+	private List<Line> lines;
+	private List<Station> stations;
 	private Station station1;
-	private Station station2;
 	private Station station3;
 	private Station station4;
 	private WeightStrategy strategy;
@@ -31,25 +32,34 @@ class GraphTest {
 	@BeforeEach
 	void setUp() {
 		station1 = new Station(1L, STATION_NAME1);
-		station2 = new Station(2L, STATION_NAME2);
+		Station station2 = new Station(2L, STATION_NAME2);
 		station3 = new Station(3L, STATION_NAME3);
 		station4 = new Station(4L, STATION_NAME4);
-		line = new Line(1L, "2호선", LocalTime.of(05, 30), LocalTime.of(22, 30), 5, "bg-green-600");
 
+		Line line = new Line(1L, "2호선", LocalTime.of(05, 30), LocalTime.of(22, 30), 5, "bg-green-600");
 		line.addLineStation(new LineStation(null, 1L, 10, 10));
 		line.addLineStation(new LineStation(1L, 2L, 10, 10));
 		line.addLineStation(new LineStation(2L, 3L, 10, 10));
 
+		Line line2 = new Line();
+		line2.addLineStation(new LineStation(null, station4.getId(), 0, 0));
+
+		lines = Arrays.asList(line, line2);
+		stations = Arrays.asList(station1, station2, station3, station4);
 		strategy = WeightType.findStrategy("DISTANCE");
+	}
+
+	@DisplayName("올바른 경로 요청 시 경로 생성")
+	@Test
+	void createPath_GivenValidPath_ReturnPath() {
+		Graph graph = new Graph(lines, stations, strategy);
+
+		assertThat(graph.createPath(station1,station3)).isNotNull();
 	}
 
 	@DisplayName("예외테스트: 연결되지 않은 역의 경로를 요청 시, 예외 발생")
 	@Test
 	void createPath_GivenNotConnectedPath_ExceptionThrown() {
-		Line line2 = new Line();
-		line2.addLineStation(new LineStation(null, station4.getId(), 0, 0));
-		List<Line> lines = Arrays.asList(line, line2);
-		List<Station> stations = Arrays.asList(station1, station2, station3, station4);
 		Graph graph = new Graph(lines, stations, strategy);
 
 		assertThatThrownBy(() -> graph.createPath(station1, station4))
