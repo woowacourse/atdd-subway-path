db232504530bd44f6505734a5ab4358e5fad8e12
kuenhwi <kueniyam@naver.com>
2020-05-25 21:15:27 +0900

    refactor: Weight 클래스 외부에서 사용하지 않는 생성자 private으로 변경

--- src/main/java/wooteco/subway/admin/domain/subwayShortestPath/Weight.java
+++ src/main/java/wooteco/subway/admin/domain/subwayShortestPath/Weight.java
@@ -6,7 +6,7 @@ public class Weight {
 	private final int distance;
 	private final int duration;
 
-	public Weight(int distance, int duration) {
+	private Weight(int distance, int duration) {
 		this.distance = distance;
 		this.duration = duration;
 	}
