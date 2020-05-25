daf28ee2aa0f1a723b2c5dae5c99f335f283ea57
icyMojito <dev.icymojito@gmail.com>
2020-05-26 03:02:35 +0900

    refactor: EdgeWeightType의 변경 범위를 줄이기 위하여 Enum 상수와 동시에 처리할 수 있도록 변경

--- src/main/java/wooteco/subway/admin/domain/DijkstraEdgeWeightType.java
+++ src/main/java/wooteco/subway/admin/domain/DijkstraEdgeWeightType.java
@@ -1,14 +1,22 @@
 package wooteco.subway.admin.domain;
 
+import java.util.function.Function;
+
 public enum DijkstraEdgeWeightType {
-    DISTANCE,
-    DURATION;
+    DISTANCE(LineStation::getDistance),
+    DURATION(LineStation::getDuration);
+
+    private Function<LineStation, Integer> expression;
+
+    DijkstraEdgeWeightType(Function<LineStation, Integer> expression) {
+        this.expression = expression;
+    }
 
     public static DijkstraEdgeWeightType of(final String typeName) {
         return valueOf(typeName.toUpperCase());
     }
 
-    public double getWeight(LineStation lineStation) {
-        return this == DISTANCE ? lineStation.getDistance() : lineStation.getDuration();
+    public double getEdgeWeight(LineStation lineStation) {
+        return expression.apply(lineStation);
     }
 }
