925f6f1dfc13b6cebc5de1c19d83fef83f96973e
김예림 <kiel0103@naver.com>
2020-05-25 12:29:54 +0900

    refactor: Exception 종류별로 메서드 분리

--- src/main/java/wooteco/subway/admin/controller/ExceptionController.java
+++ src/main/java/wooteco/subway/admin/controller/ExceptionController.java
@@ -3,13 +3,19 @@ package wooteco.subway.admin.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import wooteco.subway.admin.domain.graph.PathNotFoundException;
 import wooteco.subway.admin.dto.ErrorResponse;
 
 @ControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    @ExceptionHandler(PathNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlePathNotFoundException(PathNotFoundException e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(ErrorResponse.of(e));
     }
 
