840fb65363e599bc9bab5e3481e675be656c0304
ParkDooWon <50234855+ParkDooWon@users.noreply.github.com>
2020-05-26 19:34:13 +0900

    refactor : 에러메세지를 분기문 없이 출력하도록 수정

--- src/main/resources/static/service/api/index.js
+++ src/main/resources/static/service/api/index.js
@@ -29,15 +29,7 @@ const api = (() => {
     if (!response.ok) {
       response.json().then(data => {
         const error = data.errorMessage;
-        if (error === "INACCESSIBLE_STATION") {
-          alert(ERROR_MESSAGE.INACCESSIBLE_STATION);
-        }
-        if (error === "NON_EXISTENT_DATA") {
-          alert(ERROR_MESSAGE.NON_EXISTENT_DATA);
-        }
-        if (error === "SYSTEM_ERROR") {
-          alert(ERROR_MESSAGE.SYSTEM_ERROR);
-        }
+        alert(ERROR_MESSAGE[error]);
       });
       return;
     }
