55d435032b1aba23c908cf6dd36e95b554499e5a
soring <jnsorn@gmail.com>
2020-05-28 19:50:01 +0900

    refactor : RequestMapping을 통해 공통된 URI제거

--- src/main/java/wooteco/subway/controller/PathController.java
+++ src/main/java/wooteco/subway/controller/PathController.java
@@ -5,12 +5,14 @@ import javax.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import wooteco.subway.dto.PathResponse;
 import wooteco.subway.dto.WholeSubwayResponse;
 import wooteco.subway.service.PathService;
 
+@RequestMapping("/paths")
 @RestController
 @Validated
 public class PathController {
@@ -20,19 +22,19 @@ public class PathController {
 		this.pathService = pathService;
 	}
 
-	@GetMapping("/paths/detail")
+	@GetMapping
+	public ResponseEntity<PathResponse> searchPath(@NotBlank String source,
+		@NotBlank String target, @NotBlank String type) {
+		PathResponse pathResponse = pathService.searchPath(source, target, type);
+		return ResponseEntity.ok().body(pathResponse);
+	}
+
+	@GetMapping("/detail")
 	public ResponseEntity<WholeSubwayResponse> retrieveLines() {
 		WholeSubwayResponse wholeSubwayResponse = pathService.wholeLines();
 		return ResponseEntity.ok()
 			.eTag(String.valueOf(wholeSubwayResponse.hashCode()))
 			.body(wholeSubwayResponse);
-	}
-
-	@GetMapping("/paths")
-	public ResponseEntity<PathResponse> searchPath(@NotBlank String source,
-		@NotBlank String target, @NotBlank String type) {
-		PathResponse pathResponse = pathService.searchPath(source, target, type);
-		return ResponseEntity.ok().body(pathResponse);
 	}
 }
 
