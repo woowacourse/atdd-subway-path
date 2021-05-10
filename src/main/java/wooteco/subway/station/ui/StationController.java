47ec8df566201663f9ad60a7ddb8511fa1781f21
임동준 <im1@woowahan.com>
2021-05-11 08:21:33 +0900

    refactor: remove unnecessary annotations

--- src/main/java/wooteco/subway/station/ui/StationController.java
+++ src/main/java/wooteco/subway/station/ui/StationController.java
@@ -20,27 +20,23 @@ public class StationController {
         this.stationService = stationService;
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         StationResponse station = stationService.saveStation(stationRequest);
         return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
         return ResponseEntity.ok().body(stationService.findAllStationResponses());
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping("/stations/{id}")
     public ResponseEntity deleteStation(@PathVariable Long id) {
         stationService.deleteStationById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @ExceptionHandler(SQLException.class)
     public ResponseEntity handleSQLException() {
         return ResponseEntity.badRequest().build();
