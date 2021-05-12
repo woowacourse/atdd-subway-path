bb07f53115e5cc365e079b5c2764af1f08b81748
JinYoung Park <basejin0810@naver.com>
2021-05-12 17:31:18 +0900

    feat(AuthController): 토큰 발급하는 기능 구현

--- src/main/java/wooteco/subway/auth/application/AuthService.java
+++ src/main/java/wooteco/subway/auth/application/AuthService.java
@@ -1,7 +1,21 @@
 package wooteco.subway.auth.application;
 
 import org.springframework.stereotype.Service;
+import wooteco.subway.auth.dto.TokenRequest;
+import wooteco.subway.auth.dto.TokenResponse;
+import wooteco.subway.auth.infrastructure.JwtTokenProvider;
 
 @Service
 public class AuthService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthService(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public TokenResponse createToken(TokenRequest tokenRequest) {
+        String accessToken = jwtTokenProvider.createToken(tokenRequest.getEmail());
+        return new TokenResponse(accessToken);
+    }
 }
