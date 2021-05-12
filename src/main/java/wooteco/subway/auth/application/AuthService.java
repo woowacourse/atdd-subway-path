d205e95d1df0b4c6bdba9ed438c5d90cd3a39e23
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-05-12 16:44:05 +0900

    feat: 유효하지 않은 토큰 예외 처리 구현

--- src/main/java/wooteco/subway/auth/application/AuthService.java
+++ src/main/java/wooteco/subway/auth/application/AuthService.java
@@ -1,15 +1,19 @@
 package wooteco.subway.auth.application;
 
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import wooteco.subway.auth.dto.TokenRequest;
 import wooteco.subway.auth.dto.TokenResponse;
 import wooteco.subway.auth.infrastructure.JwtTokenProvider;
+import wooteco.subway.exception.HttpException;
 import wooteco.subway.member.dao.MemberDao;
 import wooteco.subway.member.domain.Member;
 import wooteco.subway.member.dto.MemberResponse;
 
 @Service
 public class AuthService {
+    private static final String INVALID_TOKEN_ERROR_MESSAGE = "유효하지 않은 토큰입니다.";
     private final JwtTokenProvider jwtTokenProvider;
     private final MemberDao memberDao;
 
@@ -26,17 +30,22 @@ public class AuthService {
 
     public void checkInvalidLogin(String principal, String credentials) {
         memberDao.findByEmailAndPassword(principal, credentials)
-            .orElseThrow(() -> new IllegalArgumentException("이메일 또는 비밀번호가 틀렸습니다."));
+            .orElseThrow(() -> new HttpException(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 틀렸습니다."));
     }
 
     public MemberResponse findMemberByToken(String token) {
-        String payload = jwtTokenProvider.getPayload(token);
-        return findMember(payload);
+        try {
+            jwtTokenProvider.validateToken(token);
+            String payload = jwtTokenProvider.getPayload(token);
+            return findMember(payload);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new HttpException(HttpStatus.UNAUTHORIZED, INVALID_TOKEN_ERROR_MESSAGE);
+        }
     }
 
     public MemberResponse findMember(String principal) {
         Member foundMember = memberDao.findByEmail(principal)
-            .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 토큰입니다."));
+            .orElseThrow(() -> new HttpException(HttpStatus.UNAUTHORIZED, INVALID_TOKEN_ERROR_MESSAGE));
         return new MemberResponse(foundMember);
     }
 }
