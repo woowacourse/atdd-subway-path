a449bf54545f6d68eb60e48ca9075283e221a3a4
daum7766 <daum7766@naver.com>
2021-05-12 21:02:25 +0900

    feat: 로그인시 발생할 수 있는 예외 처리

---
+++ src/main/java/wooteco/subway/auth/exception/UserLoginFailException.java
@@ -0,0 +1,14 @@
+package wooteco.subway.auth.exception;
+
+public class UserLoginFailException extends RuntimeException {
+
+    private static final int STATUS_CODE = 401;
+
+    public UserLoginFailException() {
+        super("[ERROR] 로그인 정보가 맞지 않습니다.");
+    }
+
+    public int getStatusCode() {
+        return STATUS_CODE;
+    }
+}
