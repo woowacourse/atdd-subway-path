311166897a84fe70119bf1ff341392c3339254d5
김주원 <kjw11077@naver.com>
2021-05-12 21:01:03 +0900

    refactor: 예외 핸들링

--- src/main/java/wooteco/subway/auth/exception/UnauthorizedException.java
+++ src/main/java/wooteco/subway/auth/exception/UnauthorizedException.java
@@ -1,5 +1,9 @@
 package wooteco.subway.auth.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException {
 
     public UnauthorizedException(String message) {
