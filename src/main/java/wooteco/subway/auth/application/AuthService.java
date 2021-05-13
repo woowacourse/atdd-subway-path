09bedbed45ea4b00cfc9ff2498ca946472df9c0e
kimjunseo <air.junseo@gmail.com>
2021-05-13 11:48:53 +0900

    feat : 토큰 발급 기능 추가

--- src/main/java/wooteco/subway/auth/application/AuthService.java
+++ src/main/java/wooteco/subway/auth/application/AuthService.java
@@ -1,7 +1,28 @@
 package wooteco.subway.auth.application;
 
 import org.springframework.stereotype.Service;
+import wooteco.subway.auth.application.exception.AuthorizationException;
+import wooteco.subway.auth.dto.TokenRequest;
+import wooteco.subway.auth.dto.TokenResponse;
+import wooteco.subway.auth.infrastructure.JwtTokenProvider;
+import wooteco.subway.member.dao.MemberDao;
+import wooteco.subway.member.domain.Member;
 
 @Service
 public class AuthService {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
+
+    public AuthService(JwtTokenProvider jwtTokenProvider, MemberDao memberDao) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.memberDao = memberDao;
+    }
+
+    public TokenResponse createToken(TokenRequest tokenRequest) {
+        Member member = memberDao.findByEmailAndPassword(tokenRequest.getEmail(), tokenRequest.getPassword())
+                .orElseThrow(AuthorizationException::new);
+
+        String accessToken = jwtTokenProvider.createToken(Long.toString(member.getId()));
+        return new TokenResponse(accessToken);
+    }
 }
--- src/main/java/wooteco/subway/member/dao/MemberDao.java
+++ src/main/java/wooteco/subway/member/dao/MemberDao.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import wooteco.subway.member.domain.Member;
 
 import javax.sql.DataSource;
+import java.util.Optional;
 
 @Repository
 public class MemberDao {
@@ -50,5 +51,11 @@ public class MemberDao {
     public Member findById(Long id) {
         String sql = "select * from MEMBER where id = ?";
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+    public Optional<Member> findByEmailAndPassword(String email, String password) {
+        String sql = "select * from MEMBER where email = ? and password = ?";
+        return jdbcTemplate.query(sql, rowMapper, email, password)
+                .stream().findAny();
     }
 }
