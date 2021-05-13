c213406b4e43565a967372f0b037024757298a16
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-05-13 13:00:17 +0900

    refactor: AuthenticationPrincipalArgumentResolver 매개변수 제네릭 추가

--- src/main/java/wooteco/member/controller/config/WebMvcConfiguration.java
+++ src/main/java/wooteco/member/controller/config/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package wooteco.member.controller.config;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import wooteco.member.service.AuthService;
@@ -22,8 +23,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     }
 
     @Override
-    public void addArgumentResolvers(List argumentResolvers) {
-        argumentResolvers.add(createAuthenticationPrincipalArgumentResolver());
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(createAuthenticationPrincipalArgumentResolver());
     }
 
     @Bean
