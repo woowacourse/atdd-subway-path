2f4c5c6011ac2fedafd3db2efea253f9c2f96161
JinYoung Park <basejin0810@naver.com>
2021-05-13 18:32:23 +0900

    feat: 경로 조회 기능 구현

--- src/main/java/wooteco/subway/line/dao/SectionDao.java
+++ src/main/java/wooteco/subway/line/dao/SectionDao.java
@@ -1,27 +1,47 @@
 package wooteco.subway.line.dao;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.line.domain.Line;
 import wooteco.subway.line.domain.Section;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import wooteco.subway.station.domain.Station;
+
 @Repository
 public class SectionDao {
-
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
+
+    private static RowMapper<Section> rowMapper(List<Station> stations) {
+        return (rs, rowNum) -> {
+            final long id = rs.getLong("id");
+            final long upStationId = rs.getLong("up_station_id");
+            final long downStationId = rs.getLong("down_station_id");
+            final int distance = rs.getInt("distance");
+            final Station upStation =
+                stations.stream().filter(station -> station.getId().equals(upStationId))
+                    .findAny().orElseThrow(() -> new IllegalArgumentException("없음"));
+            final Station downStation =
+                stations.stream().filter(station -> station.getId().equals(downStationId))
+                    .findAny().orElseThrow(() -> new IllegalArgumentException("없음"));
+            return new Section(id, upStation, downStation, distance);
+        };
+    }
 
     public SectionDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
         this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
-            .withTableName("SECTION")
-            .usingGeneratedKeyColumns("id");
+                .withTableName("SECTION")
+                .usingGeneratedKeyColumns("id");
     }
 
     public Section insert(Line line, Section section) {
@@ -41,16 +61,21 @@ public class SectionDao {
     public void insertSections(Line line) {
         List<Section> sections = line.getSections().getSections();
         List<Map<String, Object>> batchValues = sections.stream()
-            .map(section -> {
-                Map<String, Object> params = new HashMap<>();
-                params.put("line_id", line.getId());
-                params.put("up_station_id", section.getUpStation().getId());
-                params.put("down_station_id", section.getDownStation().getId());
-                params.put("distance", section.getDistance());
-                return params;
-            })
-            .collect(Collectors.toList());
+                .map(section -> {
+                    Map<String, Object> params = new HashMap<>();
+                    params.put("line_id", line.getId());
+                    params.put("up_station_id", section.getUpStation().getId());
+                    params.put("down_station_id", section.getDownStation().getId());
+                    params.put("distance", section.getDistance());
+                    return params;
+                })
+                .collect(Collectors.toList());
 
         simpleJdbcInsert.executeBatch(batchValues.toArray(new Map[sections.size()]));
+    }
+
+    public List<Section> findAll(final List<Station> stations) {
+        String sql = "select s.id, s.line_id, s.up_station_id, s.down_station_id, s.distance from Section s";
+        return jdbcTemplate.query(sql, rowMapper(stations));
     }
 }
--- src/main/java/wooteco/subway/path/application/PathService.java
+++ src/main/java/wooteco/subway/path/application/PathService.java
@@ -1,9 +1,18 @@
 package wooteco.subway.path.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 
 import wooteco.subway.line.dao.SectionDao;
+import wooteco.subway.line.domain.Section;
+import wooteco.subway.path.dto.PathResponse;
 import wooteco.subway.station.dao.StationDao;
+import wooteco.subway.station.domain.Station;
+import wooteco.subway.station.dto.StationResponse;
 
 @Service
 public class PathService {
@@ -14,5 +23,46 @@ public class PathService {
     public PathService(StationDao stationDao, SectionDao sectionDao) {
         this.stationDao = stationDao;
         this.sectionDao = sectionDao;
+    }
+
+    public PathResponse shortenPath(final Long sourceId, final Long targetId) {
+        final WeightedMultigraph<Station, DefaultWeightedEdge> graph = drawGraph();
+        final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+
+        final Station source = stationDao.findById(sourceId);
+        final Station target = stationDao.findById(targetId);
+        final List<StationResponse> shortestPath = makeShortestPath(dijkstraShortestPath, source, target);
+        int distance = (int) dijkstraShortestPath.getPathWeight(source, target);
+        return new PathResponse(shortestPath, distance);
+    }
+
+    private List<StationResponse> makeShortestPath(DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath,
+        Station source, Station target) {
+        return dijkstraShortestPath.getPath(source, target).getVertexList()
+            .stream()
+            .map(StationResponse::of)
+            .collect(Collectors.toList());
+    }
+
+    private WeightedMultigraph<Station, DefaultWeightedEdge> drawGraph() {
+        final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        final List<Station> stations = addVertices(graph);
+        setEdgeWeights(graph, stations);
+        return graph;
+    }
+
+    private List<Station> addVertices(WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        final List<Station> stations = stationDao.findAll();
+        for (final Station station : stations) {
+            graph.addVertex(station);
+        }
+        return stations;
+    }
+
+    private void setEdgeWeights(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Station> stations) {
+        final List<Section> sections = sectionDao.findAll(stations);
+        for (final Section section : sections) {
+            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+        }
     }
 }
