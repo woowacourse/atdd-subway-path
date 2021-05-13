7f113ef58b068afb0a57b505df5c12d0f5b0f3c8
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-13 16:01:24 +0900

    refactor: interceptor path 변경

--- src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
+++ src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
@@ -25,7 +25,7 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).addPathPatterns("/admin/**");
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/members/**");
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/members/me/**");
     }
 
     @Override
