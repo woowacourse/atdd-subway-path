ebe24b38f28d3a5bbae4ac01651ac4a633b0dab2
RinSabbit <celena1004@naver.com>
2021-05-13 19:37:11 +0900

    feat: 탈퇴하기 기능 구현

--- frontend/src/pages/member/MyPage.vue
+++ frontend/src/pages/member/MyPage.vue
@@ -64,8 +64,16 @@ export default {
         return;
       }
       try {
-        // TODO 유저를 삭제하는 API를 추가해주세요
-        // await fetch("/api/users/{this.member.id}")
+        const data = localStorage.getItem("token");
+        await fetch(`/api/members/${this.member.id}`,{
+          method: "DELETE",
+          headers: new Headers({
+            'Authorization': 'Bearer ' + data.accessToken,
+            'Content-Type': 'application/json'
+          })
+        });
+        localStorage.removeItem("token");
+
         this.setMember(null);
         this.showSnackbar(SNACKBAR_MESSAGES.MEMBER.DELETE.SUCCESS);
         await this.$router.replace("/");
