c32c61f4e60fe73cbcee27c260f57928cd73d5aa
yjksw <cairofaith@gmail.com>
2021-05-13 17:33:39 +0900

    feat: 회원탈퇴 API 추가

--- frontend/src/pages/member/MyPage.vue
+++ frontend/src/pages/member/MyPage.vue
@@ -48,7 +48,7 @@ export default {
   name: "MyPage",
   components: { ConfirmDialog },
   computed: {
-    ...mapGetters(["member"]),
+    ...mapGetters(["member", "accessToken"]),
   },
   methods: {
     ...mapMutations([SHOW_SNACKBAR, SET_MEMBER]),
@@ -64,8 +64,18 @@ export default {
         return;
       }
       try {
-        // TODO 유저를 삭제하는 API를 추가해주세요
-        // await fetch("/api/users/{this.member.id}")
+        const memberResponse = await fetch("http://localhost:8080/members/me", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + this.accessToken
+          }
+        })
+
+        if (!memberResponse.ok) {
+          throw new Error(`${memberResponse.status}`);
+        }
+
         this.setMember(null);
         this.showSnackbar(SNACKBAR_MESSAGES.MEMBER.DELETE.SUCCESS);
         await this.$router.replace("/");
