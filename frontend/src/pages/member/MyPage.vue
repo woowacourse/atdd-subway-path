699306bdcf38c501084c13b8dc1a486311f20d39
JinYoung Park <basejin0810@naver.com>
2021-05-13 14:02:09 +0900

    feat(member): 멤버 삭제 프론트 파일 구현

--- frontend/src/pages/member/MyPage.vue
+++ frontend/src/pages/member/MyPage.vue
@@ -64,8 +64,9 @@ export default {
         return;
       }
       try {
-        // TODO 유저를 삭제하는 API를 추가해주세요
-        // await fetch("/api/users/{this.member.id}")
+        await fetch(`http://localhost:8080/members/${this.member.id}`, {
+          method: 'DELETE'
+        })
         this.setMember(null);
         this.showSnackbar(SNACKBAR_MESSAGES.MEMBER.DELETE.SUCCESS);
         await this.$router.replace("/");
