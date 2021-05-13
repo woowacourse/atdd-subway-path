1f975fe6c8b3853afb8254b4671c66c10a2fbfbc
JinYoung Park <basejin0810@naver.com>
2021-05-13 14:06:39 +0900

    feat(member): 멤버 수정 프론트 파일 구현

--- frontend/src/pages/member/MyPageEdit.vue
+++ frontend/src/pages/member/MyPageEdit.vue
@@ -105,9 +105,20 @@ export default {
     },
     async onEditMember() {
       try {
-        // TODO member 정보를 update하는 API를 추가해주세요
-        // const { email, age, password } = this.editingMember;
-        // await fetch("/api/users/{this.member.id}", { email, age, password })
+        const { email, age, password } = this.editingMember;
+        const updateRequest = {
+          "password": password,
+          "age": age,
+          "email": email
+        }
+        const option = {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(updateRequest)
+        };
+        await fetch(`http://localhost:8080/members/${this.member.id}`, option)
         this.showSnackbar(SNACKBAR_MESSAGES.MEMBER.EDIT.SUCCESS);
         await this.$router.replace("/mypage");
       } catch (e) {
