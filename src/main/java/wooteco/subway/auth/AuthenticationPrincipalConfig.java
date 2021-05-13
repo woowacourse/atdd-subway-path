cd15464f14817cd50110c005310fc5dc88e3a332
dwl5 <ssop6403@gmail.com>
2021-05-13 16:39:59 +0900

    refactor: LoginInterceptor /member/me로 변경

--- src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
+++ src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
@@ -23,7 +23,7 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/admin/**");
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/members/me/**");
     }
 
     @Override
