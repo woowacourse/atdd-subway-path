33eabe53cb3ba69c520be3610c4395c18acfd147
JinYoung Park <basejin0810@naver.com>
2021-05-13 13:19:32 +0900

    style: 완료된 TODO 목록 제거

--- src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -29,14 +29,12 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
         return parameter.hasParameterAnnotation(AuthenticationPrincipal.class);
     }
 
-    // parameter에 @AuthenticationPrincipal이 붙어있는 경우 동작
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         final String accessToken =
                 AuthorizationExtractor.extract(Objects.requireNonNull(webRequest.getNativeRequest(HttpServletRequest.class)));
         authService.validateToken(accessToken);
         final MemberResponse member = memberService.findMemberByEmail(authService.parseEmail(accessToken));
-        // TODO: 유효한 로그인인 경우 LoginMember 만들어서 응답하기
         return LoginMember.from(member);
     }
 }
