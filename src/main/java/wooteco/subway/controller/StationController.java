564f31cc20959b6774357675feee230db01ac13e
younghoonkwon <younghoon.kwon91@gmail.com>
2021-05-13 15:35:45 +0900

    feat: front-end 모든 역을 불러오는 API 구현

--- src/main/java/wooteco/subway/controller/StationController.java
+++ src/main/java/wooteco/subway/controller/StationController.java
@@ -5,18 +5,13 @@ import java.sql.SQLException;
 import java.util.List;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wooteco.subway.controller.dto.request.StationRequestDto;
 import wooteco.subway.controller.dto.response.StationResponseDto;
 import wooteco.subway.service.StationService;
 
 @RestController
+@RequestMapping("/api")
 public class StationController {
     private StationService stationService;
 
