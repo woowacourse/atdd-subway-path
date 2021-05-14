21b4a4b0b3ad728abdb0dd3e9a53e3aa7ed23319
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-14 15:18:06 +0900

    feat(MyPage.vue): 멤버 정보 삭제 API 호출

--- frontend/src/pages/member/MyPage.vue
+++ frontend/src/pages/member/MyPage.vue
@@ -40,7 +40,7 @@
 
 <script>
 import { mapGetters, mapMutations } from "vuex";
-import { SET_MEMBER, SHOW_SNACKBAR } from "../../store/shared/mutationTypes";
+import { SET_MEMBER, SHOW_SNACKBAR} from "../../store/shared/mutationTypes";
 import ConfirmDialog from "../../components/dialogs/ConfirmDialog";
 import { SNACKBAR_MESSAGES } from "../../utils/constants";
 
@@ -64,8 +64,15 @@ export default {
         return;
       }
       try {
-        // TODO 유저를 삭제하는 API를 추가해주세요
-        // await fetch("/api/users/{this.member.id}")
+        const accessToken = localStorage.getItem("token");
+
+        await fetch("http://localhost:8080/members/me", {
+          method: 'DELETE',
+          headers: {
+            'Authorization': 'Bearer ' + accessToken
+          }
+        });
+
         this.setMember(null);
         this.showSnackbar(SNACKBAR_MESSAGES.MEMBER.DELETE.SUCCESS);
         await this.$router.replace("/");
