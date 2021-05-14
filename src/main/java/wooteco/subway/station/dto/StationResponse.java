8f4eedec76a2af0aa518b75e6b5d35c232b9cd0d
TaewanKimmmm <ktww88@gmail.com>
2021-05-14 14:30:26 +0900

    style: 함수 순서 변경

--- src/main/java/wooteco/subway/station/dto/StationResponse.java
+++ src/main/java/wooteco/subway/station/dto/StationResponse.java
@@ -9,16 +9,16 @@ public class StationResponse {
     private Long id;
     private String name;
 
-    public static StationResponse of(Station station) {
-        return new StationResponse(station.getId(), station.getName());
-    }
-
     public StationResponse() {
     }
 
     public StationResponse(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public static StationResponse of(Station station) {
+        return new StationResponse(station.getId(), station.getName());
     }
 
     public static List<StationResponse> listOf(List<Station> stations) {
