921efd588b3dc51b8ddfdbf5eaae2199c9630785
Kimun Kim <kkm97351@gmail.com>
2021-05-14 17:02:43 +0900

    style: AuthController 불필요 주석 삭제

--- src/main/java/wooteco/subway/controller/AuthController.java
+++ src/main/java/wooteco/subway/controller/AuthController.java
@@ -4,11 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import wooteco.subway.dto.MemberResponse;
 import wooteco.subway.dto.TokenRequest;
 import wooteco.subway.dto.TokenResponse;
 import wooteco.subway.service.AuthService;
-import wooteco.subway.service.MemberService;
 
 @RestController
 public class AuthController {
@@ -17,10 +15,7 @@ public class AuthController {
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
-
-    // TODO: 로그인(토큰 발급) 요청 처리하기
-    // todo: login에 대한 postMapping
-    // MemberService
+    
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest tokenRequest) {
         String accessToken = authService.issueToken(tokenRequest.getEmail(), tokenRequest.getPassword());
