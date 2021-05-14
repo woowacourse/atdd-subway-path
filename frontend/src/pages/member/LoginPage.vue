7847edc3692468b1b3e04fcd59323d5e4af8d9a4
Minsung <sns2831@gmail.com>
2021-05-14 14:20:58 +0900

    feat(frontend): 로그인 시 회원 정보 요청 보내도록 변경

--- frontend/src/pages/member/LoginPage.vue
+++ frontend/src/pages/member/LoginPage.vue
@@ -9,36 +9,36 @@
           <v-card-text class="px-4 pt-4 pb-0">
             <div class="d-flex">
               <v-text-field
-                color="grey darken-1"
-                label="이메일을 입력해주세요."
-                v-model="member.email"
-                prepend-inner-icon="mdi-email"
-                dense
-                outlined
-                :rules="rules.member.email"
+                  color="grey darken-1"
+                  label="이메일을 입력해주세요."
+                  v-model="member.email"
+                  prepend-inner-icon="mdi-email"
+                  dense
+                  outlined
+                  :rules="rules.member.email"
               ></v-text-field>
             </div>
             <div class="d-flex mt-2">
               <v-text-field
-                color="grey darken-1"
-                label="비밀번호를 입력해주세요."
-                v-model="member.password"
-                prepend-inner-icon="mdi-lock"
-                dense
-                outlined
-                type="password"
-                :rules="rules.member.password"
+                  color="grey darken-1"
+                  label="비밀번호를 입력해주세요."
+                  v-model="member.password"
+                  prepend-inner-icon="mdi-lock"
+                  dense
+                  outlined
+                  type="password"
+                  :rules="rules.member.password"
               ></v-text-field>
             </div>
           </v-card-text>
           <v-card-actions class="px-4 pb-4">
             <v-spacer></v-spacer>
             <v-btn
-              @click.prevent="onLogin"
-              :disabled="!valid"
-              color="amber"
-              class="w-100"
-              depressed
+                @click.prevent="onLogin"
+                :disabled="!valid"
+                color="amber"
+                class="w-100"
+                depressed
             >
               로그인
             </v-btn>
@@ -53,15 +53,15 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from "vuex";
+import {mapGetters, mapMutations} from "vuex";
 import {SET_ACCESS_TOKEN, SET_MEMBER, SHOW_SNACKBAR} from "../../store/shared/mutationTypes";
-import { SNACKBAR_MESSAGES } from "../../utils/constants";
+import {SNACKBAR_MESSAGES} from "../../utils/constants";
 import validator from "../../utils/validator";
 
 export default {
   name: "LoginPage",
   computed: {
-    ...mapGetters(["accessToken"]),
+    ...mapGetters(["accessToken", "member"]),
   },
   methods: {
     ...mapMutations([SHOW_SNACKBAR, SET_MEMBER, SET_ACCESS_TOKEN]),
@@ -73,24 +73,41 @@ export default {
         return;
       }
       try {
+        const { email, password } = this.member
         const options = {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(this.member)
+          body: JSON.stringify({
+            'email': email,
+            'password': password
+          })
         }
 
-        const response  = await fetch("/api/login/token", options)
+        const response = await fetch("/api/login/token", options)
 
         if (!response.ok) {
           throw new Error(`${response.error()}`)
         }
 
-        let token = await response.json().accessToken
+        let token = await response.json()
+        this.setAccessToken(token.accessToken)
 
-        this.setAccessToken(token)
-        this.setMember(this.member)
+        const memberResponse = await fetch("/api/members/me",
+            {
+              headers: {
+                'Authorization': `Bearer ${this.accessToken}`
+              }
+            }
+        )
+
+        if (!memberResponse.ok) {
+          throw new Error(`${response.error()}`)
+        }
+
+        const member = await memberResponse.json()
+        this.setMember(member)
 
         await this.$router.replace(`/`);
         this.showSnackbar(SNACKBAR_MESSAGES.LOGIN.SUCCESS);
@@ -103,7 +120,7 @@ export default {
   data() {
     return {
       valid: false,
-      rules: { ...validator },
+      rules: {...validator},
       member: {
         email: "",
         password: "",
