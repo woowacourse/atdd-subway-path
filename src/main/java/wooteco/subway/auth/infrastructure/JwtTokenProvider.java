ae4f7f696bfb3959a470337aa432770129f07be3
perenok <perenoke@gmail.com>
2021-05-14 11:48:57 +0900

    refactor(auth): @ResponseStatus를 이용해 에러 체크

--- src/main/java/wooteco/subway/auth/infrastructure/JwtTokenProvider.java
+++ src/main/java/wooteco/subway/auth/infrastructure/JwtTokenProvider.java
@@ -27,13 +27,13 @@ public class JwtTokenProvider {
     }
 
     public String getPayload(String token) {
+        validateToken(token);
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token) {
         try {
             Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
-
             return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
             return false;
