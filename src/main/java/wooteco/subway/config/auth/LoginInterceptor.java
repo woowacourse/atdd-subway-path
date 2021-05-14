58e22ccb5f524035b8e11b016701567cbc1b90b7
Jinhong <xlffm3@gmail.com>
2021-05-14 21:50:37 +0900

    refactor: Preflight 요청시 response allow 값 설정

--- src/main/java/wooteco/subway/config/auth/LoginInterceptor.java
+++ src/main/java/wooteco/subway/config/auth/LoginInterceptor.java
@@ -7,8 +7,15 @@ import wooteco.subway.service.AuthService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 public class LoginInterceptor implements HandlerInterceptor {
+    private static final String AC_REQUEST_METHOD = "Access-Control-Request-Method";
+    private static final String AC_REQUEST_HEADERS = "Access-Control-Request-Headers";
+    private static final String AC_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    private static final String AC_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    private static final String AC_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    private static final String ORIGIN = "Origin";
 
     private final AuthService authService;
 
@@ -19,6 +26,9 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (isPreflighted(request)) {
+            response.setHeader(AC_ALLOW_ORIGIN, request.getHeader(ORIGIN));
+            response.setHeader(AC_ALLOW_HEADERS, request.getHeader(AC_REQUEST_HEADERS));
+            response.setHeader(AC_ALLOW_METHODS, request.getHeader(AC_REQUEST_METHOD));
             return true;
         }
         String token = AuthorizationExtractor.extract(request);
@@ -27,8 +37,16 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     private boolean isPreflighted(HttpServletRequest request) {
-        String optionMethod = HttpMethod.OPTIONS.name();
-        String requestMethod = request.getMethod();
-        return optionMethod.equals(requestMethod);
+        String optionMethod = HttpMethod.OPTIONS.toString();
+        String currentMethod = request.getMethod();
+        String requestHeaders = request.getHeader(AC_REQUEST_HEADERS);
+        String requestMethods = request.getHeader(AC_REQUEST_METHOD);
+        return optionMethod.equals(currentMethod)
+                && isNotNullNorEmpty(requestHeaders)
+                && isNotNullNorEmpty(requestMethods);
+    }
+
+    private boolean isNotNullNorEmpty(String requestInput) {
+        return !Objects.isNull(requestInput) && !requestInput.isEmpty();
     }
 }
