6c391b77ec63163782f633618191a625ea3bda16
Jinho <jh8579@gmail.com>
2021-05-15 04:48:22 +0900

    refactor: List 내부 값 선언

--- src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
+++ src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
@@ -3,6 +3,7 @@ package wooteco.subway.auth;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import wooteco.subway.auth.application.AuthService;
@@ -17,7 +18,7 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addArgumentResolvers(List argumentResolvers) {
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(createAuthenticationPrincipalArgumentResolver());
     }
 
