d74d49b20e12e0348ce5d0deda5ee7dbe8cb59a3
Jinho <jh8579@gmail.com>
2021-05-15 02:37:00 +0900

    feat: vue store을 localStorage에 저장하기 위한 설정 추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,19 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from 'vuex-persistedstate';
 import station from "./modules/station";
 import line from "./modules/line";
 import member from "./modules/member";
 import auth from "./modules/auth";
 import snackbar from "./modules/snackbar";
 
+const modules = {
+  snackbar,
+  station,
+  line,
+  member,
+  auth,
+}
+
+const plugins = [
+    createPersistedState(),
+];
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    snackbar,
-    station,
-    line,
-    member,
-    auth,
-  },
+  modules: modules,
+  plugins,
 });
