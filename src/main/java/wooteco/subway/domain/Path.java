68d383dffd5c4b20b0f958f1d17a6d93caace493
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-05-14 15:59:28 +0900

    refactor: 인스턴스 변수 로컬 변수로 수정

--- src/main/java/wooteco/subway/domain/Path.java
+++ src/main/java/wooteco/subway/domain/Path.java
@@ -1,16 +1,13 @@
 package wooteco.subway.domain;
 
-import wooteco.subway.controller.dto.response.StationResponseDto;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Path {
-    private final List<Section> allSections;
     private final Graph graph;
 
     public Path(List<Line> allLines, Station sourceStation, Station targetStation) {
-        allSections = new ArrayList<>();
+        List<Section> allSections = new ArrayList<>();
         for (Line line : allLines) {
             Sections sections = line.getSections();
             allSections.addAll(sections.getSections());
