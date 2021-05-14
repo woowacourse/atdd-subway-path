60338221c447e695ef91490e4498f7d86f83658e
Kimun Kim <kkm97351@gmail.com>
2021-05-14 17:05:35 +0900

    refactor: 테스트용 출력문 삭제

--- src/main/java/wooteco/subway/config/LoginInterceptor.java
+++ src/main/java/wooteco/subway/config/LoginInterceptor.java
@@ -17,8 +17,6 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = AuthorizationExtractor.extract(request);
-        System.out.println("kimun log");
-        System.out.println(token);
         authService.validate(token);
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
