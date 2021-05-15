461acc7413f1996e7ba5747e0f69a80e63f783f0
hyeon9mak <jinha3507@gmail.com>
2021-05-15 17:17:43 +0900

    feat: 최단경로 조회 API 구현

--- src/main/java/wooteco/subway/line/dao/SectionDao.java
+++ src/main/java/wooteco/subway/line/dao/SectionDao.java
@@ -1,6 +1,7 @@
 package wooteco.subway.line.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.line.domain.Line;
@@ -11,11 +12,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import wooteco.subway.station.domain.Station;
 
 @Repository
 public class SectionDao {
     private JdbcTemplate jdbcTemplate;
     private SimpleJdbcInsert simpleJdbcInsert;
+
+    private static final RowMapper<Section> mapper = (rs, rowNum) -> {
+        Long sectionId = rs.getLong("section_id");
+        Long upStationId = rs.getLong("up_id");
+        String upStationName = rs.getString("up_name");
+        Long downStationId = rs.getLong("down_id");
+        String downStationName = rs.getString("down_name");
+        int distance = rs.getInt("distance");
+        return new Section(
+            sectionId,
+            new Station(upStationId, upStationName),
+            new Station(downStationId, downStationName),
+            distance
+        );
+    };
 
     public SectionDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
@@ -52,5 +69,20 @@ public class SectionDao {
                 .collect(Collectors.toList());
 
         simpleJdbcInsert.executeBatch(batchValues.toArray(new Map[sections.size()]));
+    }
+
+    public List<Section> findAll() {
+        String sql = "SELECT \n"
+            + "  s.id AS section_id,\n"
+            + "  up_table.id AS up_id,\n"
+            + "  up_table.name AS up_name,\n"
+            + "  down_table.id AS down_id,\n"
+            + "  down_table.name AS down_name,\n"
+            + "  distance\n"
+            + "FROM\n"
+            + "  section AS s\n"
+            + "LEFT JOIN station AS up_table      ON s.up_station_id = up_table.id\n"
+            + "LEFT JOIN station AS down_table ON s.down_station_id = down_table.id\n";
+        return jdbcTemplate.query(sql, mapper);
     }
 }
