d3ccf9de59bbeed0dab4cc215394b523697685b8
sakjung <ssangyu123@gmail.com>
2021-05-16 01:09:56 +0900

    feat: mainpage에서 새로고침했을 때도 로그인이 지속되도록 리팩토링

--- frontend/src/pages/main/MainPage.vue
+++ frontend/src/pages/main/MainPage.vue
@@ -23,9 +23,29 @@
 
 <script>
 import { NAV_ITEMS } from "../../utils/constants";
+import {mapMutations} from "vuex";
+import {SET_MEMBER} from "@/store/shared/mutationTypes";
 
 export default {
   name: "MainPage",
+  methods: {
+    ...mapMutations([SET_MEMBER]),
+  },
+  async created() {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken !== null) {
+      const member = await fetch("/api/members/me", {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + accessToken
+        }
+      });
+      if (member.ok) {
+        const memberInfo = await member.json();
+        this.setMember(memberInfo);
+      }
+    }
+  },
   data() {
     return {
       navItems: [...NAV_ITEMS],
