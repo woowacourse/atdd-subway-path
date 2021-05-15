ef715bc056aaa4858cc0beb2863bb912fa934188
pomo <bosl95@naver.com>
2021-05-15 15:09:21 +0900

    refactor: resolver의 반환 자료형 변환

--- src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -8,9 +8,10 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 import wooteco.subway.auth.application.AuthService;
 import wooteco.subway.auth.domain.AuthenticationPrincipal;
 import wooteco.subway.auth.infrastructure.AuthorizationExtractor;
-import wooteco.subway.member.domain.LoginMember;
+import wooteco.subway.member.domain.Member;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
     private AuthService authService;
@@ -26,11 +27,10 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
 
     // parameter에 @AuthenticationPrincipal이 붙어있는 경우 동작
     @Override
-    public LoginMember resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        // TODO: 유효한 로그인인 경우 LoginMember 만들어서 응답하기
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+    public Member resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws IOException {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
         String token = AuthorizationExtractor.extract(request);
         String emailByToken = authService.findEmailByToken(token);
-        return new LoginMember(emailByToken);
+        return authService.findMemberByEmail(emailByToken);
     }
 }
