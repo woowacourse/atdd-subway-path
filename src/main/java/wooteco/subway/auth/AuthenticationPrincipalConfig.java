01ea4e1525a23915f1a549fb6fc8a4f6078cec5d
ecsimsw <ecsimsw@gmail.com>
2021-05-16 02:04:21 +0900

    refactor : 커스텀 ArgumentResolver를 빈으로 등록해서 코드 정리

--- src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
+++ src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
@@ -1,9 +1,7 @@
 package wooteco.subway.auth;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import wooteco.subway.auth.application.AuthService;
 import wooteco.subway.auth.ui.AuthenticationPrincipalArgumentResolver;
 
 import java.util.List;
@@ -11,20 +9,14 @@ import java.util.List;
 @Configuration
 public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
 
-    private final AuthService authService;
+    private final AuthenticationPrincipalArgumentResolver authResolver;
 
-    public AuthenticationPrincipalConfig(AuthService authService) {
-        this.authService = authService;
+    public AuthenticationPrincipalConfig(AuthenticationPrincipalArgumentResolver authResolver) {
+        this.authResolver = authResolver;
     }
 
     @Override
     public void addArgumentResolvers(List argumentResolvers) {
-        final AuthenticationPrincipalArgumentResolver resolver = new AuthenticationPrincipalArgumentResolver(authService);
-        argumentResolvers.add(resolver);
-    }
-
-    @Bean
-    public AuthenticationPrincipalArgumentResolver createAuthenticationPrincipalArgumentResolver() {
-        return new AuthenticationPrincipalArgumentResolver(authService);
+        argumentResolvers.add(authResolver);
     }
 }
