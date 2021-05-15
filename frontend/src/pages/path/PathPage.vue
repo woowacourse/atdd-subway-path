ba193e82cd09b712c3188a72a506da906ee11232
hyeon9mak <jinha3507@gmail.com>
2021-05-15 17:49:49 +0900

    feat: 최단경로 조회 프론트 구현

--- frontend/src/pages/path/PathPage.vue
+++ frontend/src/pages/path/PathPage.vue
@@ -160,7 +160,12 @@ export default {
     async onSearchResult() {
       try {
         // TODO 최단 거리를 검색하는 API를 추가해주세요.
-        // this.pathResult = await fetch("/paths", {})
+        this.pathResult = await fetch(`http://localhost:8080/paths?source=${this.path.source}&target=${this.path.target}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(response => response.json());
       } catch (e) {
         this.showSnackbar(SNACKBAR_MESSAGES.COMMON.FAIL);
         throw new Error(e);
@@ -169,8 +174,13 @@ export default {
     async initAllStationsView() {
       try {
         // TODO 모든 역을 불러오는 API를 추가해주세요.
-        // const stations = await fetch("/stations")
-        // this.setStations(stations)
+        const stations = await fetch("http://localhost:8080/stations", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            }
+          }).then(response => response.json());
+        this.setStations(stations)
         if (this.stations.length < 1) {
           return;
         }
