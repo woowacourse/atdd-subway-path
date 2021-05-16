9cc8bd023e53cd7538f7e450019100f67d0d6659
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-05-16 16:46:17 +0900

    refactor: [ERROR] prefix 제거

--- src/main/java/wooteco/subway/auth/exception/UserLoginFailException.java
+++ src/main/java/wooteco/subway/auth/exception/UserLoginFailException.java
@@ -7,7 +7,7 @@ public class UserLoginFailException extends RuntimeException {
     private static final int STATUS_CODE = HttpStatus.UNAUTHORIZED.value();
 
     public UserLoginFailException() {
-        super("[ERROR] 로그인 정보가 맞지 않습니다.");
+        super("로그인 정보가 맞지 않습니다.");
     }
 
     public int getStatusCode() {
