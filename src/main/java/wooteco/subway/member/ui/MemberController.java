fccb5be18bc9f1ae3548474a9e0d8b3b742e54b2
Jinho <jh8579@gmail.com>
2021-05-17 08:08:01 +0900

    refactor: @RequestMapping을 사용하여 중복 사용한 url 제거 및 메소드 반환 값 수정

--- src/main/java/wooteco/subway/member/ui/MemberController.java
+++ src/main/java/wooteco/subway/member/ui/MemberController.java
@@ -8,15 +8,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.auth.domain.AuthenticationPrincipal;
-import wooteco.subway.auth.dto.TokenResponse;
 import wooteco.subway.member.application.MemberService;
 import wooteco.subway.member.domain.LoginMember;
 import wooteco.subway.member.dto.MemberRequest;
 import wooteco.subway.member.dto.MemberResponse;
 
 @RestController
+@RequestMapping("/members")
 public class MemberController {
     private MemberService memberService;
 
@@ -24,48 +25,46 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @PostMapping("/members")
+    @PostMapping
     public ResponseEntity createMember(@RequestBody MemberRequest request) {
         MemberResponse member = memberService.createMember(request);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
     }
 
-    @GetMapping("/members/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<MemberResponse> findMember(@PathVariable Long id) {
         MemberResponse member = memberService.findMember(id);
         return ResponseEntity.ok().body(member);
     }
 
-    @PutMapping("/members/{id}")
-    public ResponseEntity<MemberResponse> updateMember(@PathVariable Long id,
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateMember(@PathVariable Long id,
         @RequestBody MemberRequest param) {
         memberService.updateMember(id, param);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/members/{id}")
-    public ResponseEntity<MemberResponse> deleteMember(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMember(@PathVariable Long id) {
         memberService.deleteMember(id);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/members/me")
+    @GetMapping("/me")
     public ResponseEntity<MemberResponse> findMemberOfMine(
         @AuthenticationPrincipal LoginMember loginMember) {
         MemberResponse memberResponse = memberService.findMember(loginMember.getId());
         return ResponseEntity.ok().body(memberResponse);
     }
 
-    @PutMapping("/members/me")
-    public ResponseEntity<TokenResponse> updateMemberOfMine(
+    @PutMapping("/me")
+    public ResponseEntity<Void> updateMemberOfMine(
         @AuthenticationPrincipal LoginMember loginMember, @RequestBody MemberRequest request) {
-        System.out.println("loginMember = " + loginMember);
         memberService.updateMember(loginMember.getId(), request);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
-    // TODO: 구현 하기
-    @DeleteMapping("/members/me")
+    @DeleteMapping("/me")
     public ResponseEntity<MemberResponse> deleteMemberOfMine(
         @AuthenticationPrincipal LoginMember loginMember) {
         memberService.deleteMember(loginMember.getId());
