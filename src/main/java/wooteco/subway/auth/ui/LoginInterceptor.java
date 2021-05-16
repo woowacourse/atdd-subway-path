8a08f872254b1fb20dd6ca1a3129cfa9473a1385
dwl5 <ssop6403@gmail.com>
2021-05-16 18:51:37 +0900

    refactor: 로그인 CORS 해결

--- src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
+++ src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
@@ -3,8 +3,10 @@ package wooteco.subway.auth.ui;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.HandlerInterceptor;
 import wooteco.subway.auth.application.AuthorizationException;
 import wooteco.subway.auth.infrastructure.AuthorizationExtractor;
@@ -22,6 +24,11 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
         final Object handler) throws Exception {
+
+        if (StringUtils.equals(request.getMethod(), "OPTIONS")) {
+            return true;
+        }
+
         String accessToken = AuthorizationExtractor.extract(request);
         jwtTokenProvider.validateToken(accessToken);
         return true;
