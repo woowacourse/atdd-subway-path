b3657d762961b73e3652f5bd969ea5004a4d0f19
perenok <perenoke@gmail.com>
2021-05-17 00:55:30 +0900

    feat, refactor : 인터셉터를 통해 토큰 검증 및 피드백 반영

--- src/main/java/wooteco/subway/exception/SubwayException.java
+++ src/main/java/wooteco/subway/exception/SubwayException.java
@@ -1,7 +1,22 @@
 package wooteco.subway.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class SubwayException extends RuntimeException {
-    public SubwayException(String message) {
-        super(message);
+
+    private final HttpStatus httpStatus;
+    private final Object body;
+
+    public SubwayException(HttpStatus httpStatus, Object body) {
+        this.httpStatus = httpStatus;
+        this.body = body;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public Object getBody() {
+        return body;
     }
 }
--- src/main/java/wooteco/subway/exception/auth/InvalidMemberException.java
+++ src/main/java/wooteco/subway/exception/auth/InvalidMemberException.java
@@ -1,7 +1,9 @@
 package wooteco.subway.exception.auth;
 
+import org.springframework.http.HttpStatus;
+
 public class InvalidMemberException extends AuthException {
     public InvalidMemberException(String email) {
-        super(email + "에 해당하는 회원이 없습니다.");
+        super(HttpStatus.UNAUTHORIZED, email + "에 해당하는 회원이 없습니다.");
     }
 }
