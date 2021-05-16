4e61d9ee17679ec847078566cf5f10afc55bd020
kimjunseo <air.junseo@gmail.com>
2021-05-17 00:50:50 +0900

    refactor : tokenRequest 데이터 검증

--- src/main/java/wooteco/subway/auth/ui/AuthController.java
+++ src/main/java/wooteco/subway/auth/ui/AuthController.java
@@ -8,6 +8,8 @@ import wooteco.subway.auth.application.AuthService;
 import wooteco.subway.auth.dto.TokenRequest;
 import wooteco.subway.auth.dto.TokenResponse;
 
+import javax.validation.Valid;
+
 @RestController
 public class AuthController {
     private final AuthService authService;
@@ -17,7 +19,7 @@ public class AuthController {
     }
 
     @PostMapping("/login/token")
-    public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest tokenRequest) {
+    public ResponseEntity<TokenResponse> login(@RequestBody @Valid TokenRequest tokenRequest) {
         TokenResponse tokenResponse = authService.createToken(tokenRequest);
         return ResponseEntity.ok().body(tokenResponse);
     }
