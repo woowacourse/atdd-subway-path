99177d727e5844caf8ce80f94f7e7c316d1903f2
YeongBeom Heo <hybeom@gmail.com>
2021-05-16 21:36:40 +0900

    refactor(Exception) : Message 포멧 수정

--- src/main/java/wooteco/subway/exception/auth/LoginFailEmailException.java
+++ src/main/java/wooteco/subway/exception/auth/LoginFailEmailException.java
@@ -3,6 +3,6 @@ package wooteco.subway.exception.auth;
 public class LoginFailEmailException extends LoginFailException {
 
     public LoginFailEmailException() {
-        super("[ERROR] 해당 이메일이 존재하지 않습니다.");
+        super("해당 이메일이 존재하지 않습니다.");
     }
 }
--- src/main/java/wooteco/subway/exception/auth/LoginWrongPasswordException.java
+++ src/main/java/wooteco/subway/exception/auth/LoginWrongPasswordException.java
@@ -3,6 +3,6 @@ package wooteco.subway.exception.auth;
 public class LoginWrongPasswordException extends LoginFailException {
 
     public LoginWrongPasswordException() {
-        super("[ERROR] 해당 비밀번호가 틀렸습니다.");
+        super("해당 비밀번호가 틀렸습니다.");
     }
 }
