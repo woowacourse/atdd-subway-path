6b2d6c95b83eeba503afa7572c205fcd9d01a013
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-05-16 19:45:35 +0900

    feat : 최단 경로 조회 api 구현(프론트)

--- frontend/src/pages/path/PathPage.vue
+++ frontend/src/pages/path/PathPage.vue
@@ -159,8 +159,18 @@ export default {
     ...mapMutations([SHOW_SNACKBAR, SET_STATIONS]),
     async onSearchResult() {
       try {
-        // TODO 최단 거리를 검색하는 API를 추가해주세요.
-        // this.pathResult = await fetch("/paths", {})
+        const response = await fetch(`http://localhost:8080/paths?source=${this.path.source}
+        &target=${this.path.target}`, {
+          method: "GET",
+          headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          }
+        });
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        }
+        this.pathResult = await response.json();
       } catch (e) {
         this.showSnackbar(SNACKBAR_MESSAGES.COMMON.FAIL);
         throw new Error(e);
