abdc01c9cf7ca4edad807b3900e15ac7c6b9a5f4
hyeon9mak <jinha3507@gmail.com>
2021-05-17 16:58:55 +0900

    refactor: 투두 주석 제거

--- frontend/src/pages/path/PathPage.vue
+++ frontend/src/pages/path/PathPage.vue
@@ -159,7 +159,6 @@ export default {
     ...mapMutations([SHOW_SNACKBAR, SET_STATIONS]),
     async onSearchResult() {
       try {
-        // TODO 최단 거리를 검색하는 API를 추가해주세요.
         this.pathResult = await fetch(`http://localhost:8080/paths?source=${this.path.source}&target=${this.path.target}`, {
           method: 'GET',
           headers: {
@@ -173,7 +172,6 @@ export default {
     },
     async initAllStationsView() {
       try {
-        // TODO 모든 역을 불러오는 API를 추가해주세요.
         const stations = await fetch("http://localhost:8080/stations", {
             method: "GET",
             headers: {
