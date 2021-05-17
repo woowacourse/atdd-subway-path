51f63a149d1f5e6e291fac5f819bb428e60f208c
Kimun Kim <kkm97351@gmail.com>
2021-05-17 13:54:30 +0900

    refactor: AuthControllerAdvice가 전역이 아닌 Auth 도메인에서만 작동하도록 리팩토링

--- src/main/java/wooteco/subway/controller/AuthControllerAdvice.java
+++ src/main/java/wooteco/subway/controller/AuthControllerAdvice.java
@@ -1,12 +1,15 @@
 package wooteco.subway.controller;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.exception.AuthenticationException;
 
-@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice(assignableTypes = {AuthControllerAdvice.class})
 public class AuthControllerAdvice {
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<String> handleIllegalArgumentException(AuthenticationException e) {
