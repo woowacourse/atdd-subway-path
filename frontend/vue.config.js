9fcfad12bc85541bc546653df5b0c4792103823a
xrabcde <xrabcde@gmail.com>
2021-05-18 01:08:04 +0900

    feat(LoginInterceptor): 로그인 인터셉터 구현

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,16 @@
+const path = require('path');
+
 module.exports = {
+  outputDir: path.resolve(__dirname, '../src/main/resources/static/'),
+  devServer: {
+    proxy: {
+      '/': {
+        target: 'http://localhost:8080/',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
   transpileDependencies: [
     'vuetify'
   ]
---
+++ src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package wooteco.subway.auth.ui;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import wooteco.subway.auth.application.AuthorizationException;
+import wooteco.subway.auth.infrastructure.AuthorizationExtractor;
+import wooteco.subway.auth.infrastructure.JwtTokenProvider;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public LoginInterceptor(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        final String accessToken = AuthorizationExtractor.extract(request);
+        if(!jwtTokenProvider.validateToken(accessToken)) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+}
