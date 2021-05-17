e20d09245befe94e63206bc9f620e4996ec378ff
JinYoung Park <basejin0810@naver.com>
2021-05-17 23:27:36 +0900

    fix: Auth, Member 관련 테스트 에러 수정

--- src/main/java/wooteco/subway/member/ui/MemberController.java
+++ src/main/java/wooteco/subway/member/ui/MemberController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @PostMapping
     public ResponseEntity<Void> createMember(@RequestBody MemberRequest request) {
         MemberResponse member = memberService.createMember(request);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
--- src/test/java/wooteco/subway/member/MemberAcceptanceTest.java
+++ src/test/java/wooteco/subway/member/MemberAcceptanceTest.java
@@ -31,7 +31,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
             .given().log().all()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(memberRequest)
-            .when().post("/members")
+            .when().post("/api/members")
             .then().log().all()
             .extract();
     }
@@ -41,7 +41,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
             .given().log().all()
             .auth().oauth2(tokenResponse.getAccessToken())
             .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when().get("/members/me")
+            .when().get("/api/members/me")
             .then().log().all()
             .statusCode(HttpStatus.OK.value())
             .extract();
@@ -56,7 +56,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
             .auth().oauth2(tokenResponse.getAccessToken())
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(memberRequest)
-            .when().put("/members/me")
+            .when().put("/api/members/me")
             .then().log().all()
             .extract();
     }
@@ -65,7 +65,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         return RestAssured
             .given().log().all()
             .auth().oauth2(tokenResponse.getAccessToken())
-            .when().delete("/members/me")
+            .when().delete("/api/members/me")
             .then().log().all()
             .extract();
     }
