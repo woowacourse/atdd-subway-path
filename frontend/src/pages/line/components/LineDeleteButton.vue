574cb4eb2d1db481b89e66b6e7380bc5a1148198
Daeun Lee <leede0418@likelion.org>
2021-05-17 18:38:50 +0900

    feat(line): 노선 관련 front API 구현

--- frontend/src/pages/line/components/LineDeleteButton.vue
+++ frontend/src/pages/line/components/LineDeleteButton.vue
@@ -21,11 +21,14 @@ export default {
     ...mapMutations([SHOW_SNACKBAR, SET_LINES]),
     async onDeleteLine() {
       try {
-        // TODO Line을 삭제하는 API를 추가해주세요.
-        // await fetch("/api/lines/{id}")
-        // TODO 전체 Line 데이터를 불러오는 API를 추가해주세요.
-        // const lines = await fetch("/api/lines")
-        // this.setLines([...lines])
+        // Line을 삭제하는 API를 추가해주세요.
+        await fetch(`/api/lines/${this.line.id}`, {
+          method: 'DELETE'
+        });
+        // 전체 Line 데이터를 불러오는 API를 추가해주세요.
+        const linesResponse = await fetch("/api/lines");
+        const lines = await linesResponse.json();
+        this.setLines([...lines]);
         this.showSnackbar(SNACKBAR_MESSAGES.LINE.DELETE.SUCCESS);
       } catch (e) {
         this.showSnackbar(SNACKBAR_MESSAGES.LINE.DELETE.FAIL);
