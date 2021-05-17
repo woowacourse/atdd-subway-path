3154c1a707ca2373f855774c5aa4f7c0b96dc17f
YeongBeom Heo <hybeom@gmail.com>
2021-05-17 16:06:23 +0900

    feat(PathController) : Path Controller를 URL에 매핑

--- src/main/java/wooteco/subway/path/ui/PathController.java
+++ src/main/java/wooteco/subway/path/ui/PathController.java
@@ -1,5 +1,21 @@
 package wooteco.subway.path.ui;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.path.dto.PathResponse;
+
+@RestController
+@RequestMapping("/paths")
 public class PathController {
-    // TODO: 경로조회 기능 구현하기
+
+    @PostMapping
+    public ResponseEntity<PathResponse> findPath(@RequestParam("source") long sourceStationId,
+        @RequestParam("target") long targetStationId) {
+        PathResponse pathResponse = new PathResponse();
+        return ResponseEntity.ok()
+            .body(pathResponse);
+    }
 }
