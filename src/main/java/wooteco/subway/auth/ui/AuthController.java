47b0aa43dac63b73066a18dea7151c4839043328
dwl5 <ssop6403@gmail.com>
2021-05-18 03:03:13 +0900

    refactor: Valid로 tokenRequest검증

--- src/main/java/wooteco/subway/auth/ui/AuthController.java
+++ src/main/java/wooteco/subway/auth/ui/AuthController.java
@@ -1,12 +1,16 @@
 package wooteco.subway.auth.ui;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.auth.application.AuthService;
 import wooteco.subway.auth.dto.TokenRequest;
 import wooteco.subway.auth.dto.TokenResponse;
+
+import javax.validation.Valid;
+import java.util.Objects;
 
 @RestController
 public class AuthController {
@@ -18,7 +22,13 @@ public class AuthController {
     }
 
     @PostMapping("/login/token")
-    public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest tokenRequest) {
+    public ResponseEntity<TokenResponse> login(@Valid @RequestBody TokenRequest tokenRequest, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            throw new IllegalArgumentException(
+                    Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
+        }
+
         final TokenResponse tokenResponse = authService.createToken(tokenRequest);
         return ResponseEntity.ok(tokenResponse);
     }
