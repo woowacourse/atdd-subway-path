fb81a8bede665a4aeda52a904a0211433bf2a363
naeunkim <knae11@naver.com>
2021-05-17 18:39:50 +0900

    feat: js 경로탐색기능 구현

--- frontend/src/pages/path/PathPage.vue
+++ frontend/src/pages/path/PathPage.vue
@@ -159,8 +159,10 @@ export default {
     ...mapMutations([SHOW_SNACKBAR, SET_STATIONS]),
     async onSearchResult() {
       try {
-        // TODO 최단 거리를 검색하는 API를 추가해주세요.
-        // this.pathResult = await fetch("/paths", {})
+        const sourceId = this.path.source
+        const targetId = this.path.target
+        this.pathResult = await fetch(`/api/paths?source=${sourceId}&target=${targetId}`)
+            .then(data =>data.json())
       } catch (e) {
         this.showSnackbar(SNACKBAR_MESSAGES.COMMON.FAIL);
         throw new Error(e);
@@ -168,9 +170,8 @@ export default {
     },
     async initAllStationsView() {
       try {
-        // TODO 모든 역을 불러오는 API를 추가해주세요.
-        // const stations = await fetch("/stations")
-        // this.setStations(stations)
+        const stations = await fetch("/api/stations").then(data => data.json())
+        this.setStations([...stations])
         if (this.stations.length < 1) {
           return;
         }
