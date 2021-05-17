f15a1b4deea90402ff68e3605e2459a228794db2
Jinhong <xlffm3@gmail.com>
2021-05-17 21:12:08 +0900

    feat: front end api 구현

--- frontend/src/pages/line/components/LineDeleteButton.vue
+++ frontend/src/pages/line/components/LineDeleteButton.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-btn @click="onDeleteLine" icon>
+  <v-btn icon @click="onDeleteLine(line.id)">
     <v-icon color="grey lighten-1">mdi-delete</v-icon>
   </v-btn>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
-import { SET_LINES, SHOW_SNACKBAR } from "../../../store/shared/mutationTypes";
-import { SNACKBAR_MESSAGES } from "../../../utils/constants";
+import {mapMutations} from "vuex";
+import {SET_LINES, SHOW_SNACKBAR} from "../../../store/shared/mutationTypes";
+import {SNACKBAR_MESSAGES} from "../../../utils/constants";
 
 export default {
   name: "LineDeleteButton",
@@ -19,13 +19,23 @@ export default {
   },
   methods: {
     ...mapMutations([SHOW_SNACKBAR, SET_LINES]),
-    async onDeleteLine() {
+    async onDeleteLine(id) {
       try {
         // TODO Line을 삭제하는 API를 추가해주세요.
-        // await fetch("/api/lines/{id}")
+        await fetch('http://localhost:8080/lines/' + id, {
+          method: "DELETE",
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
         // TODO 전체 Line 데이터를 불러오는 API를 추가해주세요.
-        // const lines = await fetch("/api/lines")
-        // this.setLines([...lines])
+        const lines = await fetch("http://localhost:8080/lines", {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+          },
+        }).then(response => response.json());
+        this.setLines([...lines])
         this.showSnackbar(SNACKBAR_MESSAGES.LINE.DELETE.SUCCESS);
       } catch (e) {
         this.showSnackbar(SNACKBAR_MESSAGES.LINE.DELETE.FAIL);
