da96dfabd5085b9edaa6fdb073d05b2b12f8b233
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-18 02:12:46 +0900

    feature: cors preflight 위한 코드 추가

--- src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
+++ src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
@@ -1,6 +1,6 @@
 package wooteco.subway.auth.ui;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import wooteco.subway.auth.infrastructure.AuthorizationExtractor;
@@ -8,9 +8,14 @@ import wooteco.subway.auth.infrastructure.JwtTokenProvider;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
+
+    private static final String ORIGIN = "Origin";
+    private static final String ACCESS_REQUEST_METHOD = "Access-Control-Request-Method";
+    private static final String ACCESS_REQUEST_HEADERS = "Access-Control-Request-Headers";
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -21,8 +26,32 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
                              final Object handler) throws Exception {
+        if (isPreflightRequest(request)) {
+            return true;
+        }
         String accessToken = AuthorizationExtractor.extract(request);
         jwtTokenProvider.validateToken(accessToken);
         return true;
+    }
+
+    private boolean isPreflightRequest(HttpServletRequest request) {
+        return isOptionsMethod(request) && hasOrigin(request)
+                && hasRequestMethods(request) && hasRequestHeaders(request);
+    }
+
+    public boolean isOptionsMethod(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+    }
+
+    public boolean hasOrigin(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader(ORIGIN));
+    }
+
+    public boolean hasRequestMethods(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader(ACCESS_REQUEST_METHOD));
+    }
+
+    public boolean hasRequestHeaders(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader(ACCESS_REQUEST_HEADERS));
     }
 }
