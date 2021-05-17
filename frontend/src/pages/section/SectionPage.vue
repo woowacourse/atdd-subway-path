014cb38a1625f6ab9aa336fe4260006d09ec4e0d
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-17 16:03:04 +0900

    feat(SectionCreationButton.vue): 구간 추가 API 호출 구현

--- frontend/src/pages/section/SectionPage.vue
+++ frontend/src/pages/section/SectionPage.vue
@@ -86,12 +86,20 @@ export default {
   name: "SectionPage",
   components: { SectionDeleteButton, SectionCreateButton },
   async created() {
-    // TODO 초기 역 데이터를 불러오는 API를 추가해주세요.
-    // const stations = await fetch("/api/stations")
-    // this.setStations([...stations])
-    // TODO 초기 노선 데이터를 불러오는 API를 추가해주세요.
-    // const lines = await fetch("/api/lines");
-    // this.setLines([...lines]);
+    const stationResponse = await fetch("http://localhost:8080/stations");
+    if (!stationResponse.ok) {
+      throw new Error(`${stationResponse.status}`);
+    }
+    const stations = await stationResponse.json();
+    this.setStations([...stations]);
+
+    const lineResponse = await fetch("http://localhost:8080/lines");
+    if (!lineResponse.ok) {
+      throw new Error(`${lineResponse.status}`);
+    }
+    const lines = await lineResponse.json();
+    this.setLines([...lines]);
+
     this.initLinesView();
   },
   computed: {
@@ -124,8 +132,12 @@ export default {
     },
     async onChangeLine() {
       try {
-        // TODO 선택한 노선 데이터를 불러오는 API를 추가해주세요.
-        // this.activeLine = await fetch("/lines/{this.activeLineId}");
+        const lineId = this.activeLineId;
+        const response = await fetch("http://localhost:8080/lines/" + lineId);
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        }
+        this.activeLine = await response.json();
       } catch (e) {
         this.showSnackbar(SNACKBAR_MESSAGES.COMMON.FAIL);
         throw new Error(e);
