fdf1a7848ee4f76fd3689650d3a077f47e83717b
joanne <mathmjseo@naver.com>
2021-05-17 17:13:18 +0900

    feat: 최단 경로 조회를 위한 Dijkstra Graph로 변환하는 로직 작성

--- src/main/java/wooteco/subway/path/service/PathService.java
+++ src/main/java/wooteco/subway/path/service/PathService.java
@@ -1,9 +1,13 @@
 package wooteco.subway.path.service;
 
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.line.dao.LineDao;
 import wooteco.subway.line.domain.Line;
+import wooteco.subway.line.domain.Section;
+import wooteco.subway.path.controller.domain.ShortestPath;
 import wooteco.subway.path.controller.dto.PathResponse;
 import wooteco.subway.station.dao.StationDao;
 import wooteco.subway.station.domain.Station;
@@ -25,6 +29,28 @@ public class PathService {
         final List<Line> lines = lineDao.findAll();
         final Station source = stationDao.findById(sourceId);
         final Station target = stationDao.findById(targetId);
-        return new PathResponse();
+
+        final WeightedMultigraph<Station, DefaultWeightedEdge> graph = addAllStationsInLine(lines);
+        ShortestPath shortestPath = new ShortestPath(graph);
+        final List<Station> shor = shortestPath.getShortestPath(source, target);
+        final double distance = shortestPath.getDistance(source, target);
+        return PathResponse.of(shor, (int) distance);
+    }
+
+    private WeightedMultigraph<Station, DefaultWeightedEdge> addAllStationsInLine(List<Line> lines) {
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph
+                = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+        for (Line line : lines) {
+            final List<Station> stations = line.getStations();
+            stations.forEach(graph::addVertex);
+            final List<Section> sections = line.getSections().getSections();
+            sections.forEach(section -> graph.setEdgeWeight(
+                    graph.addEdge(section.getUpStation(), section.getDownStation()),
+                    section.getDistance()
+            ));
+        }
+
+        return graph;
     }
 }
