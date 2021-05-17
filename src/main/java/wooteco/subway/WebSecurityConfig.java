66e7c59bb6fff54f7e985a3a350f39eb0d8daa47
y_woo :) <dusdn1702@naver.com>
2021-05-18 02:42:38 +0900

    refactor: security를 이용한 비밀번호 암호화

---
+++ src/main/java/wooteco/subway/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package wooteco.subway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .formLogin().disable()
+            .csrf().disable()
+            .headers().frameOptions().disable();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/wooteco/subway/auth/application/AuthService.java
+++ src/main/java/wooteco/subway/auth/application/AuthService.java
@@ -1,24 +1,27 @@
 package wooteco.subway.auth.application;
 
-import java.util.Optional;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import wooteco.subway.auth.dto.TokenRequest;
 import wooteco.subway.auth.dto.TokenResponse;
 import wooteco.subway.auth.infrastructure.JwtTokenProvider;
 import wooteco.subway.exception.EmailNotFoundException;
+import wooteco.subway.exception.PasswordMissMatchException;
 import wooteco.subway.member.dao.MemberDao;
 import wooteco.subway.member.domain.Member;
 
 @Service
 public class AuthService {
-
     private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
     private final MemberDao memberDao;
 
-    public AuthService(JwtTokenProvider jwtTokenProvider, MemberDao memberDao) {
+    public AuthService(JwtTokenProvider jwtTokenProvider,
+        PasswordEncoder passwordEncoder, MemberDao memberDao) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.passwordEncoder = passwordEncoder;
         this.memberDao = memberDao;
     }
 
@@ -29,8 +32,10 @@ public class AuthService {
     }
 
     private TokenResponse create(TokenRequest tokenRequest, Member member) {
-        member.checkValidPassword(tokenRequest.getPassword());
-        String accessToken = jwtTokenProvider.createToken(String.valueOf(member.getId()));
-        return new TokenResponse(accessToken);
+        if (passwordEncoder.matches(tokenRequest.getPassword(), member.getPassword())) {
+            String accessToken = jwtTokenProvider.createToken(String.valueOf(member.getId()));
+            return new TokenResponse(accessToken);
+        }
+        throw new PasswordMissMatchException();
     }
 }
