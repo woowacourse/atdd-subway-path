a2f4de27751ef5d8b49905ff5bdc7e9854b992d7
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-17 17:22:28 +0900

    feat: 출발역과 도착역 사이의 최단 경로 로직과 API 구현

---
+++ src/main/java/wooteco/subway/path/application/PathService.java
@@ -0,0 +1,79 @@
+package wooteco.subway.path.application;
+
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import org.springframework.stereotype.Service;
+import wooteco.subway.line.dao.LineDao;
+import wooteco.subway.line.domain.Line;
+import wooteco.subway.line.domain.Section;
+import wooteco.subway.line.domain.Sections;
+import wooteco.subway.path.dto.PathResponse;
+import wooteco.subway.station.dao.StationDao;
+import wooteco.subway.station.domain.Station;
+import wooteco.subway.station.dto.StationResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PathService {
+    private final LineDao lineDao;
+    private final StationDao stationDao;
+
+    public PathService(LineDao lineDao, StationDao stationDao) {
+        this.lineDao = lineDao;
+        this.stationDao = stationDao;
+    }
+
+    public PathResponse findShortestPath(Long sourceId, Long targetId) {
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = loadCurrentSubwayMap();
+        Station source = stationDao.findById(sourceId);
+        Station target = stationDao.findById(targetId);
+        return calculateShortestPath(graph, source, target);
+    }
+
+    private WeightedMultigraph<Station, DefaultWeightedEdge> loadCurrentSubwayMap() {
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        loadStationInfo(graph);
+        loadLineInfo(graph);
+        return graph;
+    }
+
+    private void loadStationInfo(WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        final List<Station> stations = stationDao.findAll();
+        for (Station station : stations) {
+            graph.addVertex(station);
+        }
+    }
+
+    private void loadLineInfo(WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        final List<Line> lines = lineDao.findAll();
+        for (Line line : lines) {
+            final Sections sections = line.getSections();
+            loadSectionInfo(graph, sections);
+        }
+    }
+
+    private void loadSectionInfo(WeightedMultigraph<Station, DefaultWeightedEdge> graph, Sections sections) {
+        final List<Section> sectionsList = sections.getSections();
+        for (Section section : sectionsList) {
+            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+        }
+    }
+
+    private PathResponse calculateShortestPath(WeightedMultigraph<Station, DefaultWeightedEdge> graph, Station source,
+                                               Station target) {
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        final GraphPath shortestGraphPath = dijkstraShortestPath.getPath(source, target);
+        final List<Station> shortestPath = shortestGraphPath.getVertexList();
+        final int distance = (int) shortestGraphPath.getWeight();
+
+        List<StationResponse> shortestPathStationResponse = shortestPath.stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList());
+
+        return new PathResponse(shortestPathStationResponse, distance);
+    }
+}
