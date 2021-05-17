21d642aeeffee828741bae71891511e108b4c22e
dwl5 <ssop6403@gmail.com>
2021-05-18 02:38:10 +0900

    feat: path controller 구현

--- src/main/java/wooteco/subway/path/ui/PathController.java
+++ src/main/java/wooteco/subway/path/ui/PathController.java
@@ -1,5 +1,29 @@
 package wooteco.subway.path.ui;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import wooteco.subway.path.dto.PathResponse;
+import wooteco.subway.path.service.PathService;
+
+@Controller
+@RequestMapping("/paths")
 public class PathController {
-    // TODO: 경로조회 기능 구현하기
+
+    private final PathService pathService;
+
+    public PathController(final PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PathResponse> getShortestDistancePath(@RequestParam Long source,
+                                                                @RequestParam Long target) {
+
+        return ResponseEntity.ok(pathService.getShortestDistancePath(source, target));
+    }
 }
