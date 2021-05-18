78cc24a24de6055c269c33aced8d8ffdb2f03061
ecsimsw <ecsimsw@gmail.com>
2021-05-18 22:27:17 +0900

    refactor : 질문 사항 표시 (XXX)

--- src/main/java/wooteco/subway/auth/application/AuthService.java
+++ src/main/java/wooteco/subway/auth/application/AuthService.java
@@ -1,22 +1,29 @@
 package wooteco.subway.auth.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.auth.domain.AuthorizationPayLoad;
 import wooteco.subway.auth.dto.TokenRequest;
 import wooteco.subway.auth.infrastructure.JwtTokenProvider;
 import wooteco.subway.member.dao.MemberDao;
-import wooteco.subway.member.domain.Member;
 
 @Service
 public class AuthService {
 
+    // XXX :: MemberService와 MemberDao 중 선택의 근거
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
 
-    public AuthService(final JwtTokenProvider jwtTokenProvider) {
+    public AuthService(final JwtTokenProvider jwtTokenProvider, final MemberDao memberDao) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.memberDao = memberDao;
     }
 
+    @Transactional(readOnly = true)
     public String createToken(final TokenRequest tokenRequest) {
+        if(memberDao.isNotExistUser(tokenRequest.getEmail(), tokenRequest.getPassword())){
+            throw new AuthorizedException("올바른 사용자가 아닙니다.");
+        }
         return jwtTokenProvider.createToken(tokenRequest.getEmail());
     }
 
