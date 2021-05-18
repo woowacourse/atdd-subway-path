dedb644ff4e1957b854604933711848d7890c3a8
Minsung <sns2831@gmail.com>
2021-05-18 16:03:06 +0900

    refactor: 중복되는 url 제거

--- src/main/java/wooteco/subway/path/ui/PathController.java
+++ src/main/java/wooteco/subway/path/ui/PathController.java
@@ -1,5 +1,27 @@
 package wooteco.subway.path.ui;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.path.application.PathService;
+import wooteco.subway.path.dto.PathResponse;
+
+@RequestMapping("/paths")
+@RestController
 public class PathController {
-    // TODO: 경로조회 기능 구현하기
+
+    private final PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping
+    public ResponseEntity<PathResponse> showPath(@RequestParam Long source,
+                                                 @RequestParam Long target) {
+        return ResponseEntity.ok().body(pathService.findPath(source, target));
+    }
+
 }
