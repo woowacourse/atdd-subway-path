451e87d5bac2f434362fb5ac750ae3399433f2d2
Daeun Lee <leede0418@likelion.org>
2021-05-18 11:59:12 +0900

    refactor(interceptor): Preflight request를 처리하는 메소드 추가
    - Preflight request는 Authorization 헤더가 없음 -> 토큰 없음
    - 따라서, 토큰 유효성 검증 불필요 -> 해당 로직 전에 인터셉터를 통과하도록 구현

--- src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
+++ src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package wooteco.subway.auth.ui;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import wooteco.subway.auth.infrastructure.AuthorizationExtractor;
@@ -7,6 +8,7 @@ import wooteco.subway.auth.infrastructure.JwtTokenProvider;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
@@ -17,9 +19,32 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (isPreflightRequest(request)) {
+            return true;
+        }
         String token = AuthorizationExtractor.extract(request);
         jwtTokenProvider.validateToken(token);
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        return true;
+    }
+
+    private boolean isPreflightRequest(HttpServletRequest request) {
+        return isOptions(request) && hasHeaders(request) && hasMethod(request) && hasOrigin(request);
+    }
+
+    private boolean isOptions(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+    }
+
+    private boolean hasHeaders(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+    }
+
+    private boolean hasMethod(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+    }
+
+    private boolean hasOrigin(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Origin"));
     }
 }
