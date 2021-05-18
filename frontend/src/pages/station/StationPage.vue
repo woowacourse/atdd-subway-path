6add9cfe15039b232b9e8bc7c77a9b5ebbf2ba8b
hyeon9mak <jinha3507@gmail.com>
2021-05-18 21:15:06 +0900

    feat: 지하철 역 API 관련 프론트 페이지 구현

--- frontend/src/pages/station/StationPage.vue
+++ frontend/src/pages/station/StationPage.vue
@@ -55,9 +55,10 @@
 
 <script>
 import validator from "../../utils/validator";
-import { SNACKBAR_MESSAGES } from "../../utils/constants";
+import {FETCH_METHODS, SNACKBAR_MESSAGES} from "../../utils/constants";
 import { mapGetters, mapMutations } from "vuex";
 import { SET_STATIONS, SHOW_SNACKBAR } from "../../store/shared/mutationTypes";
+import {fetchJson, fetchJsonWithBody} from "@/utils/fetchJson";
 
 export default {
   name: "StationPage",
@@ -65,8 +66,7 @@ export default {
     ...mapGetters(["stations"]),
   },
   async created() {
-    // TODO 초기 역 데이터를 불러오는 API를 추가해주세요.
-    const response = await fetch("http://localhost:8080/stations");
+    const response = await fetch("/api/stations");
     if (!response.ok) {
       throw new Error(`${response.status}`);
     }
@@ -83,16 +83,7 @@ export default {
         return;
       }
       try {
-        // TODO 역을 추가하는 API Sample
-        const response = await fetch("http://localhost:8080/stations", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: this.stationName,
-          }),
-        });
+        const response = await fetchJsonWithBody('/api/stations', FETCH_METHODS.POST, this.stationName);
         if (!response.ok) {
           throw new Error(`${response.status}`);
         }
@@ -112,8 +103,11 @@ export default {
     },
     async onDeleteStation(stationId) {
       try {
-        // TODO 역을 삭제하는 API를 추가해주세요.
-        // await fetch("http://localhost:8080/stations/{id}");
+        const response = await fetchJson(`/api/stations/${stationId}`, FETCH_METHODS.DELETE);
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        }
+
         const idx = this.stations.findIndex(
           (station) => station.id === stationId
         );
