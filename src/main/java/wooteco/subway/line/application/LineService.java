79096774b254f84c0fa0db2c3bf11d91ddecba3c
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-18 14:21:07 +0900

    refactor: 메소드참조식으로 변경

--- src/main/java/wooteco/subway/line/application/LineService.java
+++ src/main/java/wooteco/subway/line/application/LineService.java
@@ -46,7 +46,7 @@ public class LineService {
     public List<LineResponse> findLineResponses() {
         List<Line> persistLines = findLines();
         return persistLines.stream()
-                .map(line -> LineResponse.of(line))
+                .map(LineResponse::of)
                 .collect(Collectors.toList());
     }
 
