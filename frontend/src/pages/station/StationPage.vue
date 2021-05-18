3560e7072840da0d34e4b92cc2e15bc705b19812
cjy17 <cjy1705@naver.com>
2021-05-18 17:16:54 +0900

    fix : 역 추가, 삭제 기능 프론트코드 버그 수정

--- frontend/src/pages/station/StationPage.vue
+++ frontend/src/pages/station/StationPage.vue
@@ -65,7 +65,7 @@ export default {
     ...mapGetters(["stations"]),
   },
   async created() {
-    // TODO 초기 역 데이터를 불러오는 API를 추가해주세요. (v)
+    // TODO 초기 역 데이터를 불러오는 API를 추가해주세요. (x)
     const response = await fetch("http://localhost:8080/stations");
     if (!response.ok) {
       throw new Error(`${response.status}`);
@@ -83,7 +83,7 @@ export default {
         return;
       }
       try {
-        // TODO 역을 추가하는 API Sample (v)
+        // TODO 역을 추가하는 API Sample (x)
         const response = await fetch("http://localhost:8080/stations", {
           method: "POST",
           headers: {
@@ -112,7 +112,7 @@ export default {
     },
     async onDeleteStation(stationId) {
       try {
-        // TODO 역을 삭제하는 API를 추가해주세요. (v)
+        // TODO 역을 삭제하는 API를 추가해주세요. (x)
         await fetch("http://localhost:8080/stations/" + stationId, {
           method: "DELETE"
         });
