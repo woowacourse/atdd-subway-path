e68f91063fbf06087da8ec7607a34fd41e2e9ef7
ecsimsw <ecsimsw@gmail.com>
2021-05-18 20:38:30 +0900

    refactor : 코드 리팩토링

--- src/main/java/wooteco/subway/auth/application/AuthorizedException.java
+++ src/main/java/wooteco/subway/auth/application/AuthorizedException.java
@@ -1,10 +1,6 @@
 package wooteco.subway.auth.application;
 
 public class AuthorizedException extends RuntimeException {
-    public AuthorizedException() {
-        super("적절하지 않은 권한입니다.");
-    }
-
     public AuthorizedException(final String message) {
         super(message);
     }
--- src/test/java/wooteco/subway/AcceptanceTest.java
+++ src/test/java/wooteco/subway/AcceptanceTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
