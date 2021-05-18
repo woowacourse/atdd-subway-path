63f3ee55213fdf4c49ed3cf34a5dde37c7197851
Kimun Kim <kkm97351@gmail.com>
2021-05-18 18:06:14 +0900

    feat: frontend 역 목록 조회 및 삭제 API 호출 구현

--- frontend/src/pages/station/StationPage.vue
+++ frontend/src/pages/station/StationPage.vue
@@ -9,23 +9,24 @@
           <v-form ref="stationForm" v-model="valid" @submit.prevent>
             <div class="d-flex">
               <v-text-field
-                color="grey darken-1"
-                class="mr-4"
-                @keydown.enter="onCreateStation"
-                label="지하철 역 이름을 입력해주세요."
-                v-model="stationName"
-                prepend-inner-icon="mdi-subway"
-                dense
-                outlined
-                :rules="rules.stationName"
-                autofocus
+                  color="grey darken-1"
+                  class="mr-4"
+                  @keydown.enter="onCreateStation"
+                  label="지하철 역 이름을 입력해주세요."
+                  v-model="stationName"
+                  prepend-inner-icon="mdi-subway"
+                  dense
+                  outlined
+                  :rules="rules.stationName"
+                  autofocus
               ></v-text-field>
               <v-btn
-                :disabled="!valid"
-                color="amber"
-                @click.prevent="onCreateStation"
-                depressed
-                >추가</v-btn
+                  :disabled="!valid"
+                  color="amber"
+                  @click.prevent="onCreateStation"
+                  depressed
+              >추가
+              </v-btn
               >
             </div>
           </v-form>
@@ -55,9 +56,9 @@
 
 <script>
 import validator from "../../utils/validator";
-import { SNACKBAR_MESSAGES } from "../../utils/constants";
-import { mapGetters, mapMutations } from "vuex";
-import { SET_STATIONS, SHOW_SNACKBAR } from "../../store/shared/mutationTypes";
+import {SNACKBAR_MESSAGES} from "../../utils/constants";
+import {mapGetters, mapMutations} from "vuex";
+import {SET_STATIONS, SHOW_SNACKBAR} from "../../store/shared/mutationTypes";
 
 export default {
   name: "StationPage",
@@ -65,8 +66,14 @@ export default {
     ...mapGetters(["stations"]),
   },
   async created() {
-    // TODO 초기 역 데이터를 불러오는 API를 추가해주세요.
-    const response = await fetch("/api/stations");
+    // 초기 역 데이터를 불러오는 API를 추가해주세요.
+    const response = await fetch("http://localhost:8080/stations", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Authorization": `Bearer ${localStorage.getItem("token")}`
+      }
+    });
     if (!response.ok) {
       throw new Error(`${response.status}`);
     }
@@ -84,10 +91,11 @@ export default {
       }
       try {
         // TODO 역을 추가하는 API Sample
-        const response = await fetch("/api/stations", {
+        const response = await fetch("http://localhost:8080/stations", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
           },
           body: JSON.stringify({
             name: this.stationName,
@@ -112,10 +120,15 @@ export default {
     },
     async onDeleteStation(stationId) {
       try {
-        // TODO 역을 삭제하는 API를 추가해주세요.
-        // await fetch("/api/stations/{id}");
+        // 역을 삭제하는 API를 추가해주세요.
+        await fetch(`http://localhost:8080/stations/${stationId}`, {
+          method: "DELETE",
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+          }
+        });
         const idx = this.stations.findIndex(
-          (station) => station.id === stationId
+            (station) => station.id === stationId
         );
         this.stations.splice(idx, 1);
         this.showSnackbar(SNACKBAR_MESSAGES.STATION.DELETE.SUCCESS);
@@ -127,7 +140,7 @@ export default {
   },
   data() {
     return {
-      rules: { ...validator },
+      rules: {...validator},
       valid: false,
       stationName: "",
     };
