d0da71d26214f5ad64c121e984a188269d3be094
Jinhong <xlffm3@gmail.com>
2021-05-19 17:40:01 +0900

    refactor: 등록되지 않은 경로 조회시 예외 발생

--- src/main/java/wooteco/subway/path/domain/SubwayMap.java
+++ src/main/java/wooteco/subway/path/domain/SubwayMap.java
@@ -1,8 +1,10 @@
 package wooteco.subway.path.domain;
 
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
+import wooteco.subway.exception.path.SubwayMapException;
 import wooteco.subway.section.domain.Section;
 
 import java.util.List;
@@ -31,12 +33,20 @@ public class SubwayMap {
     }
 
     public List<Long> findShortestPath(long sourceStationId, long targetStationId) {
-        return subwayMap.getPath(sourceStationId, targetStationId)
+        return findPath(sourceStationId, targetStationId)
                 .getVertexList();
     }
 
+    private GraphPath<Long, DefaultWeightedEdge> findPath(long sourceStationId, long targetStationId) {
+        try {
+            return subwayMap.getPath(sourceStationId, targetStationId);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new SubwayMapException();
+        }
+    }
+
     public int findShortestDistance(long sourceStationId, long targetStationId) {
-        return (int) subwayMap.getPath(sourceStationId, targetStationId)
+        return (int) findPath(sourceStationId, targetStationId)
                 .getWeight();
     }
 }
--- src/test/java/wooteco/subway/domain/SubwayMapTest.java
+++ src/test/java/wooteco/subway/domain/SubwayMapTest.java
@@ -3,6 +3,7 @@ package wooteco.subway.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import wooteco.subway.exception.path.SubwayMapException;
 import wooteco.subway.path.domain.SubwayMap;
 import wooteco.subway.section.domain.Section;
 import wooteco.subway.station.domain.Station;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class SubwayMapTest {
 
@@ -39,5 +41,13 @@ class SubwayMapTest {
 
         assertThat(shortestPath).containsExactly(1L, 3L, 4L);
         assertThat(shortestDistance).isEqualTo(7);
+    }
+
+    @DisplayName("없는 경로를 조회한다.")
+    @Test
+    void cannotFindShortestPath() {
+        assertThatCode(() -> subwayMap.findShortestPath(6L, 1L))
+                .isInstanceOf(SubwayMapException.class)
+                .hasMessage("존재하지 않는 역에 대해 경로를 조회할 수 없습니다.");
     }
 }
