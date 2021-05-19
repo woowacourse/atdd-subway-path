b10d2fadc6fcf3b248b71890be416536180e3380
Sol ji Choi <bssj9307@gmail.com>
2021-05-19 16:13:33 +0900

    refactor: 공통 uri RequestMapping 으로 작성

--- src/main/java/wooteco/subway/member/ui/MemberController.java
+++ src/main/java/wooteco/subway/member/ui/MemberController.java
@@ -13,6 +13,7 @@ import wooteco.subway.member.dto.TokenRequest;
 import java.net.URI;
 
 @RestController
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
@@ -20,21 +21,21 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @PostMapping("/members")
+    @PostMapping
     public ResponseEntity<Void> createMember(@RequestBody MemberRequest request) {
         MemberResponse member = memberService.createMember(request);
 
-        return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
+        return ResponseEntity.created(URI.create("/" + member.getId())).build();
     }
 
-    @GetMapping("/members/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<MemberResponse> findMember(@PathVariable Long id) {
         MemberResponse member = memberService.findMember(id);
 
         return ResponseEntity.ok().body(member);
     }
 
-    @GetMapping("/members/me")
+    @GetMapping("/me")
     public ResponseEntity<MemberResponse> findMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
         String email = loginMember.getEmail();
         Member member = memberService.findByEmail(email);
@@ -44,7 +45,7 @@ public class MemberController {
         );
     }
 
-    @PutMapping("/members/me")
+    @PutMapping("/me")
     public ResponseEntity<MemberResponse> updateMemberOfMine(@AuthenticationPrincipal LoginMember loginMember,
                                                              @RequestBody MemberRequest memberRequest) {
         String email = loginMember.getEmail();
@@ -55,14 +56,14 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/members/me")
+    @DeleteMapping("/me")
     public ResponseEntity<MemberResponse> deleteMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
         memberService.deleteMember(loginMember.getEmail());
 
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/members/authentication")
+    @PostMapping("/authentication")
     public ResponseEntity<Void> memberAuthenticate(@RequestBody TokenRequest tokenRequest) {
         memberService.authenticate(tokenRequest);
 
