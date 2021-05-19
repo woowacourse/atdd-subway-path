cdea90f85bed581f21ac52262d199ddf15c93c89
xrabcde <xrabcde@gmail.com>
2021-05-19 21:32:09 +0900

    refactor(PathService): 최단경로를 찾는 도메인 객체 Path 분리 및 테스트코드 작성

--- src/main/java/wooteco/subway/path/application/PathService.java
+++ src/main/java/wooteco/subway/path/application/PathService.java
@@ -1,13 +1,9 @@
 package wooteco.subway.path.application;
 
-import org.jgrapht.GraphPath;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 import wooteco.subway.line.application.LineService;
 import wooteco.subway.line.domain.Line;
-import wooteco.subway.line.domain.Section;
+import wooteco.subway.path.domain.Path;
 import wooteco.subway.path.dto.PathResponse;
 import wooteco.subway.station.dao.StationDao;
 import wooteco.subway.station.domain.Station;
@@ -25,48 +21,16 @@ public class PathService {
     }
 
     public PathResponse getDijkstraShortestPath(long sourceId, long targetId) {
-        GraphPath<Station, DefaultWeightedEdge> shortestPath = findShortestPath(sourceId, targetId);
+        List<Station> stations = stationDao.findAll();
+        List<Line> lines = lineService.findLines();
 
-        List<Station> stationList = shortestPath.getVertexList();
-        double distance = shortestPath.getWeight();
-
-        return PathResponse.of(stationList, distance);
-    }
-
-    private GraphPath<Station, DefaultWeightedEdge> findShortestPath(long sourceId, long targetId) {
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph = initializeGraph();
-
-        DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath(graph);
+        Path path = new Path(stations, lines);
         Station sourceStation = stationDao.findById(sourceId);
         Station targetStation = stationDao.findById(targetId);
 
-        return dijkstraShortestPath.getPath(sourceStation, targetStation);
-    }
+        List<Station> stationList = path.findShortestPath(sourceStation, targetStation);
+        double distance = path.findShortestDistance(sourceStation, targetStation);
 
-    private WeightedMultigraph<Station, DefaultWeightedEdge> initializeGraph() {
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph
-                = new WeightedMultigraph(DefaultWeightedEdge.class);
-        initializeStations(graph);
-        initializeSections(graph);
-        return graph;
-    }
-
-    private void initializeStations(WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
-        List<Station> stations = stationDao.findAll();
-        for (Station station : stations) {
-            graph.addVertex(station);
-        }
-    }
-
-    private void initializeSections(WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
-        List<Line> lines = lineService.findLines();
-        lines.forEach(line -> addSectionsByLine(graph, line));
-    }
-
-    private void addSectionsByLine(WeightedMultigraph<Station, DefaultWeightedEdge> graph, Line line) {
-        List<Section> sections = line.getSections().getSections();
-        for (Section section : sections) {
-            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
-        }
+        return PathResponse.of(stationList, distance);
     }
 }
