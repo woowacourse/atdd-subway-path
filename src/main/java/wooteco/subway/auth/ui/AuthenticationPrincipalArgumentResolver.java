db1af89931091f553ab5f42140b71e335f37c90e
binghe819 <binghe819@naver.com>
2021-05-19 13:28:12 +0900

    refactor: 아규먼트 리졸버에서 명확한 객체 타입을 반환하도록 수정

--- src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -25,7 +25,7 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+    public LoginMember resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         String token = AuthorizationExtractor.extract((HttpServletRequest) webRequest.getNativeRequest());
         if (!authService.validateToken(token)) {
             throw new UnauthorizedException();
