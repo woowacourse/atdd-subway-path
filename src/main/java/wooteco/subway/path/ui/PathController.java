312c1ed495096edeeaf3524838e2cd1582f4ee81
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-05-19 20:39:50 +0900

    feat: 최단 경로 탐색 URL 매칭

--- src/main/java/wooteco/subway/path/ui/PathController.java
+++ src/main/java/wooteco/subway/path/ui/PathController.java
@@ -1,5 +1,25 @@
 package wooteco.subway.path.ui;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import wooteco.subway.path.application.PathService;
+import wooteco.subway.path.dto.PathResponse;
+
+@Controller
 public class PathController {
-    // TODO: 경로조회 기능 구현하기
+
+    // [기능 추가]: 경로조회 기능 구현하기
+    private PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping("/paths")
+    public ResponseEntity<PathResponse> path(@RequestParam Long source, @RequestParam Long target) {
+        PathResponse pathResponse = pathService.findPath(source, target);
+        return ResponseEntity.ok(pathResponse);
+    }
 }
