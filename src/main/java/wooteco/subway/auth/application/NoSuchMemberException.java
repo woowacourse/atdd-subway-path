477814dfda52ee18f87baa265e78680028189eaa
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-20 10:28:30 +0900

    style(NoSuchMemberException): 상수 컨벤션 적용

--- src/main/java/wooteco/subway/auth/application/NoSuchMemberException.java
+++ src/main/java/wooteco/subway/auth/application/NoSuchMemberException.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class NoSuchMemberException extends EmptyResultDataAccessException {
-    private static String msg = "해당 Email로 등록된 회원이 없습니다.";
-    private static int expectedSize = 1;
+    private static String MESSAGE = "해당 Email로 등록된 회원이 없습니다.";
+    private static int EXPECTED_SIZE = 1;
 
     public NoSuchMemberException() {
-        super(msg, expectedSize);
+        super(MESSAGE, EXPECTED_SIZE);
     }
 }
