81e71a55b865528abc4ac6620268f02e8e8222f9
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-21 14:37:35 +0900

    refactor(NoSuchMemberException): EmptyResultDataAccessException 대신 RuntimeException을 상속받도록 수정

--- src/main/java/wooteco/subway/auth/application/NoSuchMemberException.java
+++ src/main/java/wooteco/subway/auth/application/NoSuchMemberException.java
@@ -1,15 +1,13 @@
 package wooteco.subway.auth.application;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class NoSuchMemberException extends EmptyResultDataAccessException {
+public class NoSuchMemberException extends RuntimeException {
     private static String MESSAGE = "해당 Email로 등록된 회원이 없습니다.";
-    private static int EXPECTED_SIZE = 1;
 
     public NoSuchMemberException() {
-        super(MESSAGE, EXPECTED_SIZE);
+        super(MESSAGE);
     }
 }
