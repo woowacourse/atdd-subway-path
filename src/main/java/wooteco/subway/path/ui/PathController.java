9467c691f74175813da66d265a0ab15fee015de8
Gomding <backchida@gmail.com>
2021-05-22 01:16:59 +0900

    feat(PathController.java) : 최단거리를 검색해서 가져오는 매핑 추가

--- src/main/java/wooteco/subway/path/ui/PathController.java
+++ src/main/java/wooteco/subway/path/ui/PathController.java
@@ -1,5 +1,23 @@
 package wooteco.subway.path.ui;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.path.application.PathService;
+import wooteco.subway.path.dto.PathResponse;
+
+@RestController
 public class PathController {
-    // TODO: 경로조회 기능 구현하기
+    private final PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping("/paths")
+    public ResponseEntity<PathResponse> getPaths(@RequestParam long source, @RequestParam long target) {
+        PathResponse pathResponse = pathService.findPaths(source, target);
+        return ResponseEntity.ok().body(pathResponse);
+    }
 }
