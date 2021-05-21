ed89d6cf256978c345080053beb868c87d8d56cc
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-21 14:54:50 +0900

    feat(AuthController): NoSuchMemberException, PasswordInvalidException에 대한 핸들러 추가

--- src/main/java/wooteco/subway/auth/ui/AuthController.java
+++ src/main/java/wooteco/subway/auth/ui/AuthController.java
@@ -1,10 +1,14 @@
 package wooteco.subway.auth.ui;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import wooteco.subway.auth.application.AuthService;
+import wooteco.subway.auth.application.NoSuchMemberException;
+import wooteco.subway.auth.application.PasswordInvalidException;
 import wooteco.subway.auth.dto.TokenRequest;
 import wooteco.subway.auth.dto.TokenResponse;
 
@@ -23,4 +27,13 @@ public class AuthController {
         return ResponseEntity.ok().body(tokenResponse);
     }
 
+    @ExceptionHandler(NoSuchMemberException.class)
+    public ResponseEntity<Void> handleNoSuchMemberException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler(PasswordInvalidException.class)
+    public ResponseEntity<Void> handlePasswordInvalidException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 }
