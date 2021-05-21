9a932020bb0119079957961d4f756fb2a06906cf
Gomding <backchida@gmail.com>
2021-05-22 01:23:05 +0900

    feat(PathPage.vue): 최단거리 검색하는 api 사용하도록 구현

--- frontend/src/pages/path/PathPage.vue
+++ frontend/src/pages/path/PathPage.vue
@@ -162,8 +162,8 @@ export default {
     ...mapMutations([SHOW_SNACKBAR, SET_STATIONS]),
     async onSearchResult() {
       try {
-        // TODO 최단 거리를 검색하는 API를 추가해주세요.
-        // this.pathResult = await fetch("/paths", {})
+        this.pathResult = await fetch("/paths", {
+        }, {})
       } catch (e) {
         this.showSnackbar(SNACKBAR_MESSAGES.COMMON.FAIL);
         throw new Error(e);
