c1c7dd5136684cc20eb8b4055d389421412143b3
TaewanKimmmm <ktww88@gmail.com>
2021-05-22 18:50:13 +0900

    refactor: PathPatterns 방식 변경

--- src/main/java/wooteco/subway/auth/InterceptorConfig.java
+++ src/main/java/wooteco/subway/auth/InterceptorConfig.java
@@ -17,9 +17,8 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(jwtTokenProvider))
-                .addPathPatterns("/stations/**")
-                .addPathPatterns("/lines/**")
-                .addPathPatterns("/sections/**")
-                .addPathPatterns("/path/**");
+                .addPathPatterns("/**")
+                .excludePathPatterns("/members/**")
+                .excludePathPatterns("/login/token");
     }
 }
