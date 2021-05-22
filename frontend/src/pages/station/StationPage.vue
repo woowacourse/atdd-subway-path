585d1f89932efbcebe4d177e1e9ff3abbf330c92
TaewanKimmmm <ktww88@gmail.com>
2021-05-22 14:57:40 +0900

    feat: StationPage 구현

--- frontend/src/pages/station/StationPage.vue
+++ frontend/src/pages/station/StationPage.vue
@@ -58,6 +58,19 @@ import validator from "../../utils/validator";
 import {SNACKBAR_MESSAGES} from "../../utils/constants";
 import {mapGetters, mapMutations} from "vuex";
 import {SET_STATIONS, SHOW_SNACKBAR} from "../../store/shared/mutationTypes";
+function getCookie(cookie_name) {
+  var x, y;
+  var val = document.cookie.split(';');
+
+  for (var i = 0; i < val.length; i++) {
+    x = val[i].substr(0, val[i].indexOf('='));
+    y = val[i].substr(val[i].indexOf('=') + 1);
+    x = x.replace(/^\s+|\s+$/g, '');
+    if (x == cookie_name) {
+      return unescape(y);
+    }
+  }
+}
 
 export default {
   name: "StationPage",
@@ -65,9 +78,14 @@ export default {
     ...mapGetters(["stations"]),
   },
   async created() {
-    // TODO 초기 역 데이터를 불러오는 API를 추가해주세요.
-    const response = await fetch("http://localhost:8080/stations");
-    console.log("리스폰스:",response);
+    const response = await fetch("http://localhost:8080/stations", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + getCookie("myCookie")
+      }
+    });
+
     if (!response.ok) {
       throw new Error(`${response.status}`);
     }
@@ -84,11 +102,11 @@ export default {
         return;
       }
       try {
-        // TODO 역을 추가하는 API Sample
         const response = await fetch("http://localhost:8080/stations", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            'Authorization': 'Bearer ' + getCookie("myCookie")
           },
           body: JSON.stringify({
             name: this.stationName,
@@ -113,8 +131,14 @@ export default {
     },
     async onDeleteStation(stationId) {
       try {
-        // TODO 역을 삭제하는 API를 추가해주세요.
-        // await fetch("/api/stations/{id}");
+        await fetch("http://localhost:8080/stations/"+ stationId, {
+          method: "DELETE",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getCookie("myCookie")
+          }
+        });
+
         const idx = this.stations.findIndex(
           (station) => station.id === stationId
         );
