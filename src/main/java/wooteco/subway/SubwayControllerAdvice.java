caa829e18f7bda1b37b14c931feb305b6373bec4
Gomding <backchida@gmail.com>
2021-05-22 10:52:13 +0900

    refactor(SubwayControllerAdvice.java) : Exception 도 핸들링 하도록 추가

--- src/main/java/wooteco/subway/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/SubwayControllerAdvice.java
@@ -2,14 +2,21 @@ package wooteco.subway;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import wooteco.subway.exception.AuthorizationException;
 
 @ControllerAdvice
 public class SubwayControllerAdvice {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
     @ExceptionHandler(AuthorizationException.class)
-    public ResponseEntity<String> duplicatedExceptionHandle(Exception e) {
+    public ResponseEntity<String> authorization(AuthorizationException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
