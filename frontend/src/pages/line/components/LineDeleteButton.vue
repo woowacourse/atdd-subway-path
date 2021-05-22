eb13c176f6d274729b648d66b803b9dc7d24165f
TaewanKimmmm <ktww88@gmail.com>
2021-05-22 14:52:07 +0900

    feat: LineDeleteButton 구현

--- frontend/src/pages/line/components/LineDeleteButton.vue
+++ frontend/src/pages/line/components/LineDeleteButton.vue
@@ -9,6 +9,20 @@ import {mapMutations} from "vuex";
 import {SET_LINES, SHOW_SNACKBAR} from "../../../store/shared/mutationTypes";
 import {SNACKBAR_MESSAGES} from "../../../utils/constants";
 
+function getCookie(cookie_name) {
+  var x, y;
+  var val = document.cookie.split(';');
+
+  for (var i = 0; i < val.length; i++) {
+    x = val[i].substr(0, val[i].indexOf('='));
+    y = val[i].substr(val[i].indexOf('=') + 1);
+    x = x.replace(/^\s+|\s+$/g, '');
+    if (x == cookie_name) {
+      return unescape(y);
+    }
+  }
+}
+
 export default {
   name: "LineDeleteButton",
   props: {
@@ -21,16 +35,29 @@ export default {
     ...mapMutations([SHOW_SNACKBAR, SET_LINES]),
     async onDeleteLine() {
       try {
-        // TODO Line을 삭제하는 API를 추가해주세요.
-        // await fetch("/api/lines/{id}")
-        // TODO 전체 Line 데이터를 불러오는 API를 추가해주세요.
-        // const lines = await fetch("/api/lines")
-        // this.setLines([...lines])
+        await fetch("http://localhost:8080/lines/" + this.line.id, {
+          method: "DELETE",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getCookie("myCookie")
+          }
+        });
+
+        const linesResponse = await fetch("http://localhost:8080/lines", {
+          method: "GET",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getCookie("myCookie")
+          }
+        });
+        const lines = await linesResponse.json()
+        this.setLines([...lines])
+
         this.showSnackbar(SNACKBAR_MESSAGES.LINE.DELETE.SUCCESS);
       } catch (e) {
         this.showSnackbar(SNACKBAR_MESSAGES.LINE.DELETE.FAIL);
       }
-    },
+    }
   },
 };
 </script>
