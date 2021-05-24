fa25f57fdbf86b9a1d59a9194a59a722e3101b88
Gomding <backchida@gmail.com>
2021-05-24 18:34:24 +0900

    feat(PathNotFoundException.java) : 최단 경로를 찾지 못했을 때 발생하는 커스텀 예외 구현

--- src/main/java/wooteco/subway/path/infrastructure/ShortestPathWithDijkstra.java
+++ src/main/java/wooteco/subway/path/infrastructure/ShortestPathWithDijkstra.java
@@ -5,6 +5,7 @@ import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Component;
+import wooteco.subway.exception.PathNotFoundException;
 import wooteco.subway.line.domain.Section;
 import wooteco.subway.line.domain.Sections;
 import wooteco.subway.path.domain.Path;
@@ -40,8 +41,12 @@ public class ShortestPathWithDijkstra implements ShortestPath {
 
     @Override
     public Path getPath(Station source, Station target) {
-        DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
-        GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(source, target);
-        return new Path(path.getVertexList(), (int) path.getWeight());
+        try {
+            DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+            GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(source, target);
+            return new Path(path.getVertexList(), (int) path.getWeight());
+        } catch (IllegalArgumentException e) {
+            throw new PathNotFoundException();
+        }
     }
 }
