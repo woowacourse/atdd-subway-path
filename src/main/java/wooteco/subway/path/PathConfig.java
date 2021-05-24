5b3a825809009149d38d88d24a2923b83bf5ea45
Gomding <backchida@gmail.com>
2021-05-24 20:19:31 +0900

    refactor(PathConfig.java) : @PostConstruct 애너테이션을 활용해서 빈 생성 후 필요한 데이터를 초기화

--- src/main/java/wooteco/subway/path/PathConfig.java
+++ src/main/java/wooteco/subway/path/PathConfig.java
@@ -2,28 +2,32 @@ package wooteco.subway.path;
 
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import wooteco.subway.line.dao.SectionDao;
 import wooteco.subway.line.domain.Section;
 import wooteco.subway.station.domain.Station;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Configuration
 public class PathConfig {
     private final SectionDao sectionDao;
+    private WeightedMultigraph<Station, DefaultWeightedEdge> graph;
 
-    @Autowired
     public PathConfig(SectionDao sectionDao) {
         this.sectionDao = sectionDao;
     }
 
     @Bean
     public WeightedMultigraph<Station, DefaultWeightedEdge> weightedMultigraph() {
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        this.graph =  new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        return this.graph;
+    }
 
+    @PostConstruct
+    public void init() {
         List<Section> sections = sectionDao.findAll();
 
         for (Section section : sections) {
@@ -31,6 +35,5 @@ public class PathConfig {
             graph.addVertex(section.getDownStation());
             graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
         }
-        return graph;
     }
 }
