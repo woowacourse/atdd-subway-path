a582fbd813961811c96775645d53d96dd3b039aa
Junyoung Choi <cjy1705@naver.com>
2021-05-24 11:25:42 +0900

    [Spring 지하철 경로 조회 3단계] 영이(최준영) 미션 제출합니다. (#133)
    * fix : 역 추가, 삭제 기능 프론트코드 버그 수정
    
    * feat : 경로 조회 기능 구현
    
    * refactor : pathService, SubwayMap 생성 및 경로조회 기능 수정

--- src/main/java/wooteco/subway/path/ui/PathController.java
+++ src/main/java/wooteco/subway/path/ui/PathController.java
@@ -1,5 +1,32 @@
 package wooteco.subway.path.ui;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import wooteco.subway.path.domain.Path;
+import wooteco.subway.path.dto.PathResponse;
+import wooteco.subway.path.service.PathService;
+import wooteco.subway.station.domain.Station;
+
+@Controller
+@RequestMapping("/paths")
 public class PathController {
+
+    private final PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
+
     // TODO: 경로조회 기능 구현하기
+    @GetMapping
+    public ResponseEntity<PathResponse> findPath(@RequestParam long source, @RequestParam long target) {
+        Station sourceStation = pathService.findStationById(source);
+        Station targetStation = pathService.findStationById(target);
+        Path path = pathService.getPath(sourceStation, targetStation);
+
+        return ResponseEntity.ok().body(PathResponse.of(path));
+    }
 }
