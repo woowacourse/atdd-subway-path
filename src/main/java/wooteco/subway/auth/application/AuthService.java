b4a769823f8e71b77ee24ac16aeb1a18cb5b73a4
TaewanKimmmm <ktww88@gmail.com>
2021-05-26 02:37:20 +0900

    refactor: 불필요한 토큰 유효성 검증 로직 제거

--- src/main/java/wooteco/subway/auth/application/AuthService.java
+++ src/main/java/wooteco/subway/auth/application/AuthService.java
@@ -1,16 +1,14 @@
 package wooteco.subway.auth.application;
 
 import org.springframework.stereotype.Service;
+import wooteco.subway.auth.dto.LoginMember;
 import wooteco.subway.auth.dto.TokenRequest;
 import wooteco.subway.auth.dto.TokenResponse;
 import wooteco.subway.auth.infrastructure.JwtTokenProvider;
 import wooteco.subway.exception.AuthorizationException;
 import wooteco.subway.exception.InvalidTokenException;
 import wooteco.subway.member.dao.MemberDao;
-import wooteco.subway.auth.dto.LoginMember;
 import wooteco.subway.member.domain.Member;
-
-import java.util.Optional;
 
 @Service
 public class AuthService {
@@ -29,10 +27,9 @@ public class AuthService {
     }
 
     private Long findMemberId(TokenRequest tokenRequest) {
-        Optional<Member> foundMember = Optional.ofNullable(memberDao.findByEmail(tokenRequest.getEmail())
-                .orElseThrow(AuthorizationException::new));
+        Member member = memberDao.findByEmail(tokenRequest.getEmail())
+                .orElseThrow(AuthorizationException::new);
 
-        Member member = foundMember.get();
         if (member.hasDifferentPassword(tokenRequest.getPassword())) {
             throw new AuthorizationException();
         }
@@ -40,10 +37,7 @@ public class AuthService {
     }
 
     public LoginMember findMemberByToken(String token) {
-        if (jwtTokenProvider.validateToken(token)) {
-            String payload = jwtTokenProvider.getPayload(token);
-            return new LoginMember(Long.valueOf(payload));
-        }
-        throw new InvalidTokenException();
+        String payload = jwtTokenProvider.getPayload(token);
+        return new LoginMember(Long.valueOf(payload));
     }
 }
