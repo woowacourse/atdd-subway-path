c1a77f06d8178a39a3eddccea51fdb4776291a01
seunghyun <ggyool@naver.com>
2021-05-26 15:24:59 +0900

    refactor: 예외 관련 리팩토링 (ErrorResponse 생성, ControllerAdvice 변경)

--- src/main/java/wooteco/subway/SpecificExceptionHandler.java
+++ src/main/java/wooteco/subway/SpecificExceptionHandler.java
@@ -5,12 +5,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import wooteco.subway.auth.exception.AuthorizationException;
+import wooteco.subway.exception.DomainRelatedException;
+import wooteco.subway.exception.ValidationFailureException;
+import wooteco.subway.exception.dto.ErrorResponse;
 
 @ControllerAdvice
 public class SpecificExceptionHandler {
 
+    @ExceptionHandler({DomainRelatedException.class, ValidationFailureException.class})
+    public ResponseEntity<ErrorResponse> handleBadRequestException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
     @ExceptionHandler(AuthorizationException.class)
-    public ResponseEntity<String> handleAuthorizationFailure(AuthorizationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleAuthorizationFailure(AuthorizationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+            .body(new ErrorResponse(e.getMessage()));
     }
 }
---
+++ src/main/java/wooteco/subway/exception/dto/ErrorResponse.java
@@ -0,0 +1,17 @@
+package wooteco.subway.exception.dto;
+
+public class ErrorResponse {
+
+    private String message;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
