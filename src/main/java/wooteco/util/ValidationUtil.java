f75aaed23254a17cadc9a3f3b95023691d783d3b
Kimun Kim <kkm97351@gmail.com>
2021-05-27 11:10:13 +0900

    refactor: ValidationUtil 인스턴스화 방지
    - 생성자에 private 속성을 부여했습니다
    - final class 속성을 부여해 상속을 통한 인스턴스화를 방지합니다.

--- src/main/java/wooteco/util/ValidationUtil.java
+++ src/main/java/wooteco/util/ValidationUtil.java
@@ -1,9 +1,12 @@
 package wooteco.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.validation.BindingResult;
 import wooteco.exception.RequiredParameterValidationException;
 
-public class ValidationUtil {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ValidationUtil {
     public static void validateRequestedParameter(BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw RequiredParameterValidationException.from(bindingResult);
