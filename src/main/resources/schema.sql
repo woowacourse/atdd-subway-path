4dbc7b6bbb3ea0242bb621d2604cf3c292b9f699
Kimun Kim <kkm97351@gmail.com>
2021-05-27 17:08:17 +0900

    refactor: Section 테이블에 line_id에 대한 외래키 제약 조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,6 +21,7 @@ create table if not exists SECTION
     down_station_id bigint not null,
     distance int not null,
     primary key(id),
+    foreign key (line_id) references LINE(id) on delete cascade,
     foreign key (up_station_id) references station(id),
     foreign key (down_station_id) references station(id)
 );
