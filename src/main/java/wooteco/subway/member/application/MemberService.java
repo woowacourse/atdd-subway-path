1457d7d90866cf9f87d7a1388f552b5da7b2b809
seunghyun <ggyool@naver.com>
2021-06-03 02:06:08 +0900

    refactor: member 패키지 예외 구조 변경

--- src/main/java/wooteco/subway/member/application/MemberService.java
+++ src/main/java/wooteco/subway/member/application/MemberService.java
@@ -1,8 +1,6 @@
 package wooteco.subway.member.application;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
-import wooteco.subway.exception.BusinessRelatedException;
 import wooteco.subway.exception.application.ObjectNotFoundException;
 import wooteco.subway.member.dao.MemberDao;
 import wooteco.subway.member.domain.Member;
@@ -19,12 +17,8 @@ public class MemberService {
     }
 
     public MemberResponse createMember(MemberRequest request) {
-        try {
-            Member member = memberDao.insert(request.toMember());
-            return MemberResponse.from(member);
-        } catch (DataAccessException e) {
-            throw new BusinessRelatedException("멤버 추가에 실패했습니다.");
-        }
+        Member member = memberDao.insert(request.toMember());
+        return MemberResponse.from(member);
     }
 
     public MemberResponse findMember(Long id) {
@@ -35,20 +29,12 @@ public class MemberService {
     }
 
     public void updateMember(Long id, MemberRequest memberRequest) {
-        try {
-            memberDao.update(
-                new Member(id, memberRequest.getEmail(), memberRequest.getPassword(), memberRequest.getAge())
-            );
-        } catch (DataAccessException e) {
-            throw new BusinessRelatedException("멤버 수정에 실패했습니다.");
-        }
+        memberDao.update(
+            new Member(id, memberRequest.getEmail(), memberRequest.getPassword(), memberRequest.getAge())
+        );
     }
 
     public void deleteMember(Long id) {
-        try {
-            memberDao.deleteById(id);
-        } catch (DataAccessException e) {
-            throw new BusinessRelatedException("멤버 제거에 실패했습니다.");
-        }
+        memberDao.deleteById(id);
     }
 }
--- src/main/java/wooteco/subway/member/dao/MemberDao.java
+++ src/main/java/wooteco/subway/member/dao/MemberDao.java
@@ -2,6 +2,7 @@ package wooteco.subway.member.dao;
 
 import java.util.Optional;
 import javax.sql.DataSource;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -9,10 +10,14 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import wooteco.subway.exception.application.DuplicatedFieldException;
+import wooteco.subway.exception.application.NonexistentTargetException;
 import wooteco.subway.member.domain.Member;
 
 @Repository
 public class MemberDao {
+
+    private static final int SUCCESSFUL_AFFECTED_COUNT = 1;
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
@@ -35,18 +40,35 @@ public class MemberDao {
 
     public Member insert(Member member) {
         SqlParameterSource params = new BeanPropertySqlParameterSource(member);
-        Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
-        return new Member(id, member.getEmail(), member.getPassword(), member.getAge());
+        try {
+            Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+            return new Member(id, member.getEmail(), member.getPassword(), member.getAge());
+        } catch (DuplicateKeyException e) {
+            throw new DuplicatedFieldException("멤버 이메일: " + member.getEmail());
+        }
     }
 
     public void update(Member member) {
         String sql = "update MEMBER set email = ?, password = ?, age = ? where id = ?";
-        jdbcTemplate.update(sql, member.getEmail(), member.getPassword(), member.getAge(), member.getId());
+
+        try {
+            int updatedCount = jdbcTemplate.update(
+                sql, member.getEmail(), member.getPassword(), member.getAge(), member.getId()
+            );
+            if (updatedCount < SUCCESSFUL_AFFECTED_COUNT) {
+                throw new NonexistentTargetException(("멤버ID: " + member.getId()));
+            }
+        } catch (DuplicateKeyException e) {
+            throw new DuplicatedFieldException(String.format("멤버 이메일: ", member.getEmail()));
+        }
     }
 
     public void deleteById(Long id) {
         String sql = "delete from MEMBER where id = ?";
-        jdbcTemplate.update(sql, id);
+        int deletedCount = jdbcTemplate.update(sql, id);
+        if (deletedCount < SUCCESSFUL_AFFECTED_COUNT) {
+            throw new NonexistentTargetException("멤버ID: " + id);
+        }
     }
 
     public Optional<Member> findById(Long id) {
