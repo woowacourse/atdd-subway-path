2ebd177aaea18039d5e05dc663250b4be7b0c0a6
2yujeong <76840965+2yujeong@users.noreply.github.com>
2022-05-10 10:46:08 +0900

    feat: 코드 가져오기

--- src/main/java/wooteco/subway/dto/LineResponse.java
+++ src/main/java/wooteco/subway/dto/LineResponse.java
@@ -3,16 +3,26 @@ package wooteco.subway.dto;
 import java.util.List;
 
 public class LineResponse {
+
     private Long id;
     private String name;
     private String color;
     private List<StationResponse> stations;
+
+    public LineResponse() {
+    }
 
     public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.stations = stations;
+    }
+
+    public LineResponse(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
     }
 
     public Long getId() {
