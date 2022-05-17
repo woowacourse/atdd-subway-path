6696f5f0a6a2b09f7608fc2ebb0d863a0907c921
nailseong <nnis9857@gmail.com>
2022-05-17 21:33:17 +0900

    feat(SubwayMap): 경로를 찾을 수 없는 경우 예외 처리 구현

--- src/main/java/wooteco/subway/domain/SubwayMap.java
+++ src/main/java/wooteco/subway/domain/SubwayMap.java
@@ -1,16 +1,43 @@
 package wooteco.subway.domain;
 
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import wooteco.subway.exception.IllegalInputException;
+import wooteco.subway.exception.path.NoSuchPathException;
 
 public class SubwayMap {
 
-    private final Sections sections;
+    private final DijkstraShortestPath<Station, DefaultWeightedEdge> shortestPathMap;
 
     public SubwayMap(final Sections sections) {
-        this.sections = sections;
+        this.shortestPathMap = toShortestPath(sections);
     }
 
-    public void getPath(final Station sourceStation, final Station targetStation) {
+    private DijkstraShortestPath<Station, DefaultWeightedEdge> toShortestPath(final Sections sections) {
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+        for (Section section : sections.getValue()) {
+            graph.addVertex(section.getUpStation());
+            graph.addVertex(section.getDownStation());
+            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+        }
+        return new DijkstraShortestPath<>(graph);
+    }
+
+    public void searchPath(final Station sourceStation, final Station targetStation) {
+        validateStations(sourceStation, targetStation);
+
+        final GraphPath<Station, DefaultWeightedEdge> shortestPath = shortestPathMap.getPath(sourceStation,
+                targetStation);
+
+        if (shortestPath == null) {
+            throw new NoSuchPathException();
+        }
+    }
+
+    private void validateStations(final Station sourceStation, final Station targetStation) {
         if (sourceStation.equals(targetStation)) {
             throw new IllegalInputException("출발역과 도착역이 동일합니다.");
         }
--- src/test/java/wooteco/subway/domain/SubwayMapTest.java
+++ src/test/java/wooteco/subway/domain/SubwayMapTest.java
@@ -3,29 +3,40 @@ package wooteco.subway.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import wooteco.subway.exception.IllegalInputException;
+import wooteco.subway.exception.path.NoSuchPathException;
 
 class SubwayMapTest {
 
-    @Test
-    @DisplayName("동일한 역의 경로를 조회할 경우 예외를 던진다.")
-    void GetPath_SameStations_ExceptionThrown() {
-        // given
+    private SubwayMap subwayMap;
+    private Station gangnam;
+    private Station yeoksam;
+    private Station seolleung;
+    private Station samsung;
+    private Station seoulForest;
+    private Station wangsimni;
+    private Station yacksu;
+    private Station geumho;
+    private Station oksu;
+
+    @BeforeEach
+    void setUpData() {
+        gangnam = new Station(1L, "강남역");
+        yeoksam = new Station(2L, "역삼역");
+        seolleung = new Station(3L, "선릉역");
+        samsung = new Station(4L, "삼성역");
+        seoulForest = new Station(5L, "서울숲역");
+        wangsimni = new Station(6L, "왕십리역");
+        yacksu = new Station(7L, "약수역");
+        geumho = new Station(8L, "금호역");
+        oksu = new Station(9L, "옥수역");
+
         final Line greenLine = new Line("2호선", "green");
         final Line yellowLine = new Line("수인분당선", "yellow");
         final Line orangeLine = new Line("3호선", "orange");
-
-        final Station gangnam = new Station(1L, "강남역");
-        final Station yeoksam = new Station(2L, "역삼역");
-        final Station seolleung = new Station(3L, "선릉역");
-        final Station samsung = new Station(4L, "삼성역");
-        final Station seoulForest = new Station(5L, "서울숲역");
-        final Station wangsimni = new Station(6L, "왕십리역");
-        final Station yacksu = new Station(7L, "약수역");
-        final Station geumho = new Station(8L, "금호역");
-        final Station oksu = new Station(9L, "옥수역");
 
         final Section greenSectionA = new Section(greenLine, gangnam, yeoksam, new Distance(10));
         final Section greenSectionB = new Section(greenLine, yeoksam, seolleung, new Distance(10));
@@ -47,11 +58,21 @@ class SubwayMapTest {
                 orangeSectionB
         ));
 
-        final SubwayMap subwayMap = new SubwayMap(sections);
+        subwayMap = new SubwayMap(sections);
+    }
 
-        // when, then
-        assertThatThrownBy(() -> subwayMap.getPath(gangnam, gangnam))
+    @Test
+    @DisplayName("출발역과 도착익이 동일한 경로를 조회할 경우 예외를 던진다.")
+    void GetPath_SameStations_ExceptionThrown() {
+        assertThatThrownBy(() -> subwayMap.searchPath(gangnam, gangnam))
                 .isInstanceOf(IllegalInputException.class)
                 .hasMessage("출발역과 도착역이 동일합니다.");
+    }
+
+    @Test
+    @DisplayName("경로를 찾을 수 없는 경우 예외를 던진다.")
+    void GetPath_InvalidPath_ExceptionThrown() {
+        assertThatThrownBy(() -> subwayMap.searchPath(gangnam, oksu))
+                .isInstanceOf(NoSuchPathException.class);
     }
 }
