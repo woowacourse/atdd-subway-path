6ab7f490ddb2a518679eb2c3718a858d6d1041c6
jaejae-yoo <wotj102@gmail.com>
2022-05-17 23:18:21 +0900

    refactor: 요금 계산 로직 수정

--- src/main/java/wooteco/subway/domain/Fare.java
+++ src/main/java/wooteco/subway/domain/Fare.java
@@ -3,12 +3,10 @@ package wooteco.subway.domain;
 public class Fare {
 
     private static final int BASIC_FARE = 1250;
-    private static final int ADDITIONAL_FARE = 100;
+    private static final int FIRST_SECTION_BASIC_FARE = 800;
     private static final int FIRST_SECTION_UNIT = 5;
     private static final int SECOND_SECTION_UNIT = 8;
-    private static final int FIRST_SECTION_BOUNDARY = 10;
-    private static final int SECOND_SECTION_BOUNDARY = 50;
-    private static final int FIRST_SECTION_BASIC_FARE = 800;
+
     private final int fare;
 
     public Fare(int distance) {
@@ -16,22 +14,21 @@ public class Fare {
     }
 
     private int calculateFare(int distance) {
-        int fare = BASIC_FARE;
-        if (distance >= FIRST_SECTION_BOUNDARY && distance <= SECOND_SECTION_BOUNDARY) {
-            fare = calcAdditionalFare(distance, fare, FIRST_SECTION_BOUNDARY, FIRST_SECTION_UNIT);
+        if (distance < 10) {
+            return BASIC_FARE;
         }
-        if (distance > SECOND_SECTION_BOUNDARY) {
-            fare = FIRST_SECTION_BASIC_FARE + calcAdditionalFare(distance, fare, SECOND_SECTION_BOUNDARY, SECOND_SECTION_UNIT);
+        if (distance <= 50) {
+            return calcAdditionalFare(distance - 10, fare, FIRST_SECTION_UNIT);
         }
-        return fare;
+        return FIRST_SECTION_BASIC_FARE + calcAdditionalFare(distance - 50, fare, SECOND_SECTION_UNIT);
     }
 
-    private int calcAdditionalFare(int distance, int fare, int boundary, int unit) {
-        fare += ((distance - boundary) / unit) * ADDITIONAL_FARE;
-        if ((distance - boundary) % unit > 0 || ((distance - boundary) / unit == 0)) {
-            fare += ADDITIONAL_FARE;
+    private int calcAdditionalFare(int distance, int fare, int unit) {
+        fare += (distance / unit) * 100;
+        if (distance % unit > 0 || (distance / unit == 0)) {
+            fare += 100;
         }
-        return fare;
+        return BASIC_FARE + fare;
     }
 
     public int getFare() {
