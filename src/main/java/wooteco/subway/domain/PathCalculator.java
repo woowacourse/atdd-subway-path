bd5f588d3ce45bb9a353586f90a1909efb600f50
pup-paw <wldusdl0310@gmail.com>
2022-05-17 21:32:43 +0900

    feat(PathCalculator): 최단 경로 조회 기능

---
+++ src/main/java/wooteco/subway/domain/PathCalculator.java
@@ -0,0 +1,50 @@
+package wooteco.subway.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+public class PathCalculator {
+    private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
+
+    private PathCalculator(
+            DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath) {
+        this.dijkstraShortestPath = dijkstraShortestPath;
+    }
+
+    public static PathCalculator from(List<Section> sections) {
+        Set<Station> stations = extractStations(sections);
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = getMultiGraph(
+                sections, stations);
+        return new PathCalculator(new DijkstraShortestPath<>(graph));
+    }
+
+    private static Set<Station> extractStations(List<Section> sections) {
+        Set<Station> stations = new HashSet<>();
+        for (Section section : sections) {
+            stations.add(section.getUpStation());
+            stations.add(section.getDownStation());
+        }
+        return stations;
+    }
+
+    private static WeightedMultigraph<Station, DefaultWeightedEdge> getMultiGraph(
+            List<Section> sections, Set<Station> stations) {
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        for (Station station : stations) {
+            graph.addVertex(station);
+        }
+        for (Section section : sections) {
+            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+        }
+        return graph;
+    }
+
+    public List<Station> calculateShortestPath(Station source, Station target) {
+        return dijkstraShortestPath.getPath(source, target).getVertexList();
+    }
+}
