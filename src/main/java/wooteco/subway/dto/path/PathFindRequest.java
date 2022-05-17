d8b6a360d4df46fc02667f111aadcfe706d431ca
brorae <jhr3239@gmail.com>
2022-05-18 00:04:03 +0900

    refactor: request validation 추가

--- src/main/java/wooteco/subway/dto/path/PathFindRequest.java
+++ src/main/java/wooteco/subway/dto/path/PathFindRequest.java
@@ -1,9 +1,16 @@
 package wooteco.subway.dto.path;
 
+import javax.validation.constraints.Positive;
+
 public class PathFindRequest {
 
+    @Positive(message = "출발지의 id는 양수 값만 들어올 수 있습니다.")
     private long source;
+
+    @Positive(message = "도착지의 id는 양수 값만 들어올 수 있습니다.")
     private long target;
+
+    @Positive(message = "나이는 양수 값만 들어올 수 있습니다.")
     private int age;
 
     private PathFindRequest() {
