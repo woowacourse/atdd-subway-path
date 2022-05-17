4160eacc44de266603462f2080835c280ca9df70
sun <syoun602@gmail.com>
2022-05-17 17:16:33 +0900

    feat: 요금 도메인 분리

--- src/main/java/wooteco/subway/domain/Path.java
+++ src/main/java/wooteco/subway/domain/Path.java
@@ -7,13 +7,6 @@ import java.util.List;
 
 public class Path {
 
-    private static final int MINIMUM_FARE_FEE = 1250;
-    private static final int MINIMUM_DISTANCE_BOUNDARY = 10;
-    private static final int OVER_FARE_FEE = 100;
-    private static final int BELOW_FIFTY_KM_POLICY = 5;
-    private static final int ABOVE_FIFTY_KM_POLICY = 8;
-    private static final int MAXIMUM_DISTANCE_BOUNDARY = 50;
-
     private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
 
     public Path(final WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
@@ -44,27 +37,5 @@ public class Path {
 
     public double getDistance(final Station source, final Station target) {
         return dijkstraShortestPath.getPath(source, target).getWeight();
-    }
-
-    public int getFare(final Station source, final Station target) {
-        final double distance = getDistance(source, target);
-
-        if (distance <= MINIMUM_DISTANCE_BOUNDARY) {
-            return MINIMUM_FARE_FEE;
-        }
-
-        return MINIMUM_FARE_FEE + calculateFareByDistance(distance);
-    }
-
-    private int calculateFareByDistance(final double distance) {
-        if (distance <= MAXIMUM_DISTANCE_BOUNDARY) {
-            return calculateFare(distance  - MINIMUM_DISTANCE_BOUNDARY, BELOW_FIFTY_KM_POLICY);
-        }
-
-        return 800 + calculateFare(distance - MAXIMUM_DISTANCE_BOUNDARY, ABOVE_FIFTY_KM_POLICY);
-    }
-
-    private int calculateFare(final double overFaredDistance, final int policy) {
-        return (int) ((Math.ceil((overFaredDistance - 1) / policy)) * OVER_FARE_FEE);
     }
 }
--- src/test/java/wooteco/subway/domain/PathTest.java
+++ src/test/java/wooteco/subway/domain/PathTest.java
@@ -46,33 +46,4 @@ class PathTest {
         final Path path = Path.from(sections);
         assertThat(path.getDistance(station1, station3)).isEqualTo(10);
     }
-
-    @DisplayName("경로의 요금을 구한다.")
-    @Test
-    void getFare() {
-        final Path path = Path.from(sections);
-        assertThat(path.getFare(station1, station2)).isEqualTo(1250);
-    }
-
-    @DisplayName("이용 거리 초과 시 추가운임을 부과한다.")
-    @Test
-    void calculateOverFare() {
-        final Station station = new Station(4L, "교대역");
-        final Section section = new Section(3L, station3, station, 10, 1L);
-        sections.add(section);
-        final Path path = Path.from(sections);
-
-        assertThat(path.getFare(station1, station)).isEqualTo(1450);
-    }
-
-    @DisplayName("이용 거리 50km 초과 시 추가운임을 부과한다.")
-    @Test
-    void calculateOverFare_over50km() {
-        final Station station = new Station(4L, "교대역");
-        final Section section = new Section(3L, station3, station, 48, 1L);
-        sections.add(section);
-        final Path path = Path.from(sections);
-
-        assertThat(path.getFare(station1, station)).isEqualTo(2150);
-    }
 }
