345aca0abb009dd9d8bfa584e1b23cedd8bfff31
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-17 15:24:54 +0900

    refactor: 메소드 분리

--- src/main/java/wooteco/subway/domain/strategy/FindDijkstraShortestPathStrategy.java
+++ src/main/java/wooteco/subway/domain/strategy/FindDijkstraShortestPathStrategy.java
@@ -1,11 +1,8 @@
 package wooteco.subway.domain.strategy;
 
 import java.util.List;
-import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultDirectedGraph;
-import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import wooteco.subway.domain.Path;
@@ -19,16 +16,26 @@ public class FindDijkstraShortestPathStrategy implements FindPathStrategy {
     public Path findPath(final Station source, final Station target, final Sections sections) {
         sections.checkExistStations(source, target);
         WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        addVertextStation(sections, graph);
+        addEdgeWeightStation(sections, graph);
+
+        GraphPath shortestPath = new DijkstraShortestPath(graph).getPath(source, target);
+        return new Path(shortestPath.getVertexList(), (int) shortestPath.getWeight());
+    }
+
+    private void addVertextStation(final Sections sections,
+                                   final WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
         List<Station> allStations = sections.getAllStations();
         for (Station station : allStations) {
             graph.addVertex(station);
         }
+    }
+
+    private void addEdgeWeightStation(final Sections sections,
+                                      final WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
         List<Section> allSections = sections.getSections();
         for (Section section : allSections) {
             graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
         }
-
-        GraphPath shortestPath = new DijkstraShortestPath(graph).getPath(source, target);
-        return new Path(shortestPath.getVertexList(), (int) shortestPath.getWeight());
     }
 }
