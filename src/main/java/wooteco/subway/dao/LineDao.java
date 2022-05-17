fc1b3c22cf80144002cc57dee369346e90af71b0
hyeonic <dev.hyeonic@gmail.com>
2022-05-17 15:04:56 +0900

    feat: 지하철 노선도 관리 코드 이전

--- src/main/java/wooteco/subway/dao/LineDao.java
+++ src/main/java/wooteco/subway/dao/LineDao.java
@@ -9,36 +9,42 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-import wooteco.subway.domain.Station;
+import wooteco.subway.domain.Line;
 
 @Repository
-public class StationDao {
+public class LineDao {
 
-    private static final String TABLE_NAME = "STATION";
-    private static final String KEY_NAME = "id";
+    public static final String TABLE_NAME = "LINE";
+    public static final String KEY_NAME = "id";
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert insertActor;
 
-    public StationDao(JdbcTemplate jdbcTemplate) {
+    public LineDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
         this.insertActor = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName(TABLE_NAME)
                 .usingGeneratedKeyColumns(KEY_NAME);
     }
 
-    public Station save(Station station) {
-        Long id = insertActor.executeAndReturnKey(Map.of("name", station.getName())).longValue();
+    public Line save(Line line) {
+        Long id = insertActor.executeAndReturnKey(
+                Map.of("name", line.getName(), "color", line.getColor())).longValue();
         return findById(id).get();
     }
 
     public boolean existsByName(String name) {
-        String sql = "select exists (select 1 from STATION where name = :name)";
+        String sql = "select exists (select 1 from LINE where name = :name)";
         return jdbcTemplate.queryForObject(sql, Map.of("name", name), Boolean.class);
     }
 
-    public Optional<Station> findById(Long id) {
-        String sql = "select * from STATION where id = :id";
+    public List<Line> findAll() {
+        String sql = "select * from LINE";
+        return jdbcTemplate.query(sql, generateMapper());
+    }
+
+    public Optional<Line> findById(Long id) {
+        String sql = "select * from LINE where id = :id";
         try {
             return Optional.of(jdbcTemplate.queryForObject(sql, Map.of("id", id), generateMapper()));
         } catch (EmptyResultDataAccessException e) {
@@ -46,21 +52,23 @@ public class StationDao {
         }
     }
 
-    public List<Station> findAll() {
-        String sql = "select * from STATION";
-        return jdbcTemplate.query(sql, generateMapper());
-    }
-
-    private RowMapper<Station> generateMapper() {
+    private RowMapper<Line> generateMapper() {
         return (resultSet, rowNum) ->
-                new Station(
+                new Line(
                         resultSet.getLong("id"),
-                        resultSet.getString("name")
+                        resultSet.getString("name"),
+                        resultSet.getString("color")
                 );
     }
 
+    public void update(Long id, Line updateLine) {
+        String sql = "update LINE set name = :name, color = :color where id = :id";
+        jdbcTemplate.update(sql,
+                Map.of("id", id, "name", updateLine.getName(), "color", updateLine.getColor()));
+    }
+
     public void deleteById(Long id) {
-        String sql = "delete from STATION where id = :id";
+        String sql = "delete from LINE where id = :id";
         jdbcTemplate.update(sql, Map.of("id", id));
     }
 }
--- src/test/java/wooteco/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/StationAcceptanceTest.java
@@ -1,135 +1,111 @@
 package wooteco.subway.acceptance;
 
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import wooteco.subway.dto.StationRequest;
 import wooteco.subway.dto.StationResponse;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@DisplayName("지하철역 관련 기능")
 public class StationAcceptanceTest extends AcceptanceTest {
-    private static final String 강남역 = "강남역";
-    private static final String 역삼역 = "역삼역";
 
-    @DisplayName("지하철역을 생성한다.")
-    @Test
-    void createStation() {
-        // when
-        ExtractableResponse<Response> response = 지하철역_생성_요청(강남역);
+    @DisplayName("지하철역 관리")
+    @TestFactory
+    Stream<DynamicTest> dynamicTestFromStation() {
+        return Stream.of(
+                dynamicTest("지하철역을 생성한다.", () -> {
+                    String name = "강남역";
 
-        // then
-        지하철역_생성됨(response);
+                    ExtractableResponse<Response> response = generateStation(name);
+
+                    assertAll(
+                            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                            () -> assertThat(response.header("Location")).isNotBlank()
+                    );
+                }),
+
+                dynamicTest("기존에 존재하는 지하철역 이름으로 지하철역을 생성한다.", () -> {
+                    String name = "강남역";
+
+                    ExtractableResponse<Response> response = generateStation(name);
+
+                    assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                }),
+
+                dynamicTest("지하철역을 조회한다.", () -> {
+                    generateStation("역삼역");
+
+                    ExtractableResponse<Response> response = RestAssured.given().log().all()
+                            .when()
+                            .get("/stations")
+                            .then().log().all()
+                            .extract();
+
+                    assertAll(
+                            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                            () -> {
+                                int size = response.jsonPath().getList(".", StationResponse.class).size();
+                                assertThat(size).isEqualTo(2);
+                            }
+                    );
+                }),
+
+                dynamicTest("지하철역을 제거한다.", () -> {
+                    ExtractableResponse<Response> response = RestAssured.given().log().all()
+                            .when()
+                            .get("/stations")
+                            .then().log().all()
+                            .extract();
+
+                    List<Long> stationIds = response.jsonPath()
+                            .getList(".", StationResponse.class)
+                            .stream()
+                            .map(StationResponse::getId)
+                            .collect(toList());
+
+                    List<Integer> statusCodes = stationIds.stream()
+                            .map(this::deleteStation)
+                            .map(ExtractableResponse::statusCode)
+                            .collect(toList());
+                    List<Integer> expectedStatusCodes = IntStream.range(0, stationIds.size())
+                            .mapToObj(ignored -> HttpStatus.NO_CONTENT.value())
+                            .collect(toList());
+                    assertThat(statusCodes).containsAll(expectedStatusCodes);
+                })
+        );
     }
 
-    @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성한다.")
-    @Test
-    void createStationWithDuplicateName() {
-        //given
-        지하철역_등록되어_있음(강남역);
+    private ExtractableResponse<Response> generateStation(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
 
-        // when
-        ExtractableResponse<Response> response = 지하철역_생성_요청(강남역);
-
-        // then
-        지하철역_생성_실패됨(response);
-    }
-
-    @DisplayName("지하철역을 조회한다.")
-    @Test
-    void getStations() {
-        // given
-        StationResponse stationResponse1 = 지하철역_등록되어_있음(강남역);
-        StationResponse stationResponse2 = 지하철역_등록되어_있음(역삼역);
-
-        // when
-        ExtractableResponse<Response> response = 지하철역_목록_조회_요청();
-
-        // then
-        지하철역_목록_응답됨(response);
-        지하철역_목록_포함됨(response, Arrays.asList(stationResponse1, stationResponse2));
-    }
-
-    @DisplayName("지하철역을 제거한다.")
-    @Test
-    void deleteStation() {
-        // given
-        StationResponse stationResponse = 지하철역_등록되어_있음(강남역);
-
-        // when
-        ExtractableResponse<Response> response = 지하철역_제거_요청(stationResponse);
-
-        // then
-        지하철역_삭제됨(response);
-    }
-
-    public static StationResponse 지하철역_등록되어_있음(String name) {
-        return 지하철역_생성_요청(name).as(StationResponse.class);
-    }
-
-    public static ExtractableResponse<Response> 지하철역_생성_요청(String name) {
-        StationRequest stationRequest = new StationRequest(name);
-
-        return RestAssured
-                .given().log().all()
-                .body(stationRequest)
+        return RestAssured.given().log().all()
+                .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
+                .when()
+                .post("/stations")
                 .then().log().all()
                 .extract();
     }
 
-    public static ExtractableResponse<Response> 지하철역_목록_조회_요청() {
-        return RestAssured
-                .given().log().all()
-                .when().get("/stations")
+    private ExtractableResponse<Response> deleteStation(Long id) {
+        return RestAssured.given().log().all()
+                .when()
+                .delete("/stations/" + id)
                 .then().log().all()
                 .extract();
-    }
-
-    public static ExtractableResponse<Response> 지하철역_제거_요청(StationResponse stationResponse) {
-        return RestAssured
-                .given().log().all()
-                .when().delete("/stations/" + stationResponse.getId())
-                .then().log().all()
-                .extract();
-    }
-
-    public static void 지하철역_생성됨(ExtractableResponse response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response.header("Location")).isNotBlank();
-    }
-
-    public static void 지하철역_생성_실패됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    public static void 지하철역_목록_응답됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    public static void 지하철역_삭제됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-    }
-
-    public static void 지하철역_목록_포함됨(ExtractableResponse<Response> response, List<StationResponse> createdResponses) {
-        List<Long> expectedLineIds = createdResponses.stream()
-                .map(it -> it.getId())
-                .collect(Collectors.toList());
-
-        List<Long> resultLineIds = response.jsonPath().getList(".", StationResponse.class).stream()
-                .map(StationResponse::getId)
-                .collect(Collectors.toList());
-
-        assertThat(resultLineIds).containsAll(expectedLineIds);
     }
 }
