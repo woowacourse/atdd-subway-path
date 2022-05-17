fae3d00753fe1aef84f3e7f414c48212abdf367c
jaejae-yoo <wotj102@gmail.com>
2022-05-17 16:02:35 +0900

    test: 추가요금 음수 예외 상황 구현

--- src/test/java/wooteco/subway/domain/LineTest.java
+++ src/test/java/wooteco/subway/domain/LineTest.java
@@ -9,10 +9,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LineTest {
 
     @Test
-    @DisplayName("지하철 노선 - 공백 예와")
+    @DisplayName("지하철 노선 - 공백 예외")
     void checkNull() {
         assertThatThrownBy(() -> new Line("2호선", "", 10))
                 .isInstanceOf(ClientException.class)
                 .hasMessageContaining("지하철 노선의 이름과 색을 모두 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("지하철 노선 - 음수 추가요금 예외")
+    void checkNegativeExtraFare() {
+        assertThatThrownBy(() -> new Line("2호선", "yellow", -1))
+                .isInstanceOf(ClientException.class)
+                .hasMessageContaining("추가요금은 음수가 될 수 없습니다.");
     }
 }
