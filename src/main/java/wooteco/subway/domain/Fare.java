08d6f0be70aef9662ab43917895643129f2e6666
DWL21 <nggus5@naver.com>
2022-05-17 20:15:58 +0900

    refactor: 요금 계산 단위 상수화

--- src/main/java/wooteco/subway/domain/Fare.java
+++ src/main/java/wooteco/subway/domain/Fare.java
@@ -2,17 +2,25 @@ package wooteco.subway.domain;
 
 public class Fare {
 
+    private static final int BASIC_FARES = 1250;
+    private static final int SECOND_SECTION_FARES = 800;
+    private static final int BASIC_DISTANCE = 10;
+    private static final int SECOND_DISTANCE = 50;
+    private static final int SECOND_CHARGING_UNITS = 5;
+    private static final int THIRD_CHARGING_UNITS = 8;
+    private static final int UNIT_FARES = 100;
+
     public int calculateFare(int distance) {
-        if (distance <= 10) {
-            return 1250;
+        if (distance <= BASIC_DISTANCE) {
+            return BASIC_FARES;
         }
-        if (distance <= 50) {
-            return 1250 + calculateOverFare(distance-10, 5);
+        if (distance <= SECOND_DISTANCE) {
+            return BASIC_FARES + calculateOverFare(distance - BASIC_DISTANCE, SECOND_CHARGING_UNITS);
         }
-        return 1250 + 800 + calculateOverFare(distance-50, 8);
+        return BASIC_FARES + SECOND_SECTION_FARES + calculateOverFare(distance - SECOND_DISTANCE, THIRD_CHARGING_UNITS);
     }
 
     private int calculateOverFare(int distance, int farePerKilometre) {
-        return (int) ((Math.ceil((distance - 1) / farePerKilometre) + 1) * 100);
+        return (int) ((Math.ceil((distance - 1) / farePerKilometre) + 1) * UNIT_FARES);
     }
 }
