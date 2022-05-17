392265e688a67b9bf12fd46f558f291a9701f067
이한울 <dkfghg3@naver.com>
2022-05-17 20:24:46 +0900

    test: 연결된 구간을 찾을 수 없을 때 발생하는 404 Not Found 에러 테스트

--- src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
@@ -6,6 +6,7 @@ import static wooteco.subway.TestFixtures.LINE_COLOR;
 import static wooteco.subway.TestFixtures.LINE_SIX;
 import static wooteco.subway.TestFixtures.STANDARD_DISTANCE;
 import static wooteco.subway.TestFixtures.동묘앞역;
+import static wooteco.subway.TestFixtures.보문역;
 import static wooteco.subway.TestFixtures.신당역;
 import static wooteco.subway.TestFixtures.창신역;
 
@@ -16,7 +17,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import wooteco.subway.acceptance.AcceptanceTest;
 import wooteco.subway.domain.Line;
 import wooteco.subway.domain.Section;
 import wooteco.subway.domain.Station;
@@ -63,5 +63,24 @@ class PathAcceptanceTest extends AcceptanceTest {
             assertThat(pathResponse.getDistance()).isEqualTo(STANDARD_DISTANCE + STANDARD_DISTANCE);
             assertThat(pathResponse.getFare()).isEqualTo(1450);
         });
+    }
+
+    @DisplayName("경로 조회 시, 연결된 구간을 찾을 수 없으면 404 Not Found 에러를 발생한다.")
+    @Test
+    void getPathsException() {
+        Station saved_신당역 = stationRepository.save(신당역);
+        Station saved_동묘앞역 = stationRepository.save(동묘앞역);
+        Station saved_창신역 = stationRepository.save(창신역);
+        Station saved_보문역 = stationRepository.save(보문역);
+
+        Long lineId = lineRepository.save(new Line(LINE_SIX, LINE_COLOR));
+
+        sectionRepository.save(new Section(lineId, saved_신당역, saved_동묘앞역, STANDARD_DISTANCE));
+        sectionRepository.save(new Section(lineId, saved_보문역, saved_창신역, STANDARD_DISTANCE));
+
+        ExtractableResponse<Response> response = httpGetTest(
+                "/paths?source=" + saved_신당역.getId() + "&target=" + saved_창신역.getId() + "&age=15");
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 }
