72e3006de22b0a30ee6a033f83bf6d16b20c4d1c
beomWhale <cndqjacndqja@gmail.com>
2022-05-17 21:20:42 +0900

    test: PathController findShortestPath 테스트 추가

--- src/main/java/wooteco/subway/ui/PathController.java
+++ src/main/java/wooteco/subway/ui/PathController.java
@@ -1,15 +1,10 @@
 package wooteco.subway.ui;
 
-import java.util.List;
-import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import wooteco.subway.service.LineService;
 import wooteco.subway.service.PathService;
-import wooteco.subway.service.dto.LineServiceResponse;
 import wooteco.subway.service.dto.PathServiceResponse;
 import wooteco.subway.ui.dto.PathRequest;
 
--- src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
@@ -1,6 +1,10 @@
 package wooteco.subway.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.HashMap;
@@ -9,14 +13,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import wooteco.subway.service.dto.PathServiceResponse;
 import wooteco.subway.ui.dto.LineRequest;
 import wooteco.subway.ui.dto.SectionRequest;
-
 
 class PathAcceptanceTest extends AcceptanceTest {
 
     @Test
-    @DisplayName("존재하지 않는 노선을 생성한다.")
+    @DisplayName("upStationId와 downStationId를 입력 받아, 두 역 간에 최단 경로, 거리, 요금을 조회한다.")
     void findShortestPath() {
         // given
         createStation("강남역");
@@ -24,8 +28,38 @@ class PathAcceptanceTest extends AcceptanceTest {
         createStation("역삼역");
         createStation("선릉역");
 
-        LineRequest lineRequest = new LineRequest("3호선", "bg-orange-600", 1L, 2L, 3);
+        createLine("3호선", "bg-orange-600", 1L, 2L, 3);
+
+        createSection(2L, 3L, 4);
+        createSection(3L, 4L, 5);
+
+        // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .queryParam("source", 1)
+            .queryParam("target", 3)
+            .queryParam("age", 20)
+            .when()
+            .get("/paths")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+
+        // then
+        JsonPath jsonPath = response.jsonPath();
+        PathServiceResponse result = jsonPath.getObject("", PathServiceResponse.class);
+        assertAll(
+            () -> assertThat(result.getStations().size()).isEqualTo(3),
+            () -> assertThat(result.getFare()).isEqualTo(1250),
+            () -> assertThat(result.getDistance()).isEqualTo(7)
+        );
+    }
+
+    private void createLine(String name, String color, Long upStationId, Long downStationId,
+        int distance) {
+        LineRequest lineRequest = new LineRequest(name, color, upStationId, downStationId,
+            distance);
+        RestAssured.given().log().all()
             .body(lineRequest)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .when()
@@ -33,8 +67,10 @@ class PathAcceptanceTest extends AcceptanceTest {
             .then().log().all()
             .statusCode(HttpStatus.CREATED.value())
             .extract();
+    }
 
-        SectionRequest sectionRequest = new SectionRequest(2L, 3L, 4);
+    private void createSection(Long upStationId, Long downStationId, int distance) {
+        SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
         RestAssured.given().log().all()
             .body(sectionRequest)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -43,35 +79,13 @@ class PathAcceptanceTest extends AcceptanceTest {
             .then().log().all()
             .statusCode(HttpStatus.OK.value())
             .extract();
-
-        SectionRequest sectionRequest2 = new SectionRequest(3L, 4L, 5);
-        RestAssured.given().log().all()
-            .body(sectionRequest2)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/lines/" + 1L + "/sections")
-            .then().log().all()
-            .statusCode(HttpStatus.OK.value())
-            .extract();
-
-        // when
-        RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .queryParam("source", 1)
-            .queryParam("target", 6)
-            .queryParam("age", 20)
-            .when()
-            .get("/paths")
-            .then().log().all()
-            .statusCode(HttpStatus.OK.value())
-            .extract();
     }
 
     void createStation(String name) {
-        Map<String, String> params1 = new HashMap<>();
-        params1.put("name", name);
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
         RestAssured.given().log().all()
-            .body(params1)
+            .body(params)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .when()
             .post("/stations")
