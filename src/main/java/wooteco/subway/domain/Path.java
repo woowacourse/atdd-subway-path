ddfbe4b9b6d2b46e8a2d46d8dd497a24f18b57e9
brorae <jhr3239@gmail.com>
2022-05-17 23:23:02 +0900

    refactor: 상수 이름 변경

--- src/main/java/wooteco/subway/domain/Path.java
+++ src/main/java/wooteco/subway/domain/Path.java
@@ -5,62 +5,63 @@ import java.util.List;
 public class Path {
 
     private static final int DEFAULT_FARE = 1250;
-    private static final int DEFAULT_FARE_DISTANCE = 10;
-
-    private static final int ADDITIONAL_UNIT_FARE = 100;
-    private static final int FIRST_ADDITIONAL_UNIT_DISTANCE = 5;
-    private static final int OVER_ADDITIONAL_UNIT_DISTANCE = 8;
-    private static final int FIRST_ADDITIONAL_FARE_DISTANCE = 50;
+    private static final int UNIT_OF_ADDITIONAL_FARE = 100;
+    
+    private static final int DISTANCE_OF_DEFAULT_FARE = 10;
+    private static final int DISTANCE_OF_FIRST_ADDITIONAL_UNIT = 5;
+    private static final int DISTANCE_OF_OVER_ADDITIONAL_UNIT = 8;
+    private static final int DISTANCE_OF_OVER_ADDITIONAL_FARE = 50;
 
     private final List<Station> stations;
     private final int distance;
 
     public Path(final List<Station> stations, final int distance) {
-        validateStations(stations);
-        validateDistance(distance);
+        validateEmptyStations(stations);
+        validatePositiveDistance(distance);
         this.stations = stations;
         this.distance = distance;
     }
 
-    public void validateStations(final List<Station> stations) {
+    public void validateEmptyStations(final List<Station> stations) {
         if(stations.isEmpty()) {
             throw new IllegalArgumentException("경로는 비어서는 안됩니다.");
         }
     }
 
-    public void validateDistance(final int distance) {
+    public void validatePositiveDistance(final int distance) {
         if (distance <= 0) {
             throw new IllegalArgumentException("거리는 0보다 커야합니다.");
         }
     }
 
     public int calculateFare() {
-        if (distance <= DEFAULT_FARE_DISTANCE) {
+        if (distance <= DISTANCE_OF_DEFAULT_FARE) {
             return DEFAULT_FARE;
         }
-        if (distance <= FIRST_ADDITIONAL_FARE_DISTANCE) {
+        if (distance <= DISTANCE_OF_OVER_ADDITIONAL_FARE) {
             return DEFAULT_FARE + calculateFirstAdditionalFare();
         }
         return DEFAULT_FARE + calculateFirstAdditionalMaxFare() + calculateOverAdditionalFare();
     }
 
-    private int calculateOverFare(int distance, int unitDistance, int overFare) {
-        return (int) ((Math.ceil((distance - 1) / unitDistance) + 1) * overFare);
+    private int calculateFirstAdditionalFare() {
+        return calculateOverFare(distance - DISTANCE_OF_DEFAULT_FARE, DISTANCE_OF_FIRST_ADDITIONAL_UNIT
+        );
     }
 
-    private int calculateFirstAdditionalFare() {
-        return calculateOverFare(distance - DEFAULT_FARE_DISTANCE, FIRST_ADDITIONAL_UNIT_DISTANCE,
-                ADDITIONAL_UNIT_FARE);
+    private int calculateOverFare(int distance, int unitDistance) {
+        return (int) ((Math.ceil((distance - 1) / unitDistance) + 1) * UNIT_OF_ADDITIONAL_FARE);
     }
 
     private int calculateFirstAdditionalMaxFare() {
-        return calculateOverFare(FIRST_ADDITIONAL_FARE_DISTANCE - DEFAULT_FARE_DISTANCE, FIRST_ADDITIONAL_UNIT_DISTANCE,
-                ADDITIONAL_UNIT_FARE);
+        return calculateOverFare(DISTANCE_OF_OVER_ADDITIONAL_FARE - DISTANCE_OF_DEFAULT_FARE,
+                DISTANCE_OF_FIRST_ADDITIONAL_UNIT
+        );
     }
 
     private int calculateOverAdditionalFare() {
-        return calculateOverFare(distance - FIRST_ADDITIONAL_FARE_DISTANCE, OVER_ADDITIONAL_UNIT_DISTANCE,
-                ADDITIONAL_UNIT_FARE);
+        return calculateOverFare(distance - DISTANCE_OF_OVER_ADDITIONAL_FARE, DISTANCE_OF_OVER_ADDITIONAL_UNIT
+        );
     }
 
     public List<Station> getStations() {
