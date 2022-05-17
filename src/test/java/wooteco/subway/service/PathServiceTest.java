280b48419ef84b1d4bfbb1d57b62fde1c7af1544
DOES <ldk980130@gmail.com>
2022-05-17 21:21:02 +0900

    test: 테스트 픽스처 리펙터링

--- src/test/java/wooteco/subway/service/PathServiceTest.java
+++ src/test/java/wooteco/subway/service/PathServiceTest.java
@@ -1,9 +1,12 @@
 package wooteco.subway.service;
 
+import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -27,35 +30,40 @@ class PathServiceTest {
 	private LineService lineService;
 	@Autowired
 	private PathService pathService;
+	private final Map<String, Station> stations = new HashMap<>();
 
 	@BeforeEach
 	void init() {
-		List<Station> stations7 = saveStations("내방역", "고속터미널역", "반포역", "논현역");
-		Line line7 = lineService.create("7호선", "red",
-			new Section(stations7.get(0), stations7.get(1), 10));
-		lineService.addSection(line7.getId(), makeSection(stations7.get(1), stations7.get(2), 10));
-		lineService.addSection(line7.getId(), makeSection(stations7.get(2), stations7.get(3), 10));
+		stations.putAll(saveStations("내방역", "고속터미널역", "반포역", "논현역"));
 
-		List<Station> stations3 = saveStations("신사역", "잠원역", "서초역");
+		Line line7 = lineService.create("7호선", "red",
+			new Section(stations.get("내방역"), stations.get("고속터미널역"), 10));
+		lineService.addSection(line7.getId(),
+			makeSection(stations.get("고속터미널역"), stations.get("반포역"), 10));
+		lineService.addSection(line7.getId(),
+			makeSection(stations.get("반포역"), stations.get("논현역"), 10));
+
+		stations.putAll(saveStations("신사역", "잠원역", "서초역"));
+
 		Line line3 = lineService.create("3호선", "red",
-			new Section(stations3.get(0), stations3.get(1), 10));
-		lineService.addSection(line3.getId(), makeSection(stations3.get(1), stations7.get(1), 10));
-		lineService.addSection(line3.getId(), makeSection(stations7.get(1), stations3.get(2), 10));
+			new Section(stations.get("신사역"), stations.get("잠원역"), 10));
+		lineService.addSection(line3.getId(),
+			makeSection(stations.get("잠원역"), stations.get("고속터미널역"), 10));
+		lineService.addSection(line3.getId(),
+			makeSection(stations.get("고속터미널역"), stations.get("서초역"), 10));
 
 		Station sapyung = stationService.create("사평역");
+		stations.put("사평역", sapyung);
 		lineService.create("9호선", "red", makeSection(
-			stations7.get(1), sapyung, 14));
+			stations.get("고속터미널역"), sapyung, 14));
 
-		lineService.create("새호선", "red", makeSection(stations3.get(2), sapyung, 3));
+		lineService.create("새호선", "red",
+			makeSection(stations.get("서초역"), sapyung, 3));
 	}
 
-	private List<Station> saveStations(String... names) {
-		List<Station> stations = new ArrayList<>();
-		for (String name : names) {
-			Station station = stationService.create(name);
-			stations.add(station);
-		}
-		return stations;
+	private Map<String, Station> saveStations(String... names) {
+		return Arrays.stream(names)
+			.collect(toMap(name -> name, name -> stationService.create(name)));
 	}
 
 	private Section makeSection(Station source, Station target, int distance) {
@@ -66,8 +74,8 @@ class PathServiceTest {
 	@Test
 	void findPath() {
 		// given
-		Station source = findByName("내방역");
-		Station target = findByName("논현역");
+		Station source = stations.get("내방역");
+		Station target = stations.get("논현역");
 
 		// when
 		Path path = pathService.findPath(source, target);
@@ -82,8 +90,8 @@ class PathServiceTest {
 	@Test
 	void findPathReverse() {
 		// given
-		Station source = findByName("논현역");
-		Station target = findByName("내방역");
+		Station source = stations.get("논현역");
+		Station target = stations.get("내방역");
 
 		// when
 		List<Station> stations = pathService.findPath(source, target)
@@ -99,8 +107,8 @@ class PathServiceTest {
 	@Test
 	void doubleLine() {
 		// given
-		Station source = findByName("신사역");
-		Station target = findByName("논현역");
+		Station source = stations.get("신사역");
+		Station target = stations.get("논현역");
 
 		// when
 		List<Station> stations = pathService.findPath(source, target)
@@ -116,8 +124,8 @@ class PathServiceTest {
 	@Test
 	void shortestPath() {
 		// given
-		Station source = findByName("내방역");
-		Station target = findByName("사평역");
+		Station source = stations.get("내방역");
+		Station target = stations.get("사평역");
 
 		// when
 		List<Station> stations = pathService.findPath(source, target)
@@ -133,8 +141,8 @@ class PathServiceTest {
 	@Test
 	void pathDistance() {
 		// given
-		Station source = findByName("내방역");
-		Station target = findByName("논현역");
+		Station source = stations.get("내방역");
+		Station target = stations.get("논현역");
 
 		// when
 		Path path = pathService.findPath(source, target);
@@ -147,21 +155,13 @@ class PathServiceTest {
 	@Test
 	void fare() {
 		// given
-		Station source = findByName("신사역");
-		Station target = findByName("논현역");
+		Station source = stations.get("신사역");
+		Station target = stations.get("논현역");
 
 		// when
 		Path path = pathService.findPath(source, target);
 
 		// then
 		assertThat(path.calculateFare()).isEqualTo(1950);
-	}
-
-	private Station findByName(String name) {
-		return stationService.findAllStations()
-			.stream()
-			.filter(station -> station.isSameName(name))
-			.findAny()
-			.orElseThrow();
 	}
 }
