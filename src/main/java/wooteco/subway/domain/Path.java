0037243ba71caf9c58e484ab5fe3467b44937356
shindong96 <2006peter@naver.com>
2022-05-17 19:58:32 +0900

    feat: 구간 정보를 이용하여 최단 거리, 최단 경로를 구하는 도메인(Path)를 구현한다

---
+++ src/main/java/wooteco/subway/domain/Path.java
@@ -0,0 +1,39 @@
+package wooteco.subway.domain;
+
+import java.util.List;
+
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+public class Path {
+    private final Sections sections;
+
+    public Path(Sections sections) {
+        this.sections = sections;
+    }
+
+    public int getShortestDistance(Station source, Station target) {
+        DijkstraShortestPath dijkstraShortestPath = getDijkstraShortestPath();
+
+        return (int)dijkstraShortestPath.getPath(source, target).getWeight();
+    }
+
+    public List<Station> getShortestPath(Station source, Station target) {
+        DijkstraShortestPath dijkstraShortestPath = getDijkstraShortestPath();
+
+        return dijkstraShortestPath.getPath(source, target).getVertexList();
+    }
+
+    private DijkstraShortestPath getDijkstraShortestPath() {
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        for (Station station : sections.getStations()) {
+            graph.addVertex(station);
+        }
+        sections.forEach(section -> graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()),
+            section.getDistance()));
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        return dijkstraShortestPath;
+    }
+}
