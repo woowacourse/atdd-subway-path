efadf0755606ff07ca008abfaad3ac35cc734c1d
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-17 22:43:14 +0900

    refactor(test): parameterized test for boundary tests

--- src/test/java/wooteco/subway/domain/path/PathTest.java
+++ src/test/java/wooteco/subway/domain/path/PathTest.java
@@ -3,9 +3,9 @@ package wooteco.subway.domain.path;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import wooteco.subway.domain.station.Station;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -46,58 +46,12 @@ class PathTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("경계값 검증")
-    @Nested
-    class BoundaryTest {
+    @ParameterizedTest
+    @CsvSource({"9,1250", "10,1250", "11,1350", "49,2050", "50,2050", "51,2150"})
+    void 경계값_유효성_검증(int input, int expected) {
+        Path path = new Path(input, STATIONS);
+        int actual = path.calculateFare();
 
-        @Test
-        void 거리가_10인_경우_1250원_반환() {
-            Path path = new Path(10, STATIONS);
-
-            int actual = path.calculateFare();
-            int expected = 1250;
-
-            assertThat(actual).isEqualTo(expected);
-        }
-
-        @Test
-        void 거리가_11인_경우_1350원_반환() {
-            Path path = new Path(11, STATIONS);
-
-            int actual = path.calculateFare();
-            int expected = 1350;
-
-            assertThat(actual).isEqualTo(expected);
-        }
-
-        @Test
-        void 거리가_49인_경우_2050원_반환() {
-            Path path = new Path(49, STATIONS);
-
-            int actual = path.calculateFare();
-            int expected = 2050;
-
-            assertThat(actual).isEqualTo(expected);
-        }
-
-        @Test
-        void 거리가_50인_경우_2050원_반환() {
-            Path path = new Path(50, STATIONS);
-
-            int actual = path.calculateFare();
-            int expected = 2050;
-
-            assertThat(actual).isEqualTo(expected);
-        }
-
-        @Test
-        void 거리가_51인_경우_2150원_반환() {
-            Path path = new Path(51, STATIONS);
-
-            int actual = path.calculateFare();
-            int expected = 2150;
-
-            assertThat(actual).isEqualTo(expected);
-        }
+        assertThat(actual).isEqualTo(expected);
     }
 }
