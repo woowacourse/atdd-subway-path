761b61203393eac788f8b98b11181ee449af52de
DWL21 <nggus5@naver.com>
2022-05-17 20:41:55 +0900

    refactor: PathAcceptanceTest 메서드 분리

--- src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
@@ -3,9 +3,10 @@ package wooteco.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,57 +25,51 @@ class PathAcceptanceTest extends AcceptanceTest {
         createStation("선릉역");
 
         LineRequest lineRequest = new LineRequest("3호선", "bg-orange-600", 1L, 2L, 3);
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-            .body(lineRequest)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/lines")
-            .then().log().all()
-            .statusCode(HttpStatus.CREATED.value())
-            .extract();
-
-        SectionRequest sectionRequest = new SectionRequest(2L, 3L, 4);
         RestAssured.given().log().all()
-            .body(sectionRequest)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/lines/" + 1L + "/sections")
-            .then().log().all()
-            .statusCode(HttpStatus.OK.value())
-            .extract();
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
 
-        SectionRequest sectionRequest2 = new SectionRequest(3L, 4L, 5);
-        RestAssured.given().log().all()
-            .body(sectionRequest2)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/lines/" + 1L + "/sections")
-            .then().log().all()
-            .statusCode(HttpStatus.OK.value())
-            .extract();
+        createSection(1L, new SectionRequest(2L, 3L, 4));
+        createSection(1L, new SectionRequest(3L, 4L, 5));
 
         // when
         RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .queryParam("source", 1)
-            .queryParam("target", 4)
-            .queryParam("age", 20)
-            .when()
-            .get("/paths")
-            .then().log().all()
-            .statusCode(HttpStatus.OK.value())
-            .extract();
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", 1)
+                .queryParam("target", 4)
+                .queryParam("age", 20)
+                .when()
+                .get("/paths")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
     }
 
-    void createStation(String name) {
+    private void createStation(String name) {
         Map<String, String> params1 = new HashMap<>();
         params1.put("name", name);
         RestAssured.given().log().all()
-            .body(params1)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/stations")
-            .then().log().all()
-            .extract();
+                .body(params1)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private void createSection(Long lineId, SectionRequest sectionRequest) {
+        RestAssured.given().log().all()
+                .body(sectionRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines/" + lineId + "/sections")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
     }
 }
