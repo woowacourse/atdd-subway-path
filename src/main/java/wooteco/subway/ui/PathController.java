33bef9901968127ca9f010c97625f84c6eebe6bb
yeon-06 <rnjstldus2@gmail.com>
2022-05-17 19:07:23 +0900

    style: 미사용 import 제거

--- src/main/java/wooteco/subway/ui/PathController.java
+++ src/main/java/wooteco/subway/ui/PathController.java
@@ -1,23 +1,11 @@
 package wooteco.subway.ui;
 
-import java.net.URI;
-import java.util.List;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.service.PathService;
-import wooteco.subway.service.StationService;
 import wooteco.subway.service.dto.PathResponse;
-import wooteco.subway.service.dto.StationResponse;
-import wooteco.subway.ui.dto.StationRequest;
 
 @RestController
 @RequestMapping("/paths")
@@ -33,5 +21,4 @@ public class PathController {
     public PathResponse searchPaths(@RequestParam Long source, @RequestParam Long target, @RequestParam int age) {
         return pathService.searchPaths(source, target);
     }
-
 }
