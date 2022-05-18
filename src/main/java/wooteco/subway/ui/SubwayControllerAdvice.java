950e3dbe99b3fee5b152b4909752a2af13d39c7b
yxxnghwan <younghwan960@gmail.com>
2022-05-18 20:15:15 +0900

    refactor: 예외처리 핸들러 메서드 수정

--- src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
@@ -1,23 +1,21 @@
 package wooteco.subway.ui;
 
-import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import wooteco.subway.exception.DuplicateLineException;
-import wooteco.subway.exception.DuplicateStationException;
+import wooteco.subway.exception.SubwayException;
 
 @ControllerAdvice
 public class SubwayControllerAdvice {
 
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> handleNotFound(Exception exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(SubwayException.class)
+    public ResponseEntity<String> subwayException(SubwayException exception) {
+        return ResponseEntity.status(exception.getHttpStatus()).body(exception.getMessage());
     }
 
-    @ExceptionHandler({DuplicateStationException.class, DuplicateLineException.class})
-    public ResponseEntity<String> handleBadRequest(Exception exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> subwayException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 }
