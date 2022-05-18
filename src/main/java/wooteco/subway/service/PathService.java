3d7746d4e202ea67df9a6c275a529a2e77029762
nailseong <nnis9857@gmail.com>
2022-05-18 13:43:34 +0900

    feat(PathService): 출발역과 도착역의 최단 경로에 대한 정보를 조회하는 기능 구현

--- src/main/java/wooteco/subway/service/PathService.java
+++ src/main/java/wooteco/subway/service/PathService.java
@@ -1,14 +1,41 @@
 package wooteco.subway.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
-import wooteco.subway.exception.IllegalInputException;
+import wooteco.subway.dao.SectionDao;
+import wooteco.subway.dao.StationDao;
+import wooteco.subway.domain.Distance;
+import wooteco.subway.domain.Fare;
+import wooteco.subway.domain.Sections;
+import wooteco.subway.domain.Station;
+import wooteco.subway.domain.SubwayMap;
+import wooteco.subway.dto.path.PathResponse;
+import wooteco.subway.exception.station.NoSuchStationException;
 
 @Service
 public class PathService {
 
-    public void find(final Long sourceStationId, final Long targetStationId) {
-        if (sourceStationId.equals(targetStationId)) {
-            throw new IllegalInputException("출발역과 도착역이 동일합니다.");
-        }
+    private final SectionDao sectionDao;
+    private final StationDao stationDao;
+
+    public PathService(final SectionDao sectionDao, final StationDao stationDao) {
+        this.sectionDao = sectionDao;
+        this.stationDao = stationDao;
+    }
+
+    public PathResponse find(final Long sourceStationId, final Long targetStationId) {
+        final Station sourceStation = stationDao.findById(sourceStationId)
+                .orElseThrow(NoSuchStationException::new);
+        final Station targetStation = stationDao.findById(targetStationId)
+                .orElseThrow(NoSuchStationException::new);
+
+        final Sections sections = sectionDao.findAll();
+        SubwayMap subwayMap = new SubwayMap(sections);
+
+        final List<Station> stations = subwayMap.searchPath(sourceStation, targetStation);
+        final Distance distance = subwayMap.searchDistance(sourceStation, targetStation);
+        final Fare fare = new Fare(distance);
+
+        return PathResponse.of(stations, distance, fare);
     }
 }
--- src/test/java/wooteco/subway/service/PathServiceTest.java
+++ src/test/java/wooteco/subway/service/PathServiceTest.java
@@ -1,21 +1,85 @@
 package wooteco.subway.service;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
 
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import wooteco.subway.exception.IllegalInputException;
+import org.mockito.InjectMocks;
+import wooteco.subway.domain.Distance;
+import wooteco.subway.domain.Fare;
+import wooteco.subway.domain.Line;
+import wooteco.subway.domain.Section;
+import wooteco.subway.domain.Sections;
+import wooteco.subway.domain.Station;
+import wooteco.subway.dto.path.PathResponse;
 
 class PathServiceTest extends ServiceTest {
 
-    private final PathService pathService = new PathService();
+    @InjectMocks
+    private PathService pathService;
 
     @Test
-    @DisplayName("동일한 역의 경로를 조회할 경우 예외를 던진다.")
-    void Find_SameStations_ExceptionThrown() {
-        // when, then
-        assertThatThrownBy(() -> pathService.find(1L, 1L))
-                .isInstanceOf(IllegalInputException.class)
-                .hasMessage("출발역과 도착역이 동일합니다.");
+    @DisplayName("출발역과 도착역의 최단 경로에 대한 정보를 조회한다.")
+    void Find() {
+        // given
+        Station gangnam = new Station(1L, "강남역");
+        Station yeoksam = new Station(2L, "역삼역");
+        Station seolleung = new Station(3L, "선릉역");
+        Station samsung = new Station(4L, "삼성역");
+        Station seoulForest = new Station(5L, "서울숲역");
+        Station wangsimni = new Station(6L, "왕십리역");
+        Station yacksu = new Station(7L, "약수역");
+        Station geumho = new Station(8L, "금호역");
+        Station oksu = new Station(9L, "옥수역");
+
+        final Line greenLine = new Line("2호선", "green");
+        final Line yellowLine = new Line("수인분당선", "yellow");
+        final Line orangeLine = new Line("3호선", "orange");
+
+        final Section greenSectionA = new Section(greenLine, gangnam, yeoksam, new Distance(10));
+        final Section greenSectionB = new Section(greenLine, yeoksam, seolleung, new Distance(7));
+        final Section greenSectionC = new Section(greenLine, seolleung, samsung, new Distance(11));
+
+        final Section yellowSectionA = new Section(yellowLine, seolleung, seoulForest, new Distance(3));
+        final Section yellowSectionB = new Section(yellowLine, seoulForest, wangsimni, new Distance(8));
+
+        final Section orangeSectionA = new Section(orangeLine, yacksu, geumho, new Distance(12));
+        final Section orangeSectionB = new Section(orangeLine, geumho, oksu, new Distance(6));
+
+        final Sections sections = new Sections(List.of(
+                greenSectionA,
+                greenSectionB,
+                greenSectionC,
+                yellowSectionA,
+                yellowSectionB,
+                orangeSectionA,
+                orangeSectionB
+        ));
+
+        given(stationDao.findById(any(Long.class)))
+                .willReturn(Optional.of(gangnam))
+                .willReturn(Optional.of(seoulForest));
+
+        given(sectionDao.findAll())
+                .willReturn(sections);
+
+        final List<Station> expectedStations = List.of(
+                gangnam,
+                yeoksam,
+                seolleung,
+                seoulForest
+        );
+        final Distance expectedDistance = new Distance(20);
+        final PathResponse expected = PathResponse.of(expectedStations, expectedDistance, new Fare(expectedDistance));
+
+        // when
+        final PathResponse actual = pathService.find(gangnam.getId(), seoulForest.getId());
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
