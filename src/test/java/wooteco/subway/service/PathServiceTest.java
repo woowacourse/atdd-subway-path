a33573776b93e2cb017f93b1c6fa6e0dc1490cbd
koo <gudonghee2000@gmail.com>
2022-05-18 14:17:16 +0900

    test: 경로 조회 테스트 작성

--- src/test/java/wooteco/subway/service/PathServiceTest.java
+++ src/test/java/wooteco/subway/service/PathServiceTest.java
@@ -1,11 +1,20 @@
 package wooteco.subway.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import wooteco.subway.dao.FakeLineDao;
 import wooteco.subway.dao.FakeSectionDao;
 import wooteco.subway.dao.FakeStationDao;
+import wooteco.subway.domain.Line;
+import wooteco.subway.domain.Section;
+import wooteco.subway.domain.Station;
+import wooteco.subway.dto.PathResponse;
 
 class PathServiceTest {
 
@@ -21,9 +30,46 @@ class PathServiceTest {
     @DisplayName("경로를 조회한다.")
     @Test
     void showRoute() {
-        Long sourceStationId = 1L;
-        Long targetStationId = 5L;
+        Long sourceStationId = 4L;
+        Long targetStationId = 7L;
         int age = 15;
-        pathService.createPath(sourceStationId, targetStationId, age);
+
+        FakeStationDao fakeStationDao = new FakeStationDao();
+        fakeStationDao.save(new Station("이대역"));
+        fakeStationDao.save(new Station("학동역"));
+        fakeStationDao.save(new Station("이수역"));
+        fakeStationDao.save(new Station("건대역"));
+        fakeStationDao.save(new Station("사가정역"));
+
+        FakeLineDao fakeLineDao = new FakeLineDao();
+        fakeLineDao.save(new Line("1호선", "blue", 900));
+        fakeLineDao.save(new Line("2호선", "green", 900));
+        fakeLineDao.save(new Line("3호선", "orange", 900));
+        fakeLineDao.save(new Line("4호선", "black", 900));
+
+        FakeSectionDao fakeSectionDao = new FakeSectionDao();
+        fakeSectionDao.save(new Section(1L, new Station(4L, "이대역"), new Station(5L, "학동역"), 3));
+        fakeSectionDao.save(new Section(2L, new Station(5L, "학동역"), new Station(6L, "이수역"), 3));
+        fakeSectionDao.save(new Section(2L, new Station(6L, "이수역"), new Station(7L, "건대역"), 4));
+        fakeSectionDao.save(new Section(3L, new Station(7L, "건대역"), new Station(8L, "사가정역"), 5));
+        fakeSectionDao.save(new Section(4L, new Station(8L, "사가정역"), new Station(4L, "이대역"), 7));
+
+        PathResponse pathResponse = pathService.createPath(sourceStationId, targetStationId, age);
+
+        assertAll(
+                () -> assertThat(createStation(pathResponse)).isEqualTo(
+                        List.of(new Station(4L, "이대역"), new Station(5L, "학동역")
+                                , new Station(6L, "이수역"), new Station(7L, "건대역"))
+                ),
+                () -> assertThat(pathResponse.getDistance()).isEqualTo(10),
+                () -> assertThat(pathResponse.getFare()).isEqualTo(1250)
+        );
+    }
+
+    private List<Station> createStation(PathResponse pathResponse) {
+        return pathResponse.getStations().stream()
+                .map(it -> new Station(it.getId(), it.getName()))
+                .collect(Collectors.toList());
+
     }
 }
