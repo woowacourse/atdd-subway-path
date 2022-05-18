f65acf10428335f5eefe160dbbf26058f1cb77cf
devHudi <devhudi@gmail.com>
2022-05-18 14:35:19 +0900

    refactor: 초과 요금 계산 로직 개선

--- src/main/java/wooteco/subway/domain/Fare.java
+++ src/main/java/wooteco/subway/domain/Fare.java
@@ -1,8 +1,16 @@
 package wooteco.subway.domain;
 
+import java.util.Objects;
+
 public class Fare {
 
     public static final int DEFAULT_FARE = 1250;
+    public static final int OVER_FARE_AT_50 = 800;
+    public static final int OVER_FARE_AMOUNT_PER_UNIT_DISTANCE = 100;
+    public static final int NO_OVER_FARE_DISTANCE = 10;
+    public static final int FIRST_OVER_FARE_DISTANCE = 50;
+    public static final int FIRST_OVER_FARE_UNIT_DISTANCE = 5;
+    public static final int SECOND_OVER_FARE_UNIT_DISTANCE = 8;
 
     private final int fare;
 
@@ -15,20 +23,48 @@ public class Fare {
     }
 
     private static int calculateOverFare(int distance) {
-        if (distance <= 10) {
+        if (distance <= NO_OVER_FARE_DISTANCE) {
             return 0;
         }
 
-        if (distance <= 50) {
-            distance -= 10;
-            return (int) ((Math.ceil((distance - 1) / 5) + 1) * 100);
+        if (distance <= FIRST_OVER_FARE_DISTANCE) {
+            return calculateOverFareWithDistanceUnit(distance - NO_OVER_FARE_DISTANCE, FIRST_OVER_FARE_UNIT_DISTANCE);
         }
 
-        distance -= 50;
-        return 800 + (int) ((Math.ceil((distance - 1) / 8) + 1) * 100);
+        return OVER_FARE_AT_50 + calculateOverFareWithDistanceUnit(distance - FIRST_OVER_FARE_DISTANCE,
+                SECOND_OVER_FARE_UNIT_DISTANCE);
+    }
+
+    private static int calculateOverFareWithDistanceUnit(int distance, int unitDistance) {
+        int numberOfImposition = (distance - 1) / unitDistance + 1;
+        return numberOfImposition * OVER_FARE_AMOUNT_PER_UNIT_DISTANCE;
     }
 
     public int getValue() {
         return fare;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Fare fare1 = (Fare) o;
+        return fare == fare1.fare;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fare);
+    }
+
+    @Override
+    public String toString() {
+        return "Fare{" +
+                "fare=" + fare +
+                '}';
     }
 }
--- src/test/java/wooteco/subway/domain/FareTest.java
+++ src/test/java/wooteco/subway/domain/FareTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class FareTest {
     @DisplayName("거리를 전달받아 요금을 계산한다.")
     @ParameterizedTest
-    @CsvSource(value = {"10,1250", "12,1350", "16,1450", "30,1650", "50,2050", "58,2150"})
+    @CsvSource(value = {"10,1250", "12,1350", "16,1450", "30,1650", "40,1850", "41,1950", "50,2050", "58,2150"})
     void calculate_fare_with_distance(int distance, int expectedFare) {
         // when
         Fare createdFare = Fare.from(distance);
