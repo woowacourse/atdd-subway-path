e6b0cd38972a82eb4bef0041d3ac7dae08a5528c
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-18 17:05:21 +0900

    feat(domain): implement Fare for calculating and holding total fare
    - add test cases

---
+++ src/main/java/wooteco/subway/domain/path/fare/Fare.java
@@ -0,0 +1,49 @@
+package wooteco.subway.domain.path.fare;
+
+import java.util.Objects;
+
+public class Fare {
+
+    private static final int BASIC_FARE = 1250;
+
+    private final int value;
+
+    Fare(int value) {
+        this.value = value;
+    }
+
+    public static Fare of(int distance) {
+        return new Fare(calculateFare(distance));
+    }
+
+    public static int calculateFare(int distance) {
+        DistanceOverFare distanceOverFare = DistanceOverFare.of(distance);
+        return BASIC_FARE + distanceOverFare.getValue();
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Fare fare = (Fare) o;
+        return value == fare.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Fare{" + "value=" + value + '}';
+    }
+}
