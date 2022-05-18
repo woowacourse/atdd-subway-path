eb9e32a611c7bef82ee7198f0527d5c4b760531d
nailseong <nnis9857@gmail.com>
2022-05-18 14:57:04 +0900

    test(PathAcceptanceTest): 경로 조회 테스트 추가

--- src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
@@ -25,8 +25,14 @@ class PathAcceptanceTest extends AcceptanceTest {
     private Station yeoksam;
     private Station seolleung;
     private Station samsung;
+
     private Station seoulForest;
     private Station wangsimni;
+
+    private Station heangdang;
+    private Station majang;
+    private Station dapsimni;
+
     private Station yacksu;
     private Station geumho;
     private Station oksu;
@@ -37,8 +43,14 @@ class PathAcceptanceTest extends AcceptanceTest {
         yeoksam = createStation(new StationRequest("역삼역")).as(Station.class);
         seolleung = createStation(new StationRequest("선릉역")).as(Station.class);
         samsung = createStation(new StationRequest("삼성역")).as(Station.class);
+
         seoulForest = createStation(new StationRequest("서울숲역")).as(Station.class);
         wangsimni = createStation(new StationRequest("왕십리역")).as(Station.class);
+
+        heangdang = createStation(new StationRequest("행당역")).as(Station.class);
+        majang = createStation(new StationRequest("마장역")).as(Station.class);
+        dapsimni = createStation(new StationRequest("답십리역")).as(Station.class);
+
         yacksu = createStation(new StationRequest("약수역")).as(Station.class);
         geumho = createStation(new StationRequest("금호역")).as(Station.class);
         oksu = createStation(new StationRequest("옥수역")).as(Station.class);
@@ -52,6 +64,11 @@ class PathAcceptanceTest extends AcceptanceTest {
                 new LineRequest("수인분당선", "yellow", seolleung.getId(), seoulForest.getId(), 12));
         createSection(new SectionRequest(seoulForest.getId(), wangsimni.getId(), 7), yellowLineId);
 
+        final long purpleLineId = createAndGetLineId(
+                new LineRequest("5호선", "purple", heangdang.getId(), wangsimni.getId(), 11));
+        createSection(new SectionRequest(wangsimni.getId(), majang.getId(), 17), purpleLineId);
+        createSection(new SectionRequest(majang.getId(), dapsimni.getId(), 15), purpleLineId);
+
         final long orangeLineId = createAndGetLineId(
                 new LineRequest("3호선", "orange", yacksu.getId(), geumho.getId(), 7));
         createSection(new SectionRequest(geumho.getId(), oksu.getId(), 12), orangeLineId);
@@ -61,37 +78,36 @@ class PathAcceptanceTest extends AcceptanceTest {
     @DisplayName("동일한 역의 경로를 조회할 경우 400 을 응답한다.")
     void ShowPath_SameStations_BadRequestReturned() {
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .queryParam("source", gangnam.getId())
-                .queryParam("target", gangnam.getId())
+        final ExtractableResponse<Response> response = getResponseExtractableResponse(gangnam.getId(), gangnam.getId());
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private ExtractableResponse<Response> getResponseExtractableResponse(final Long sourceStationId,
+                                                                         final Long targetStationId) {
+        return RestAssured.given().log().all()
+                .queryParam("source", sourceStationId)
+                .queryParam("target", targetStationId)
                 .when()
                 .get("/paths")
                 .then().log().all()
                 .extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     @Test
     @DisplayName("이동할 수 없는 경로를 조회할 경우 404 을 응답한다.")
     void ShowPath_InvalidPath_BadRequestReturned() {
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .queryParam("source", gangnam.getId())
-                .queryParam("target", oksu.getId())
-                .when()
-                .get("/paths")
-                .then().log().all()
-                .extract();
+        final ExtractableResponse<Response> response = getResponseExtractableResponse(gangnam.getId(), oksu.getId());
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 
     @Test
-    @DisplayName("출발역과 도착역의 경로 정보를 조회한다.")
-    void ShowPath() {
+    @DisplayName("환승을 한 번하는 출발역과 도착역의 경로 정보를 조회한다.")
+    void ShowPath_TransferOnce_OK() {
         // given
         final List<Station> expectedStations = List.of(
                 gangnam,
@@ -103,13 +119,36 @@ class PathAcceptanceTest extends AcceptanceTest {
         final PathResponse expected = PathResponse.of(expectedStations, expectedDistance, new Fare(expectedDistance));
 
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .queryParam("source", gangnam.getId())
-                .queryParam("target", seoulForest.getId())
-                .when()
-                .get("/paths")
-                .then().log().all()
-                .extract();
+        final ExtractableResponse<Response> response = getResponseExtractableResponse(gangnam.getId(),
+                seoulForest.getId());
+
+        final PathResponse actual = response.as(PathResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(actual).isEqualTo(expected)
+        );
+    }
+
+    @Test
+    @DisplayName("환승을 두 번하는 출발역과 도착역의 경로 정보를 조회한다.")
+    void ShowPath_TransferTwice_OK() {
+        // given
+        final List<Station> expectedStations = List.of(
+                yeoksam,
+                seolleung,
+                seoulForest,
+                wangsimni,
+                majang,
+                dapsimni
+        );
+        final Distance expectedDistance = new Distance(59);
+        final PathResponse expected = PathResponse.of(expectedStations, expectedDistance, new Fare(expectedDistance));
+
+        // when
+        final ExtractableResponse<Response> response = getResponseExtractableResponse(yeoksam.getId(),
+                dapsimni.getId());
 
         final PathResponse actual = response.as(PathResponse.class);
 
