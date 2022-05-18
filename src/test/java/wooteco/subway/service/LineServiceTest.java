01b7be47e0bd076ce72c1bbf596e9c35692c446c
Kim Seunglae <obey1342@gmail.com>
2022-05-18 13:15:07 +0900

    test: LineService 테스트 케이스 수정

--- src/test/java/wooteco/subway/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/service/LineServiceTest.java
@@ -33,16 +33,9 @@ class LineServiceTest {
         new StationResponse(2L, "가천대역"),
         new StationResponse(3L, "태평역")
     ));
-    private static final LineResponse LINE_RESPONSE2 = new LineResponse(
-        2L, "2호선", "초록색", List.of(
-        new StationResponse(4L, "잠실역"),
-        new StationResponse(5L, "잠실나루역"),
-        new StationResponse(6L, "강변역")
-    ));
 
     @Autowired
     private LineService lineService;
-
 
     @DisplayName("save 메서드는 데이터를 저장한다")
     @Nested
@@ -53,7 +46,7 @@ class LineServiceTest {
             LineRequest lineRequest = new LineRequest("5호선", "보라색", 1L, 2L, 10);
 
             LineResponse actual = lineService.save(lineRequest);
-            LineResponse expected = new LineResponse(3L, "5호선", "보라색", List.of(
+            LineResponse expected = new LineResponse(4L, "5호선", "보라색", List.of(
                 new StationResponse(1L, "복정역"),
                 new StationResponse(2L, "가천대역")
             ));
@@ -88,10 +81,22 @@ class LineServiceTest {
 
     @Test
     void findAll_메서드는_모든_데이터를_조회한다() {
+        LineResponse lineResponse2 = new LineResponse(
+            2L, "2호선", "초록색", List.of(
+            new StationResponse(4L, "잠실역"),
+            new StationResponse(5L, "잠실나루역"),
+            new StationResponse(6L, "강변역")
+        ));
+        LineResponse lineResponse3 = new LineResponse(
+            3L, "3호선", "주황색", List.of(
+            new StationResponse(1L, "복정역"),
+            new StationResponse(3L, "태평역")
+        ));
+
         List<LineResponse> actual = lineService.findAll();
 
         List<LineResponse> expected = List.of(
-            LINE_RESPONSE1, LINE_RESPONSE2
+            LINE_RESPONSE1, lineResponse2, lineResponse3
         );
 
         assertThat(actual).isEqualTo(expected);
