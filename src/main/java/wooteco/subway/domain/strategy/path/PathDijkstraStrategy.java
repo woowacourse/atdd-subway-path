91f9cb6b470649112f837eaef56cb647257eca4d
Wishoon <heonga2@gmail.com>
2022-05-18 11:20:46 +0900

    refactor: 최단경로를 구하는 기능을 전략으로 변경

---
+++ src/main/java/wooteco/subway/domain/strategy/path/PathDijkstraStrategy.java
@@ -0,0 +1,45 @@
+package wooteco.subway.domain.strategy.path;
+
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import wooteco.subway.domain.Path;
+import wooteco.subway.domain.Section;
+import wooteco.subway.domain.Sections;
+import wooteco.subway.domain.Station;
+
+public class PathDijkstraStrategy implements PathStrategy {
+
+    @Override
+    public Path calculatePath(Station source, Station target, Sections sections) {
+        validateSameStation(source, target);
+
+        WeightedMultigraph<Station , DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        addVertex(sections, graph);
+        addEdgeWeight(sections, graph);
+
+        DijkstraShortestPath shortPath = new DijkstraShortestPath(graph);
+
+        return new Path(
+                shortPath.getPath(source, target).getVertexList(),
+                (int) shortPath.getPathWeight(source, target));
+    }
+
+    private void addEdgeWeight(Sections sections, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        for (Section value : sections.getValues()) {
+            graph.setEdgeWeight(graph.addEdge(value.getUpStation(), value.getDownStation()), value.getDistance());
+        }
+    }
+
+    private void addVertex(Sections sections, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        for (Station station : sections.getDistinctStations()) {
+            graph.addVertex(station);
+        }
+    }
+
+    private void validateSameStation(Station source, Station target) {
+        if (source.equals(target)) {
+            throw new IllegalArgumentException("출발역과 도착역이 같을 수 없습니다.");
+        }
+    }
+}
--- src/test/java/wooteco/subway/domain/strategy/path/PathDijkstraStrategyTest.java
+++ src/test/java/wooteco/subway/domain/strategy/path/PathDijkstraStrategyTest.java
@@ -1,4 +1,4 @@
-package wooteco.subway.domain;
+package wooteco.subway.domain.strategy.path;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,48 +13,33 @@ import static wooteco.subway.Fixture.종합운동장역;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import wooteco.subway.domain.Path;
+import wooteco.subway.domain.Sections;
 
-class PathFinderTest {
+class PathDijkstraStrategyTest {
 
     @Test
     @DisplayName("각 구간 리스트들을 통해서 최단 경로를 조회할 수 있다.")
     void getDijkstraShortestPath() {
         // given
-        Sections sections = new Sections(전체_구간);
+        PathStrategy pathStrategy = new PathDijkstraStrategy();
 
         // when
-        PathFinder finder = PathFinder.createPathFinder(sections);
-        List<Station> stations = finder.findShortestPath(강남역, 삼전역);
+        Path path = pathStrategy.calculatePath(강남역, 삼전역, new Sections(전체_구간));
 
         // then
-        assertThat(stations).hasSize(6)
-                .containsExactly(강남역, 역삼역, 선릉역, 삼성역, 종합운동장역, 삼전역);
+        assertThat(path).isEqualTo(new Path(List.of(강남역, 역삼역, 선릉역, 삼성역, 종합운동장역, 삼전역), 25));
     }
 
     @Test
     @DisplayName("시작역과 종점역이 같을 경우 예외가 발생한다.")
     void sameSourceTarget() {
         // given
-        Sections sections = new Sections(전체_구간);
-        PathFinder finder = PathFinder.createPathFinder(sections);
+        PathStrategy pathStrategy = new PathDijkstraStrategy();
 
         // when & then
-        assertThatThrownBy(() -> finder.findShortestPath(강남역, 강남역))
+        assertThatThrownBy(() -> pathStrategy.calculatePath(강남역, 강남역, new Sections(전체_구간)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("출발역과 도착역이 같을 수 없습니다.");
-    }
-
-    @Test
-    @DisplayName("각 구간 리스트들의 총 거리를 구할 수 있다.")
-    void calculatePathDistance() {
-        // given
-        Sections sections = new Sections(전체_구간);
-        PathFinder finder = PathFinder.createPathFinder(sections);
-
-        // when
-        double pathDistance = finder.findShortestPathDistance(강남역, 삼전역);
-
-        // then
-        assertThat(pathDistance).isEqualTo(25);
     }
 }
