493539d8218445c05844f8f3db597d0512f18b80
cjlee38 <cjlee38@github.com>
2022-05-18 15:30:39 +0900

    feat: 경로조회 기능 컨트롤러 연결

---
+++ src/main/java/wooteco/subway/service/PathService.java
@@ -0,0 +1,42 @@
+package wooteco.subway.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import wooteco.subway.domain.line.LineSeries;
+import wooteco.subway.domain.path.PathFinder;
+import wooteco.subway.domain.property.Distance;
+import wooteco.subway.domain.property.Fare;
+import wooteco.subway.domain.station.Station;
+import wooteco.subway.dto.response.PathResponse;
+import wooteco.subway.repository.LineRepository;
+import wooteco.subway.repository.StationRepository;
+
+@Repository
+public class PathService {
+
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
+
+    public PathService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    public PathResponse findPath(Long source, Long target, Long age) {
+        LineSeries lineSeries = new LineSeries(lineRepository.findAllLines());
+        PathFinder finder = PathFinder.from(lineSeries);
+
+        final Station sourceStation = stationRepository.findById(source);
+        final Station targetStation = stationRepository.findById(target);
+
+        final List<Station> paths = finder.findShortestPath(sourceStation, targetStation);
+        final Distance distance = new Distance(finder.getDistance(sourceStation, targetStation));
+
+        Fare fare = new Fare();
+        final Fare calculatedFare = fare.calculate(distance);
+
+        return PathResponse.of(paths, distance, calculatedFare);
+    }
+}
