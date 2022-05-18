0745ec46ecb9e13ba5a4481c48a0c5cdb08cd4c3
hyeonic <dev.hyeonic@gmail.com>
2022-05-18 15:44:17 +0900

    test: fixtures 데이터 추가

--- src/test/java/wooteco/subway/domain/SectionFixtures.java
+++ src/test/java/wooteco/subway/domain/SectionFixtures.java
@@ -1,9 +1,12 @@
 package wooteco.subway.domain;
 
 import static wooteco.subway.domain.LineFixtures.LINE_1;
+import static wooteco.subway.domain.LineFixtures.LINE_2;
 import static wooteco.subway.domain.StationFixtures.개봉역;
 import static wooteco.subway.domain.StationFixtures.부천역;
+import static wooteco.subway.domain.StationFixtures.서울대입구역;
 import static wooteco.subway.domain.StationFixtures.소사역;
+import static wooteco.subway.domain.StationFixtures.신대방역;
 import static wooteco.subway.domain.StationFixtures.신도림역;
 import static wooteco.subway.domain.StationFixtures.역곡역;
 import static wooteco.subway.domain.StationFixtures.온수역;
@@ -23,4 +26,7 @@ public class SectionFixtures {
     public static final Section 신도림역_역곡역_13 = new Section(8L, LINE_1, 신도림역, 역곡역, 13);
     public static final Section 신도림역_개봉역_10 = new Section(9L, LINE_1, 신도림역, 개봉역, 10);
     public static final Section 소사역_부천역_5 = new Section(10L, LINE_1, 소사역, 부천역, 5);
+
+    public static final Section 신도림역_신대방역_10 = new Section(11L, LINE_2, 신도림역, 신대방역, 10);
+    public static final Section 신대방역_서울대입구역_15 = new Section(12L, LINE_2, 신대방역, 서울대입구역, 15);
 }
--- src/test/java/wooteco/subway/domain/StationFixtures.java
+++ src/test/java/wooteco/subway/domain/StationFixtures.java
@@ -9,4 +9,9 @@ public class StationFixtures {
     public static final Station 소사역 = new Station(5L, "소사역");
     public static final Station 부천역 = new Station(6L, "부천역");
     public static final Station 중동역 = new Station(7L, "중동역");
+
+    public static final Station 신대방역 = new Station(8L, "신대방역");
+    public static final Station 서울대입구역 = new Station(9L, "서울대입구역");
+
+    public static final Station 센트럴파크역 = new Station(10L, "센트럴파크역");
 }
