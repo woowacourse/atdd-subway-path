faf3c5d82e3253f876c8ded9ccb341d7a5f2d80f
seong-wooo <seongwoo0513@uos.ac.kr>
2022-05-18 16:24:23 +0900

    refactor: 매직넘버 상수화

--- src/main/java/wooteco/subway/domain/Fare.java
+++ src/main/java/wooteco/subway/domain/Fare.java
@@ -4,6 +4,11 @@ public class Fare {
 
     public static final int BASIC_FARE = 1250;
     public static final int ADDITIONAL_FARE = 100;
+    public static final int MINIMUM_DISTANCE_BOUNDARY = 10;
+    public static final int MAXIMUM_DISTANCE_BOUNDARY = 50;
+    public static final int BELOW_FIFTY_KM_POLICY = 5;
+    public static final int ABOVE_FIFTY_KM_POLICY = 8;
+    public static final int ADDITIONAL_FARE_50_KM = 800;
 
     private final int distance;
 
@@ -20,14 +25,18 @@ public class Fare {
 
 
     public int calculate() {
-        if (distance <= 10) {
+        if (distance <= MINIMUM_DISTANCE_BOUNDARY) {
             return BASIC_FARE;
         }
 
-        if (distance <= 50) {
-            return BASIC_FARE + (int) (Math.ceil((double) (distance - 10) / 5)) * 100;
+        if (distance <= MAXIMUM_DISTANCE_BOUNDARY) {
+            return BASIC_FARE
+                    + (int) (Math.ceil((double) (distance - MINIMUM_DISTANCE_BOUNDARY) / BELOW_FIFTY_KM_POLICY))
+                    * ADDITIONAL_FARE;
         }
 
-        return BASIC_FARE + 800 + (int) Math.ceil(((double) (distance - 50) / 8)) * ADDITIONAL_FARE;
+        return BASIC_FARE + ADDITIONAL_FARE_50_KM
+                + (int) Math.ceil(((double) (distance - MAXIMUM_DISTANCE_BOUNDARY) / ABOVE_FIFTY_KM_POLICY))
+                * ADDITIONAL_FARE;
     }
 }
