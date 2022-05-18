75e41c82b0d5243b7a17ddafeec137a453863fed
leokim <kunga077@gmail.com>
2022-05-18 15:13:33 +0900

    refactor: 전역적 예외 처리 수정

--- src/main/java/wooteco/subway/controller/ControllerAdvice.java
+++ src/main/java/wooteco/subway/controller/ControllerAdvice.java
@@ -11,7 +11,7 @@ import java.util.NoSuchElementException;
 public class ControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> IllegalHandle(Exception e) {
+    public ResponseEntity<String> handleIllegalArgumentException(RuntimeException e) {
         return new ResponseEntity<>(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST
@@ -19,10 +19,26 @@ public class ControllerAdvice {
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> NoSuchHandle(Exception e) {
+    public ResponseEntity<String> handleNoSuchException(RuntimeException e) {
         return new ResponseEntity<>(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        return new ResponseEntity<>(
+                "서버에서 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.",
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleCheckedException(Exception e) {
+        return new ResponseEntity<>(
+                "확인되지 않은 오류가 있습니다. 잠시 후 다시 시도해주세요.",
+                HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
 }
