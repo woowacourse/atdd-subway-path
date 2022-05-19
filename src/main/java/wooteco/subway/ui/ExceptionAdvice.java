e43cd1cd04f52740d9684e0da0022cbc29a3180d
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-05-19 15:09:20 +0900

    refactor(ExceptionAdvice): EmptyResultDataAccessException 처리 추가

--- src/main/java/wooteco/subway/ui/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/ui/ExceptionAdvice.java
@@ -1,5 +1,6 @@
 package wooteco.subway.ui;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +11,13 @@ public class ExceptionAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Void> duplicatedNameFound() {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Void> dataNotFoundException() {
+        return ResponseEntity
+                .internalServerError()
+                .build();
     }
 
     @ExceptionHandler(Exception.class)
--- src/main/java/wooteco/subway/ui/LineController.java
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -1,14 +1,22 @@
 package wooteco.subway.ui;
 
+import java.net.URI;
+import java.util.List;
+import java.util.NoSuchElementException;
+import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.LineResponse;
 import wooteco.subway.service.LineService;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.*;
 
 @RequestMapping("/lines")
 @RestController
