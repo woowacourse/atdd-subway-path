f38c601372be5e517cad76ff2219ff23c3995899
ozragwort <ozragwort@gmail.com>
2022-05-20 00:30:24 +0900

    refactor: FareTest를 DistanceFareStrategyTest로 수정

--- src/test/java/wooteco/subway/strategy/DistanceFareStrategyTest.java
+++ src/test/java/wooteco/subway/strategy/DistanceFareStrategyTest.java
@@ -1,19 +1,17 @@
-package wooteco.subway.domain;
+package wooteco.subway.strategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import wooteco.subway.strategy.DistanceFareStrategy;
-import wooteco.subway.strategy.FareStrategy;
 
-public class FareTest {
+class DistanceFareStrategyTest {
 
     @DisplayName("운임에 따른 요금을 반환한다.")
     @ParameterizedTest(name = "{displayName} : {0} km, {1} 원")
     @CsvSource(value = {"1,1250", "10,1250", "11,1350", "50,2050", "51,2150", "59,2250"})
-    void getBaseFare(Integer distance, Integer resultFare) {
+    void calculate(Integer distance, Integer resultFare) {
         FareStrategy fare = new DistanceFareStrategy();
         assertThat(fare.calculate(distance)).isEqualTo(resultFare);
     }
