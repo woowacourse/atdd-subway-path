1d5c71b59e7a15ae8f72e89787d5f2aa26ab5f8e
progress0407 <progress0407@gmail.com>
2022-05-19 19:26:36 +0900

    refactor(FareCalculator): 사용되지 않는 문장 제거

--- src/main/java/wooteco/subway/domain/FareCalculator.java
+++ src/main/java/wooteco/subway/domain/FareCalculator.java
@@ -16,22 +16,6 @@ public class FareCalculator {
     private static final int FIRST_FARE_INCREASE_STANDARD_UNIT = 5;
     private static final int LAST_FARE_INCREASE_STANDARD_UNIT = 8;
     private static final int INCREASE_RATE = 100;
-
-    public int calculateFare2(int distance) {
-        if (distance <= 0) {
-            throw new IllegalArgumentException();
-        }
-        if (distance <= FIRST_FARE_INCREASE_STANDARD) {
-            return BASIS_FARE;
-        }
-        if (distance <= LAST_FARE_INCREASE_STANDARD) {
-            return BASIS_FARE + INCREASE_RATE *
-                    (int) Math.ceil((double) (distance - FIRST_FARE_INCREASE_STANDARD) / FIRST_FARE_INCREASE_STANDARD_UNIT);
-        }
-        return BASIC_FARE_OVER_50KM + INCREASE_RATE *
-                (int) Math.ceil((double) (distance - LAST_FARE_INCREASE_STANDARD) / LAST_FARE_INCREASE_STANDARD_UNIT);
-    }
-
     public int calculateFare(int distance) {
         return findFareByDistance(distance);
     }
