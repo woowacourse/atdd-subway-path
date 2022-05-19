223e97add1fb90d7efc902718261206ab3af1637
ozragwort <ozragwort@gmail.com>
2022-05-19 14:25:15 +0900

    test: 거리에 따른 운임 요금을 게산하는 경계 값 테스트

--- src/test/java/wooteco/subway/domain/FareTest.java
+++ src/test/java/wooteco/subway/domain/FareTest.java
@@ -1,6 +1,6 @@
 package wooteco.subway.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,7 +10,7 @@ public class FareTest {
 
     @DisplayName("운임에 따른 요금을 반환한다.")
     @ParameterizedTest(name = "{displayName} : {arguments}")
-    @CsvSource(value = {"9,1250", "12,1350", "16,1450", "58,2150"})
+    @CsvSource(value = {"1,1250", "10,1250", "11,1350", "50,2050", "51,2150", "59,2250"})
     void getBaseFare(String distance, String resultFare) {
         Fare fare = new Fare(new DistanceFareStrategy());
         assertThat(fare.calculate(Integer.parseInt(distance))).isEqualTo(Integer.parseInt(resultFare));
