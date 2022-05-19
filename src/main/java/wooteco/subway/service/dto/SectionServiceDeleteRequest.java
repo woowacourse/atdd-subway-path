6323a91fc08db6de41eecaf585e5acb7c88aa2f9
beomWhale <cndqjacndqja@gmail.com>
2022-05-19 13:46:52 +0900

    refactor: 불필요한 validate annotation 제거

--- src/main/java/wooteco/subway/service/dto/SectionServiceDeleteRequest.java
+++ src/main/java/wooteco/subway/service/dto/SectionServiceDeleteRequest.java
@@ -1,12 +1,8 @@
 package wooteco.subway.service.dto;
 
-import javax.validation.constraints.NotNull;
-
 public class SectionServiceDeleteRequest {
 
-    @NotNull(message = "lineId를 입력해주세요.")
     private final Long lineId;
-    @NotNull(message = "stationId를 입력해주세요.")
     private final Long stationId;
 
     public SectionServiceDeleteRequest(Long lineId, Long stationId) {
