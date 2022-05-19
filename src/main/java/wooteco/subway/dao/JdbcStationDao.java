70a596f32ef2e3e484f0c319e5592bef7046a6e0
tonic523 <diddmlvkf@gmail.com>
2022-05-19 16:06:20 +0900

    refactor(JdbcStationDao): 커넥션 객체 변경
    - JdbcTemplate -> NamedParameterJdbcTemplate로 변경

--- src/main/java/wooteco/subway/dao/JdbcStationDao.java
+++ src/main/java/wooteco/subway/dao/JdbcStationDao.java
@@ -8,7 +8,10 @@ import javax.sql.DataSource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
@@ -20,13 +23,13 @@ import wooteco.subway.domain.Station;
 public class JdbcStationDao implements StationDao {
 
     private final SimpleJdbcInsert jdbcInsert;
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public JdbcStationDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+    public JdbcStationDao(DataSource dataSource) {
         this.jdbcInsert = new SimpleJdbcInsert(dataSource)
                 .withTableName("station")
                 .usingGeneratedKeyColumns("id");
-        this.jdbcTemplate = jdbcTemplate;
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Override
@@ -46,29 +49,33 @@ public class JdbcStationDao implements StationDao {
     @Override
     public List<Station> findAll() {
         final String sql = "SELECT * FROM station";
-        return jdbcTemplate.query(sql, (resultSet, rowNum) -> new Station(
-                resultSet.getLong("id"),
-                resultSet.getString("name")
-        ));
+        return jdbcTemplate.query(sql, rowMapper());
     }
 
     @Override
     public Optional<Station> findById(Long id) {
-        final String sql = "SELECT * FROM station WHERE id = ?";
+        final String sql = "SELECT * FROM station WHERE id = :id";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
         try {
-            return Optional.of(jdbcTemplate.queryForObject(sql, (resultSet, rowNum) -> new Station(
-                    resultSet.getLong("id"),
-                    resultSet.getString("name")
-            ), id));
+            return Optional.of(jdbcTemplate.queryForObject(sql, namedParameters, rowMapper()));
         } catch (DataAccessException e) {
             return Optional.empty();
         }
     }
 
+    private RowMapper<Station> rowMapper() {
+        return (rs, rowNum) -> {
+            final Long id = rs.getLong("id");
+            final String name = rs.getString("name");
+            return new Station(id, name);
+        };
+    }
+
     @Override
     public int deleteById(Long id) {
-        final String sql = "DELETE FROM station WHERE id = ?";
-        final int deletedCount = jdbcTemplate.update(sql, id);
+        final String sql = "DELETE FROM station WHERE id = :id";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
+        final int deletedCount = jdbcTemplate.update(sql, namedParameters);
         validateRemoved(deletedCount, id);
         return deletedCount;
     }
