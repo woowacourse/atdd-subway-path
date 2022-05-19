bb11de2010452dc8622022ba8067229d2757f0a5
jurlring <78091011+jurlring@users.noreply.github.com>
2022-05-19 20:32:49 +0900

    test: FareCalculator test 이름 수정 및 테스트 케이스 추가

--- src/test/java/wooteco/subway/domain/FareCalculatorTest.java
+++ src/test/java/wooteco/subway/domain/FareCalculatorTest.java
@@ -4,37 +4,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class FareCalculatorTest {
 
     @DisplayName("경로 거리가 10Km 이하면 1250이 부과된다.")
     @Test
     void calculateFare_basic() {
-        double distance = 11;
+        double distance = 10;
         FareCalculator fareCalculator = new FareCalculator(distance);
         int fare = fareCalculator.calculateFare();
 
-        assertThat(fare).isEqualTo(1350);
+        assertThat(fare).isEqualTo(1250);
     }
 
     @DisplayName("경로 거리가 10Km 초과이고 50Km 이하면 5km당 100원이 부과된다.")
-    @Test
-    void calculateFare_middle() {
-        double distance = 50;
+    @ParameterizedTest
+    @CsvSource({"11,1350", "15,1350", "16,1450", "50,2050"})
+    void calculateFare_middle(int distance, int fare) {
         FareCalculator fareCalculator = new FareCalculator(distance);
-        int fare = fareCalculator.calculateFare();
+        int actualFare = fareCalculator.calculateFare();
 
-        assertThat(fare).isEqualTo(2050);
+        assertThat(fare).isEqualTo(actualFare);
     }
 
     @DisplayName("경로 거리가 50Km 이상이면 8km당 100원이 부과된다.")
-    @Test
-    void calculateFare_high() {
-        double distance = 58;
+    @ParameterizedTest
+    @CsvSource({"51,2150", "58,2150", "59,2250"})
+    void calculateFare_high(int distance, int fare) {
         FareCalculator fareCalculator = new FareCalculator(distance);
-        int fare = fareCalculator.calculateFare();
+        int actualFare = fareCalculator.calculateFare();
 
-        assertThat(fare).isEqualTo(2150);
+        assertThat(actualFare).isEqualTo(fare);
     }
 
 }
