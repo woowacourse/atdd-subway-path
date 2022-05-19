0e99449c1b25267c6deefeed31c654cfc4551458
hyunrrr <whgusrms96@gmail.com>
2022-05-19 09:58:09 +0900

    refactor: 상수 네이밍 변경

--- src/main/java/wooteco/subway/domain/DistanceProportionalPricingStrategy.java
+++ src/main/java/wooteco/subway/domain/DistanceProportionalPricingStrategy.java
@@ -5,21 +5,21 @@ import java.util.List;
 public class DistanceProportionalPricingStrategy implements PricingStrategy {
 
     private static final int BASIC_FEE = 1250;
-    private static final int SECOND_FEE_CONDITION = 50;
-    private static final int SECOND_FEE_UNIT = 8;
-    private static final int FIRST_FEE_CONDITION = 10;
-    private static final int FIRST_FEE_UNIT = 5;
+    private static final int SECOND_CONDITION_START_DISTANCE = 50;
+    private static final int SECOND_CONDITION_DISTANCE_UNIT = 8;
+    private static final int FIRST_CONDITION_START_DISTANCE = 10;
+    private static final int FIRST_CONDITION_DISTANCE_UNIT = 5;
     private static final int UNIT_MONEY = 100;
 
     @Override
     public int calculateFee(List<Section> sections) {
         int distance = calculateDistance(sections);
-        if (distance > SECOND_FEE_CONDITION) {
-            return BASIC_FEE + calculateCost(distance - SECOND_FEE_CONDITION, SECOND_FEE_UNIT)
-                    + calculateCost(SECOND_FEE_CONDITION - FIRST_FEE_CONDITION, FIRST_FEE_UNIT);
+        if (distance > SECOND_CONDITION_START_DISTANCE) {
+            return BASIC_FEE + calculateCost(distance - SECOND_CONDITION_START_DISTANCE, SECOND_CONDITION_DISTANCE_UNIT)
+                    + calculateCost(SECOND_CONDITION_START_DISTANCE - FIRST_CONDITION_START_DISTANCE, FIRST_CONDITION_DISTANCE_UNIT);
         }
-        if (distance > FIRST_FEE_CONDITION) {
-            return BASIC_FEE + calculateCost(distance - FIRST_FEE_CONDITION, FIRST_FEE_UNIT);
+        if (distance > FIRST_CONDITION_START_DISTANCE) {
+            return BASIC_FEE + calculateCost(distance - FIRST_CONDITION_START_DISTANCE, FIRST_CONDITION_DISTANCE_UNIT);
         }
         return BASIC_FEE;
     }
