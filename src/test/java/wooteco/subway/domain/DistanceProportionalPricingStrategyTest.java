eac65918f2013ea022634b6d250cc0e09c908a95
hyunrrr <whgusrms96@gmail.com>
2022-05-19 12:42:12 +0900

    test: 경계값 테스트 케이스 추가

--- src/test/java/wooteco/subway/domain/DistanceProportionalPricingStrategyTest.java
+++ src/test/java/wooteco/subway/domain/DistanceProportionalPricingStrategyTest.java
@@ -11,7 +11,8 @@ public class DistanceProportionalPricingStrategyTest {
 
     @DisplayName("요금 계산하기")
     @ParameterizedTest(name = "{0} km -> 요금 {1}원 예상")
-    @CsvSource(value = {"5,1250", "44,1950", "60,2250"})
+    @CsvSource(value = {"5, 1250", "10, 1250", "11, 1350", "15, 1350", "16, 1450",
+            "50, 2050", "51, 2150", "58, 2150", "59, 2250"})
     void calculateScore2(int distance, int expected) {
         // given
         List<Section> sections = List.of(new Section(1L, 1L, 1L, 2L, distance));
