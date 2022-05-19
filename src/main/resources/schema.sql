4c2cbf14e7512f9ac672e6f485b3ff1f3229cbc7
ozragwort <ozragwort@gmail.com>
2022-05-19 14:49:28 +0900

    refactor: DB schema 수정
    - 노선 색 unique 설정
    - 구간의 노선 id, 상행 역 id, 하행 역 id

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,7 +9,7 @@ create table if not exists LINE
 (
     id    bigint auto_increment not null,
     name  varchar(255) not null unique,
-    color varchar(20)  not null,
+    color varchar(20)  not null unique,
     extra_fare int not null ,
     primary key (id)
 );
@@ -21,5 +21,8 @@ create table if not exists SECTION
     up_station_id   bigint not null,
     down_station_id bigint not null,
     distance        int,
-    primary key (id)
+    primary key (id),
+    FOREIGN KEY (line_id) REFERENCES LINE (id),
+    FOREIGN KEY (up_station_id) REFERENCES STATION (id),
+    FOREIGN KEY (down_station_id) REFERENCES STATION (id)
 );
