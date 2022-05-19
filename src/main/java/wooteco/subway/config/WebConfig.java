c47be5db3f7e9d9f8bfd415b3180fe442b26df5d
Richard JEON <ztzy1907@gmail.com>
2022-05-19 12:59:46 +0900

    refactor(web-config): WebConfig 클래스 config 패키지 내로 이동

--- src/main/java/wooteco/subway/config/WebConfig.java
+++ src/main/java/wooteco/subway/config/WebConfig.java
@@ -1,4 +1,4 @@
-package wooteco.subway;
+package wooteco.subway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
