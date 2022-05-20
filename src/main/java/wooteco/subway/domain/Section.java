fce57114627bdc19626e2ff7a7efd8ffe8c8bb82
yeon-06 <rnjstldus2@gmail.com>
2022-05-20 20:37:08 +0900

    refactor: 생성자의 검증 로직 위치 변경

--- src/main/java/wooteco/subway/domain/Section.java
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -14,17 +14,17 @@ public class Section {
     private Long downStationId;
     private int distance;
 
-    public Section(Long lineId, Long upStationId, Long downStationId, int distance) {
+    public Section(Long id, Long lineId, Long upStationId, Long downStationId, int distance) {
         validSection(upStationId, downStationId, distance);
+        this.id = id;
         this.lineId = lineId;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
     }
 
-    public Section(Long id, Long lineId, Long upStationId, Long downStationId, int distance) {
-        this(lineId, upStationId, downStationId, distance);
-        this.id = id;
+    public Section(Long lineId, Long upStationId, Long downStationId, int distance) {
+        this(null, lineId, upStationId, downStationId, distance);
     }
 
     private void validSection(Long upStationId, Long downStationId, int distance) {
