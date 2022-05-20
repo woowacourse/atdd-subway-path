34bd7397a2001a9416a7b0163416d605d85cf7bb
injoon2019 <ison@sfu.ca>
2022-05-20 15:21:25 +0900

    refactor: 추가 요금 변수명 분리

--- src/main/java/wooteco/subway/domain/strategy/fare/FareBasicStrategy.java
+++ src/main/java/wooteco/subway/domain/strategy/fare/FareBasicStrategy.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Component;
 public class FareBasicStrategy implements FareStrategy {
 
     private static final int BASIC_FARE = 1250;
-    private static final int SURCHARGE = 100;
+    private static final int FIRST_SURCHARGE = 100;
+    private static final int SECOND_SURCHARGE = 100;
 
     private static final int BASIC_DISTANCE = 10;
     private static final int INTERVAL_ONE = 50;
@@ -23,10 +24,10 @@ public class FareBasicStrategy implements FareStrategy {
     private int calculateIntervalOne(int distance) {
         return (int) Math.ceil(Math.min(
                 Math.max(distance - BASIC_DISTANCE, NO_FARE_DISTANCE), INTERVAL_ONE - BASIC_DISTANCE) / INTERVAL_ONE_SURCHARGE_UNIT)
-                * SURCHARGE;
+                * FIRST_SURCHARGE;
     }
 
     private int calculateIntervalTwo(int distance) {
-        return (int) Math.ceil(Math.max(distance - INTERVAL_ONE, NO_FARE_DISTANCE) / INTERVAL_TWO_SURCHARGE_UNIT) * SURCHARGE;
+        return (int) Math.ceil(Math.max(distance - INTERVAL_ONE, NO_FARE_DISTANCE) / INTERVAL_TWO_SURCHARGE_UNIT) * SECOND_SURCHARGE;
     }
 }
