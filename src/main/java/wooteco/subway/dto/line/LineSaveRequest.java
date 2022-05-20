91dde0d799248101d211101db5cfa47c83f6dbd9
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-20 16:44:04 +0900

    feat: LineSaveRequest 추가요금 필드 추가

--- src/main/java/wooteco/subway/dto/line/LineSaveRequest.java
+++ src/main/java/wooteco/subway/dto/line/LineSaveRequest.java
@@ -2,6 +2,7 @@ package wooteco.subway.dto.line;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import wooteco.subway.domain.Line;
 
 public class LineSaveRequest {
@@ -21,16 +22,20 @@ public class LineSaveRequest {
     @Positive(message = "상행-하행 노선 길이는 양수 값만 들어올 수 있습니다.")
     private int distance;
 
+    @PositiveOrZero(message = "추가요금은 음수가 들어올 수 없습니다.")
+    private int extraFare;
+
     private LineSaveRequest() {
     }
 
     public LineSaveRequest(final String name, final String color, final long upStationId, final long downStationId,
-                           final int distance) {
+                           final int distance, final int extraFare) {
         this.name = name;
         this.color = color;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
+        this.extraFare = extraFare;
     }
 
     public Line toLine() {
@@ -55,5 +60,9 @@ public class LineSaveRequest {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getExtraFare() {
+        return extraFare;
     }
 }
