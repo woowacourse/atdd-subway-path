a9980c113358ab35c98ef369c6f1c5864083e9e4
yeon-06 <rnjstldus2@gmail.com>
2022-05-20 16:41:19 +0900

    feat: 최단 경로를 거치는 노선 id 목록 찾는 메서드 구현

--- src/main/java/wooteco/subway/domain/ShortestPath.java
+++ src/main/java/wooteco/subway/domain/ShortestPath.java
@@ -3,6 +3,7 @@ package wooteco.subway.domain;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
@@ -16,27 +17,30 @@ public class ShortestPath {
     private static final int CHARGED_UNIT_1 = 5;
     private static final int CHARGED_UNIT_2 = 8;
 
-    private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
+    private final DijkstraShortestPath<Station, SectionEdge> dijkstraShortestPath;
 
     public ShortestPath(List<Station> stations, List<Section> sections) {
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        WeightedMultigraph<Station, SectionEdge> graph = new WeightedMultigraph<>(SectionEdge.class);
         setVertex(graph, stations);
         setEdgeWeight(graph, sections, stations);
         dijkstraShortestPath = new DijkstraShortestPath<>(graph);
     }
 
-    private void setVertex(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Station> stations) {
+    private void setVertex(WeightedMultigraph<Station, SectionEdge> graph, List<Station> stations) {
         stations.forEach(graph::addVertex);
     }
 
-    private void setEdgeWeight(WeightedMultigraph<Station, DefaultWeightedEdge> graph,
+    private void setEdgeWeight(WeightedMultigraph<Station, SectionEdge> graph,
                                List<Section> sections, List<Station> stations) {
         Map<Long, Station> sectionMap = initSectionMap(stations);
 
         for (Section section : sections) {
             Station downStation = sectionMap.get(section.getDownStationId());
             Station upStation = sectionMap.get(section.getUpStationId());
-            graph.setEdgeWeight(graph.addEdge(downStation, upStation), section.getDistance());
+            SectionEdge sectionEdge = new SectionEdge(section.getLineId(), section.getDistance());
+
+            graph.addEdge(downStation, upStation, sectionEdge);
+            graph.setEdgeWeight(sectionEdge, section.getDistance());
         }
     }
 
@@ -71,5 +75,32 @@ public class ShortestPath {
 
     private int calculateCost(int distance, int baseDistance, int unit) {
         return ((distance - baseDistance - 1) / unit + 1) * CHARGED_COST;
+    }
+
+    public List<Long> findLineIds(Station source, Station target) {
+        return dijkstraShortestPath.getPath(source, target).getEdgeList()
+                .stream()
+                .map(SectionEdge::getLineId)
+                .distinct()
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static class SectionEdge extends DefaultWeightedEdge {
+
+        private final Long lineId;
+        private final int distance;
+
+        public SectionEdge(Long lineId, int distance) {
+            this.lineId = lineId;
+            this.distance = distance;
+        }
+
+        public Long getLineId() {
+            return lineId;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
     }
 }
--- src/test/java/wooteco/subway/domain/ShortestPathTest.java
+++ src/test/java/wooteco/subway/domain/ShortestPathTest.java
@@ -10,18 +10,21 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class ShortestPathTest {
 
-    private final Station station1 = new Station(1L, "v1");
-    private final Station station2 = new Station(2L, "v2");
-    private final Station station3 = new Station(3L, "v3");
-    private final Station station4 = new Station(4L, "v4");
+    private static final Station station1 = new Station(1L, "v1");
+    private static final Station station2 = new Station(2L, "v2");
+    private static final Station station3 = new Station(3L, "v3");
+    private static final Station station4 = new Station(4L, "v4");
 
-    private final List<Station> stations = List.of(station1, station2, station3, station4);
+    private static final List<Station> stations = List.of(station1, station2, station3, station4);
 
-    private final List<Section> sections = List.of(
-            new Section(1L, 1L, station1.getId(), station4.getId(), 1),
-            new Section(2L, 2L, station1.getId(), station4.getId(), 2),
-            new Section(3L, 1L, station4.getId(), station2.getId(), 2),
-            new Section(4L, 1L, station2.getId(), station3.getId(), 2)
+    private static final Long lineId1 = 1L;
+    private static final Long lineId2 = 2L;
+
+    private static final List<Section> sections = List.of(
+            new Section(1L, lineId2, station1.getId(), station4.getId(), 1),
+            new Section(2L, lineId1, station1.getId(), station4.getId(), 2),
+            new Section(3L, lineId1, station4.getId(), station2.getId(), 2),
+            new Section(4L, lineId1, station2.getId(), station3.getId(), 2)
     );
 
     @DisplayName("최단 경로 찾아주는 객체를 초기화한다.")
@@ -65,5 +68,14 @@ class ShortestPathTest {
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("순회하는 노선 목록 찾기")
+    @Test
+    void findLines() {
+        ShortestPath shortestPath = new ShortestPath(stations, sections);
+        List<Long> ids = shortestPath.findLineIds(station1, station2);
+
+        assertThat(ids).contains(lineId1, lineId2);
     }
 }
