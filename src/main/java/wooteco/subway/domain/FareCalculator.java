4a5ecc38f523126704116b66130c99b523fae072
Hongdonggeon <ghd700@naver.com>
2022-05-20 18:56:33 +0900

    refactor: 매직넘버 상수화

--- src/main/java/wooteco/subway/domain/FareCalculator.java
+++ src/main/java/wooteco/subway/domain/FareCalculator.java
@@ -8,10 +8,11 @@ public class FareCalculator {
     private static final int SECOND_STANDARD = 50;
     private static final int FIRST_STANDARD_UNIT = 5;
     private static final int SECOND_STANDARD_UNIT = 8;
+    private static final int FIRST_STANDARD_MAXIMUM_COUNT = 8;
 
     public static int calculate(int distance) {
         if (distance > SECOND_STANDARD) {
-            return DEFAULT_FARE + SURCHARGE_PER_UNIT * 8 + (int) (
+            return DEFAULT_FARE + SURCHARGE_PER_UNIT * FIRST_STANDARD_MAXIMUM_COUNT + (int) (
                     ((Math.ceil((distance - SECOND_STANDARD) / (double) SECOND_STANDARD_UNIT))) * SURCHARGE_PER_UNIT);
         }
         if (distance > FIRST_STANDARD) {
