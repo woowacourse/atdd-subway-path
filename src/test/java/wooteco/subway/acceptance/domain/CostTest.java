ee840291215157ebce9aef5edcaf205fad82291e
EastHShin <xldk78@gmail.com>
2022-05-21 03:11:42 +0900

    refactor(CostTest): 경계값 위주의 테스트 케이스 추가

--- src/test/java/wooteco/subway/acceptance/domain/CostTest.java
+++ src/test/java/wooteco/subway/acceptance/domain/CostTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 import wooteco.subway.domain.Cost;
 
 class CostTest {
-    @DisplayName("거리를 전달받아 요금을 계산한다.")
+    @DisplayName("거리에 비례해 계산된 요금이 정확한지 확인한다.")
     @ParameterizedTest
-    @CsvSource(value = {"10,1250", "12,1350", "16,1450", "30,1650", "50,2050", "58,2150"})
+    @CsvSource(value = {"10,1250", "11,1350", "16,1450", "50,2050", "51,2150", "59, 2250"})
     void calculate_cost_with_distance(int distance, int expectedCost) {
         // when
         Cost createdCost = Cost.from(distance);
