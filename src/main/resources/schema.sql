54608887915087c6a5bd20dac832545e8e476b3a
Richard JEON <ztzy1907@gmail.com>
2022-05-20 13:19:20 +0900

    fix(schema): 각 테이블 PK 자료형 선언 변경. integer -> bigint

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,29 @@
-CREATE TABLE IF NOT EXISTS Line
-(
-    id    integer     not null auto_increment,
-    name  varchar(30) not null unique,
-    color varchar(30) not null unique,
-    primary key (id)
-);
-
 CREATE TABLE IF NOT EXISTS Station
 (
-    id   integer     not null auto_increment,
-    name varchar(30) not null unique,
+    id   bigint       not null auto_increment,
+    name varchar(255) not null unique,
     primary key (id)
 );
 
-create table if not exists SECTION
+CREATE TABLE IF NOT EXISTS Line
 (
-    id              bigint auto_increment not null,
-    line_id         bigint                not null,
-    up_station_id   bigint                not null,
-    down_station_id bigint                not null,
-    distance        int,
+    id    bigint       not null auto_increment,
+    name  varchar(255) not null unique,
+    color varchar(20)  not null unique,
     primary key (id)
 );
 
+CREATE TABLE IF NOT EXISTS Section
+(
+    id              bigint not null auto_increment,
+    line_id         bigint not null,
+    up_station_id   bigint not null,
+    down_station_id bigint not null,
+    distance        int,
+    primary key (id),
+    constraint section_line_id_fk foreign key (line_id) references LINE (id)
+        on update cascade on delete cascade
+);
 
 insert into station(name)
 values ('선릉역');
