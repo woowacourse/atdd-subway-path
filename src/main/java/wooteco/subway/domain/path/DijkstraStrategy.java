7c0c98e299b8f86aaf1e60e221a4cf382b4dc19e
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-05-21 15:50:23 +0900

    refactor(DijkstraStrategy): findShortestDijkstraPath null 예외처리 추가

--- src/main/java/wooteco/subway/domain/path/DijkstraStrategy.java
+++ src/main/java/wooteco/subway/domain/path/DijkstraStrategy.java
@@ -16,7 +16,12 @@ public class DijkstraStrategy implements PathFindingStrategy {
         return findShortestDijkstraPath(graph, source, target).getVertexList();
     }
 
-    private GraphPath<Long, DefaultWeightedEdge> findShortestDijkstraPath(Graph<Long, DefaultWeightedEdge> graph, Long source, Long target){
-        return DijkstraShortestPath.findPathBetween(graph, source, target);
+    private GraphPath<Long, DefaultWeightedEdge> findShortestDijkstraPath(Graph<Long, DefaultWeightedEdge> graph, Long source, Long target) {
+        GraphPath<Long, DefaultWeightedEdge> graphPath = DijkstraShortestPath.findPathBetween(graph, source, target);
+
+        if (graphPath == null) {
+            throw new IllegalArgumentException("최단 경로를 찾을 수 없습니다.");
+        }
+        return graphPath;
     }
 }
