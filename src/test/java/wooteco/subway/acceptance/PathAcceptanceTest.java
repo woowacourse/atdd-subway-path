06ff40fd152dd1413578f5231b1cf5e15f31abf8
hyeonic <dev.hyeonic@gmail.com>
2022-05-22 00:01:36 +0900

    test: PathAcceptanceTest 직관적인 변수명으로 수정

--- src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
@@ -7,39 +7,39 @@ import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.SectionRequest;
+import wooteco.subway.dto.StationRequest;
 
 @DisplayName("경로 관련 기능")
 public class PathAcceptanceTest extends AcceptanceTest {
 
-    @DisplayName("경로 조회")
+    @DisplayName("경로 조회 관련 기능을 확인한다.")
     @TestFactory
     Stream<DynamicTest> dynamicTestFromPath() {
-        Long 건대입구역 = generateStationId("건대입구역");
-        Long 강남구청역 = generateStationId("강남구청역");
-        Long 대림역 = generateStationId("대림역");
-        Long 낙성대역 = generateStationId("낙성대역");
+        Long 건대입구역Id = generateStationId("건대입구역");
+        Long 강남구청역Id = generateStationId("강남구청역");
+        Long 대림역Id = generateStationId("대림역");
+        Long 낙성대역Id = generateStationId("낙성대역");
 
-        Long line7 = generateLineId("7호선", "deep green", 건대입구역, 강남구청역, 10);
-        addSection(line7, 강남구청역, 대림역, 10);
+        Long line7 = generateLineId("7호선", "deep green", 건대입구역Id, 강남구청역Id, 10);
+        addSection(line7, 강남구청역Id, 대림역Id, 10);
 
-        Long line2 = generateLineId("2호선", "green", 건대입구역, 낙성대역, 5);
-        addSection(line2, 낙성대역, 대림역, 5);
+        Long line2 = generateLineId("2호선", "green", 건대입구역Id, 낙성대역Id, 5);
+        addSection(line2, 낙성대역Id, 대림역Id, 5);
 
         return Stream.of(
                 dynamicTest("서로 다른 노선을 포함한 경로를 조회한다.", () -> {
                     ExtractableResponse<Response> response = RestAssured.given().log().all()
                             .when()
                             .get("/paths?source={sourceId}&target={targetId}&age={age}",
-                                    건대입구역, 대림역, 24)
+                                    건대입구역Id, 대림역Id, 24)
                             .then().log().all()
                             .extract();
 
@@ -53,7 +53,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
                     ExtractableResponse<Response> response = RestAssured.given().log().all()
                             .when()
                             .get("/paths?source={sourceId}&target={targetId}&age={age}",
-                                    강남구청역, 낙성대역, 24)
+                                    강남구청역Id, 낙성대역Id, 24)
                             .then().log().all()
                             .extract();
 
@@ -67,7 +67,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
                     ExtractableResponse<Response> response = RestAssured.given().log().all()
                             .when()
                             .get("/paths?source={sourceId}&target={targetId}&age={age}",
-                                    강남구청역, 강남구청역, 24)
+                                    강남구청역Id, 강남구청역Id, 24)
                             .then().log().all()
                             .extract();
 
@@ -78,15 +78,15 @@ public class PathAcceptanceTest extends AcceptanceTest {
                 }),
 
                 dynamicTest("다른 노선의 갈 수 없는 경로를 조회한다.", () -> {
-                    Long 부천역 = generateStationId("부천역");
-                    Long 중동역 = generateStationId("중동역");
+                    Long 부천역Id = generateStationId("부천역");
+                    Long 중동역Id = generateStationId("중동역");
 
-                    generateLineId("1호선", "blue", 부천역, 중동역, 10);
+                    generateLineId("1호선", "blue", 부천역Id, 중동역Id, 10);
 
                     ExtractableResponse<Response> response = RestAssured.given().log().all()
                             .when()
                             .get("/paths?source={sourceId}&target={targetId}&age={age}",
-                                    부천역, 대림역, 24)
+                                    부천역Id, 대림역Id, 24)
                             .then().log().all()
                             .extract();
 
@@ -96,11 +96,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
     }
 
     private Long generateStationId(String name) {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", name);
-
         ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .body(params)
+                .body(new StationRequest(name))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .post("/stations")
@@ -110,17 +107,9 @@ public class PathAcceptanceTest extends AcceptanceTest {
         return response.jsonPath().getLong("id");
     }
 
-    private Long generateLineId(String name, String color, Long upStationId,
-                                Long downStationId, Integer distance) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("name", name);
-        params.put("color", color);
-        params.put("upStationId", upStationId);
-        params.put("downStationId", downStationId);
-        params.put("distance", distance);
-
+    private Long generateLineId(String name, String color, Long upStationId, Long downStationId, Integer distance) {
         ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .body(params)
+                .body(new LineRequest(name, color, upStationId, downStationId, distance))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .post("/lines")
