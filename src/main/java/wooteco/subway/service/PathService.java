43ecf6ee83841a2147816e1e01786d0cc429e5fb
kth990303 <kth990303@naver.com>
2022-05-21 17:09:32 +0900

    feat: 요금정책 추가 기능 구현

--- src/main/java/wooteco/subway/service/PathService.java
+++ src/main/java/wooteco/subway/service/PathService.java
@@ -2,39 +2,45 @@ package wooteco.subway.service;
 
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import wooteco.subway.dao.LineDao;
 import wooteco.subway.dao.SectionDao;
 import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Fare;
+import wooteco.subway.domain.Line;
 import wooteco.subway.domain.Section;
 import wooteco.subway.domain.Station;
 import wooteco.subway.dto.PathResponse;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 @Transactional(readOnly = true)
 @Service
 public class PathService {
     private final StationDao stationDao;
+    private final LineDao lineDao;
     private final SectionDao sectionDao;
 
-    public PathService(StationDao stationDao, SectionDao sectionDao) {
+    public PathService(StationDao stationDao, LineDao lineDao, SectionDao sectionDao) {
         this.stationDao = stationDao;
+        this.lineDao = lineDao;
         this.sectionDao = sectionDao;
     }
 
-    public PathResponse findShortestPath(Long upStationId, Long downStationId) {
+    public PathResponse findShortestPath(Long upStationId, Long downStationId, int age) {
         validateNotSameStations(upStationId, downStationId);
-        final GraphPath<Station, DefaultWeightedEdge> graphPath = findGraphPath(upStationId, downStationId);
+        final GraphPath<Station, ShortestPathEdge> graphPath = findGraphPath(upStationId, downStationId);
         validatePathExist(graphPath);
 
         final List<Station> stations = graphPath.getVertexList();
         final int shortestDistance = (int) graphPath.getWeight();
-        final Fare fare = Fare.from(shortestDistance);
+        final int extraFare = findMaximumExtraFare(graphPath);
+        final Fare fare = Fare.of(shortestDistance, extraFare, age);
         return new PathResponse(stations, shortestDistance, fare.getValue());
     }
 
@@ -44,8 +50,8 @@ public class PathService {
         }
     }
 
-    private GraphPath<Station, DefaultWeightedEdge> findGraphPath(Long upStationId, Long downStationId) {
-        final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath =
+    private GraphPath<Station, ShortestPathEdge> findGraphPath(Long upStationId, Long downStationId) {
+        final DijkstraShortestPath<Station, ShortestPathEdge> dijkstraShortestPath =
                 new DijkstraShortestPath<>(initSubwayMap());
 
         final Station upStation = findStation(upStationId);
@@ -53,38 +59,50 @@ public class PathService {
         return dijkstraShortestPath.getPath(upStation, downStation);
     }
 
-    private void validatePathExist(GraphPath<Station, DefaultWeightedEdge> graphPath) {
+    private void validatePathExist(GraphPath<Station, ShortestPathEdge> graphPath) {
         if (graphPath == null) {
             throw new IllegalArgumentException("해당 역 사이 경로가 존재하지 않습니다.");
         }
     }
 
-    private WeightedMultigraph<Station, DefaultWeightedEdge> initSubwayMap() {
-        final WeightedMultigraph<Station, DefaultWeightedEdge> graph
-                = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+    private WeightedMultigraph<Station, ShortestPathEdge> initSubwayMap() {
+        final WeightedMultigraph<Station, ShortestPathEdge> graph
+                = new WeightedMultigraph<>(ShortestPathEdge.class);
         addAllStations(graph);
         addAllSections(graph);
         return graph;
     }
 
-    private void addAllStations(WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+    private void addAllStations(WeightedMultigraph<Station, ShortestPathEdge> graph) {
         final List<Station> stations = stationDao.findAll();
         for (Station station : stations) {
             graph.addVertex(station);
         }
     }
 
-    private void addAllSections(WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+    private void addAllSections(WeightedMultigraph<Station, ShortestPathEdge> graph) {
         final List<Section> sections = sectionDao.findAll();
         for (Section section : sections) {
             final Station upStation = findStation(section.getUpStationId());
             final Station downStation = findStation(section.getDownStationId());
-            graph.setEdgeWeight(graph.addEdge(upStation, downStation), section.getDistance());
+            final Long lineId = section.getLineId();
+            final int distance = section.getDistance();
+            graph.addEdge(upStation, downStation, new ShortestPathEdge(lineId, distance));
         }
     }
 
     private Station findStation(Long id) {
         return stationDao.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 역이 존재하지 않습니다."));
+    }
+
+    private int findMaximumExtraFare(GraphPath<Station, ShortestPathEdge> graphPath) {
+        return graphPath.getEdgeList().stream()
+                .map(ShortestPathEdge::getLineId)
+                .map(lineId -> lineDao.findById(lineId)
+                        .orElseThrow(() -> new NoSuchElementException("경로 라인을 찾는 과정 중 오류가 발생했습니다.")))
+                .max(Comparator.comparingInt(Line::getExtraFare))
+                .orElseThrow(() -> new NoSuchElementException("추가 요금을 찾는 과정 중 오류가 발생했습니다."))
+                .getExtraFare();
     }
 }
--- src/test/java/wooteco/subway/service/PathServiceTest.java
+++ src/test/java/wooteco/subway/service/PathServiceTest.java
@@ -3,9 +3,12 @@ package wooteco.subway.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import wooteco.subway.domain.Line;
 import wooteco.subway.domain.Section;
 import wooteco.subway.domain.Station;
+import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.PathResponse;
+import wooteco.subway.service.fakeDao.LineDaoImpl;
 import wooteco.subway.service.fakeDao.SectionDaoImpl;
 import wooteco.subway.service.fakeDao.StationDaoImpl;
 
@@ -16,11 +19,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PathServiceTest {
     private final PathService pathService
-            = new PathService(StationDaoImpl.getInstance(), SectionDaoImpl.getInstance());
+            = new PathService(StationDaoImpl.getInstance(), LineDaoImpl.getInstance(), SectionDaoImpl.getInstance());
 
-    private Station station1;
-    private Station station2;
-    private Station station3;
+    private Station 애플역;
+    private Station 갤럭시역;
+    private Station 옵티머스역;
+    private Long 애플역_id;
+    private Long 갤럭시역_id;
+    private Long 옵티머스역_id;
+
+    private LineRequest 국내선;
+    private LineRequest 해외경유선;
 
     private Section 애플_갤럭시;
     private Section 갤럭시_옵티머스;
@@ -29,29 +38,23 @@ public class PathServiceTest {
     void setUp() {
         final List<Station> stations = StationDaoImpl.getInstance().findAll();
         stations.clear();
+        final List<Line> lines = LineDaoImpl.getInstance().findAll();
+        lines.clear();
         final List<Section> sections = SectionDaoImpl.getInstance().findAll();
         sections.clear();
 
-        station1 = new Station("애플역");
-        station2 = new Station("갤럭시역");
-        station3 = new Station("옵티머스역");
+        initStations();
     }
 
     @Test
     @DisplayName("역 사이 최단 경로를 구한다.")
     void calculateDistance() {
         // given
-        final Long station1_id = StationDaoImpl.getInstance().save(station1);
-        final Long station2_id = StationDaoImpl.getInstance().save(station2);
-        final Long station3_id = StationDaoImpl.getInstance().save(station3);
-        애플_갤럭시 = new Section(station1_id, station2_id, 10);
-        갤럭시_옵티머스 = new Section(station2_id, station3_id, 20);
-        SectionDaoImpl.getInstance().save(애플_갤럭시);
-        SectionDaoImpl.getInstance().save(갤럭시_옵티머스);
+        initLineAndSectionWithNotExtraFare();
         final PathResponse expected =
-                new PathResponse(List.of(station1, station2, station3), 30, 1650);
+                new PathResponse(List.of(애플역, 갤럭시역, 옵티머스역), 30, 1650);
         // when
-        PathResponse actual = pathService.findShortestPath(station1_id, station3_id);
+        PathResponse actual = pathService.findShortestPath(애플역_id, 옵티머스역_id, 20);
 
         //then
         assertThat(actual)
@@ -63,14 +66,14 @@ public class PathServiceTest {
     @DisplayName("역 사이 경로가 존재하지 않을 때 예외를 발생시킨다.")
     void calculatePathNotExist() {
         // given
-        final Long station1_id = StationDaoImpl.getInstance().save(station1);
-        final Long station2_id = StationDaoImpl.getInstance().save(station2);
-        final Long station3_id = StationDaoImpl.getInstance().save(station3);
-        애플_갤럭시 = new Section(station1_id, station2_id, 100);
+        해외경유선 = new LineRequest("해외경유선", "blue", 애플역_id, 갤럭시역_id, 15, 400);
+        LineDaoImpl.getInstance().save(해외경유선);
+
+        애플_갤럭시 = new Section(애플역_id, 갤럭시역_id, 100);
         SectionDaoImpl.getInstance().save(애플_갤럭시);
 
         //when then
-        assertThatThrownBy(() -> pathService.findShortestPath(station1_id, station3_id))
+        assertThatThrownBy(() -> pathService.findShortestPath(애플역_id, 옵티머스역_id, 20))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 역 사이 경로가 존재하지 않습니다.");
     }
@@ -78,13 +81,92 @@ public class PathServiceTest {
     @Test
     @DisplayName("요청에 해당 역이 존재하지 않을 때 예외를 발생시킨다.")
     void stationNotExistByRequest() {
-        // given
-        final Long station1_id = StationDaoImpl.getInstance().save(station1);
-        final Long station2_id = StationDaoImpl.getInstance().save(station2);
-
-        //when then
-        assertThatThrownBy(() -> pathService.findShortestPath(station1_id, station2_id + 1L))
+        // when then
+        assertThatThrownBy(() -> pathService.findShortestPath(애플역_id, 갤럭시역_id + 1L, 20))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("해당 역이 존재하지 않습니다.");
+                .hasMessage("해당 역 사이 경로가 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("역 사이 최단 경로의 요금을 올바르게 계산한다.")
+    void calculateFareByShortestPath() {
+        // given
+        initLineAndSectionWithExtraFare();
+        final PathResponse expected =
+                new PathResponse(List.of(애플역, 갤럭시역, 옵티머스역), 25, 2150);
+        // when
+        PathResponse actual = pathService.findShortestPath(애플역_id, 옵티머스역_id, 19);
+
+        //then
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("어린이가 이용할 경우의 역 사이 최단 경로 요금을 올바르게 계산한다.")
+    void calculateChildrenFareByShortestPath() {
+        // given
+        initLineAndSectionWithExtraFare();
+        final PathResponse expected =
+                new PathResponse(List.of(애플역, 갤럭시역, 옵티머스역), 25, 1250);
+        // when
+        PathResponse actual = pathService.findShortestPath(애플역_id, 옵티머스역_id, 6);
+
+        //then
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+
+    @Test
+    @DisplayName("청소년이 이용할 경우의 역 사이 최단 경로 요금을 올바르게 계산한다.")
+    void calculateTeenagerFareByShortestPath() {
+        // given
+        initLineAndSectionWithExtraFare();
+        final PathResponse expected =
+                new PathResponse(List.of(애플역, 갤럭시역, 옵티머스역), 25, 1790);
+        // when
+        PathResponse actual = pathService.findShortestPath(애플역_id, 옵티머스역_id, 18);
+
+        //then
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    private void initLineAndSectionWithNotExtraFare() {
+        해외경유선 = new LineRequest("해외경유선", "blue", 애플역_id, 갤럭시역_id, 10, 0);
+        Long 해외경유선_id = LineDaoImpl.getInstance().save(해외경유선);
+        국내선 = new LineRequest("국내선", "red", 갤럭시역_id, 옵티머스역_id, 20, 0);
+        Long 국내선_id = LineDaoImpl.getInstance().save(국내선);
+
+        애플_갤럭시 = new Section(해외경유선_id, 애플역_id, 갤럭시역_id, 10);
+        SectionDaoImpl.getInstance().save(애플_갤럭시);
+        갤럭시_옵티머스 = new Section(국내선_id, 갤럭시역_id, 옵티머스역_id, 20);
+        SectionDaoImpl.getInstance().save(갤럭시_옵티머스);
+    }
+
+    private void initLineAndSectionWithExtraFare() {
+        해외경유선 = new LineRequest("해외경유선", "blue", 애플역_id, 갤럭시역_id, 15, 400);
+        Long 해외경유선_id = LineDaoImpl.getInstance().save(해외경유선);
+        국내선 = new LineRequest("국내선", "red", 갤럭시역_id, 옵티머스역_id, 10, 600);
+        Long 국내선_id = LineDaoImpl.getInstance().save(국내선);
+
+        애플_갤럭시 = new Section(해외경유선_id, 애플역_id, 갤럭시역_id, 15);
+        SectionDaoImpl.getInstance().save(애플_갤럭시);
+        갤럭시_옵티머스 = new Section(국내선_id, 갤럭시역_id, 옵티머스역_id, 10);
+        SectionDaoImpl.getInstance().save(갤럭시_옵티머스);
+    }
+
+    private void initStations() {
+        애플역 = new Station("애플역");
+        갤럭시역 = new Station("갤럭시역");
+        옵티머스역 = new Station("옵티머스역");
+
+        애플역_id = StationDaoImpl.getInstance().save(애플역);
+        갤럭시역_id = StationDaoImpl.getInstance().save(갤럭시역);
+        옵티머스역_id = StationDaoImpl.getInstance().save(옵티머스역);
     }
 }
