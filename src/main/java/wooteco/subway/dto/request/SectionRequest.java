e3711093cbec92dfdf38d35c4a589608308fdecf
skullKim <kyk990328@gmail.com>
2022-05-22 04:21:36 +0900

    test(SectionControllerTest.java): 섹션 컨트롤러 테스트 추가
    섹션 관련 요청에 포함된 데이터 유효 여부와 그에 따른 응답에 대한 테스트
    추가

--- src/main/java/wooteco/subway/dto/request/SectionRequest.java
+++ src/main/java/wooteco/subway/dto/request/SectionRequest.java
@@ -1,9 +1,18 @@
 package wooteco.subway.dto.request;
 
+import javax.validation.constraints.Min;
+
 public class SectionRequest {
 
+    private static final String NUMBER_MIN_RANGE_ERROR = " 1 이상이여야 합니다.";
+
+    @Min(value = 1, message = "상행 종점 아이디는" + NUMBER_MIN_RANGE_ERROR)
     private Long upStationId;
+
+    @Min(value = 1, message = "하행 종점 아이디는" + NUMBER_MIN_RANGE_ERROR)
     private Long downStationId;
+
+    @Min(value = 1, message = "구간 거리는" + NUMBER_MIN_RANGE_ERROR)
     private int distance;
 
     private SectionRequest() {
--- src/main/java/wooteco/subway/ui/SectionController.java
+++ src/main/java/wooteco/subway/ui/SectionController.java
@@ -1,7 +1,10 @@
 package wooteco.subway.ui;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +14,10 @@ import wooteco.subway.dto.request.SectionRequest;
 import wooteco.subway.service.SectionService;
 
 @Controller
+@Validated
 public class SectionController {
+
+    private static final String NUMBER_MIN_RANGE_ERROR = " 1 이상이여야 합니다.";
 
     private final SectionService sectionService;
 
@@ -20,13 +26,18 @@ public class SectionController {
     }
 
     @PostMapping("/lines/{lineId}/sections")
-    public ResponseEntity<Void> createSection(@PathVariable final Long lineId, @RequestBody final SectionRequest request) {
+    public ResponseEntity<Void> createSection(@PathVariable @Min(value = 1, message = "라인 아이디는" + NUMBER_MIN_RANGE_ERROR)
+                                                  final Long lineId,
+                                              @RequestBody @Valid final SectionRequest request) {
         sectionService.saveSection(lineId, request);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/lines/{lineId}/sections")
-    public ResponseEntity<Void> deleteSection(@PathVariable final Long lineId, @RequestParam final Long stationId) {
+    public ResponseEntity<Void> deleteSection(@PathVariable @Min(value = 1, message = "라인 아이디는" + NUMBER_MIN_RANGE_ERROR)
+                                                  final Long lineId,
+                                              @RequestParam @Min(value = 1, message = "정거장 아이디는" + NUMBER_MIN_RANGE_ERROR)
+                                              final Long stationId) {
         sectionService.deleteSection(lineId, stationId);
         return ResponseEntity.ok().build();
     }
