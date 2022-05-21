af7957025dd09bbfa0c79f77b4ef6d99664954d2
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-21 21:02:58 +0900

    feat(domain): add validation for line extra fare value
    - throw IllegalArgumentException if extra fare is under 0

--- src/main/java/wooteco/subway/domain/line/LineInfo.java
+++ src/main/java/wooteco/subway/domain/line/LineInfo.java
@@ -4,12 +4,16 @@ import java.util.Objects;
 
 public class LineInfo {
 
+    private static final int MIN_EXTRA_FARE = 0;
+    private static final String INVALID_EXTRA_FARE_VALUE_EXCEPTION = "노선 추가비용은 0원 이상이어야 합니다.";
+
     private final Long id;
     private final String name;
     private final String color;
     private final int extraFare;
 
     public LineInfo(Long id, String name, String color, int extraFare) {
+        validateExtraFare(extraFare);
         this.id = id;
         this.name = name;
         this.color = color;
@@ -18,6 +22,12 @@ public class LineInfo {
 
     public LineInfo(String name, String color, int extraFare) {
         this(null, name, color, extraFare);
+    }
+
+    private void validateExtraFare(int extraFare) {
+        if (extraFare < MIN_EXTRA_FARE) {
+            throw new IllegalArgumentException(INVALID_EXTRA_FARE_VALUE_EXCEPTION);
+        }
     }
 
     public Long getId() {
