e2122218287bfff9b4b9d08ba1b8393416599d23
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-21 21:18:30 +0900

    test(ui): add missing tests on extra fare validation
    - fix wrong tests

--- src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -24,13 +24,15 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Nested
     class CreateLineTest {
 
+        private static final String PATH = "/lines";
+
         @Test
         void 성공시_201_CREATED() {
             testFixtureManager.saveStations("강남역", "선릉역");
             Map<String, Object> params = jsonLineOf("신분당선", "bg-red-600",
                     900, 1L, 2L, 10);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/lines", params);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, PATH, params);
             LineResponse actualBody = extractSingleLineResponseBody(response);
             LineResponse expectedBody = new LineResponse(1L, "신분당선", "bg-red-600", 900,
                     List.of(new StationResponse(1L, "강남역"), new StationResponse(2L, "선릉역")));
@@ -44,7 +46,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         void 정보가_담기지_않은_경우_400_BAD_REQUEST() {
             Map<String, String> emptyParams = new HashMap<>();
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/stations", emptyParams);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, PATH, emptyParams);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         }
@@ -53,7 +55,18 @@ public class LineAcceptanceTest extends AcceptanceTest {
         void 정보가_공백으로_구성된_경우_400_BAD_REQUEST() {
             Map<String, Object> blankParams = jsonLineOf("   ", "  ", 100, 1L, 2L, 10);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/stations", blankParams);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, PATH, blankParams);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        @Test
+        void 거리_정보가_0이하인_경우_400_BAD_REQUEST() {
+            testFixtureManager.saveStations("강남역", "선릉역");
+            Map<String, Object> params = jsonLineOf("신분당선", "bg-red-600",
+                    900, 1L, 2L, 0);
+
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, PATH, params);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         }
@@ -64,7 +77,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
             Map<String, Object> duplicateNameParams = jsonLineOf("중복되는 노선명", "bg-red-600", 100, 1L, 2L, 10);
 
             HttpUtils.send(HttpMethod.POST, "/lines", duplicateNameParams);
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/lines", duplicateNameParams);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, PATH, duplicateNameParams);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         }
@@ -73,15 +86,33 @@ public class LineAcceptanceTest extends AcceptanceTest {
         void 존재하지_않는_지하철역을_종점으로_입력하면_404_NOT_FOUND() {
             Map<String, Object> params = jsonLineOf("노선명", "색상", 100, 9999L, 2L, 10);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/lines", params);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, PATH, params);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        }
+
+        private HashMap<String, Object> jsonLineOf(String name,
+                                                   String color,
+                                                   int extraFare,
+                                                   Long upStationId,
+                                                   Long downStationId,
+                                                   int distance) {
+            return new HashMap<>() {{
+                put("name", name);
+                put("color", color);
+                put("extraFare", extraFare);
+                put("upStationId", upStationId);
+                put("downStationId", downStationId);
+                put("distance", distance);
+            }};
         }
     }
 
     @DisplayName("GET /lines - 지하철 노선 목록 조회 테스트")
     @Nested
     class ShowLinesTest {
+
+        private static final String PATH = "/lines";
 
         private final StationResponse STATION_1 = new StationResponse(1L, "강남역");
         private final StationResponse STATION_2 = new StationResponse(2L, "선릉역");
@@ -96,7 +127,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
             testFixtureManager.saveSection(1L, 1L, 2L);
             testFixtureManager.saveSection(2L, 1L, 3L);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.GET, "/lines");
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.GET, PATH);
             List<LineResponse> actualBody = extractJsonBody(response);
             List<LineResponse> expectedBody = List.of(
                     new LineResponse(1L, "1호선", "노란색", 1000, List.of(STATION_1, STATION_2, STATION_3)),
@@ -122,7 +153,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
             testFixtureManager.saveSection(1L, 1L, 2L);
             testFixtureManager.saveSection(1L, 3L, 1L);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.GET, "/lines/1");
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.GET, toPath(1L));
             LineResponse actualBody = extractSingleLineResponseBody(response);
             LineResponse expectedBody = new LineResponse(1L, "1호선", "노란색", 1000,
                     List.of(new StationResponse(3L, "잠실역"),
@@ -135,9 +166,13 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         @Test
         void 존재하지_않는_노선인_경우_404_NOT_FOUND() {
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.GET, "/lines/99999");
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.GET, toPath(99999L));
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        }
+
+        private String toPath(Long id) {
+            return String.format("/lines/%d", id);
         }
     }
 
@@ -150,7 +185,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
             testFixtureManager.saveLine("신분당선", "노란색", 0);
             Map<String, Object> params = jsonLineOf("NEW 분당선", "bg-red-800", 900);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, "/lines/1", params);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, toPath(1L), params);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         }
@@ -159,7 +194,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         void 수정하려는_지하철_노선이_존재하지_않는_경우_404_NOT_FOUND() {
             Map<String, Object> params = jsonLineOf("NEW 분당선", "bg-red-600", 100);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, "/lines/9999", params);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, toPath(9999L), params);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
         }
@@ -170,7 +205,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
             testFixtureManager.saveLine("존재하는 노선명", "노란색", 1000);
             Map<String, Object> duplicateNameParams = jsonLineOf("존재하는 노선명", "bg-red-600", 100);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, "/lines/2", duplicateNameParams);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, toPath(2L), duplicateNameParams);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         }
@@ -179,7 +214,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         void 이름_혹은_색상_정보가_담기지_않은_경우_400_BAD_REQUEST() {
             Map<String, Object> emptyParams = new HashMap<>();
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/stations", emptyParams);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, toPath(1L), emptyParams);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         }
@@ -188,9 +223,32 @@ public class LineAcceptanceTest extends AcceptanceTest {
         void 이름_혹은_색상_정보가_공백으로_구성된_경우_400_BAD_REQUEST() {
             Map<String, Object> blankParams = jsonLineOf("   ", "  ", 1000);
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/stations", blankParams);
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, toPath(1L), blankParams);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        @Test
+        void 추가비용을_0미만으로_수정하려는_경우_400_BAD_REQUEST() {
+            Map<String, Object> negativeExtraFareParams = jsonLineOf("분당선", "bg-red-800", -1);
+
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, toPath(1L), negativeExtraFareParams);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        private String toPath(Long id) {
+            return String.format("/lines/%d", id);
+        }
+
+        private HashMap<String, Object> jsonLineOf(String name,
+                                                   String color,
+                                                   int extraFare) {
+            return new HashMap<>() {{
+                put("name", name);
+                put("color", color);
+                put("extraFare", extraFare);
+            }};
         }
     }
 
@@ -202,43 +260,21 @@ public class LineAcceptanceTest extends AcceptanceTest {
         void 성공시_204_OK() {
             testFixtureManager.saveLine("존재하는 노선", "노란색");
 
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.DELETE, "/lines/1");
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.DELETE, toPath(1L));
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
         }
 
         @Test
         void 삭제하려는_지하철_노선이_존재하지_않는_경우_404_NOT_FOUND() {
-            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.DELETE, "/lines/99999");
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.DELETE, toPath(99999L));
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
         }
-    }
 
-    private HashMap<String, Object> jsonLineOf(String name,
-                                               String color,
-                                               int extraFare) {
-        return new HashMap<>() {{
-            put("name", name);
-            put("color", color);
-            put("extraFare", extraFare);
-        }};
-    }
-
-    private HashMap<String, Object> jsonLineOf(String name,
-                                               String color,
-                                               int extraFare,
-                                               Long upStationId,
-                                               Long downStationId,
-                                               int distance) {
-        return new HashMap<>() {{
-            put("name", name);
-            put("color", color);
-            put("extraFare", extraFare);
-            put("upStationId", upStationId);
-            put("downStationId", downStationId);
-            put("distance", distance);
-        }};
+        private String toPath(Long id) {
+            return String.format("/lines/%d", id);
+        }
     }
 
     private LineResponse extractSingleLineResponseBody(ExtractableResponse<Response> response) {
