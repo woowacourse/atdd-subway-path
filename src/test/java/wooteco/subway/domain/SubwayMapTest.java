b9c01ffd3efca0b1af903f581493e41c0f5420f0
sure-why-not <pkse1213@gmail.com>
2022-05-21 14:49:40 +0900

    refactor: 테스트 데이터 선언 및 초기화 방법 변경

--- src/test/java/wooteco/subway/domain/SubwayMapTest.java
+++ src/test/java/wooteco/subway/domain/SubwayMapTest.java
@@ -37,28 +37,25 @@ class SubwayMapTest {
         geumho = new Station(8L, "금호역");
         oksu = new Station(9L, "옥수역");
 
-        Line greenLine = null;
-        final Section greenSectionA = new Section(greenLine, gangnam, yeoksam, new Distance(10));
-        final Section greenSectionB = new Section(greenLine, yeoksam, seolleung, new Distance(7));
-        final Section greenSectionC = new Section(greenLine, seolleung, samsung, new Distance(11));
-        greenLine = new Line(1L, new Name("2호선"), "green", new Sections(List.of(
+        final Section greenSectionA = new Section(null, gangnam, yeoksam, new Distance(10));
+        final Section greenSectionB = new Section(null, yeoksam, seolleung, new Distance(7));
+        final Section greenSectionC = new Section(null, seolleung, samsung, new Distance(11));
+        final Line greenLine = new Line(1L, new Name("2호선"), "green", new Sections(List.of(
                 greenSectionA,
                 greenSectionB,
                 greenSectionC
         )));
 
-        Line yellowLine = null;
-        final Section yellowSectionA = new Section(yellowLine, seolleung, seoulForest, new Distance(3));
-        final Section yellowSectionB = new Section(yellowLine, seoulForest, wangsimni, new Distance(8));
-        yellowLine = new Line(2L, new Name("수인분당선"), "yellow", new Sections(List.of(
+        final Section yellowSectionA = new Section(null, seolleung, seoulForest, new Distance(3));
+        final Section yellowSectionB = new Section(null, seoulForest, wangsimni, new Distance(8));
+        final Line yellowLine = new Line(2L, new Name("수인분당선"), "yellow", new Sections(List.of(
                 yellowSectionA,
                 yellowSectionB
         )));
 
-        Line orangeLine = null;
-        final Section orangeSectionA = new Section(orangeLine, yacksu, geumho, new Distance(12));
-        final Section orangeSectionB = new Section(orangeLine, geumho, oksu, new Distance(6));
-        orangeLine = new Line(2L, new Name("3호선"), "orange", new Sections(List.of(
+        final Section orangeSectionA = new Section(null, yacksu, geumho, new Distance(12));
+        final Section orangeSectionB = new Section(null, geumho, oksu, new Distance(6));
+        final Line orangeLine = new Line(2L, new Name("3호선"), "orange", new Sections(List.of(
                 orangeSectionA,
                 orangeSectionB
         )));
