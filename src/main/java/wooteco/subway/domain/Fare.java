ca6755cc6bb7c404b48e684733dc97f51adfedb4
DWL21 <nggus5@naver.com>
2022-05-21 20:41:33 +0900

    test: 요금 계산에 관한 테스트 추가와 예외 사항 처리

--- src/main/java/wooteco/subway/domain/Fare.java
+++ src/main/java/wooteco/subway/domain/Fare.java
@@ -9,11 +9,13 @@ public class Fare {
     private static final int SECOND_CHARGING_UNITS = 5;
     private static final int THIRD_CHARGING_UNITS = 8;
     private static final int UNIT_FARES = 100;
+    private static final int MINIMUM_DISTANCE = 0;
 
     private Fare() {
     }
 
     public static int calculateFare(int distance) {
+        validateZeroOrPositiveDistance(distance);
         if (distance <= BASIC_DISTANCE) {
             return BASIC_FARES;
         }
@@ -21,6 +23,12 @@ public class Fare {
             return BASIC_FARES + calculateOverFare(distance - BASIC_DISTANCE, SECOND_CHARGING_UNITS);
         }
         return BASIC_FARES + SECOND_SECTION_FARES + calculateOverFare(distance - SECOND_DISTANCE, THIRD_CHARGING_UNITS);
+    }
+
+    private static void validateZeroOrPositiveDistance(int distance) {
+        if (distance <= MINIMUM_DISTANCE) {
+            throw new IllegalArgumentException("거리는 음수일 수 없습니다.");
+        }
     }
 
     private static int calculateOverFare(int distance, int farePerKilometre) {
--- src/test/java/wooteco/subway/domain/FareTest.java
+++ src/test/java/wooteco/subway/domain/FareTest.java
@@ -1,6 +1,7 @@
 package wooteco.subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,32 +9,71 @@ import org.junit.jupiter.api.Test;
 public class FareTest {
 
     @Test
-    @DisplayName("9km = 1250원. 기본운임(10㎞ 이내): 기본운임 1,250원")
-    void calculate1() {
+    @DisplayName("1km = 1250원. 기본운임(10㎞ 이내): 기본운임 1,250원")
+    void calculate_1Km() {
         // when
-        int result = Fare.calculateFare(9);
+        int result = Fare.calculateFare(1);
 
         // then
         assertThat(result).isEqualTo(1250);
     }
 
     @Test
-    @DisplayName("12km = 10km + 2km = 1350원. 10km~50km: 5km 까지 마다 100원 추가")
-    void calculate2() {
+    @DisplayName("10km = 1250원. 기본운임(10㎞ 이내): 기본운임 1,250원")
+    void calculate_10Km() {
         // when
-        int result = Fare.calculateFare(12);
+        int result = Fare.calculateFare(10);
+
+        // then
+        assertThat(result).isEqualTo(1250);
+    }
+
+    @Test
+    @DisplayName("15km = 10km + 5km = 1350원. 10km~50km: 5km 까지 마다 100원 추가")
+    void calculate_15km() {
+        // when
+        int result = Fare.calculateFare(15);
 
         // then
         assertThat(result).isEqualTo(1350);
     }
 
     @Test
-    @DisplayName("58km = 10km + 40km + 8km = 2150원. 50km 초과: 8km 까지 마다 100원 추가")
-    void calculate3() {
+    @DisplayName("20km = 10km + 5km * 2 = 2150원. 10km~50km: 5km 까지 마다 100원 추가 50km 초과: 8km 까지 마다 100원 추가")
+    void calculate_20km() {
+        // when
+        int result = Fare.calculateFare(20);
+
+        // then
+        assertThat(result).isEqualTo(1450);
+    }
+
+    @Test
+    @DisplayName("50km = 10km + 5km * 8 = 2150원. 50km 초과: 8km 까지 마다 100원 추가")
+    void calculate_50km() {
+        // when
+        int result = Fare.calculateFare(50);
+
+        // then
+        assertThat(result).isEqualTo(2050);
+    }
+
+    @Test
+    @DisplayName("58km = 10km + 5km * 8 + 8km = 2250원. 50km 초과: 8km 까지 마다 100원 추가")
+    void calculate_58km() {
         // when
         int result = Fare.calculateFare(58);
 
         // then
         assertThat(result).isEqualTo(2150);
+    }
+
+    @Test
+    @DisplayName("거리가 0 미만일 경우 예외를 발생시킨다.")
+    void calculate_exception() {
+        assertThatThrownBy(() ->
+                Fare.calculateFare(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("거리는 음수일 수 없습니다.");
     }
 }
