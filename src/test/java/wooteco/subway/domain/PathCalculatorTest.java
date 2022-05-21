370c34d719bf797b3a6f40d3ac7742f382c159a0
tonic523 <diddmlvkf@gmail.com>
2022-05-21 13:19:23 +0900

    refactor(PathCalculatorTest): 공통된 테스트 픽스쳐를 멤버변수로 추출

--- src/test/java/wooteco/subway/domain/PathCalculatorTest.java
+++ src/test/java/wooteco/subway/domain/PathCalculatorTest.java
@@ -9,18 +9,18 @@ import org.junit.jupiter.api.Test;
 
 public class PathCalculatorTest {
 
+    private Station 강남 = new Station(1L, "강남");
+    private Station 역삼 = new Station(2L, "역삼");
+    private Station 선릉 = new Station(3L, "선릉");
+    private List<Section> sections = List.of(
+            new Section(강남, 역삼, 10),
+            new Section(역삼, 선릉, 10),
+            new Section(선릉, 강남, 300)
+    );
+
     @Test
     @DisplayName("주어진 구간으로 최단 경로를 구한다.")
     void calculatePath() {
-        Station 강남 = new Station(1L, "강남");
-        Station 역삼 = new Station(2L, "역삼");
-        Station 선릉 = new Station(3L, "선릉");
-        List<Section> sections = List.of(
-                new Section(강남, 역삼, 10),
-                new Section(역삼, 선릉, 10),
-                new Section(선릉, 강남, 300)
-        );
-
         PathCalculator pathCalculator = PathCalculator.from(sections);
         List<Station> stations = pathCalculator.calculateShortestPath(선릉, 강남);
 
@@ -30,15 +30,6 @@ public class PathCalculatorTest {
     @Test
     @DisplayName("주어진 구간으로 최단 경로의 거리를 구한다.")
     void calculateDistance() {
-        Station 강남 = new Station(1L, "강남");
-        Station 역삼 = new Station(2L, "역삼");
-        Station 선릉 = new Station(3L, "선릉");
-        List<Section> sections = List.of(
-                new Section(강남, 역삼, 10),
-                new Section(역삼, 선릉, 10),
-                new Section(선릉, 강남, 300)
-        );
-
         PathCalculator pathCalculator = PathCalculator.from(sections);
         double distance = pathCalculator.calculateShortestDistance(선릉, 강남);
 
