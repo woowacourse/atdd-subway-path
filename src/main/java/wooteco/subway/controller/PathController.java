0ccbb3edd430f276c6f081a9838c6e41259335ba
Seongwon97 <osw970325@naver.com>
2022-05-22 00:54:15 +0900

    style: 불필요한 Import 제거

--- src/main/java/wooteco/subway/controller/PathController.java
+++ src/main/java/wooteco/subway/controller/PathController.java
@@ -1,7 +1,10 @@
 package wooteco.subway.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.dto.PathFindRequest;
 import wooteco.subway.dto.PathFindResponse;
 import wooteco.subway.service.PathService;
