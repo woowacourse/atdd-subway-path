f63e2be5bcc4dc1142487eeb486bacbe49c668fb
yeon-06 <rnjstldus2@gmail.com>
2022-05-21 14:10:56 +0900

    style: 상수 이름 변경

--- src/main/java/wooteco/subway/domain/CostCalculator.java
+++ src/main/java/wooteco/subway/domain/CostCalculator.java
@@ -3,11 +3,11 @@ package wooteco.subway.domain;
 public class CostCalculator {
 
     private static final int BASIC_COST = 1250;
-    private static final int CHARGED_COST = 100;
-    private static final int CHARGED_DISTANCE_1 = 10;
-    private static final int CHARGED_DISTANCE_2 = 50;
-    private static final int CHARGED_UNIT_1 = 5;
-    private static final int CHARGED_UNIT_2 = 8;
+    private static final int EXTRA_COST = 100;
+    private static final int DISTANCE_1 = 10;
+    private static final int DISTANCE_2 = 50;
+    private static final int UNIT_1 = 5;
+    private static final int UNIT_2 = 8;
 
     private final int distance;
     private final int extraFare;
@@ -25,17 +25,17 @@ public class CostCalculator {
         int cost = BASIC_COST;
         int distance = this.distance;
 
-        if (distance > CHARGED_DISTANCE_2) {
-            cost += calculateCost(distance, CHARGED_DISTANCE_2, CHARGED_UNIT_2);
-            distance = CHARGED_DISTANCE_2;
+        if (distance > DISTANCE_2) {
+            cost += calculateCost(distance, DISTANCE_2, UNIT_2);
+            distance = DISTANCE_2;
         }
-        if (distance > CHARGED_DISTANCE_1) {
-            cost += calculateCost(distance, CHARGED_DISTANCE_1, CHARGED_UNIT_1);
+        if (distance > DISTANCE_1) {
+            cost += calculateCost(distance, DISTANCE_1, UNIT_1);
         }
         return cost;
     }
 
-    private int calculateCost(int distance, int baseDistance, int unit) {
-        return ((distance - baseDistance - 1) / unit + 1) * CHARGED_COST;
+    private int calculateCost(int distance, int paidDistance, int unit) {
+        return ((distance - paidDistance - 1) / unit + 1) * EXTRA_COST;
     }
 }
