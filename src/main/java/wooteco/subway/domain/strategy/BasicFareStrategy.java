062e113046e0d9dbe20cc3643af81ea6d39e684f
her0807 <her0807@naver.com>
2022-05-21 21:20:37 +0900

    refactor: BasicFareStrategy 전략 비지니스 로직 가독성 개선

--- src/main/java/wooteco/subway/domain/strategy/BasicFareStrategy.java
+++ src/main/java/wooteco/subway/domain/strategy/BasicFareStrategy.java
@@ -2,28 +2,36 @@ package wooteco.subway.domain.strategy;
 
 public class BasicFareStrategy implements FareStrategy {
 
-    private static final int BASE_DISTANCE = 10;
     private static final int MIN_EXTRA_FARE = 0;
-    private static final int FIRST_ROLE_STANDARD_DISTANCE = 40;
     private static final double FIRST_ROLE_EXTRA_FARE_STANDARD = 5;
     private static final double SECOND_ROLE_EXTRA_FARE_STANDARD = 8;
+    private static final int BASE_DISTANCE = 10;
+    private static final int FIRST_ROLE_STANDARD_DISTANCE = 40;
     private static final int FARE_UNIT = 100;
 
     @Override
     public int calculate(int distance) {
         int overDistance = distance - BASE_DISTANCE;
         if (overDistance >= MIN_EXTRA_FARE) {
-            return calculateOverFare(overDistance);
+            return calculateExtraFare(overDistance);
         }
         return MIN_EXTRA_FARE;
     }
 
-    private int calculateOverFare(int distance) {
+    private int calculateExtraFare(int distance) {
         if (distance > FIRST_ROLE_STANDARD_DISTANCE) {
-            return (int) (Math.ceil((FIRST_ROLE_STANDARD_DISTANCE) / FIRST_ROLE_EXTRA_FARE_STANDARD)) * FARE_UNIT
-                    + (int) (Math.ceil((distance - FIRST_ROLE_STANDARD_DISTANCE) / SECOND_ROLE_EXTRA_FARE_STANDARD))
-                    * FARE_UNIT;
+            return calculateExtraFareWithFirstRole(FIRST_ROLE_STANDARD_DISTANCE)
+                    + calculateExtraFateWithSecondRole(distance);
         }
-        return (int) (Math.ceil((distance) / FIRST_ROLE_EXTRA_FARE_STANDARD)) * FARE_UNIT;
+        return calculateExtraFareWithFirstRole(distance);
+    }
+
+    private int calculateExtraFareWithFirstRole(int distance) {
+        return (int) (Math.ceil(distance / FIRST_ROLE_EXTRA_FARE_STANDARD)) * FARE_UNIT;
+    }
+
+    private int calculateExtraFateWithSecondRole(int distance) {
+        return (int) (Math.ceil((distance - FIRST_ROLE_STANDARD_DISTANCE) / SECOND_ROLE_EXTRA_FARE_STANDARD))
+                * FARE_UNIT;
     }
 }
