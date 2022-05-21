e6783360ff29b31556430100ef1110eeefb3a6ce
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-21 17:40:57 +0900

    feat(domain): implement Path2 with jgrapht library
    - add tests

---
+++ src/main/java/wooteco/subway/domain/path/Path2.java
@@ -0,0 +1,150 @@
+package wooteco.subway.domain.path;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import wooteco.subway.domain.path.fare.Fare;
+import wooteco.subway.domain.section.Section;
+import wooteco.subway.domain.station.Station;
+
+public class Path2 {
+
+    private static final String NOT_REGISTERED_STATION_EXCEPTION = "노선에 등록되지 않은 역을 입력하였습니다.";
+    private static final String SELF_LOOP_EXCEPTION = "출발점과 도착점은 동일할 수 없습니다.";
+    private static final String PATH_NOT_CONNECTED_EXCEPTION = "해당 역으로 이동하는 경로는 존재하지 않습니다.";
+
+    private final GraphPath<Station, SectionEdge> value;
+
+    private Path2(GraphPath<Station, SectionEdge> value) {
+        this.value = value;
+    }
+
+    public static Path2 of(Station source, Station target, List<Section> sections) {
+        validateNonSelfLoop(source, target);
+        validateRegisteredStations(source, target, sections);
+        DijkstraShortestPath<Station, SectionEdge> shortestPath = new DijkstraShortestPath<>(toGraph(sections));
+        return new Path2(findPath(source, target, shortestPath));
+    }
+
+    private static void validateNonSelfLoop(Station source, Station target) {
+        if (source.equals(target)) {
+            throw new IllegalArgumentException(SELF_LOOP_EXCEPTION);
+        }
+    }
+
+    private static void validateRegisteredStations(Station source, Station target, List<Section> sections) {
+        boolean sourceNotRegistered = sections.stream()
+                .noneMatch(station -> station.hasStationOf(source));
+        boolean targetNotRegistered = sections.stream()
+                .noneMatch(station -> station.hasStationOf(target));
+        if (sourceNotRegistered || targetNotRegistered) {
+            throw new IllegalArgumentException(NOT_REGISTERED_STATION_EXCEPTION);
+        }
+    }
+
+    private static WeightedMultigraph<Station, SectionEdge> toGraph(List<Section> sections) {
+        WeightedMultigraph<Station, SectionEdge> graph = new WeightedMultigraph<>(SectionEdge.class);
+        for (Station vertex : toVertices(sections)) {
+            graph.addVertex(vertex);
+        }
+        for (SectionEdge edge : toEdges(sections)) {
+            graph.addEdge(edge.getSourceVertex(), edge.getTargetVertex(), edge);
+        }
+        return graph;
+    }
+
+    private static List<Station> toVertices(List<Section> sections) {
+        List<Station> vertices = new ArrayList<>();
+        for (Section section : sections) {
+            vertices.addAll(section.toStations());
+        }
+        return vertices;
+    }
+
+    private static List<SectionEdge> toEdges(List<Section> sections) {
+        return sections.stream()
+                .map(SectionEdge::of)
+                .collect(Collectors.toList());
+    }
+
+    private static GraphPath<Station, SectionEdge> findPath(Station source,
+                                                            Station target,
+                                                            DijkstraShortestPath<Station, SectionEdge> shortestPath) {
+        GraphPath<Station, SectionEdge>  path = shortestPath.getPath(source, target);
+        if (path == null) {
+            throw new IllegalArgumentException(PATH_NOT_CONNECTED_EXCEPTION);
+        }
+        return path;
+    }
+
+    public List<Station> toStations() {
+        return value.getVertexList();
+    }
+
+    public int calculateDistance() {
+        return (int) value.getWeight();
+    }
+
+    public int calculateFare() {
+        return Fare.of((int) value.getWeight()).getValue();
+    }
+
+    public List<Long> getRegisteredLineIds() {
+        return value.getEdgeList().stream().map(SectionEdge::toDomain)
+                .map(Section::getLineId)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private static class SectionEdge extends DefaultWeightedEdge {
+
+        final Section section;
+
+        private SectionEdge(Section section) {
+            this.section = section;
+        }
+
+        private static SectionEdge of(Section section) {
+            return new SectionEdge(section);
+        }
+
+        Station getSourceVertex() {
+            return section.getUpStation();
+        }
+
+        Station getTargetVertex() {
+            return section.getDownStation();
+        }
+
+        @Override
+        protected double getWeight() {
+            return section.getDistance();
+        }
+
+        public Section toDomain() {
+            return section;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            SectionEdge that = (SectionEdge) o;
+            return Objects.equals(section, that.section);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(section);
+        }
+    }
+}
---
+++ src/test/java/wooteco/subway/domain/path/Path2Test.java
@@ -0,0 +1,103 @@
+package wooteco.subway.domain.path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import wooteco.subway.domain.section.Section;
+import wooteco.subway.domain.station.Station;
+
+@SuppressWarnings("NonAsciiCharacters")
+class Path2Test {
+
+    private final Station STATION1 = new Station(1L, "역1");
+    private final Station STATION2 = new Station(2L, "역2");
+    private final Station STATION3 = new Station(3L, "역3");
+    private final Station STATION4 = new Station(4L, "역4");
+    private final Station STATION5 = new Station(5L, "역5");
+    private final Station STATION6 = new Station(6L, "역6");
+
+    @Test
+    void 인접한_두_역_사이가_최단거리인_경우_해당_경로를_그대로_조회() {
+        List<Section> sections = List.of(
+                new Section(STATION1, STATION2, 100),
+                new Section(STATION2, STATION3, 1),
+                new Section(STATION2, STATION4, 100),
+                new Section(STATION4, STATION5, 100),
+                new Section(STATION2, STATION5, 100),
+                new Section(STATION5, STATION3, 100),
+                new Section(STATION3, STATION6, 100));
+
+        Path2 path = Path2.of(STATION2, STATION3, sections);
+
+        assertThat(path.toStations()).containsExactly(STATION2, STATION3);
+        assertThat(path.calculateDistance()).isEqualTo(1);
+    }
+
+    @Test
+    void 인접한_두_역_사이가_최단거리가_아닌_경우_최적의_경로를_계산하여_조회() {
+        List<Section> sections = List.of(
+                new Section(STATION1, STATION2, 1),
+                new Section(STATION2, STATION3, 100),
+                new Section(STATION2, STATION4, 2),
+                new Section(STATION4, STATION5, 3),
+                new Section(STATION2, STATION5, 100),
+                new Section(STATION5, STATION3, 5),
+                new Section(STATION3, STATION6, 6));
+
+        Path2 path = Path2.of(STATION2, STATION3, sections);
+
+        assertThat(path.toStations()).containsExactly(STATION2, STATION4, STATION5, STATION3);
+        assertThat(path.calculateDistance()).isEqualTo(10);
+    }
+
+    @Test
+    void 출발점과_도착점이_반대가_되면_지하철역들의_순서만_반대로_반환() {
+        List<Section> sections = List.of(
+                new Section(STATION1, STATION2, 1),
+                new Section(STATION2, STATION3, 100),
+                new Section(STATION2, STATION4, 2),
+                new Section(STATION4, STATION5, 3),
+                new Section(STATION2, STATION5, 100),
+                new Section(STATION5, STATION3, 5),
+                new Section(STATION3, STATION6, 6));
+
+        Path2 path1 = Path2.of(STATION2, STATION3, sections);
+        Path2 path2 = Path2.of(STATION3, STATION2, sections);
+
+        assertThat(path1.toStations()).containsExactly(STATION2, STATION4, STATION5, STATION3);
+        assertThat(path2.toStations()).containsExactly(STATION3, STATION5, STATION4, STATION2);
+        assertThat(path1.calculateDistance()).isEqualTo(path2.calculateDistance());
+    }
+
+    @Test
+    void 노선에_구간으로_등록되지_않은_역에_대한_경로를_조회하려는_경우_예외_발생() {
+        Station nonRegisteredStation = new Station(999L, "등록되지 않은 역");
+        List<Section> sections = List.of(
+                new Section(STATION1, STATION2, 10),
+                new Section(STATION2, STATION3, 100),
+                new Section(STATION3, STATION4, 20));
+
+        assertThatThrownBy(() -> Path2.of(STATION1, nonRegisteredStation, sections))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 출발점과_도착점이_동일한_경우_예외_발생() {
+        List<Section> sections = List.of(new Section(STATION1, STATION2, 10));
+
+        assertThatThrownBy(() -> Path2.of(STATION1, STATION1, sections))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 도달할_수_없는_경로를_조회하려는_경우_예외_발생() {
+        List<Section> sections = List.of(
+                new Section(STATION1, STATION2, 10),
+                new Section(STATION3, STATION4, 20));
+
+        assertThatThrownBy(() -> Path2.of(STATION1, STATION3, sections))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
