e868a848055d0552e507d1a1835e265cb50153a5
meatsby <meatsby@gmail.com>
2022-05-21 18:44:25 +0900

    refactor: controller 레이어 line 응답 dto 기본 생성자 접근제어자 변경

--- src/main/java/wooteco/subway/ui/dto/LineResponse.java
+++ src/main/java/wooteco/subway/ui/dto/LineResponse.java
@@ -10,7 +10,7 @@ public class LineResponse {
     private String color;
     private List<StationResponse> stations;
 
-    public LineResponse() {
+    private LineResponse() {
     }
 
     public LineResponse(LineServiceResponse lineServiceResponse) {
