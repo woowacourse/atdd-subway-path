890578a80e5de95b668fc3127c361490640138a6
yh20studio(tiki) <yh20studio@gmail.com>
2022-05-22 00:14:25 +0900

    fix: 상행종점 또는 하행좀점을 새로 등록하는 경우 구간의 거리가 잘못 계산되는 에러 해결

--- src/main/java/wooteco/subway/domain/LineSections.java
+++ src/main/java/wooteco/subway/domain/LineSections.java
@@ -98,36 +98,36 @@ public class LineSections {
     public List<Section> findOverlapSection(long upStationId, long downStationId, int distance) {
         if (existByUpStationId(upStationId)) {
             Section section = findById(findIdByUpStationId(upStationId));
-            return separateSectionInExistUpMatchCase(upStationId, downStationId, distance, section);
+            return separateSectionInExistUpMatchCase(upStationId, downStationId, distance, section, section.getDistance() - distance);
         }
         if (existByDownStationId(downStationId)) {
             Section section = findById(findIdByDownStationId(downStationId));
-            return separateSectionInExistDownMatchCase(upStationId, downStationId, distance, section);
+            return separateSectionInExistDownMatchCase(upStationId, downStationId, distance, section, section.getDistance() - distance);
         }
         if (existByUpStationId(downStationId)) {
             Section section = findById(findIdByUpStationId(downStationId));
-            return separateSectionInExistUpMatchCase(upStationId, downStationId, distance, section);
+            return separateSectionInExistUpMatchCase(upStationId, downStationId, distance, section, section.getDistance());
         }
         Section section = findById(findIdByDownStationId(upStationId));
-        return separateSectionInExistDownMatchCase(upStationId, downStationId, distance, section);
+        return separateSectionInExistDownMatchCase(upStationId, downStationId, distance, section, section.getDistance());
     }
 
     private List<Section> separateSectionInExistUpMatchCase(
-            long upStationId, long downStationId, int distance, Section section) {
+            long upStationId, long downStationId, int distance, Section section, int backSectionDistance) {
         Section upSection = new Section(section.getId(), section.getLineId(), upStationId, downStationId,
-                section.getDistance() - distance, section.getLineOrder());
+                distance, section.getLineOrder());
         Section downSection = new Section(section.getId(), section.getLineId(), downStationId,
-                section.getDownStationId(), section.getDistance() - distance, section.getLineOrder() + 1);
+                section.getDownStationId(), backSectionDistance, section.getLineOrder() + 1);
 
         return List.of(upSection, downSection);
     }
 
     private List<Section> separateSectionInExistDownMatchCase(
-            long upStationId, long downStationId, int distance, Section section) {
+            long upStationId, long downStationId, int distance, Section section, int frontSectionDistance) {
         Section upSection = new Section(section.getId(), section.getLineId(), section.getUpStationId(),
-                upStationId, section.getDistance() - distance, section.getLineOrder());
+                upStationId, frontSectionDistance, section.getLineOrder());
         Section downSection = new Section(section.getId(), section.getLineId(), upStationId, downStationId,
-                section.getDistance() - distance, section.getLineOrder() + 1);
+                distance, section.getLineOrder() + 1);
 
         return List.of(upSection, downSection);
     }
