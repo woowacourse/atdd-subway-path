a9a8a3ddaa00a3808bee9ba7a9f0023d970019a4
kth990303 <kth990303@naver.com>
2022-05-21 16:23:59 +0900

    feat: `jgrapht`의 `DefaultWeightedEdge`를 상속받아 `Line`의 정보를 담도록 구현

---
+++ src/main/java/wooteco/subway/service/ShortestPathEdge.java
@@ -0,0 +1,22 @@
+package wooteco.subway.service;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+public class ShortestPathEdge extends DefaultWeightedEdge {
+    private final Long lineId;
+    private final int distance;
+
+    public ShortestPathEdge(Long lineId, int distance) {
+        this.lineId = lineId;
+        this.distance = distance;
+    }
+
+    public Long getLineId() {
+        return lineId;
+    }
+
+    @Override
+    protected double getWeight() {
+        return distance;
+    }
+}
