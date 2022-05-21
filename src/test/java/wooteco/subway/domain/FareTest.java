b4716814a2ad1540e9b046b00a9d21dd51f2b965
hyeonic <dev.hyeonic@gmail.com>
2022-05-21 18:02:32 +0900

    test: FareTest 테스트 중복 제거 및 엣지 케이스 추가

--- src/test/java/wooteco/subway/domain/FareTest.java
+++ src/test/java/wooteco/subway/domain/FareTest.java
@@ -3,32 +3,17 @@ package wooteco.subway.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FareTest {
 
-    @DisplayName("길이가 10km 이내인 경우 기본운임 1,250을 반환한다.")
-    @Test
-    void basicFare() {
-        Fare fare = new Fare(9);
+    @DisplayName("길이가 주어지면 요금을 계산하여 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"9,1250", "10,1250", "14,1350", "15,1350", "50,2050", "58,2150", "59,2250"})
+    void 요금_계산(int distance, int expected) {
+        Fare fare = new Fare(distance);
 
-        assertThat(fare.calculateFare()).isEqualTo(1250);
-    }
-
-
-    @DisplayName("10km ~ 50km 인 경우 5km 까지 마다 100원씩 추가된다.")
-    @Test
-    void additionalFareBetween10To50() {
-        Fare fare = new Fare(12);
-
-        assertThat(fare.calculateFare()).isEqualTo(1350);
-    }
-
-    @DisplayName("50km 초과인 경우 8km 까지 마다 100원씩 추가된다.")
-    @Test
-    void additionalFareOver50() {
-        Fare fare = new Fare(58);
-
-        assertThat(fare.calculateFare()).isEqualTo(2150);
+        assertThat(fare.calculateFare()).isEqualTo(expected);
     }
 }
