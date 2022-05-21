10d11e8d288912c2e9ee70d485fcf39eac87fa38
hyunrrr <whgusrms96@gmail.com>
2022-05-21 18:10:45 +0900

    fix: 테스트 한번에 돌릴때 깨지는 버그 수정

--- src/test/java/wooteco/subway/acceptance/AcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/AcceptanceTest.java
@@ -4,8 +4,11 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"classpath:schema.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 public class AcceptanceTest {
     @LocalServerPort
     int port;
