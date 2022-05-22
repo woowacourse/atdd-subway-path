e0b9703505af3fb627ca4f17dc4a74a17bef05bf
her0807 <her0807@naver.com>
2022-05-22 23:19:12 +0900

    refactor: Repository 내 Dao 를 의존하도록 변경

--- src/main/java/wooteco/subway/reopository/SectionRepository.java
+++ src/main/java/wooteco/subway/reopository/SectionRepository.java
@@ -8,21 +8,24 @@ import wooteco.subway.domain.Line;
 import wooteco.subway.domain.Section;
 import wooteco.subway.domain.Station;
 import wooteco.subway.exception.NotFoundException;
+import wooteco.subway.reopository.dao.LineDao;
 import wooteco.subway.reopository.dao.SectionDao;
+import wooteco.subway.reopository.dao.StationDao;
+import wooteco.subway.reopository.entity.LineEntity;
 import wooteco.subway.reopository.entity.SectionEntity;
+import wooteco.subway.reopository.entity.StationEntity;
 
 @Repository
 public class SectionRepository {
 
     private final SectionDao sectionDao;
-    private final StationRepository stationRepository;
-    private final LineRepository lineRepository;
+    private final StationDao stationDao;
+    private final LineDao lineDao;
 
-    public SectionRepository(SectionDao sectionDao, StationRepository stationRepository,
-                             LineRepository lineRepository) {
+    public SectionRepository(SectionDao sectionDao, StationDao stationDao, LineDao lineDao) {
         this.sectionDao = sectionDao;
-        this.stationRepository = stationRepository;
-        this.lineRepository = lineRepository;
+        this.stationDao = stationDao;
+        this.lineDao = lineDao;
     }
 
     public Long save(Section section) {
@@ -50,12 +53,21 @@ public class SectionRepository {
     }
 
     private Section toSection(SectionEntity entity) {
-        Line line = lineRepository.findById(entity.getLineId())
-                .orElseThrow(() -> new IllegalArgumentException("조회하려는 노선이 없습니다."));
-        Station upStation = stationRepository
-                .findById(entity.getUpStationId()).orElseThrow(() -> new NotFoundException("조회 하려는 상행역이 없습니다."));
-        Station downStation = stationRepository
-                .findById(entity.getDownStationId()).orElseThrow(() -> new NotFoundException("조회 하려는 하행역이 없습니다."));
+        Line line = findLine(entity);
+        Station upStation = findStation(entity.getUpStationId(), "조회 하려는 상행역이 없습니다.");
+        Station downStation = findStation(entity.getDownStationId(), "조회 하려는 하행역이 없습니다.");
         return new Section(entity.getId(), line, upStation, downStation, entity.getDistance());
+    }
+
+    private Station findStation(Long id, String message) {
+        StationEntity stationEntity = stationDao
+                .findById(id).orElseThrow(() -> new NotFoundException(message));
+        return new Station(stationEntity.getId(), stationEntity.getName());
+    }
+
+    private Line findLine(SectionEntity entity) {
+        LineEntity lineEntity = lineDao.findById(entity.getLineId())
+                .orElseThrow(() -> new IllegalArgumentException("조회하려는 노선이 없습니다."));
+        return new Line(lineEntity.getId(), lineEntity.getName(), lineEntity.getColor());
     }
 }
