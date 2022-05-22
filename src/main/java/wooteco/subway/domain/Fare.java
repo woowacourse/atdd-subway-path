fa878c01343e21e254cdbd028225874d3a5d1836
hyeonic <dev.hyeonic@gmail.com>
2022-05-22 13:11:08 +0900

    feat: 연령 및 추가 요금 별 요금 계산 로직 추가

--- src/main/java/wooteco/subway/domain/Fare.java
+++ src/main/java/wooteco/subway/domain/Fare.java
@@ -9,23 +9,34 @@ public class Fare {
     private static final int ADDITIONAL_FARE = 100;
     private static final int FIRST_ADDITIONAL_STANDARD = 5;
     private static final int SECOND_ADDITIONAL_STANDARD = 8;
+    private static final int DEDUCTION = 350;
 
     private final int distance;
+    private final int age;
+    private final int extraFare;
+
+    public Fare(int distance, int age, int extraFare) {
+        this.distance = distance;
+        this.age = age;
+        this.extraFare = extraFare;
+    }
 
     public Fare(int distance) {
-        this.distance = distance;
+        this(distance, 0, 0);
     }
 
     public int calculateFare() {
         if (distance > FIRST_STANDARD_FARE_DISTANCE && distance <= SECOND_STANDARD_FARE_DISTANCE) {
-            return FIRST_DEFAULT_FARE + calculateFirstOverFare(distance - FIRST_STANDARD_FARE_DISTANCE);
+            int fare = FIRST_DEFAULT_FARE + calculateFirstOverFare(distance - FIRST_STANDARD_FARE_DISTANCE);
+            return reduceFare(fare + extraFare);
         }
 
         if (distance > SECOND_STANDARD_FARE_DISTANCE) {
-            return SECOND_DEFAULT_FARE + calculateSecondOverFare(distance - SECOND_STANDARD_FARE_DISTANCE);
+            int fare = SECOND_DEFAULT_FARE + calculateSecondOverFare(distance - SECOND_STANDARD_FARE_DISTANCE);
+            return reduceFare(fare + extraFare);
         }
 
-        return FIRST_DEFAULT_FARE;
+        return reduceFare(FIRST_DEFAULT_FARE + extraFare);
     }
 
     private int calculateFirstOverFare(int distance) {
@@ -34,5 +45,17 @@ public class Fare {
 
     private int calculateSecondOverFare(int distance) {
         return (int) ((Math.ceil((distance - 1) / SECOND_ADDITIONAL_STANDARD) + 1) * ADDITIONAL_FARE);
+    }
+
+    private int reduceFare(int fare) {
+        if (age >= 13 && age < 19) {
+            return (int) ((fare - DEDUCTION) * 0.8);
+        }
+
+        if (age >= 6 && age < 13) {
+            return (int) ((fare - DEDUCTION) * 0.5);
+        }
+
+        return fare;
     }
 }
--- src/test/java/wooteco/subway/domain/FareTest.java
+++ src/test/java/wooteco/subway/domain/FareTest.java
@@ -8,11 +8,35 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class FareTest {
 
-    @DisplayName("길이가 주어지면 요금을 계산하여 반환한다.")
+    @DisplayName("길이와 나이 및 추가 요금이 주어지면 요금을 계산하여 반환한다.")
     @ParameterizedTest
-    @CsvSource({"9,1250", "10,1250", "14,1350", "15,1350", "50,2050", "58,2150", "59,2250"})
-    void 요금_계산(int distance, int expected) {
-        Fare fare = new Fare(distance);
+    @CsvSource({"9,24,0,1250", "10,24,0,1250", "14,24,0,1350",
+            "15,24,0,1350", "50,24,0,2050", "58,24,0,2150", "59,24,0,2250",
+            "8,24,900,2150", "12,24,900,2250"})
+    void 요금_계산(int distance, int age, int extraFare, int expected) {
+        Fare fare = new Fare(distance, age, extraFare);
+
+        assertThat(fare.calculateFare()).isEqualTo(expected);
+    }
+
+    @DisplayName("길이와 청소년 나이 및 추가 요금이 주어지면 요금을 계산하여 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"9,13,0,720", "10,18,0,720", "14,13,0,800",
+            "15,18,0,800", "50,13,0,1360", "58,13,0,1440", "59,13,0,1520",
+            "8,13,900,1440", "12,13,900,1520"})
+    void 청소년_요금_계산(int distance, int age, int extraFare, int expected) {
+        Fare fare = new Fare(distance, age, extraFare);
+
+        assertThat(fare.calculateFare()).isEqualTo(expected);
+    }
+
+    @DisplayName("길이와 어린이 나이 및 추가 요금이 주어지면 요금을 계산하여 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"9,6,0,450", "10,12,0,450", "14,6,0,500",
+            "15,12,0,500", "50,6,0,850", "58,6,0,900", "59,6,0,950",
+            "8,6,900,900", "12,6,900,950"})
+    void 어린이_요금_계산(int distance, int age, int extraFare, int expected) {
+        Fare fare = new Fare(distance, age, extraFare);
 
         assertThat(fare.calculateFare()).isEqualTo(expected);
     }
