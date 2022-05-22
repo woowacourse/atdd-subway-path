3b759f5b16865224842b9379df5a5dd5ff16f382
hyeonic <dev.hyeonic@gmail.com>
2022-05-22 15:46:39 +0900

    feat: 지하철역은 가진 구간의 노선 중 가장 큰 추가 요금 조회 기능 추가

--- src/main/java/wooteco/subway/service/LineService.java
+++ src/main/java/wooteco/subway/service/LineService.java
@@ -105,6 +105,16 @@ public class LineService {
         return new LineResponse(line, stationResponses);
     }
 
+    public int getMaxExtraFare(List<Station> stations) {
+        List<Section> sections = sectionDao.findAllIn(stations);
+        return sections.stream()
+                .map(Section::getLine)
+                .distinct()
+                .mapToInt(Line::getExtraFare)
+                .max()
+                .orElse(0);
+    }
+
     @Transactional
     public void update(Long id, LineUpdateRequest lineRequest) {
         Line line = getLine(id);
--- src/test/java/wooteco/subway/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/service/LineServiceTest.java
@@ -40,15 +40,15 @@ class LineServiceTest {
     @DisplayName("노선 저장과 관련된 기능")
     @TestFactory
     Stream<DynamicTest> dynamicTestFromSvaeStation() {
-        Station upStation = generateStation("선릉역");
-        Station downStation = generateStation("잠실역");
+        Station 선릉역 = generateStation("선릉역");
+        Station 잠실역 = generateStation("잠실역");
 
         return Stream.of(
                 dynamicTest("노선을 저장한다.", () -> {
                     String name = "2호선";
                     String color = "bg-green-600";
                     LineRequest lineRequest = new LineRequest(
-                            name, color, upStation.getId(), downStation.getId(), 10, 0);
+                            name, color, 선릉역.getId(), 잠실역.getId(), 10, 0);
 
                     LineResponse lineResponse = lineService.save(lineRequest);
 
@@ -63,7 +63,7 @@ class LineServiceTest {
                     String name = "2호선";
                     String color = "bg-green-600";
                     LineRequest lineRequest = new LineRequest(
-                            name, color, upStation.getId(), downStation.getId(), 10, 0);
+                            name, color, 선릉역.getId(), 잠실역.getId(), 10, 0);
 
                     assertThatThrownBy(() -> lineService.save(lineRequest))
                             .isInstanceOf(IllegalArgumentException.class)
@@ -71,11 +71,11 @@ class LineServiceTest {
                 }),
 
                 dynamicTest("노선의 저장 시 상행과 하행이 같은 지하철역인 경우 예외를 던진다.", () -> {
-                    Station station = generateStation("중동역");
+                    Station 중동역 = generateStation("중동역");
                     String name = "3호선";
                     String color = "bg-green-600";
                     LineRequest lineRequest = new LineRequest(
-                            name, color, station.getId(), station.getId(), 10, 0);
+                            name, color, 중동역.getId(), 중동역.getId(), 10, 0);
 
                     assertThatThrownBy(() -> lineService.save(lineRequest))
                             .isInstanceOf(IllegalArgumentException.class)
@@ -98,23 +98,23 @@ class LineServiceTest {
     @DisplayName("노선 조작과 관련된 기능")
     @TestFactory
     Stream<DynamicTest> dynamicTestFromStation() {
-        Station upStation = generateStation("선릉역");
-        Station downStation = generateStation("잠실역");
+        Station 선릉역 = generateStation("선릉역");
+        Station 잠실역 = generateStation("잠실역");
 
         String name1 = "2호선";
         String color1 = "bg-green-600";
-        Integer distance1 = 7;
+        int distance1 = 7;
         LineResponse lineResponse1 = lineService.save(
-                new LineRequest(name1, color1, upStation.getId(), downStation.getId(), distance1, 0));
+                new LineRequest(name1, color1, 선릉역.getId(), 잠실역.getId(), distance1, 900));
 
-        Station upStation2 = generateStation("중동역");
-        Station downStation2 = generateStation("신도림역");
+        Station 중동역 = generateStation("중동역");
+        Station 신도림역 = generateStation("신도림역");
 
         String name2 = "1호선";
         String color2 = "bg-blue-600";
-        Integer distance2 = 10;
+        int distance2 = 10;
         LineResponse lineResponse2 = lineService.save(
-                new LineRequest(name2, color2, upStation2.getId(), downStation2.getId(), distance2, 0));
+                new LineRequest(name2, color2, 중동역.getId(), 신도림역.getId(), distance2, 0));
 
         return Stream.of(
                 dynamicTest("노선을 조회한다.", () -> {
@@ -128,6 +128,12 @@ class LineServiceTest {
                     List<LineResponse> lines = lineService.findAll();
 
                     assertThat(lines.size()).isEqualTo(2);
+                }),
+
+                dynamicTest("지하철역을 가진 구간 중 가장 높은 추가 요금을 확인한다.", () -> {
+                    int maxExtraFare = lineService.getMaxExtraFare(List.of(선릉역, 잠실역, 중동역, 신도림역));
+
+                    assertThat(maxExtraFare).isEqualTo(900);
                 }),
 
                 dynamicTest("존재하는 노선을 수정한다.", () -> {
@@ -189,44 +195,38 @@ class LineServiceTest {
                 .hasMessage(0 + "의 노선은 존재하지 않습니다.");
     }
 
-    @DisplayName("노선에 구간을 추가하는 기능")
+    @DisplayName("노선에 구간을 추가하는 기능을 확인한다.")
     @TestFactory
     Stream<DynamicTest> dynamicTestFromAddSection() {
-        Long basedUpStationId = generateStation("신도림역").getId();
-        Long basedDownStationId = generateStation("중동역").getId();
-        Integer basedDistance = 10;
+        Long 신도림역Id = generateStation("신도림역").getId();
+        Long 중동역Id = generateStation("중동역").getId();
+        int basedDistance = 10;
 
         String name = "1호선";
         String color = "bg-blue-600";
-        LineResponse line = generateLine(name, color, basedUpStationId, basedDownStationId, basedDistance);
+        LineResponse line = generateLine(name, color, 신도림역Id, 중동역Id, basedDistance, 0);
 
         return Stream.of(
                 dynamicTest("상행 종점이 같은 경우 가장 앞단의 구간 보다 길이가 크거나 같으면 예외를 던진다.", () -> {
-                    Long upStationId = basedUpStationId;
-                    Long downStationId = generateStation("온수역").getId();
-                    Integer distance = 10;
+                    Long 온수역Id = generateStation("온수역").getId();
 
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(신도림역Id, 온수역Id, 10);
                     assertThatThrownBy(() -> lineService.addSection(line.getId(), sectionRequest))
                             .isInstanceOf(IllegalArgumentException.class)
                             .hasMessage("새로운 구간의 길이가 기존 구간의 길이 보다 크거나 같으므로 추가할 수 없습니다.");
                 }),
 
                 dynamicTest("상행 종점이 같은 경우 가장 앞단의 구간 보다 길이가 작으면 추가한다.", () -> {
-                    Long upStationId = basedUpStationId;
-                    Long downStationId = generateStation("개봉역").getId();
-                    Integer distance = 4;
+                    Long 개봉역Id = generateStation("개봉역").getId();
 
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(신도림역Id, 개봉역Id, 4);
                     assertDoesNotThrow(() -> lineService.addSection(line.getId(), sectionRequest));
                 }),
 
                 dynamicTest("상행 종점에 구간을 추가한다.", () -> {
-                    Long upStationId = generateStation("영등포역").getId();
-                    Long downStationId = basedUpStationId;
-                    Integer distance = 7;
+                    Long 영등포역Id = generateStation("영등포역").getId();
 
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(영등포역Id, 신도림역Id, 7);
                     assertDoesNotThrow(() -> lineService.addSection(line.getId(), sectionRequest));
                 }),
 
@@ -239,11 +239,7 @@ class LineServiceTest {
                 }),
 
                 dynamicTest("상행 종점 추가 시 지하철이 존재하지 않는 경우 예외를 던진다.", () -> {
-                    Long upStationId = 0L;
-                    Long downStationId = basedUpStationId;
-                    Integer distance = 7;
-
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(0L, 신도림역Id, 7);
 
                     assertThatThrownBy(() -> lineService.addSection(line.getId(), sectionRequest))
                             .isInstanceOf(NotFoundException.class)
@@ -251,11 +247,7 @@ class LineServiceTest {
                 }),
 
                 dynamicTest("상행 종점 추가 시 상행역이 기존 노선에 존재하는 경우 예외를 던진다.", () -> {
-                    Long upStationId = basedDownStationId;
-                    Long downStationId = basedUpStationId;
-                    Integer distance = 7;
-
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(중동역Id, 신도림역Id, 7);
 
                     assertThatThrownBy(() -> lineService.addSection(line.getId(), sectionRequest))
                             .isInstanceOf(IllegalArgumentException.class)
@@ -264,10 +256,8 @@ class LineServiceTest {
 
                 dynamicTest("하행 종점이 같은 경우 가장 뒷간의 구간 보다 길이가 크거나 같으면 예외를 던진다.", () -> {
                     Long upStationId = generateStation("역곡역").getId();
-                    Long downStationId = basedDownStationId;
-                    Integer distance = 10;
 
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(upStationId, 중동역Id, 10);
 
                     assertThatThrownBy(() -> lineService.addSection(line.getId(), sectionRequest))
                             .isInstanceOf(IllegalArgumentException.class)
@@ -276,30 +266,22 @@ class LineServiceTest {
 
                 dynamicTest("하행 종점이 같은 경우 가장 앞단의 구간보다 길이가 작으면 추가한다.", () -> {
                     Long upStationId = generateStation("부천역").getId();
-                    Long downStationId = basedDownStationId;
-                    Integer distance = 3;
 
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(upStationId, 중동역Id, 3);
 
                     assertDoesNotThrow(() -> lineService.addSection(line.getId(), sectionRequest));
                 }),
 
                 dynamicTest("하행 종점에 구간을 추가한다.", () -> {
-                    Long upStationId = basedDownStationId;
                     Long downStationId = generateStation("부평역").getId();
-                    Integer distance = 10;
 
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(중동역Id, downStationId, 10);
 
                     assertDoesNotThrow(() -> lineService.addSection(line.getId(), sectionRequest));
                 }),
 
                 dynamicTest("상행역과 하행역이 노선에 모두 존재하면 예외를 던진다.", () -> {
-                    Long upStationId = basedUpStationId;
-                    Long downStationId = basedDownStationId;
-                    Integer distance = 1;
-
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(신도림역Id, 중동역Id, 1);
 
                     assertThatThrownBy(() -> lineService.addSection(line.getId(), sectionRequest))
                             .isInstanceOf(IllegalArgumentException.class)
@@ -309,9 +291,8 @@ class LineServiceTest {
                 dynamicTest("상행역과 하행역이 노선에 모두 존재하지 않으면 예외를 던진다.", () -> {
                     Long upStationId = generateStation("서울역").getId();
                     Long downStationId = generateStation("노량진역").getId();
-                    Integer distance = 1;
 
-                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
+                    SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, 1);
 
                     assertThatThrownBy(() -> lineService.addSection(line.getId(), sectionRequest))
                             .isInstanceOf(IllegalArgumentException.class)
@@ -323,48 +304,49 @@ class LineServiceTest {
     @DisplayName("구간 삭제 기능")
     @TestFactory
     Stream<DynamicTest> dynamicTestRemoveSection() {
-        Long stationId1 = generateStation("신도림역").getId();
-        Long stationId2 = generateStation("온수역").getId();
-        Long stationId3 = generateStation("역곡역").getId();
-        Long stationId4 = generateStation("부천역").getId();
-        Long stationId5 = generateStation("중동역").getId();
+        Long 신도림역 = generateStation("신도림역").getId();
+        Long 온수역 = generateStation("온수역").getId();
+        Long 역곡역 = generateStation("역곡역").getId();
+        Long 부천역 = generateStation("부천역").getId();
+        Long 중동역 = generateStation("중동역").getId();
 
-        LineResponse response = generateLine("1호선", "bg-blue-600", stationId1, stationId2, 10);
+        LineResponse response = generateLine("1호선", "bg-blue-600", 신도림역, 온수역, 10, 0);
         Long lineId = response.getId();
 
-        lineService.addSection(lineId, new SectionRequest(stationId2, stationId3, 10));
-        lineService.addSection(lineId, new SectionRequest(stationId3, stationId4, 10));
-        lineService.addSection(lineId, new SectionRequest(stationId4, stationId5, 10));
+        lineService.addSection(lineId, new SectionRequest(온수역, 역곡역, 10));
+        lineService.addSection(lineId, new SectionRequest(역곡역, 부천역, 10));
+        lineService.addSection(lineId, new SectionRequest(부천역, 중동역, 10));
 
         return Stream.of(
                 dynamicTest("중간에 위치한 역을 삭제한다.", () -> {
-                    assertDoesNotThrow(() -> lineService.deleteSection(lineId, stationId2));
+                    assertDoesNotThrow(() -> lineService.deleteSection(lineId, 온수역));
                 }),
 
                 dynamicTest("상행 종점의 구간을 삭제한다.", () -> {
-                    assertDoesNotThrow(() -> lineService.deleteSection(lineId, stationId1));
+                    assertDoesNotThrow(() -> lineService.deleteSection(lineId, 신도림역));
                 }),
 
                 dynamicTest("존재하지 않는 역을 삭제할 경우 예외를 던진다.", () -> {
-                    assertThatThrownBy(() -> lineService.deleteSection(lineId, stationId1))
+                    assertThatThrownBy(() -> lineService.deleteSection(lineId, 신도림역))
                             .isInstanceOf(IllegalArgumentException.class)
                             .hasMessage("일치하는 구간이 존재하지 않습니다.");
                 }),
 
                 dynamicTest("하행 종점의 구간을 삭제한다.", () -> {
-                    assertDoesNotThrow(() -> lineService.deleteSection(lineId, stationId5));
+                    assertDoesNotThrow(() -> lineService.deleteSection(lineId, 중동역));
                 }),
 
                 dynamicTest("구간이 한개 뿐인 경우 예외를 던진다.", () -> {
-                    assertThatThrownBy(() -> lineService.deleteSection(lineId, stationId2))
+                    assertThatThrownBy(() -> lineService.deleteSection(lineId, 온수역))
                             .isInstanceOf(IllegalArgumentException.class)
                             .hasMessage("구간이 1개만 존재하므로 삭제가 불가능 합니다.");
                 })
         );
     }
 
-    private LineResponse generateLine(String name, String color, Long upStationId, Long downStationId, int distance) {
-        return lineService.save(new LineRequest(name, color, upStationId, downStationId, distance, 0));
+    private LineResponse generateLine(
+            String name, String color, Long upStationId, Long downStationId, int distance, int extraFare) {
+        return lineService.save(new LineRequest(name, color, upStationId, downStationId, distance, extraFare));
     }
 
     private Station generateStation(String name) {
