f6d2c0b0c50bbc4d2fcf296c70f6221d2addb432
Seongwon97 <osw970325@naver.com>
2022-05-22 16:49:26 +0900

    test(/acceptance): RestAssured의 기능들을 이용해 테스트하도록 수정

--- src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
@@ -1,7 +1,5 @@
 package wooteco.subway.acceptance;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.SectionRequest;
 import wooteco.subway.dto.StationRequest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class PathAcceptanceTest extends AcceptanceTest {
@@ -24,20 +20,20 @@ public class PathAcceptanceTest extends AcceptanceTest {
 
     @BeforeEach
     void setUpStations() {
-        선릉역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("선릉역"), "/stations");
-        선정릉역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("선정릉역"), "/stations");
-        한티역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("한티역"), "/stations");
-        모란역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("모란역"), "/stations");
-        기흥역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("기흥역"), "/stations");
-        강남역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("강남역"), "/stations");
+        선릉역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("선릉역"), "/stations");
+        선정릉역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("선정릉역"), "/stations");
+        한티역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("한티역"), "/stations");
+        모란역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("모란역"), "/stations");
+        기흥역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("기흥역"), "/stations");
+        강남역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("강남역"), "/stations");
 
-        Long 분당선_id = RestAssuredConvenienceMethod.postLineAndGetId(
+        Long 분당선_id = RestAssuredConvenienceMethod.postRequestAndGetId(
                 new LineRequest("분당선", "yellow", 선릉역_id, 선정릉역_id, 50, 0), "/lines");
         RestAssuredConvenienceMethod.postRequest(new SectionRequest(선정릉역_id, 한티역_id, 8), "/lines/" + 분당선_id + "/sections");
         RestAssuredConvenienceMethod.postRequest(new SectionRequest(한티역_id, 강남역_id, 20), "/lines/" + 분당선_id + "/sections");
-        Long 신분당선_id = RestAssuredConvenienceMethod.postLineAndGetId(
+        Long 신분당선_id = RestAssuredConvenienceMethod.postRequestAndGetId(
                 new LineRequest("신분당선", "red", 모란역_id, 선정릉역_id, 6, 500), "/lines");
-        Long 우테코선_id = RestAssuredConvenienceMethod.postLineAndGetId(
+        Long 우테코선_id = RestAssuredConvenienceMethod.postRequestAndGetId(
                 new LineRequest("우테코선", "blue", 기흥역_id, 모란역_id, 10, 700), "/lines");
         RestAssuredConvenienceMethod.postRequest(new SectionRequest(모란역_id, 강남역_id, 5), "/lines/" + 우테코선_id + "/sections");
 
@@ -48,9 +44,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
     void findPath() {
         String uri = pathRequestFormat(기흥역_id, 강남역_id);
 
-        ExtractableResponse<Response> response = RestAssuredConvenienceMethod.getRequest(uri);
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        RestAssuredConvenienceMethod.getRequest(uri)
+                .statusCode(HttpStatus.OK.value());
     }
 
     @DisplayName("존재하지 않는 역의 경로를 찾을때는 400 코드가 반환된다.")
@@ -58,9 +53,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
     void findPathWithNotExistStation() {
         String uri = pathRequestFormat(기흥역_id, 100L);
 
-        ExtractableResponse<Response> response = RestAssuredConvenienceMethod.getRequest(uri);
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        RestAssuredConvenienceMethod.getRequest(uri)
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("출발지와 도착지가 같은 경로를 찾을때는 400 코드가 반환된다.")
@@ -68,34 +62,31 @@ public class PathAcceptanceTest extends AcceptanceTest {
     void findPathWithSameSourceAndTarget() {
         String uri = pathRequestFormat(기흥역_id, 기흥역_id);
 
-        ExtractableResponse<Response> response = RestAssuredConvenienceMethod.getRequest(uri);
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        RestAssuredConvenienceMethod.getRequest(uri)
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("노선에 연결되어있지 않은 역을 이용해 경로를 찾을 때는 400 코드가 반환된다.")
     @Test
     void findPathWithNotConnectedStation() {
-        Long 수서역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("수서역"), "/stations");
+        Long 수서역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("수서역"), "/stations");
         String uri = pathRequestFormat(기흥역_id, 수서역_id);
 
-        ExtractableResponse<Response> response = RestAssuredConvenienceMethod.getRequest(uri);
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        RestAssuredConvenienceMethod.getRequest(uri)
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("이동할 수 없는 경로를 찾을 때는 400 코드가 반환된다.")
     @Test
     void findPathWithNotConnectedRoute() {
-        Long 인천역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("인천역"), "/stations");
-        Long 수원역_id = RestAssuredConvenienceMethod.postStationAndGetId(new StationRequest("수원역"), "/stations");
-        RestAssuredConvenienceMethod.postLineAndGetId(
+        Long 인천역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("인천역"), "/stations");
+        Long 수원역_id = RestAssuredConvenienceMethod.postRequestAndGetId(new StationRequest("수원역"), "/stations");
+        RestAssuredConvenienceMethod.postRequestAndGetId(
                 new LineRequest("수인선", "green", 인천역_id, 수원역_id, 10, 400), "/lines");
         String uri = pathRequestFormat(기흥역_id, 수원역_id); // 변경 필요
 
-        ExtractableResponse<Response> response = RestAssuredConvenienceMethod.getRequest(uri);
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        RestAssuredConvenienceMethod.getRequest(uri)
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     private String pathRequestFormat(Long from, Long to) {
--- src/test/java/wooteco/subway/acceptance/RestAssuredConvenienceMethod.java
+++ src/test/java/wooteco/subway/acceptance/RestAssuredConvenienceMethod.java
@@ -1,69 +1,52 @@
 package wooteco.subway.acceptance;
 
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.springframework.http.MediaType;
-import wooteco.subway.dto.LineResponse;
-import wooteco.subway.dto.StationResponse;
 
 public class RestAssuredConvenienceMethod {
 
-    public static ExtractableResponse<Response> postRequest(Object body, String path) {
+    public static ValidatableResponse postRequest(Object body, String path) {
         return RestAssured.given().log().all()
                 .body(body)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .post(path)
-                .then().log().all()
-                .extract();
+                .then().log().all();
     }
 
-    public static Long postStationAndGetId(Object body, String path) {
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+    public static Long postRequestAndGetId(Object body, String path) {
+        Object id = RestAssured.given().log().all()
                 .body(body)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .post(path)
                 .then().log().all()
-                .extract();
-        return response.jsonPath().getObject(".", StationResponse.class).getId();
+                .extract()
+                .path("id");
+        return Long.parseLong(String.valueOf(id));
     }
 
-    public static Long postLineAndGetId(Object body, String path) {
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .body(body)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post(path)
-                .then().log().all()
-                .extract();
-        return response.jsonPath().getObject(".", LineResponse.class).getId();
-    }
-
-    public static ExtractableResponse<Response> deleteRequest(String path) {
-        return RestAssured.given().log().all()
-                .when()
-                .delete(path)
-                .then().log().all()
-                .extract();
-    }
-
-    public static ExtractableResponse<Response> getRequest(String path) {
+    public static ValidatableResponse getRequest(String path) {
         return RestAssured.given().log().all()
                 .when()
                 .get(path)
-                .then().log().all()
-                .extract();
+                .then().log().all();
     }
 
-    public static ExtractableResponse<Response> putRequest(Object body, String path) {
+    public static ValidatableResponse putRequest(Object body, String path) {
         return RestAssured.given().log().all()
                 .body(body)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .put(path)
-                .then().log().all()
-                .extract();
+                .then().log().all();
+    }
+
+    public static ValidatableResponse deleteRequest(String path) {
+        return RestAssured.given().log().all()
+                .when()
+                .delete(path)
+                .then().log().all();
     }
 }
