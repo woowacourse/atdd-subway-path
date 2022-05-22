7cecc8844a9dc15717b964da3cfe575ad3108e9b
= <rerub0831@gmail.com>
2022-05-22 21:43:03 +0900

    feat: 노선 추가요금 로직 구현

---
+++ src/main/java/wooteco/subway/domain/policy/LinePolicy.java
@@ -0,0 +1,27 @@
+package wooteco.subway.domain.policy;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import wooteco.subway.domain.element.Line;
+import wooteco.subway.domain.fare.FarePolicy;
+
+public class LinePolicy implements FarePolicy {
+
+    private final List<Line> lines;
+
+    public LinePolicy(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    @Override
+    public double calculate(double baseFare) {
+        return baseFare + getMaxFeeLine(lines).getExtraFare();
+    }
+
+    private Line getMaxFeeLine(List<Line> lines) {
+        return lines.stream()
+                .max(Comparator.comparingDouble(Line::getExtraFare))
+                .orElseThrow(NoSuchElementException::new);
+    }
+}
---
+++ src/test/java/wooteco/subway/domain/policy/LinePolicyTest.java
@@ -0,0 +1,29 @@
+package wooteco.subway.domain.policy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static wooteco.subway.domain.fixtures.TestFixtures.분당선;
+import static wooteco.subway.domain.fixtures.TestFixtures.이호선;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LinePolicyTest {
+
+    @Test
+    @DisplayName("추가요금이 있는 노선을 탈 경우 요금이 추가된다.")
+    void calculate_oneLine() {
+        LinePolicy linePolicy = new LinePolicy(List.of(이호선));
+        assertThat(linePolicy.calculate(1250))
+                .isEqualTo(1350);
+    }
+
+    @Test
+    @DisplayName("추가요금이 있는 노선을 탈 경우 요금이 추가된다.")
+    void calculate_twoLine() {
+        LinePolicy linePolicy = new LinePolicy(List.of(이호선, 분당선));
+        assertThat(linePolicy.calculate(1250))
+                .isEqualTo(1450);
+    }
+}
