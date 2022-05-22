0e36fbad6b23a32daab5633a4ba85d238b3a781e
bcc0830 <bcc0830@naver.com>
2022-05-22 16:45:56 +0900

    feat : 노선의 추가와 수정 시 음수인지 검증하는 테스트케이스 추가

--- src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -27,7 +27,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         @Test
         void 성공시_201_CREATED() {
             testFixtureManager.saveStations("강남역", "선릉역");
-            Map<String, Object> params = jsonLineOf("신분당선", "bg-red-600", 1L, 2L, 10);
+            Map<String, Object> params = jsonLineOf("신분당선", "bg-red-600", 1L, 2L, 10, 0);
 
             ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/lines", params);
             LineResponse actualBody = extractSingleLineResponseBody(response);
@@ -50,7 +50,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         @Test
         void 정보가_공백으로_구성된_경우_400_BAD_REQUEST() {
-            Map<String, Object> blankParams = jsonLineOf("   ", "  ", 1L, 2L, 10);
+            Map<String, Object> blankParams = jsonLineOf("   ", "  ", 1L, 2L, 10, 0);
 
             ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/stations", blankParams);
 
@@ -58,9 +58,18 @@ public class LineAcceptanceTest extends AcceptanceTest {
         }
 
         @Test
+        void 추가_요금이_음수인_경우_400_BAD_REQUEST() {
+            Map<String, Object> negativeFareParams = jsonLineOf("2호선", "초록색", 1L, 2L, 10, -1);
+
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/lines", negativeFareParams);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        @Test
         void 이미_존재하는_노선명_입력시_400_BAD_REQUEST() {
             testFixtureManager.saveStations("강남역", "선릉역");
-            Map<String, Object> duplicateNameParams = jsonLineOf("중복되는 노선명", "bg-red-600", 1L, 2L, 10);
+            Map<String, Object> duplicateNameParams = jsonLineOf("중복되는 노선명", "bg-red-600", 1L, 2L, 10, 0);
 
             HttpUtils.send(HttpMethod.POST, "/lines", duplicateNameParams);
             ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/lines", duplicateNameParams);
@@ -70,7 +79,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         @Test
         void 존재하지_않는_지하철역을_종점으로_입력하면_404_NOT_FOUND() {
-            Map<String, Object> params = jsonLineOf("노선명", "색상", 9999L, 2L, 10);
+            Map<String, Object> params = jsonLineOf("노선명", "색상", 9999L, 2L, 10, 0);
 
             ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/lines", params);
 
@@ -147,7 +156,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         @Test
         void 성공시_200_OK() {
             testFixtureManager.saveLine("신분당선", "노란색");
-            Map<String, String> params = jsonLineOf("NEW 분당선", "bg-red-800");
+            Map<String, Object> params = jsonLineOf("NEW 분당선", "bg-red-800", 10);
 
             ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, "/lines/1", params);
 
@@ -156,7 +165,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         @Test
         void 수정하려는_지하철_노선이_존재하지_않는_경우_404_NOT_FOUND() {
-            Map<String, String> params = jsonLineOf("NEW 분당선", "bg-red-600");
+            Map<String, Object> params = jsonLineOf("NEW 분당선", "bg-red-600", 10);
 
             ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, "/lines/9999", params);
 
@@ -167,7 +176,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         void 이미_존재하는_지하철_노선_이름으로_수정시_400_BAD_REQUEST() {
             testFixtureManager.saveLine("현재 노선명", "노란색");
             testFixtureManager.saveLine("존재하는 노선명", "노란색");
-            Map<String, String> duplicateNameParams = jsonLineOf("존재하는 노선명", "bg-red-600");
+            Map<String, Object> duplicateNameParams = jsonLineOf("존재하는 노선명", "bg-red-600", 10);
 
             ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, "/lines/2", duplicateNameParams);
 
@@ -185,9 +194,19 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         @Test
         void 이름_혹은_색상_정보가_공백으로_구성된_경우_400_BAD_REQUEST() {
-            Map<String, String> blankParams = jsonLineOf("   ", "  ");
+            Map<String, Object> blankParams = jsonLineOf("   ", "  ", 10);
 
             ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.POST, "/stations", blankParams);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        @Test
+        void 추가요금을_음수로_수정시_400_BAD_REQUEST() {
+            testFixtureManager.saveLine("현재 노선명", "노란색");
+            Map<String, Object> negativeFareParams = jsonLineOf("존재하는 노선명", "bg-red-600", -1);
+
+            ExtractableResponse<Response> response = HttpUtils.send(HttpMethod.PUT, "/lines/1", negativeFareParams);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         }
@@ -214,11 +233,13 @@ public class LineAcceptanceTest extends AcceptanceTest {
         }
     }
 
-    private HashMap<String, String> jsonLineOf(String name,
-                                               String color) {
+    private HashMap<String, Object> jsonLineOf(String name,
+                                               String color,
+                                               int extraFare) {
         return new HashMap<>() {{
             put("name", name);
             put("color", color);
+            put("extraFare", extraFare);
         }};
     }
 
@@ -226,13 +247,15 @@ public class LineAcceptanceTest extends AcceptanceTest {
                                                String color,
                                                Long upStationId,
                                                Long downStationId,
-                                               int distance) {
+                                               int distance,
+                                               int extraFare) {
         return new HashMap<>() {{
             put("name", name);
             put("color", color);
             put("upStationId", upStationId);
             put("downStationId", downStationId);
             put("distance", distance);
+            put("extraFare", extraFare);
         }};
     }
 
