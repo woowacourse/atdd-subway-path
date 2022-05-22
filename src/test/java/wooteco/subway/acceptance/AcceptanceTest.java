e2297cfeb9f0d9a88efbd437a7b8c6a345c9fbc3
yeon-06 <rnjstldus2@gmail.com>
2022-05-22 16:11:34 +0900

    refactor: @Sql 활용

--- src/test/java/wooteco/subway/acceptance/AcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/AcceptanceTest.java
@@ -2,25 +2,19 @@ package wooteco.subway.acceptance;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("classpath:truncate.sql")
 public class AcceptanceTest {
 
     @LocalServerPort
     int port;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
-        jdbcTemplate.update("truncate table LINE");
-        jdbcTemplate.update("truncate table SECTION");
-        jdbcTemplate.update("truncate table STATION");
     }
 }
