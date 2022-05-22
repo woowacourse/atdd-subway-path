181d5b6d662d865942586cd0bbaa64ea0b8836ee
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-22 14:49:30 +0900

    test: Line 객체 정상 생성 테스트 추가

--- src/test/java/wooteco/subway/domain/line/LineTest.java
+++ src/test/java/wooteco/subway/domain/line/LineTest.java
@@ -1,11 +1,18 @@
 package wooteco.subway.domain.line;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LineTest {
+
+    @Test
+    @DisplayName("정상적으로 Line 생성")
+    void createLine() {
+        assertDoesNotThrow(() -> new Line(1L, "2호선", "green", 0));
+    }
 
     @Test
     @DisplayName("추가요금이 음수가 들어올 경우 예외 발생")
