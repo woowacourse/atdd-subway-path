d0b9873f006ee1d6fe4b29176c3f12984778b7d7
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-22 17:08:44 +0900

    test(LineAcceptanceTest): 노선 관리 인수테스트 작성

--- src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -5,39 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import wooteco.subway.exception.ExceptionMessage;
-import wooteco.subway.repository.dao.StationDao;
-import wooteco.subway.repository.entity.StationEntity;
 import wooteco.subway.service.dto.LineRequest;
 import wooteco.subway.service.dto.LineResponse;
-import wooteco.subway.service.dto.StationResponse;
+import wooteco.subway.service.dto.StationRequest;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
 
     private static final String LOCATION = "Location";
 
-    @Autowired
-    private StationDao stationDao;
-
-    private StationEntity 강남역;
-    private StationEntity 노원역;
-
-    @Override
-    @BeforeEach
-    void setUp() {
-        super.setUp();
-        강남역 = stationDao.save(new StationEntity(null, "강남역"));
-        노원역 = stationDao.save(new StationEntity(null, "노원역"));
-    }
+    private StationRequest 하계_요청;
+    private StationRequest 상계_요청;
+    private ExtractableResponse<Response> 하계_응답;
+    private ExtractableResponse<Response> 상계_응답;
+    private long 하계_id;
+    private long 상계_id;
 
     @Test
     @DisplayName("지하철 노선을 생성한다.")
@@ -46,24 +32,42 @@ public class LineAcceptanceTest extends AcceptanceTest {
         String lineName = "7호선";
         String lineColor = "bg-red-600";
 
-        // when
-        LineRequest requestBody = new LineRequest(lineName, lineColor, 노원역.getId(), 강남역.getId(), 10);
+        하계역_상계역_등록();
+
+        //when 노선을 등록한다.
+        LineRequest requestBody = new LineRequest(lineName, lineColor, 하계_id, 상계_id, 10);
         ExtractableResponse<Response> response = postWithBody("/lines", requestBody);
 
-        // then
+        // then 노선 등록에 성공한다.
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.header(LOCATION)).isNotBlank();
 
+        // and 노선 정보와 역 목록 2개를 얻을 수 있다.
         LineResponse lineResponse = response.body().as(LineResponse.class);
-        List<Long> stationIds = lineResponse.getStations().stream()
-                .map(StationResponse::getId)
-                .collect(Collectors.toList());
 
         assertAll(() -> {
             assertThat(lineResponse.getId()).isNotNull();
             assertThat(lineResponse.getName()).isEqualTo(lineName);
             assertThat(lineResponse.getColor()).isEqualTo(lineColor);
-            assertThat(stationIds).containsExactly(노원역.getId(), 강남역.getId());
+            assertThat(lineResponse.getStations()).hasSize(2);
+        });
+    }
+
+    private void 하계역_상계역_등록() {
+        // when 역 두개를 등록한다.
+        하계_요청 = new StationRequest("하계역");
+        하계_응답 = postWithBody("/stations", 하계_요청);
+
+        상계_요청 = new StationRequest("상계역");
+        상계_응답 = postWithBody("/stations", 상계_요청);
+
+        하계_id = 하계_응답.jsonPath().getLong("id");
+        상계_id = 상계_응답.jsonPath().getLong("id");
+
+        //then 역 등록에 성공한다.
+        assertAll(() -> {
+            assertThat(하계_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+            assertThat(상계_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         });
     }
 
@@ -88,11 +92,13 @@ public class LineAcceptanceTest extends AcceptanceTest {
         String redColor = "bg-red-600";
         String blueColor = "bg-blue-600";
 
-        LineRequest lineRequest = new LineRequest(lineName, redColor, 노원역.getId(), 강남역.getId(), 10);
+        하계역_상계역_등록();
+
+        LineRequest lineRequest = new LineRequest(lineName, redColor, 하계_id, 상계_id, 10);
         postWithBody("/lines", lineRequest);
 
         // when
-        LineRequest duplicatedNameRequest = new LineRequest(lineName, blueColor, 노원역.getId(), 강남역.getId(), 10);
+        LineRequest duplicatedNameRequest = new LineRequest(lineName, blueColor, 상계_id, 하계_id, 10);
         ExtractableResponse<Response> response = postWithBody("/lines", duplicatedNameRequest);
 
         // then
@@ -105,59 +111,59 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("모든 노선을 조회한다.")
     @Test
     void getLines() {
-        /// given
+        하계역_상계역_등록();
 
-        LineRequest requestBody1 = new LineRequest("7호선", "bg-green-600", 강남역.getId(), 노원역.getId(), 10);
-        ExtractableResponse<Response> createResponse1 = postWithBody("/lines", requestBody1);
+        // when 노선 m개를 등록한다.
+        LineRequest 칠호선_요청 = new LineRequest("7호선", "bg-green-600", 상계_id, 하계_id, 10);
+        ExtractableResponse<Response> 칠호선_응답 = postWithBody("/lines", 칠호선_요청);
 
-        LineRequest requestBody2 = new LineRequest("5호선", "bg-red-600", 노원역.getId(), 강남역.getId(), 10);
-        ExtractableResponse<Response> createResponse2 = postWithBody("/lines", requestBody2);
+        LineRequest 오호선_요청 = new LineRequest("5호선", "bg-red-600", 하계_id, 상계_id, 10);
+        ExtractableResponse<Response> 오호선_응답 = postWithBody("/lines", 오호선_요청);
 
-        // when
-        ExtractableResponse<Response> response = get("/lines");
+        // then 노선 m개 등록에 성공한다.
+        assertAll(() -> {
+            assertThat(칠호선_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+            assertThat(오호선_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        });
 
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // when 노선을 조회한다.
+        ExtractableResponse<Response> 노선_조회_응답 = get("/lines");
 
-        List<Long> expectedLineIds = Stream.of(createResponse1, createResponse2)
-                .map(this::getIdFromLocation)
-                .collect(Collectors.toList());
-
-        List<LineResponse> lineResponses = response.jsonPath().getList(".", LineResponse.class);
-
-        List<Long> resultLineIds = lineResponses.stream()
-                .map(LineResponse::getId)
-                .collect(Collectors.toList());
-
-        long validResponsesCount = lineResponses.stream()
-                .filter(it -> it.getStations().size() == 2)
-                .count();
-
-        assertThat(resultLineIds).containsAll(expectedLineIds);
-        assertThat(validResponsesCount).isEqualTo(2);
+        // then 노선 조회에 성공한다.
+        // and 조회된 노선의 갯수는 m개다.
+        assertAll(() -> {
+            assertThat(노선_조회_응답.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertThat(노선_조회_응답.jsonPath().getList(".")).hasSize(2);
+        });
     }
 
-    @DisplayName("id로 노선을 조회한다.")
+    @DisplayName("특정 노선 조회한다")
     @Test
     void findById() {
         /// given
         String lineName = "7호선";
         String lineColor = "bg-green-600";
 
-        LineRequest lineRequest = new LineRequest(lineName, lineColor, 강남역.getId(), 노원역.getId(), 5);
+       하계역_상계역_등록();
 
-        ExtractableResponse<Response> createResponse = postWithBody("/lines", lineRequest);
-        long id = getIdFromLocation(createResponse);
+        // when 노선 등록한다.
+        LineRequest 칠호선_요청 = new LineRequest("7호선", "bg-green-600", 상계_id, 하계_id, 10);
+        ExtractableResponse<Response> 칠호선_응답 = postWithBody("/lines", 칠호선_요청);
 
-        // when
-        ExtractableResponse<Response> response = get("/lines/" + id);
+        // then 노선 등록에 성공한다.
+        assertThat(칠호선_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // when 노선을 조회한다.
+        long 칠호선_id = getIdFromLocation(칠호선_응답);
+        ExtractableResponse<Response> 칠호선_조회_응답 = get("/lines/" + 칠호선_id);
 
-        LineResponse lineResponse = response.body().as(LineResponse.class);
+        // then 노선 조회에 성공한다.
+        assertThat(칠호선_조회_응답.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        // and 조회된 노선은 등록한 노선의 이름과 동일하다.
+        // and 조회된 노선에는 등록된 노선의 역 정보 2개가 있다.
+        LineResponse lineResponse = 칠호선_조회_응답.body().as(LineResponse.class);
         assertAll(() -> {
-            assertThat(lineResponse.getId()).isEqualTo(id);
             assertThat(lineResponse.getName()).isEqualTo(lineName);
             assertThat(lineResponse.getColor()).isEqualTo(lineColor);
             assertThat(lineResponse.getStations()).hasSize(2);
@@ -180,36 +186,51 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     @DisplayName("지하철 노선 정보를 수정한다.")
     void updateLine() {
-        // given
+        하계역_상계역_등록();
 
-        LineRequest requestBody = new LineRequest("7호선", "bg-red-600", 강남역.getId(), 노원역.getId(), 10);
-        ExtractableResponse<Response> response = postWithBody("/lines", requestBody);
+        // when 노선 등록한다.
+        LineRequest 칠호선_요청 = new LineRequest("7호선", "bg-green-600", 상계_id, 하계_id, 10);
+        ExtractableResponse<Response> 칠호선_응답 = postWithBody("/lines", 칠호선_요청);
 
-        long id = getIdFromLocation(response);
+        // then 노선 등록에 성공한다.
+        assertThat(칠호선_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
-        // when
-        LineRequest updateBody = new LineRequest("5호선", "bg-green-600", 노원역.getId(), 강남역.getId(), 10);
+        long id = getIdFromLocation(칠호선_응답);
 
-        ExtractableResponse<Response> updateResponse = putWithBody("/lines/" + id, updateBody);
+        // when 등록된 노선 수정 요청한다.
+        LineRequest 수정_요청_바디 = new LineRequest("5호선", "bg-green-600", 하계_id, 상계_id, 10);
+        ExtractableResponse<Response> 수정_응답 = putWithBody("/lines/" + id, 수정_요청_바디);
 
-        // then
-        assertThat(updateResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // then 조회된 노선의 이름이 수정하려한 내용과 같다.
+        // and 조회된 노선의 색깔이 수정하려한 내용과 같다.
+        // and 조회된 노선의 상행 종점과 하행 종점이 수정하려한 내용과 같다.
+        LineResponse 수정_역_조회 = get("/lines/"+ id).body().as(LineResponse.class);
+        assertAll(() -> {
+            assertThat(수정_응답.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertThat(수정_역_조회.getName()).isEqualTo(수정_요청_바디.getName());
+            assertThat(수정_역_조회.getColor()).isEqualTo(수정_요청_바디.getColor());
+            assertThat(수정_역_조회.getStations().get(0).getId()).isEqualTo(수정_요청_바디.getUpStationId());
+            assertThat(수정_역_조회.getStations().get(1).getId()).isEqualTo(수정_요청_바디.getDownStationId());
+        });
     }
 
     @Test
     @DisplayName("지하철 노선 정보를 삭제한다.")
     void deleteLine() {
-        // given
+        하계역_상계역_등록();
 
-        LineRequest requestBody = new LineRequest("7호선", "bg-red-600", 강남역.getId(), 노원역.getId(), 0);
-        ExtractableResponse<Response> response = postWithBody("/lines", requestBody);
+        // when 노선을 등록한다.
+        LineRequest 칠호선_요청 = new LineRequest("7호선", "red", 상계_id, 하계_id, 10);
+        ExtractableResponse<Response> 칠호선_생성_응답 = postWithBody("/lines", 칠호선_요청);
 
-        long id = getIdFromLocation(response);
+        // then 노선 등록에 성공한다.
+        assertThat(칠호선_생성_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
-        // when
-        ExtractableResponse<Response> deleteResponse = delete("/lines/" + id);
+        // when 등록된 노선을 삭제 요청한다.
+        long id = getIdFromLocation(칠호선_생성_응답);
+        ExtractableResponse<Response> 칠호선_삭제_응답 = delete("/lines/" + id);
 
-        // then
-        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        // then 노선 삭제에 성공한다.
+        assertThat(칠호선_삭제_응답.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
