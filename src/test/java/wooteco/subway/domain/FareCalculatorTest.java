15df824d363d70db60e5d4eaa574b4770bf3a6c7
wilgur513 <wilgur513@naver.com>
2022-05-22 14:12:08 +0900

    test: 거리별 요금 계산 테스트 분리

--- src/test/java/wooteco/subway/domain/FareCalculatorTest.java
+++ src/test/java/wooteco/subway/domain/FareCalculatorTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,14 +13,51 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class FareCalculatorTest {
 
-    @DisplayName("거리에 따른 요금 계산")
-    @ParameterizedTest
-    @CsvSource({"10,1250","15,1350","21,1550","58,2150", "59,2250"})
-    void calculateFare (int distance, int fare) {
-        List<Station> stations = List.of(
+    private List<Station> stations;
+
+    @BeforeEach
+    void setUp() {
+        stations = List.of(
             new Station(1L, "강남역"),
-            new Station(2L, "강남역")
+            new Station(2L, "강남역"),
+            new Station(3L, "역삼역")
         );
+    }
+
+    @DisplayName("기본 거리 요금 계산")
+    @ParameterizedTest
+    @ValueSource(ints = {5, 6, 10})
+    void calculateFare (int distance) {
+        List<Section> sections = List.of(
+            new Section(1L, 1L, new SectionEdge(1L, 2L, distance))
+        );
+        Path path = new Path(stations, sections, distance);
+        Map<Long, Integer> extraFares = Map.of(1L, 0);
+
+        int actual = new FareCalculator(extraFares, Passenger.valueOf(19)).calculateFare(path);
+
+        assertThat(actual).isEqualTo(1250);
+    }
+
+    @DisplayName("1차 기준 거리 초과 시 요금 계산")
+    @ParameterizedTest
+    @CsvSource({"11,1350", "15,1350","21,1550","26,1650"})
+    void calculateOverPrimaryDistance(int distance, int fare) {
+        List<Section> sections = List.of(
+            new Section(1L, 1L, new SectionEdge(1L, 2L, distance))
+        );
+        Path path = new Path(stations, sections, distance);
+        Map<Long, Integer> extraFares = Map.of(1L, 0);
+
+        int actual = new FareCalculator(extraFares, Passenger.valueOf(19)).calculateFare(path);
+
+        assertThat(actual).isEqualTo(fare);
+    }
+
+    @DisplayName("2차 기준 거리 초과 시 요금 계산")
+    @ParameterizedTest
+    @CsvSource({"51, 2150","58,2150", "59,2250", "67,2350"})
+    void calculateOverSecondaryDistance(int distance, int fare) {
         List<Section> sections = List.of(
             new Section(1L, 1L, new SectionEdge(1L, 2L, distance))
         );
@@ -34,11 +72,6 @@ public class FareCalculatorTest {
     @DisplayName("추가 요금이 있는 노선의 요금 계산")
     @Test
     void calculateHasExtraFare () {
-        List<Station> stations = List.of(
-            new Station(1L, "강남역"),
-            new Station(2L, "강남역"),
-            new Station(3L, "역삼역")
-        );
         List<Section> sections = List.of(
             new Section(1L, 1L, new SectionEdge(1L, 2L, 10)),
             new Section(1L, 2L, new SectionEdge(2L, 3L, 3))
@@ -55,11 +88,6 @@ public class FareCalculatorTest {
     @ParameterizedTest
     @ValueSource(ints = {13, 16, 18})
     void calculateYouthFare (int age) {
-        List<Station> stations = List.of(
-            new Station(1L, "강남역"),
-            new Station(2L, "강남역"),
-            new Station(3L, "역삼역")
-        );
         List<Section> sections = List.of(
             new Section(1L, 1L, new SectionEdge(1L, 2L, 10)),
             new Section(1L, 2L, new SectionEdge(2L, 3L, 3))
@@ -76,11 +104,6 @@ public class FareCalculatorTest {
     @ParameterizedTest
     @ValueSource(ints = {6, 10, 12})
     void calculateChildFare (int age) {
-        List<Station> stations = List.of(
-            new Station(1L, "강남역"),
-            new Station(2L, "강남역"),
-            new Station(3L, "역삼역")
-        );
         List<Section> sections = List.of(
             new Section(1L, 1L, new SectionEdge(1L, 2L, 10)),
             new Section(1L, 2L, new SectionEdge(2L, 3L, 3))
