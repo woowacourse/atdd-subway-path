bcf7727d5e2fb304d1fc4bec3d722d4e444d69bd
skullKim <kyk990328@gmail.com>
2022-05-22 20:05:14 +0900

    refactor(Fare.java): 코드 가독성 개선
    메서드 분리, 조건문 조건 수정을 통해 코드 가독성 개선
    
    BREAKING CHANGE: 조건문의 조건을 메서드로 분리, 조건문 조건 수정
    as-is:
    public class Fare {
    
        ...
    
        public int calculate(final int distance, final List<Station> stations, final List<Line> lines) {
            ...
            if (distance >= ADDITIONAL_DISTANCE_PER_5KM && distance < ADDITIONAL_DISTANCE_PER_8KM) {
                ...
            }
            if (distance >= ADDITIONAL_DISTANCE_PER_8KM) {
                ...
            }
            ...
        }
    
        ...
    
        private int findExtraLineFare(final Line line, final List<Station> stations) {
            final boolean doesLineContainStation = stations.stream()
                    .anyMatch(line::containStation);
            if (doesLineContainStation) {
                ...
            }
            ....
        }
    
        ...
    }
    
    to-be:
    public class Fare {
    
        ...
    
        public int calculate(final int distance, final List<Station> stations, final List<Line> lines) {
            ...
            if (ADDITIONAL_DISTANCE_PER_5KM <= distance && distance < ADDITIONAL_DISTANCE_PER_8KM) {
                ...
            }
            if (ADDITIONAL_DISTANCE_PER_8KM <= distance) {
                ...
            }
            ...
        }
    
        ...
    
        private int findExtraLineFare(final Line line, final List<Station> stations) {
            if (doesLineContainStation(line, stations)) {
                ...
            }
            ....
        }
    
        private boolean doesLineContainStation(final Line line, final List<Station> stations) {
            return stations.stream().anyMatch(line::containStation);
        }
    
        ...
    }

--- src/main/java/wooteco/subway/domain/Fare.java
+++ src/main/java/wooteco/subway/domain/Fare.java
@@ -22,10 +22,10 @@ public class Fare {
     public int calculate(final int distance, final List<Station> stations, final List<Line> lines) {
         final int extraLineFare = calculateExtraLineFare(stations, lines);
         int fare = DEFAULT_FARE + extraLineFare;
-        if (distance >= ADDITIONAL_DISTANCE_PER_5KM && distance < ADDITIONAL_DISTANCE_PER_8KM) {
+        if (ADDITIONAL_DISTANCE_PER_5KM <= distance && distance < ADDITIONAL_DISTANCE_PER_8KM) {
              fare += addExtraFare(distance, DISTANCE_UNIT_UNDER_50, ADDITIONAL_DISTANCE_PER_5KM);
         }
-        if (distance >= ADDITIONAL_DISTANCE_PER_8KM) {
+        if (ADDITIONAL_DISTANCE_PER_8KM <= distance) {
             fare = fare
                     + addExtraFare(ADDITIONAL_DISTANCE_PER_8KM - 1, DISTANCE_UNIT_UNDER_50, ADDITIONAL_DISTANCE_PER_5KM)
                     + addExtraFare(distance, DISTANCE_UNIT_OVER_50, ADDITIONAL_DISTANCE_PER_8KM - 1);
@@ -42,12 +42,14 @@ public class Fare {
     }
 
     private int findExtraLineFare(final Line line, final List<Station> stations) {
-        final boolean doesLineContainStation = stations.stream()
-                .anyMatch(line::containStation);
-        if (doesLineContainStation) {
+        if (doesLineContainStation(line, stations)) {
             return line.getExtraFare();
         }
         return DEFAULT_EXTRA_LINE_FARE;
+    }
+
+    private boolean doesLineContainStation(final Line line, final List<Station> stations) {
+        return stations.stream().anyMatch(line::containStation);
     }
 
     private int addExtraFare(final int distance, final int distanceUnit, final int limit) {
