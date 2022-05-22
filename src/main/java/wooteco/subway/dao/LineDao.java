aeabf6143bd77958cdd7d0c3e3189b0d9a4df686
youngwooyoo <fbduddn97@naver.com>
2022-05-22 16:15:36 +0900

    feat: extraFare db에 추가

--- src/main/java/wooteco/subway/dao/LineDao.java
+++ src/main/java/wooteco/subway/dao/LineDao.java
@@ -17,15 +17,16 @@ public class LineDao {
 
     public Optional<LineResponse> queryById(Long id, StationResponseSorter sorter) {
         String sql =
-            "SELECT l.id AS line_id, l.name AS line_name, l.color AS line_color, "
-                + "up.id AS up_station_id, up.name AS up_station_name, "
-                + "down.id AS down_station_id, down.name AS down_station_name "
-                + "FROM LINE AS l JOIN SECTION AS sec ON l.id = sec.line_id "
-                + "JOIN STATION AS up ON up.id = sec.up_station_id "
-                + "JOIN STATION AS down ON down.id = sec.down_station_id "
-                + "WHERE l.id = ?";
+                "SELECT l.id AS line_id, l.name AS line_name, l.color AS line_color, l.extra_fare AS line_extra_fare,"
+                        + "up.id AS up_station_id, up.name AS up_station_name, "
+                        + "down.id AS down_station_id, down.name AS down_station_name "
+                        + "FROM LINE AS l "
+                        + "JOIN SECTION AS sec ON l.id = sec.line_id "
+                        + "JOIN STATION AS up ON up.id = sec.up_station_id "
+                        + "JOIN STATION AS down ON down.id = sec.down_station_id "
+                        + "WHERE l.id = ?";
         List<LineResponse> responses = jdbcTemplate
-            .query(sql, new LineResponsesExtractor(sorter), id);
+                .query(sql, new LineResponsesExtractor(sorter), id);
 
         if (responses.size() == 1) {
             return Optional.of(responses.get(0));
@@ -35,12 +36,12 @@ public class LineDao {
 
     public List<LineResponse> queryAll(StationResponseSorter sorter) {
         String sql =
-            "SELECT l.id AS line_id, l.name AS line_name, l.color AS line_color, "
-                + "up.id AS up_station_id, up.name AS up_station_name, "
-                + "down.id AS down_station_id, down.name AS down_station_name "
-                + "FROM LINE AS l JOIN SECTION AS sec ON l.id = sec.line_id "
-                + "JOIN STATION AS up ON up.id = sec.up_station_id "
-                + "JOIN STATION AS down ON down.id = sec.down_station_id ";
+                "SELECT l.id AS line_id, l.name AS line_name, l.color AS line_color, l.extra_fare As line_extra_fare,"
+                        + "up.id AS up_station_id, up.name AS up_station_name, "
+                        + "down.id AS down_station_id, down.name AS down_station_name "
+                        + "FROM LINE AS l JOIN SECTION AS sec ON l.id = sec.line_id "
+                        + "JOIN STATION AS up ON up.id = sec.up_station_id "
+                        + "JOIN STATION AS down ON down.id = sec.down_station_id ";
         return jdbcTemplate.query(sql, new LineResponsesExtractor(sorter));
     }
 
