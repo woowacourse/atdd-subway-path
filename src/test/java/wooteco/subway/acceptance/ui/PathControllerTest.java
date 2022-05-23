d2cbea4b9dc6566be13610c169e175bdeab60e2b
EastHShin <xldk78@gmail.com>
2022-05-23 14:48:02 +0900

    test: 노선 추가요금에 대한 컨트롤러 통합 테스트 작성

--- src/test/java/wooteco/subway/acceptance/ui/PathControllerTest.java
+++ src/test/java/wooteco/subway/acceptance/ui/PathControllerTest.java
@@ -42,4 +42,21 @@ public class PathControllerTest {
                 .andExpect(jsonPath("stations[1].name").value(arrival.getName()))
                 .andDo(print());
     }
+
+    @DisplayName("추가 요금이 있는 노선 여러개를 지나면 가장 높은 추가요금을 적용한다.")
+    @Test
+    void getPathHavingExtraFareLine() throws Exception {
+        final Station departure = stationService.findStationById(4L);
+        final Station arrival = stationService.findStationById(6L);
+        final int expectedFare = 1750;
+
+        mockMvc.perform(get("/paths?source=" + departure.getId() + "&target=" + arrival.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("distance").value(10))
+                .andExpect(jsonPath("fare").value(expectedFare))
+                .andExpect(jsonPath("stations[0].name").value(departure.getName()))
+                .andExpect(jsonPath("stations[1].name").value("공덕역"))
+                .andExpect(jsonPath("stations[2].name").value(arrival.getName()))
+                .andDo(print());
+    }
 }
--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -2,10 +2,21 @@ INSERT INTO station (name) values('삼전역');
 INSERT INTO station (name) values('잠실역');
 INSERT INTO station (name) values('석촌역');
 
+INSERT INTO station (name) values('신촌역');
+INSERT INTO station (name) values('공덕역');
+INSERT INTO station (name) values('용산역');
+
 INSERT INTO line (name, color, extra_fare) values('9호선', '갈색', 0);
 INSERT INTO line (name, color, extra_fare) values('신분당선', '빨간색', 0);
 INSERT INTO line (name, color, extra_fare) values('경의중앙선', '민트색', 0);
 
+INSERT INTO line (name, color, extra_fare) values('2호선', '초록색', 300);
+INSERT INTO line (name, color, extra_fare) values('경의선', '파란색', 500);
+
 INSERT INTO sections (line_id, up_station_id, down_station_id, distance) values(1,1,2,10);
 INSERT INTO sections (line_id, up_station_id, down_station_id, distance) values(2,2,3,10);
 INSERT INTO sections (line_id, up_station_id, down_station_id, distance) values(3,1,3,10);
+
+INSERT INTO sections (line_id, up_station_id, down_station_id, distance) values(4,4,5,5);
+INSERT INTO sections (line_id, up_station_id, down_station_id, distance) values(5,5,6,5);
+
