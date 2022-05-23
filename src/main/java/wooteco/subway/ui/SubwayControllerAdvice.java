459c9d47d1a5d15fcf3bf92811bb7304d6e7655f
Richard JEON <ztzy1907@gmail.com>
2022-05-23 20:15:11 +0900

    refactor(controller-advice): 요청값 검증 예외 부모 클래스 핸들링하도록 개선

--- src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
@@ -3,8 +3,8 @@ package wooteco.subway.ui;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.exception.SubwayNotFoundException;
@@ -22,21 +22,26 @@ public class SubwayControllerAdvice {
         return new ResponseEntity<>(ExceptionResponse.from(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    public ResponseEntity<ExceptionResponse> requestValidationException(MethodArgumentNotValidException e) {
-        final String errorMessage = e.getBindingResult()
+    @ExceptionHandler({BindException.class})
+    public ResponseEntity<ExceptionResponse> requestValidationException(BindException e) {
+        final String errorMessage = parseErrorMessage(e);
+
+        return new ResponseEntity<>(ExceptionResponse.from(errorMessage), HttpStatus.BAD_REQUEST);
+    }
+
+    private String parseErrorMessage(BindException e) {
+        return e.getBindingResult()
                 .getAllErrors()
                 .stream()
                 .map(error -> ((FieldError) error).getField() + " : " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
-
-        return new ResponseEntity<>(ExceptionResponse.from(errorMessage), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SubwayNotFoundException.class)
     public ResponseEntity<ExceptionResponse> notFoundException(Exception e) {
         return new ResponseEntity<>(ExceptionResponse.from(e.getMessage()), HttpStatus.NOT_FOUND);
     }
+
 
     @ExceptionHandler({Exception.class, SubwayUnknownException.class})
     public ResponseEntity<ExceptionResponse> unknownException(Exception e) {
