0343f6d879f4025b655868ff2237f25de926b232
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-05-24 04:04:27 +0900

    refactor: RuntimeExceptionHandler에 로깅 추가

--- src/main/java/wooteco/subway/ui/ErrorControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/ErrorControllerAdvice.java
@@ -1,5 +1,7 @@
 package wooteco.subway.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +10,8 @@ import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ErrorControllerAdvice {
+
+    private final Logger logger = LoggerFactory.getLogger(ErrorControllerAdvice.class);
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> illegalStateExceptionHandler(Exception exception) {
@@ -26,7 +30,7 @@ public class ErrorControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Void> runtimeExceptionHandler(Exception exception) {
-        System.out.println(exception.getMessage());
+        logger.error("런타임 에러가 발생했습니다.", exception);
         return ResponseEntity.internalServerError().build();
     }
 }
