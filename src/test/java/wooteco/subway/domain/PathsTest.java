2726d4ed76f1e117ebfa9361cdce440e15e42072
koo <gudonghee2000@gmail.com>
2022-05-23 13:27:56 +0900

    refactor: Paths의 경로 조회 테스트시 거리도 함께 테스트하도록 변경

--- src/test/java/wooteco/subway/domain/PathsTest.java
+++ src/test/java/wooteco/subway/domain/PathsTest.java
@@ -8,36 +8,26 @@ import org.junit.jupiter.api.Test;
 
 class PathsTest {
 
-    @DisplayName("최단 경로를 구한다.")
+    @DisplayName("최단 경로와 경로에 따른 거리를 구한다.")
     @Test
     void calculateShortestPath() {
-        Section section1 = new Section(1L, 1L, new Station(1L, "잠실역"), new Station(2L, "선릉역"), 3);
-        Section section2 = new Section(2L, 2L, new Station(2L, "선릉역"), new Station(3L, "강남역"), 3);
-        Section section3 = new Section(3L, 2L, new Station(3L, "강남역"), new Station(4L, "건대역"), 4);
-        Section section4 = new Section(4L, 3L, new Station(4L, "건대역"), new Station(5L, "사가정역"), 5);
-        Section section5 = new Section(5L, 4L, new Station(5L, "사가정역"), new Station(1L, "잠실역"), 7);
-        Sections sections = new Sections(List.of(section1, section2, section3, section4, section5));
+        Station Jamsil = new Station(1L, "잠실역");
+        Station Seolleung = new Station(2L, "선릉역");
+        Station Gangnam = new Station(3L, "강남역");
+        Station Konkuk = new Station(4L, "건대역");
+        Station Sagajeong = new Station(5L, "사가정역");
+
+        Section first = new Section(1L, 1L, Jamsil, Seolleung, 3);
+        Section second = new Section(2L, 2L, Seolleung, Gangnam, 3);
+        Section third = new Section(3L, 2L, Gangnam, Konkuk, 4);
+        Section fourth = new Section(4L, 3L, Konkuk, Sagajeong, 5);
+        Section fifth = new Section(5L, 4L, Sagajeong, Jamsil, 7);
+        Sections sections = new Sections(List.of(first, second, third, fourth, fifth));
 
         SubwayGraph subwayGraph = new SubwayGraph(sections);
-        Paths paths = subwayGraph.createPathsResult(new Station(1L, "잠실역"), new Station(4L, "건대역"));
+        Paths paths = subwayGraph.createPathsResult(Jamsil, Konkuk);
 
-        assertThat(paths.getStations()).isEqualTo(List.of(new Station(1L, "잠실역"), new Station(2L, "선릉역")
-                , new Station(3L, "강남역"), new Station(4L, "건대역")));
-    }
-
-    @DisplayName("최단 경로의 거리를 구한다.")
-    @Test
-    void calculateShortestDistance() {
-        Section section1 = new Section(1L, 1L, new Station(1L, "잠실역"), new Station(2L, "선릉역"), 3);
-        Section section2 = new Section(2L, 2L, new Station(2L, "선릉역"), new Station(3L, "강남역"), 3);
-        Section section3 = new Section(3L, 2L, new Station(3L, "강남역"), new Station(4L, "건대역"), 4);
-        Section section4 = new Section(4L, 3L, new Station(4L, "건대역"), new Station(5L, "사가정역"), 5);
-        Section section5 = new Section(5L, 4L, new Station(5L, "사가정역"), new Station(1L, "잠실역"), 7);
-        Sections sections = new Sections(List.of(section1, section2, section3, section4, section5));
-
-        SubwayGraph subwayGraph = new SubwayGraph(sections);
-        Paths paths = subwayGraph.createPathsResult(new Station(1L, "잠실역"), new Station(4L, "건대역"));
-
+        assertThat(paths.getStations()).isEqualTo(List.of(Jamsil, Seolleung, Gangnam, Konkuk));
         assertThat(paths.getDistance()).isEqualTo(10);
     }
 }
