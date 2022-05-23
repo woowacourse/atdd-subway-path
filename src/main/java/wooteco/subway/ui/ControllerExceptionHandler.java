6edb3c902286d6185fa55cd61fec63a0fb2e1be5
dongho <dongho108@naver.com>
2022-05-23 20:52:04 +0900

    refactor: api 요청 예외 반환시 ExceptionResponse에 담아 보내도록 수정

--- src/main/java/wooteco/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/wooteco/subway/ui/ControllerExceptionHandler.java
@@ -1,8 +1,6 @@
 package wooteco.subway.ui;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -17,6 +15,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import wooteco.subway.exception.EmptyResultException;
 import wooteco.subway.exception.LowFareException;
+import wooteco.subway.ui.dto.ExceptionResponse;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -24,43 +23,36 @@ public class ControllerExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler
-    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException exception) {
-        Map<String, String> body = new HashMap<>();
-        body.put("message", exception.getMessage());
-        return ResponseEntity.badRequest().body(body);
+    public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(exception.getMessage()));
     }
 
     @ExceptionHandler
-    public ResponseEntity<Map<String, String>> handleEmptyResultException(EmptyResultException exception) {
+    public ResponseEntity<ExceptionResponse> handleEmptyResultException(EmptyResultException exception) {
         logger.error(exception.getMessage());
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler
-    public ResponseEntity<Map<String, String>> handleSqlException(DuplicateKeyException exception) {
+    public ResponseEntity<ExceptionResponse> handleSqlException(DuplicateKeyException exception) {
         logger.error(exception.getMessage());
-        Map<String, String> body = new HashMap<>();
-        body.put("message", "이미 존재하는 데이터 입니다.");
-        return ResponseEntity.badRequest().body(body);
+        return ResponseEntity.badRequest().body(new ExceptionResponse("이미 존재하는 데이터 입니다."));
     }
 
     @ExceptionHandler
-    public ResponseEntity<Map<String, List<String>>> handleValidateException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<List<ExceptionResponse>> handleValidateException(MethodArgumentNotValidException exception) {
         logger.error(exception.getMessage());
-        Map<String, List<String>> body = new HashMap<>();
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
-        List<String> messages = fieldErrors.stream()
+        List<ExceptionResponse> responses = fieldErrors.stream()
             .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .map(ExceptionResponse::new)
             .collect(Collectors.toList());
-        body.put("messages", messages);
-        return ResponseEntity.badRequest().body(body);
+        return ResponseEntity.badRequest().body(responses);
     }
 
     @ExceptionHandler({Exception.class, LowFareException.class})
-    public ResponseEntity<Map<String, String>> handleException(Exception exception) {
+    public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
         logger.error(exception.getMessage());
-        Map<String, String> body = new HashMap<>();
-        body.put("message", "서버 에러가 발생했습니다.");
-        return ResponseEntity.internalServerError().body(body);
+        return ResponseEntity.internalServerError().body(new ExceptionResponse("서버 에러가 발생했습니다."));
     }
 }
---
+++ src/main/java/wooteco/subway/ui/dto/ExceptionResponse.java
@@ -0,0 +1,13 @@
+package wooteco.subway.ui.dto;
+
+public class ExceptionResponse {
+    private final String message;
+
+    public ExceptionResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
