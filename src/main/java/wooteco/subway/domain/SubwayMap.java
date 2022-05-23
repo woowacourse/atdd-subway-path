025ed8ab651c4e6b30fa9e8ad87bb30622547f4c
sure-why-not <pkse1213@gmail.com>
2022-05-24 03:05:27 +0900

    feat(SubwayMap): 경로에 포함된 노선들의 최대 초과 요금 계산 기능 구현

--- src/main/java/wooteco/subway/domain/SubwayMap.java
+++ src/main/java/wooteco/subway/domain/SubwayMap.java
@@ -1,11 +1,10 @@
 package wooteco.subway.domain;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import wooteco.subway.exception.IllegalInputException;
 import wooteco.subway.exception.path.NoSuchPathException;
@@ -19,15 +18,15 @@ public class SubwayMap {
     }
 
     public List<Station> searchPath(final Station sourceStation, final Station targetStation) {
-        final GraphPath<Station, DefaultWeightedEdge> shortestPath = searchShortestPath(sourceStation, targetStation);
+        final GraphPath<Station, ShortestPathEdge> shortestPath = searchShortestPath(sourceStation, targetStation);
         return shortestPath.getVertexList();
     }
 
-    private GraphPath<Station, DefaultWeightedEdge> searchShortestPath(final Station sourceStation,
-                                                                       final Station targetStation) {
+    private GraphPath<Station, ShortestPathEdge> searchShortestPath(final Station sourceStation,
+                                                                    final Station targetStation) {
         validateStations(sourceStation, targetStation);
 
-        final GraphPath<Station, DefaultWeightedEdge> shortestPath = toShortestPath()
+        final GraphPath<Station, ShortestPathEdge> shortestPath = toShortestPath()
                 .getPath(sourceStation, targetStation);
 
         if (shortestPath == null) {
@@ -42,28 +41,46 @@ public class SubwayMap {
         }
     }
 
-    private DijkstraShortestPath<Station, DefaultWeightedEdge> toShortestPath() {
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
-
-        for (Section section : toSectionList()) {
-            graph.addVertex(section.getUpStation());
-            graph.addVertex(section.getDownStation());
-            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+    private DijkstraShortestPath<Station, ShortestPathEdge> toShortestPath() {
+        final WeightedMultigraph<Station, ShortestPathEdge> graph = new WeightedMultigraph<>(ShortestPathEdge.class);
+        for (Line line : lines) {
+            addVertexAndEdge(graph, line);
         }
 
         return new DijkstraShortestPath<>(graph);
     }
 
-    private List<Section> toSectionList() {
-        return lines.stream()
-                .map(Line::getSections)
-                .map(Sections::getValue)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+    private void addVertexAndEdge(final WeightedMultigraph<Station, ShortestPathEdge> graph, final Line line) {
+        for (Section section : toSectionList(line)) {
+            graph.addVertex(section.getUpStation());
+            graph.addVertex(section.getDownStation());
+            graph.addEdge(section.getUpStation(), section.getDownStation(),
+                    new ShortestPathEdge(line, section.getDistance()));
+        }
+    }
+
+    private List<Section> toSectionList(final Line line) {
+        return line.getSections().getValue();
     }
 
     public Distance searchDistance(final Station sourceStation, final Station targetStation) {
-        final GraphPath<Station, DefaultWeightedEdge> shortestPath = searchShortestPath(sourceStation, targetStation);
+        final GraphPath<Station, ShortestPathEdge> shortestPath = searchShortestPath(sourceStation, targetStation);
         return new Distance((int) shortestPath.getWeight());
+    }
+
+    public int searchExtraFareOfPath(final Station sourceStation, final Station targetStation) {
+        Set<Line> linesOfPath = searchLinesOfPath(sourceStation, targetStation);
+        return linesOfPath.stream()
+                .mapToInt(Line::getExtraFare)
+                .max()
+                .orElseThrow();
+    }
+
+    private Set<Line> searchLinesOfPath(final Station sourceStation, final Station targetStation) {
+        final GraphPath<Station, ShortestPathEdge> shortestPath = searchShortestPath(sourceStation, targetStation);
+        return shortestPath.getEdgeList()
+                .stream()
+                .map(ShortestPathEdge::getLine)
+                .collect(Collectors.toSet());
     }
 }
--- src/test/java/wooteco/subway/domain/SubwayMapTest.java
+++ src/test/java/wooteco/subway/domain/SubwayMapTest.java
@@ -37,31 +37,21 @@ class SubwayMapTest {
         geumho = new Station(8L, "금호역");
         oksu = new Station(9L, "옥수역");
 
-        final Section greenSectionA = new Section(null, gangnam, yeoksam, new Distance(10));
-        final Section greenSectionB = new Section(null, yeoksam, seolleung, new Distance(7));
-        final Section greenSectionC = new Section(null, seolleung, samsung, new Distance(11));
-        final Line greenLine = new Line(1L, new Name("2호선"), "green", new ExtraFare(1000),
-                new Sections(List.of(
-                        greenSectionA,
-                        greenSectionB,
-                        greenSectionC
-                )));
+        Line greenLine = new Line(1L, new Name("2호선"), "green", new ExtraFare(1000));
+        final Section greenSectionA = new Section(greenLine, gangnam, yeoksam, new Distance(10));
+        final Section greenSectionB = new Section(greenLine, yeoksam, seolleung, new Distance(7));
+        final Section greenSectionC = new Section(greenLine, seolleung, samsung, new Distance(11));
+        greenLine = greenLine.addSections(new Sections(List.of(greenSectionA, greenSectionB, greenSectionC)));
 
-        final Section yellowSectionA = new Section(null, seolleung, seoulForest, new Distance(3));
-        final Section yellowSectionB = new Section(null, seoulForest, wangsimni, new Distance(8));
-        final Line yellowLine = new Line(2L, new Name("수인분당선"), "yellow", new ExtraFare(800),
-                new Sections(List.of(
-                        yellowSectionA,
-                        yellowSectionB
-                )));
+        Line yellowLine = new Line(2L, new Name("수인분당선"), "yellow", new ExtraFare(800));
+        final Section yellowSectionA = new Section(yellowLine, seolleung, seoulForest, new Distance(3));
+        final Section yellowSectionB = new Section(yellowLine, seoulForest, wangsimni, new Distance(8));
+        yellowLine = yellowLine.addSections(new Sections(List.of(yellowSectionA, yellowSectionB)));
 
-        final Section orangeSectionA = new Section(null, yacksu, geumho, new Distance(12));
-        final Section orangeSectionB = new Section(null, geumho, oksu, new Distance(6));
-        final Line orangeLine = new Line(2L, new Name("3호선"), "orange", new ExtraFare(300),
-                new Sections(List.of(
-                        orangeSectionA,
-                        orangeSectionB
-                )));
+        Line orangeLine = new Line(3L, new Name("3호선"), "orange", new ExtraFare(300));
+        final Section orangeSectionA = new Section(orangeLine, yacksu, geumho, new Distance(12));
+        final Section orangeSectionB = new Section(orangeLine, geumho, oksu, new Distance(6));
+        orangeLine = orangeLine.addSections(new Sections(List.of(orangeSectionA, orangeSectionB)));
 
         subwayMap = new SubwayMap(List.of(greenLine, yellowLine, orangeLine));
     }
@@ -110,5 +100,15 @@ class SubwayMapTest {
 
         // then
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("경로에 포함된 노선들 중에 최대 초과 요금을 계산한다. (강남역 -> 서울숲역)")
+    void SearchExtraFareOfPath() {
+        //when
+        int actual = subwayMap.searchExtraFareOfPath(gangnam, seoulForest);
+
+        //then
+        assertThat(actual).isEqualTo(1000);
     }
 }
