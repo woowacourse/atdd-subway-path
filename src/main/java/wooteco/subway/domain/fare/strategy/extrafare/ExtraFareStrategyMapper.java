6fa012e8272a0571a9f9a1392ac4d65c04befd06
nailseong <nnis9857@gmail.com>
2022-05-23 17:16:56 +0900

    refactor(ExtraFareStrategyMapper): 메서드 추출 및 매직 넘버 상수 추출

--- src/main/java/wooteco/subway/domain/fare/strategy/extrafare/ExtraFareStrategyMapper.java
+++ src/main/java/wooteco/subway/domain/fare/strategy/extrafare/ExtraFareStrategyMapper.java
@@ -6,17 +6,32 @@ import wooteco.subway.domain.Distance;
 
 public enum ExtraFareStrategyMapper {
 
-    DEFAULT_STRATEGY(it -> it <= 10, DefaultExtraFareStrategy.getInstance()),
-    OVER_TEN_STRATEGY(it -> 10 < it && it <= 50, TenExtraFareStrategy.getInstance()),
-    OVER_FIFTY_STRATEGY(it -> 50 < it, FiftyExtraFareStrategy.getInstance()),
+    DEFAULT_STRATEGY(ExtraFareStrategyMapper::isDefault, DefaultExtraFareStrategy.getInstance()),
+    OVER_TEN_STRATEGY(ExtraFareStrategyMapper::isOverTen, TenExtraFareStrategy.getInstance()),
+    OVER_FIFTY_STRATEGY(ExtraFareStrategyMapper::isOverFifty, FiftyExtraFareStrategy.getInstance()),
     ;
 
+    private static final int DEFAULT_FARE_DISTANCE = 10;
+    private static final int EXTRA_FARE_DISTANCE = 50;
+    
     private final Predicate<Integer> predicate;
     private final ExtraFareStrategy strategy;
 
     ExtraFareStrategyMapper(final Predicate<Integer> predicate, final ExtraFareStrategy strategy) {
         this.predicate = predicate;
         this.strategy = strategy;
+    }
+
+    private static boolean isDefault(final Integer distance) {
+        return distance <= DEFAULT_FARE_DISTANCE;
+    }
+
+    private static boolean isOverTen(final Integer distance) {
+        return DEFAULT_FARE_DISTANCE < distance && distance <= EXTRA_FARE_DISTANCE;
+    }
+
+    private static boolean isOverFifty(final Integer distance) {
+        return EXTRA_FARE_DISTANCE < distance;
     }
 
     public static ExtraFareStrategy findStrategyBy(final Distance distance) {
