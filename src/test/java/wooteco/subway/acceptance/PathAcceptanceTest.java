4e2519b667a51870fdaed732855682f0b5cac55a
injoon2019 <ison@sfu.ca>
2022-05-23 12:41:04 +0900

    test: 경로 인수테스트 예외 케이스 추가

--- src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/PathAcceptanceTest.java
@@ -1,12 +1,14 @@
 package wooteco.subway.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.SectionRequest;
 import wooteco.subway.dto.StationRequest;
@@ -130,9 +132,6 @@ public class PathAcceptanceTest extends AcceptanceTest {
         Long 신분당선_id = httpPost("/lines", new LineRequest("신분당선", "빨간색", 강남역_id, 양재역_id, 5, 900))
                 .jsonPath().getLong("id");
 
-        //httpPost("/lines/" + 이호선_id + "/sections", new SectionRequest(교대역_id, 강남역_id, 5));
-        //httpPost("/lines/" + 신분당선_id + "/sections", new SectionRequest(강남역_id, 양재역_id, 5));
-
         // when
         ExtractableResponse<Response> response = httpGet("/paths?source=" + 교대역_id + "&target=" + 양재역_id + "&age=18");
 
@@ -143,5 +142,50 @@ public class PathAcceptanceTest extends AcceptanceTest {
                 () -> assertThat(response.jsonPath().getInt("distance")).isEqualTo(10),
                 () -> assertThat(response.jsonPath().getInt("fare")).isEqualTo((int)(2150 - (2150 - 350) * 0.2))
         );
+    }
+
+    @Test
+    @DisplayName("어린이의 추가 요금이 있는 노선과 없는 노선을 환승했을 때 최단 경로와 요금을 구한다.")
+    void showTransferLowestExtraShortestPath_Child() {
+        // given
+        Long 교대역_id = httpPost("/stations", new StationRequest("교대역")).jsonPath().getLong("id");
+        Long 강남역_id = httpPost("/stations", new StationRequest("강남역")).jsonPath().getLong("id");
+        Long 양재역_id = httpPost("/stations", new StationRequest("양재역")).jsonPath().getLong("id");
+
+        Long 이호선_id = httpPost("/lines", new LineRequest("2호선", "초록색", 교대역_id, 강남역_id, 5))
+                .jsonPath().getLong("id");
+        Long 신분당선_id = httpPost("/lines", new LineRequest("신분당선", "빨간색", 강남역_id, 양재역_id, 5, 900))
+                .jsonPath().getLong("id");
+
+        // when
+        ExtractableResponse<Response> response = httpGet("/paths?source=" + 교대역_id + "&target=" + 양재역_id + "&age=6");
+
+        // then
+        assertAll(
+                () -> assertThat(response.jsonPath().getList("stations")).extracting("name")
+                        .containsExactly("교대역", "강남역", "양재역"),
+                () -> assertThat(response.jsonPath().getInt("distance")).isEqualTo(10),
+                () -> assertThat(response.jsonPath().getInt("fare")).isEqualTo((int)(2150 - (2150 - 350) * 0.5))
+        );
+    }
+
+    @Test
+    @DisplayName("출발역과 도착역이 같을 경우 예외를 반환한다.")
+    void sameSourceAndTarget_throws_exception() {
+        // given
+        Long 교대역_id = httpPost("/stations", new StationRequest("교대역")).jsonPath().getLong("id");
+        Long 강남역_id = httpPost("/stations", new StationRequest("강남역")).jsonPath().getLong("id");
+        Long 양재역_id = httpPost("/stations", new StationRequest("양재역")).jsonPath().getLong("id");
+
+        Long 이호선_id = httpPost("/lines", new LineRequest("2호선", "초록색", 교대역_id, 강남역_id, 5))
+                .jsonPath().getLong("id");
+        Long 신분당선_id = httpPost("/lines", new LineRequest("신분당선", "빨간색", 강남역_id, 양재역_id, 5, 900))
+                .jsonPath().getLong("id");
+
+        // when && then
+        ExtractableResponse<Response> response = httpGet("/paths?source=" + 교대역_id + "&target=" + 교대역_id + "&age=6");
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
