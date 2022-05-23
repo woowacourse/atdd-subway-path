5c567ee5ff449afe5c85efe49f0e7388cc079400
koo <gudonghee2000@gmail.com>
2022-05-23 13:31:00 +0900

    refactor: 최단경로를 구하는 라이브러리의 클래스에 제네릭 추가

--- src/main/java/wooteco/subway/domain/SubwayGraph.java
+++ src/main/java/wooteco/subway/domain/SubwayGraph.java
@@ -11,7 +11,7 @@ public class SubwayGraph {
     private final WeightedMultigraph<Station, DefaultWeightedEdge> subwayGraph;
 
     public SubwayGraph(final Sections sections) {
-        subwayGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        subwayGraph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
         addStationsToSubwayVertex(sections);
         addSectionsToSubwayEdge(sections);
     }
@@ -30,15 +30,15 @@ public class SubwayGraph {
     }
 
     public Paths createPathsResult(final Station source, final Station target) {
-        GraphPath subwayGraphResult = createSubwayGraphResult(source, target);
+        GraphPath<Station, DefaultWeightedEdge> subwayGraphResult = createSubwayGraphResult(source, target);
         List<Station> stations = subwayGraphResult.getVertexList();
         double distance = subwayGraphResult.getWeight();
         int fare = createFare(distance);
         return new Paths(stations, distance, fare);
     }
 
-    private GraphPath createSubwayGraphResult(final Station source, final Station target) {
-        DijkstraShortestPath pathFinder = new DijkstraShortestPath(subwayGraph);
+    private GraphPath<Station, DefaultWeightedEdge> createSubwayGraphResult(final Station source, final Station target) {
+        DijkstraShortestPath<Station, DefaultWeightedEdge> pathFinder = new DijkstraShortestPath<>(subwayGraph);
         return pathFinder.getPath(source, target);
     }
 
