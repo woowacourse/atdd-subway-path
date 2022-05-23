61b902e013fa46fa34f65d6e74267d310767e22b
2yujeong <yjfflq4872@gmail.com>
2022-05-23 17:45:49 +0900

    refactor: 매직 넘버 상수화, 개행 정리

--- src/main/java/wooteco/subway/domain/DiscountPolicy.java
+++ src/main/java/wooteco/subway/domain/DiscountPolicy.java
@@ -1,14 +1,22 @@
 package wooteco.subway.domain;
 
+import static wooteco.subway.domain.DiscountPolicy.Constants.*;
+
 import java.util.Arrays;
 import java.util.function.IntPredicate;
 import java.util.function.IntUnaryOperator;
 
 public enum DiscountPolicy {
 
-    CHILDREN(age -> age >= 13 && age < 19, value -> (int) (value - ((value - 350) * 0.2))),
-    TEENAGER(age -> age >= 6 && age < 13, value -> (int) (value - ((value - 350) * 0.5))),
-    NO_DISCOUNT_AGE_GROUP(age -> false, value -> value);
+    CHILDREN(
+        age -> age >= 13 && age < 19,
+        value -> (int) (value - ((value - DEDUCTION_AMOUNT) * CHILDREN_DISCOUNT_PERCENTAGE))),
+    TEENAGER(
+        age -> age >= 6 && age < 13,
+        value -> (int) (value - ((value - DEDUCTION_AMOUNT) * TEENAGER_DISCOUNT_PERCENTAGE))),
+    NO_DISCOUNT_AGE_GROUP(
+        age -> false,
+        value -> value);
 
     private final IntPredicate agePolicy;
     private final IntUnaryOperator fareDiscountPolicy;
@@ -35,5 +43,12 @@ public enum DiscountPolicy {
 
     private IntUnaryOperator getFareDiscountPolicy() {
         return fareDiscountPolicy;
+    }
+
+    public static class Constants {
+
+        public static final int DEDUCTION_AMOUNT = 350;
+        public static final double CHILDREN_DISCOUNT_PERCENTAGE = 0.2;
+        public static final double TEENAGER_DISCOUNT_PERCENTAGE = 0.5;
     }
 }
