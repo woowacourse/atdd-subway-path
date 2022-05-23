e567cb15a3da0bf182d8107ec0af6a349df02fb3
bomi <bonita9931@gmail.com>
2022-05-23 17:13:54 +0900

    refactor: 사용하지 않는 변수 삭제

--- src/test/java/wooteco/subway/service/PathServiceTest.java
+++ src/test/java/wooteco/subway/service/PathServiceTest.java
@@ -8,10 +8,10 @@ import org.springframework.test.context.jdbc.Sql;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.dao.SectionDao;
 import wooteco.subway.dao.StationDao;
-import wooteco.subway.domain.Station;
 import wooteco.subway.dao.entity.LineEntity;
-import wooteco.subway.dto.PathResponse;
 import wooteco.subway.dao.entity.SectionEntity;
+import wooteco.subway.domain.Station;
+import wooteco.subway.dto.PathResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -45,11 +45,11 @@ public class PathServiceTest {
         LineEntity 이호선 = lineDao.save(new LineEntity("2호선", "pink"));
         LineEntity 삼호선 = lineDao.save(new LineEntity("3호선", "blue"));
 
-        SectionEntity 강남_선릉 = sectionDao.save(new SectionEntity(일호선.getId(), 강남역.getId(), 선릉역.getId(), 3));
-        SectionEntity 강남_잠실 = sectionDao.save(new SectionEntity(삼호선.getId(), 강남역.getId(), 잠실역.getId(), 3));
-        SectionEntity 강남_홍대 = sectionDao.save(new SectionEntity(이호선.getId(), 강남역.getId(), 홍대역.getId(), 10));
-        SectionEntity 선릉_홍대 = sectionDao.save(new SectionEntity(일호선.getId(), 선릉역.getId(), 홍대역.getId(), 2));
-        SectionEntity 잠실_홍대 = sectionDao.save(new SectionEntity(삼호선.getId(), 잠실역.getId(), 홍대역.getId(), 5));
+        sectionDao.save(new SectionEntity(일호선.getId(), 강남역.getId(), 선릉역.getId(), 3));
+        sectionDao.save(new SectionEntity(삼호선.getId(), 강남역.getId(), 잠실역.getId(), 3));
+        sectionDao.save(new SectionEntity(일호선.getId(), 선릉역.getId(), 홍대역.getId(), 2));
+        sectionDao.save(new SectionEntity(이호선.getId(), 강남역.getId(), 홍대역.getId(), 10));
+        sectionDao.save(new SectionEntity(삼호선.getId(), 잠실역.getId(), 홍대역.getId(), 5));
 
         //when
         PathResponse response = pathService.findPath(강남역.getId(), 홍대역.getId(), 10);
