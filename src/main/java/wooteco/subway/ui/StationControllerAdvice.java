2623d09b8f66668a6e1bfe2b6634e4f3cb3eb05a
jaejae-yoo <wotj102@gmail.com>
2022-05-23 11:42:42 +0900

    refactor: @RequestParam null 예외 처리

--- src/main/java/wooteco/subway/ui/StationControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/StationControllerAdvice.java
@@ -2,6 +2,7 @@ package wooteco.subway.ui;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.exception.ClientException;
@@ -12,7 +13,12 @@ import java.sql.SQLIntegrityConstraintViolationException;
 public class StationControllerAdvice {
 
     @ExceptionHandler({ClientException.class})
-    public ResponseEntity<String> IllegalArgumentExceptionHandle(ClientException exception) {
+    public ResponseEntity<String> handleIllegalArgumentException(ClientException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<String> handleMissingParams() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("입력 값을 모두 입력해주세요.");
     }
 }
