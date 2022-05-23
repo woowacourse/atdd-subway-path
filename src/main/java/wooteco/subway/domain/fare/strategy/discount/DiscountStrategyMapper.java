2ef2abca4e706c54f3bee7c491afe8b90d7baa89
nailseong <nnis9857@gmail.com>
2022-05-23 17:12:12 +0900

    refactor(DiscountStrategyMapper): 메서드 추출 및 매직 넘버 상수 추출

--- src/main/java/wooteco/subway/domain/fare/strategy/discount/DiscountStrategyMapper.java
+++ src/main/java/wooteco/subway/domain/fare/strategy/discount/DiscountStrategyMapper.java
@@ -5,18 +5,34 @@ import java.util.function.Predicate;
 
 public enum DiscountStrategyMapper {
 
-    DEFAULT_STRATEGY(it -> it < 6 || it >= 19, DefaultDiscountStrategy.getInstance()),
-    CHILD_STRATEGY(it -> 6 <= it && it < 13, ChildDiscountStrategy.getInstance()),
-    TEENAGER_STRATEGY(it -> 13 <= it && it < 19, TeenagerDiscountStrategy.getInstance()),
+    DEFAULT_STRATEGY(DiscountStrategyMapper::isDefault, DefaultDiscountStrategy.getInstance()),
+    CHILD_STRATEGY(DiscountStrategyMapper::isChild, ChildDiscountStrategy.getInstance()),
+    TEENAGER_STRATEGY(DiscountStrategyMapper::isTeenager, TeenagerDiscountStrategy.getInstance()),
     ;
+
+    private static final int CHILD_AGE_LOWER_BOUND = 6;
+    private static final int CHILD_AGE_UPPER_BOUND = 12;
+    private static final int TEENAGER_AGE_LOWER_BOUND = 13;
+    private static final int TEENAGER_AGE_UPPER_BOUND = 18;
 
     private final Predicate<Integer> predicate;
     private final DiscountStrategy strategy;
 
-    DiscountStrategyMapper(final Predicate<Integer> predicate,
-                           final DiscountStrategy strategy) {
+    DiscountStrategyMapper(final Predicate<Integer> predicate, final DiscountStrategy strategy) {
         this.predicate = predicate;
         this.strategy = strategy;
+    }
+
+    private static boolean isDefault(final Integer age) {
+        return age < CHILD_AGE_LOWER_BOUND || TEENAGER_AGE_UPPER_BOUND < age;
+    }
+
+    private static boolean isChild(final Integer age) {
+        return CHILD_AGE_LOWER_BOUND <= age && age <= CHILD_AGE_UPPER_BOUND;
+    }
+
+    private static boolean isTeenager(final Integer age) {
+        return TEENAGER_AGE_LOWER_BOUND <= age && age <= TEENAGER_AGE_UPPER_BOUND;
     }
 
     public static DiscountStrategy findStrategyBy(final int age) {
