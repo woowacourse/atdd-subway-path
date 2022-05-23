eb623f9bfda65ad80a1604372b9f5c4da45850da
koo <gudonghee2000@gmail.com>
2022-05-23 14:08:30 +0900

    refactor: 예상치 못한 예외를 처리하도록 ControllerAdvice를 함수 구조 변경

--- src/main/java/wooteco/subway/ui/ControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/ControllerAdvice.java
@@ -1,34 +1,34 @@
 package wooteco.subway.ui;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.exception.AddSectionException;
+import wooteco.subway.exception.CalculatePathsException;
 import wooteco.subway.exception.ClientException;
 import wooteco.subway.exception.DeleteSectionException;
 import wooteco.subway.exception.datanotfound.DataNotFoundException;
 import wooteco.subway.exception.duplicatename.DuplicateNameException;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
-    @ExceptionHandler({DuplicateNameException.class})
+    @ExceptionHandler({DuplicateNameException.class, CalculatePathsException.class,
+            AddSectionException.class, DeleteSectionException.class})
     public ResponseEntity<String> handleDuplicateNameException(ClientException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     @ExceptionHandler({DataNotFoundException.class})
     public ResponseEntity<String> handleDataNotFoundException(ClientException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+        return ResponseEntity.internalServerError().body(exception.getMessage());
     }
 
-    @ExceptionHandler({AddSectionException.class})
-    public ResponseEntity<String> handleAddSectionException(ClientException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
-    }
-
-    @ExceptionHandler({DeleteSectionException.class})
-    public ResponseEntity<String> handleDeleteSectionException(ClientException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<String> handleUnexpectedException(Exception exception) {
+        log.error(exception.getMessage());
+        return ResponseEntity.internalServerError().body("예상치 못한 에러가 발생했습니다.");
     }
 }
