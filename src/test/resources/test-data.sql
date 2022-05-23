8c7c09bf78981ab87c2422871dbf2b09817d3621
EastHShin <xldk78@gmail.com>
2022-05-23 14:35:01 +0900

    refactor: extra_fare 를 위한 sql 수정

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -2,9 +2,9 @@ INSERT INTO station (name) values('삼전역');
 INSERT INTO station (name) values('잠실역');
 INSERT INTO station (name) values('석촌역');
 
-INSERT INTO line (name, color) values('9호선', '갈색');
-INSERT INTO line (name, color) values('신분당선', '빨간색');
-INSERT INTO line (name, color) values('경의중앙선', '민트색');
+INSERT INTO line (name, color, extra_fare) values('9호선', '갈색', 0);
+INSERT INTO line (name, color, extra_fare) values('신분당선', '빨간색', 0);
+INSERT INTO line (name, color, extra_fare) values('경의중앙선', '민트색', 0);
 
 INSERT INTO sections (line_id, up_station_id, down_station_id, distance) values(1,1,2,10);
 INSERT INTO sections (line_id, up_station_id, down_station_id, distance) values(2,2,3,10);
