7da6885e6ea8caac0a1d2da04f24024bea75378b
devHudi <devhudi@gmail.com>
2022-05-24 15:18:47 +0900

    refactor: DiscountPolicy 의 필드로 할인율을 명시하는 대신 직접 할인 계산 로직을 갖도록 개선

--- src/main/java/wooteco/subway/domain/fare/Age.java
+++ src/main/java/wooteco/subway/domain/fare/Age.java
@@ -5,6 +5,14 @@ import wooteco.subway.exception.NegativeAgeException;
 
 public class Age {
 
+    private static final int BABY_MAX_AGE = 5;
+    private static final int CHILDREN_MIN_AGE = 6;
+    private static final int CHILDREN_MAX_AGE = 12;
+    private static final int TEENAGER_MIN_AGE = 13;
+    private static final int TEENAGER_MAX_AGE = 18;
+    private static final int ADULT_MIN_AGE = 19;
+    private static final int SENIOR_MIN_AGE = 65;
+
     private final int value;
 
     public Age(int value) {
@@ -18,16 +26,20 @@ public class Age {
         }
     }
 
-    public boolean isGreaterThanOrEqualTo(int age) {
-        return value >= age;
+    public boolean isBabyOrSenior() {
+        return value <= BABY_MAX_AGE || SENIOR_MIN_AGE <= value;
     }
 
-    public boolean isLessThanOrEqualTo(int age) {
-        return value <= age;
+    public boolean isChildren() {
+        return CHILDREN_MIN_AGE <= value && value <= CHILDREN_MAX_AGE;
     }
 
-    public boolean isBetween(int smallAge, int bigAge) {
-        return smallAge <= value && value <= bigAge;
+    public boolean isTeenager() {
+        return TEENAGER_MIN_AGE <= value && value <= TEENAGER_MAX_AGE;
+    }
+
+    public boolean isAdult() {
+        return ADULT_MIN_AGE <= value;
     }
 
     @Override
--- src/main/java/wooteco/subway/domain/fare/DiscountPolicy.java
+++ src/main/java/wooteco/subway/domain/fare/DiscountPolicy.java
@@ -2,22 +2,39 @@ package wooteco.subway.domain.fare;
 
 import java.util.Arrays;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public enum DiscountPolicy {
-    FREE((age) -> age.isLessThanOrEqualTo(5) || age.isGreaterThanOrEqualTo(65), 1),
-    CHILDREN((age) -> age.isBetween(6, 12), 0.5),
-    TEENAGER((age) -> age.isBetween(13, 18), 0.2),
-    ADULT((age) -> age.isGreaterThanOrEqualTo(19), 0),
+    BABY_OR_SENIOR(Age::isBabyOrSenior, DiscountPolicy::discountForBabyOrSenior),
+    CHILDREN(Age::isChildren, DiscountPolicy::discountForChildren),
+    TEENAGER(Age::isTeenager, DiscountPolicy::discountForTeenager),
+    ADULT(Age::isAdult, DiscountPolicy::discountForAdult),
     ;
 
     private static final int DEDUCTING_AMOUNT = 350;
 
     private final Predicate<Age> agePredicate;
-    private final double discountRate;
+    private final UnaryOperator<Fare> discount;
 
-    DiscountPolicy(Predicate<Age> agePredicate, double discountRate) {
+    DiscountPolicy(Predicate<Age> agePredicate, UnaryOperator<Fare> discount) {
         this.agePredicate = agePredicate;
-        this.discountRate = discountRate;
+        this.discount = discount;
+    }
+
+    private static Fare discountForBabyOrSenior(Fare fare) {
+        return new Fare(0);
+    }
+
+    private static Fare discountForChildren(Fare fare) {
+        return fare.subtract(DiscountPolicy.DEDUCTING_AMOUNT).multiply(0.5);
+    }
+
+    private static Fare discountForTeenager(Fare fare) {
+        return fare.subtract(DiscountPolicy.DEDUCTING_AMOUNT).multiply(0.8);
+    }
+
+    private static Fare discountForAdult(Fare fare) {
+        return fare;
     }
 
     public static DiscountPolicy from(Age age) {
@@ -28,11 +45,6 @@ public enum DiscountPolicy {
     }
 
     public Fare discount(Fare original) {
-        if (discountRate == 0) {
-            return original;
-        }
-
-        Fare deducted = original.subtract(DEDUCTING_AMOUNT);
-        return deducted.multiply(1 - discountRate);
+        return discount.apply(original);
     }
 }
