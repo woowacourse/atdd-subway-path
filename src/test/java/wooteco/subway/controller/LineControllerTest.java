629b2f50ba4f66321f4cb18a0a4a9129f82693be
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-05-25 03:40:24 +0900

    test: ControllerTest RestAssured -> mockMvc 테스트로 변경

--- src/test/java/wooteco/subway/controller/LineControllerTest.java
+++ src/test/java/wooteco/subway/controller/LineControllerTest.java
@@ -1,201 +1,245 @@
 package wooteco.subway.controller;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static wooteco.subway.acceptance.RestUtil.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import com.ori.acceptancetest.SpringBootAcceptanceTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import wooteco.subway.acceptance.RestUtil;
 import wooteco.subway.controller.dto.LineRequest;
 import wooteco.subway.controller.dto.LineUpdateRequest;
 import wooteco.subway.controller.dto.SectionRequest;
-import wooteco.subway.controller.dto.StationRequest;
+import wooteco.subway.domain.Station;
+import wooteco.subway.service.LineService;
+import wooteco.subway.service.PathService;
+import wooteco.subway.service.StationService;
 
-@SpringBootAcceptanceTest
+@WebMvcTest
+@AutoConfigureMockMvc
 public class LineControllerTest {
 
     private final String DEFAULT_LINE_URL = "/lines";
-    private Long stationId1;
-    private Long stationId2;
+    private final Station station1 = new Station(1L, "강남역");
+    private final Station station2 = new Station(2L, "역삼역");
 
-    @BeforeEach
-    void setStation() {
-        ExtractableResponse<Response> stationResponse1 = post(new StationRequest("강남역"));
-        ExtractableResponse<Response> stationResponse2 = post(new StationRequest("역삼역"));
-        stationId1 = RestUtil.getIdFromStation(stationResponse1);
-        stationId2 = RestUtil.getIdFromStation(stationResponse2);
-    }
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private LineService lineService;
+
+    @MockBean
+    private PathService pathService;
+
+    @MockBean
+    private StationService stationService;
 
     @DisplayName("노선 이름이 빈 생성 요청을 하면 400 에러가 발생한다.")
     @Test
-    void createLineWithNullName() {
-        // when
-        LineRequest lineRequest = new LineRequest(null, "bg-red-600", stationId1, stationId2, 10, 1000);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL, lineRequest);
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("노선 이름이 비었습니다.")
-        );
+    void createLineWithNullName() throws Exception {
+        // given
+        LineRequest lineRequest = new LineRequest(null, "bg-red-600", station1.getId(),
+            station2.getId(), 10, 1000);
+
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("노선 이름이 비었습니다."));
     }
 
     @DisplayName("노선 색상이 빈 생성 요청을 하면 400 에러가 발생한다.")
     @Test
-    void createLineWithNullColor() {
-        // when
-        LineRequest lineRequest = new LineRequest("2호선", null, stationId1, stationId2, 10, 1000);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL, lineRequest);
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("노선 색상이 비었습니다.")
-        );
+    void createLineWithNullColor() throws Exception {
+        // given
+        LineRequest lineRequest = new LineRequest("2호선", null, station1.getId(),
+            station2.getId(), 10, 1000);
+
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("노선 색상이 비었습니다."));
     }
 
     @DisplayName("상행 역 아이디가 빈 생성 요청을 하면 400 에러가 발생한다.")
     @Test
-    void createLineWithNullUpStationId() {
-        // when
-        LineRequest lineRequest = new LineRequest("2호선", "bg-red-600", null, stationId2, 10, 1000);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL, lineRequest);
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("상행 역의 id 가 비었습니다.")
-        );
+    void createLineWithNullUpStationId() throws Exception {
+        // given
+        LineRequest lineRequest = new LineRequest("2호선", "bg-red-600", null,
+            station2.getId(), 10, 1000);
+
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("상행 역의 id 가 비었습니다."));
     }
 
     @DisplayName("하행 역 아이디가 빈 생성 요청을 하면 400 에러가 발생한다.")
     @Test
-    void createLineWithNullDownStationId() {
-        // when
-        LineRequest lineRequest = new LineRequest("2호선", "bg-red-600", stationId1, null, 10, 1000);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL, lineRequest);
+    void createLineWithNullDownStationId() throws Exception {
+        // given
+        LineRequest lineRequest = new LineRequest("2호선", "bg-red-600", station1.getId(),
+            null, 10, 1000);
 
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("하행 역의 id 가 비었습니다.")
-        );
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("하행 역의 id 가 비었습니다."));
     }
 
     @DisplayName("거리가 1 이하인 생성 요청을 하면 400 에러가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, -1})
-    void createLineWithLessThan1(int distance) {
-        // when
-        LineRequest lineRequest = new LineRequest("2호선", "bg-red-600", stationId1, stationId2, distance, 1000);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL, lineRequest);
+    void createLineWithLessThan1(int distance) throws Exception {
+        // given
+        LineRequest lineRequest = new LineRequest("2호선", "bg-red-600", station1.getId(),
+            station2.getId(), distance, 1000);
 
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("거리는 1 이상이어야 합니다.")
-        );
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("거리는 1 이상이어야 합니다."));
     }
 
     @DisplayName("추가 요금이 음수인 생성 요청을 하면 400 에러가 발생한다.")
     @Test
-    void createLineWithNegative() {
-        // when
-        LineRequest lineRequest = new LineRequest("2호선", "bg-red-600", stationId1, stationId2, 10, -1);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL, lineRequest);
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("추가 요금 값은 음수일 수 없습니다.")
-        );
+    void createLineWithNegative() throws Exception {
+        // given
+        LineRequest lineRequest = new LineRequest("2호선", "bg-red-600", station1.getId(),
+            station2.getId(), 10, -1);
+
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("추가 요금 값은 음수일 수 없습니다."));
     }
 
     @DisplayName("노선 이름이 빈 수정 요청을 하면 400 에러가 발생한다.")
     @Test
-    void updateLineWithNullName() {
-        // when
+    void updateLineWithNullName() throws Exception {
+        // given
         LineUpdateRequest lineUpdateRequest = new LineUpdateRequest(null, "bg-red-600", 1000);
-        ExtractableResponse<Response> response = put(DEFAULT_LINE_URL + "/" + 1, lineUpdateRequest);
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("노선 이름이 비었습니다.")
-        );
+
+        // when, then
+        mvc.perform(put(DEFAULT_LINE_URL + "/" + 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineUpdateRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("노선 이름이 비었습니다."));
     }
 
     @DisplayName("노선 색상이 빈 수정 요청을 하면 400 에러가 발생한다.")
     @Test
-    void updateLineWithNullColor() {
-        // when
+    void updateLineWithNullColor() throws Exception {
+        // given
         LineUpdateRequest lineUpdateRequest = new LineUpdateRequest("2호선", null, 1000);
-        ExtractableResponse<Response> response = put(DEFAULT_LINE_URL + "/" + 1, lineUpdateRequest);
 
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("노선 색상이 비었습니다.")
-        );
+        // when, then
+        mvc.perform(put(DEFAULT_LINE_URL + "/" + 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineUpdateRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("노선 색상이 비었습니다."));
     }
 
     @DisplayName("노선 추가요금이 음수인 요청을 하면 400 에러가 발생한다.")
     @Test
-    void updateLineWithNullExtraFare() {
-        // when
+    void updateLineWithNullExtraFare() throws Exception {
+        // given
         LineUpdateRequest lineUpdateRequest = new LineUpdateRequest("2호선", "bg-red-600", -1);
-        ExtractableResponse<Response> response = put(DEFAULT_LINE_URL + "/" + 1, lineUpdateRequest);
 
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("추가 요금 값은 음수일 수 없습니다.")
-        );
+        // when, then
+        mvc.perform(put(DEFAULT_LINE_URL + "/" + 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(lineUpdateRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("추가 요금 값은 음수일 수 없습니다."));
     }
 
     @DisplayName("상행 역 아이디가 빈 구간 생성 요청을 하면 400 에러가 발생한다.")
     @Test
-    void createSectionWithNullUpStationId() {
-        // when
-        SectionRequest sectionRequest = new SectionRequest(null, stationId2, 10);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL + "/1/sections", sectionRequest);
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("상행 역의 id 가 비었습니다.")
-        );
+    void createSectionWithNullUpStationId() throws Exception {
+        // given
+        SectionRequest sectionRequest = new SectionRequest(null, station2.getId(), 10);
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL + "/1/sections")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(sectionRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("상행 역의 id 가 비었습니다."));
     }
 
     @DisplayName("하행 역 아이디가 빈 구간 생성 요청을 하면 400 에러가 발생한다.")
     @Test
-    void createSectionWithNullDownStationId() {
-        // when
-        SectionRequest sectionRequest = new SectionRequest(stationId1, null, 10);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL + "/1/sections", sectionRequest);
-
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("하행 역의 id 가 비었습니다.")
-        );
+    void createSectionWithNullDownStationId() throws Exception {
+        // given
+        SectionRequest sectionRequest = new SectionRequest(station1.getId(), null, 10);
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL + "/1/sections")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(sectionRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("하행 역의 id 가 비었습니다."));
     }
 
     @DisplayName("구간 거리가 1 미만인 구간 생성 요청을 하면 400 에러가 발생한다.")
     @Test
-    void createSectionWithDistanceLessThan1() {
-        // when
-        SectionRequest sectionRequest = new SectionRequest(stationId1, stationId2, -1);
-        ExtractableResponse<Response> response = post(DEFAULT_LINE_URL + "/1/sections", sectionRequest);
-
-        // then
-        assertAll(
-            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-            () -> assertThat(response.jsonPath().getString("message")).isEqualTo("거리는 1 이상이어야 합니다.")
-        );
+    void createSectionWithDistanceLessThan1() throws Exception {
+        // given
+        SectionRequest sectionRequest = new SectionRequest(station1.getId(), station2.getId(), 0);
+        // when, then
+        mvc.perform(post(DEFAULT_LINE_URL + "/1/sections")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(sectionRequest)
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("거리는 1 이상이어야 합니다."));
     }
 }
--- src/test/java/wooteco/subway/controller/StationControllerTest.java
+++ src/test/java/wooteco/subway/controller/StationControllerTest.java
@@ -1,28 +1,53 @@
 package wooteco.subway.controller;
 
-import static org.assertj.core.api.Assertions.*;
-import static wooteco.subway.acceptance.RestUtil.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import com.ori.acceptancetest.SpringBootAcceptanceTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import wooteco.subway.controller.dto.StationRequest;
+import wooteco.subway.service.LineService;
+import wooteco.subway.service.PathService;
+import wooteco.subway.service.StationService;
 
-@SpringBootAcceptanceTest
+@WebMvcTest
+@AutoConfigureMockMvc
 public class StationControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PathService pathService;
+
+    @MockBean
+    private LineService lineService;
+
+    @MockBean
+    private StationService stationService;
 
     @Test
     @DisplayName("이름이 빈 값을 요청하면 400 응답을 던진다.")
-    void createStationWithNull() {
-        // when
-        ExtractableResponse<Response> response = post("/stations", new StationRequest());
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        assertThat(response.jsonPath().getString("message")).isEqualTo("역 이름이 비었습니다.");
+    void createStationWithNull() throws Exception {
+        // when, then
+        mvc.perform(post("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectMapper.writeValueAsString(new StationRequest())
+                ))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("역 이름이 비었습니다."));
     }
 }
