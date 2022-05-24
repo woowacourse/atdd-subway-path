b739ccc4fafe7a92104f784286c4a973f0f4ef54
= <rerub0831@gmail.com>
2022-05-24 17:36:06 +0900

    refactor: getFare 호출 시 계산되어 미리 저장된 최종요금이 나오도록 로직 수정

--- src/main/java/wooteco/subway/domain/fare/Fare.java
+++ src/main/java/wooteco/subway/domain/fare/Fare.java
@@ -4,15 +4,17 @@ import java.util.List;
 import wooteco.subway.domain.fare.policy.FarePolicy;
 
 public class Fare {
-    private final List<FarePolicy> policies;
-    private final int baseFare;
+    private final int fare;
 
     public Fare(List<FarePolicy> policies, int baseFare) {
-        this.policies = policies;
-        this.baseFare = baseFare;
+        this.fare = calculateTotalFare(policies, baseFare);
     }
 
     public int getFare() {
+        return fare;
+    }
+
+    private int calculateTotalFare(List<FarePolicy> policies, int baseFare) {
         double result = baseFare;
         for (FarePolicy policy : policies) {
             result = policy.calculate(result);
