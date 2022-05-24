8f164a649849b2fb391c9ff86f078daebfccd475
Ohzzi <clover7even@naver.com>
2022-05-24 14:44:18 +0900

    refactor: PathStrategy 메서드 분리로 가독성 확보

--- src/main/java/wooteco/subway/domain/strategy/DijkstraPathStrategy.java
+++ src/main/java/wooteco/subway/domain/strategy/DijkstraPathStrategy.java
@@ -21,14 +21,19 @@ public class DijkstraPathStrategy implements PathStrategy {
     @Override
     public Path calculatePath(Station source, Station target, Sections sections) {
         validateSourceAndTargetNotSame(source, target);
-        WeightedMultigraph<Station, ShortestPathEdge> graph = new WeightedMultigraph<>(ShortestPathEdge.class);
-        addVertices(sections, graph);
-        addEdges(sections, graph);
+        WeightedMultigraph<Station, ShortestPathEdge> graph = createGraph(sections);
+        return createPath(source, target, graph);
+    }
+
+    private Path createPath(Station source, Station target, WeightedMultigraph<Station, ShortestPathEdge> graph) {
         DijkstraShortestPath<Station, ShortestPathEdge> shortestPath = new DijkstraShortestPath<>(graph);
         try {
             GraphPath<Station, ShortestPathEdge> graphPath = shortestPath.getPath(source, target);
-            return new Path(graphPath.getVertexList(), findPassedLines(graphPath),
-                    (int) shortestPath.getPathWeight(source, target));
+            List<Station> passedStations = graphPath.getVertexList();
+            List<Line> passedLines = findPassedLines(graphPath);
+            int distance = (int) shortestPath.getPathWeight(source, target);
+            
+            return new Path(passedStations, passedLines, distance);
         } catch (IllegalArgumentException e) {
             throw new InvalidPathRequestException("목적지까지 도달할 수 없습니다.");
         }
@@ -38,6 +43,13 @@ public class DijkstraPathStrategy implements PathStrategy {
         if (source.equals(target)) {
             throw new DuplicateStationException("경로의 시작과 끝은 같은 역일 수 없습니다.");
         }
+    }
+
+    private WeightedMultigraph<Station, ShortestPathEdge> createGraph(Sections sections) {
+        WeightedMultigraph<Station, ShortestPathEdge> graph = new WeightedMultigraph<>(ShortestPathEdge.class);
+        addVertices(sections, graph);
+        addEdges(sections, graph);
+        return graph;
     }
 
     private void addVertices(Sections sections, WeightedMultigraph<Station, ShortestPathEdge> graph) {
