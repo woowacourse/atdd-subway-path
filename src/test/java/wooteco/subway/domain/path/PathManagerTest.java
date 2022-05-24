5ba63aabaa8fef93645c8aa5b852820a0394a007
bcc0830 <bcc0830@naver.com>
2022-05-24 13:08:36 +0900

    refactor : 테스트케이스의 그래프에 대한 주석 작성

--- src/test/java/wooteco/subway/domain/path/PathManagerTest.java
+++ src/test/java/wooteco/subway/domain/path/PathManagerTest.java
@@ -21,7 +21,7 @@ class PathManagerTest {
     private final Station STATION5 = new Station(5L, "역5");
     private final Station STATION6 = new Station(6L, "역6");
 
-    private final Map<Long, Integer> COSTS = Map.of(1L, 10, 2L, 15, 3L, 10, 4L, 70, 5L, 100);
+    private final Map<Long, Integer> COSTS = Map.of(1L, 10, 2L, 15, 3L, 10, 4L, 70, 5L, 100, 6L, 30);
 
     @DisplayName("calculateOptimalPath 메서드는 두 지하철역 사이의 최단거리와 그 경로 정보를 계산하여_반환")
     @Nested
@@ -29,14 +29,21 @@ class PathManagerTest {
 
         @Test
         void 인접한_두_역_사이가_최단거리인_경우_해당_경로를_그대로_조회() {
+            /*
+            전체 호선 : 1호선, 2호선, 3호선, 4호선
+            1호선 구간 정보 : 1번역 - 2번역 - 3번역 - 5번역
+            2호선 구간 정보 : 2번역 - 4번역 - 5번역
+            3호선 구간 정보 : 2번역 - 5번역
+            4호선 구간 정보 : 3번역 - 6번역
+            */
             List<Section> sections = List.of(
                     new Section(1L, STATION1, STATION2, 100),
                     new Section(1L, STATION2, STATION3, 1),
-                    new Section(1L, STATION2, STATION4, 100),
-                    new Section(1L, STATION4, STATION5, 100),
-                    new Section(1L, STATION2, STATION5, 100),
+                    new Section(2L, STATION2, STATION4, 100),
+                    new Section(2L, STATION4, STATION5, 100),
+                    new Section(3L, STATION2, STATION5, 100),
                     new Section(1L, STATION5, STATION3, 100),
-                    new Section(1L, STATION3, STATION6, 100));
+                    new Section(4L, STATION3, STATION6, 100));
             PathManager pathManager = PathManager.of(GraphGenerator.toAdjacentPath(sections, COSTS));
 
             Path actual = pathManager.calculateOptimalPath(STATION2, STATION3);
@@ -47,26 +54,38 @@ class PathManagerTest {
 
         @Test
         void 인접한_두_역_사이가_최단거리가_아닌_경우_최적의_경로를_계산하여_조회() {
+            /*
+            전체 호선 : 1호선, 2호선, 3호선, 4호선
+            1호선 구간 정보 : 1번역 - 2번역 - 3번역 - 5번역
+            2호선 구간 정보 : 2번역 - 4번역 - 5번역
+            3호선 구간 정보 : 2번역 - 5번역
+            4호선 구간 정보 : 3번역 - 6번역
+            */
             List<Section> sections = List.of(
                     new Section(1L, STATION1, STATION2, 1),
                     new Section(1L, STATION2, STATION3, 100),
-                    new Section(1L,STATION2, STATION4, 2),
-                    new Section(1L,STATION4, STATION5, 3),
-                    new Section(1L,STATION2, STATION5, 100),
+                    new Section(2L,STATION2, STATION4, 2),
+                    new Section(2L,STATION4, STATION5, 3),
+                    new Section(3L,STATION2, STATION5, 100),
                     new Section(1L,STATION5, STATION3, 5),
-                    new Section(1L,STATION3, STATION6, 6));
+                    new Section(4L,STATION3, STATION6, 6));
             PathManager pathManager = PathManager.of(GraphGenerator.toAdjacentPath(sections, COSTS));
 
             Path actual = pathManager.calculateOptimalPath(STATION2, STATION3);
-            Path expected = new Path(10, List.of(STATION2, STATION4, STATION5, STATION3), 10);
+            Path expected = new Path(10, List.of(STATION2, STATION4, STATION5, STATION3), 15);
 
             assertThat(actual).isEqualTo(expected);
         }
 
         @Test
         void 노선에_구간으로_등록되지_않은_역에_대한_경로를_조회하려는_경우_예외_발생() {
+            /*
+            전체 호선 : 1호선
+            1호선 구간 정보 : 1번역 - 2번역 - 3번역 - 4번역
+            */
             Station nonRegisteredStation = new Station(999L, "등록되지 않은 역");
-            List<Section> sections = List.of(new Section(1L,STATION1, STATION2, 10),
+            List<Section> sections = List.of(
+                    new Section(1L,STATION1, STATION2, 10),
                     new Section(1L,STATION2, STATION3, 100),
                     new Section(1L,STATION3, STATION4, 20));
             PathManager pathManager = PathManager.of(GraphGenerator.toAdjacentPath(sections, COSTS));
@@ -77,7 +96,12 @@ class PathManagerTest {
 
         @Test
         void 출발점과_도착점이_동일한_경우_예외_발생() {
-            List<Section> sections = List.of(new Section(1L, STATION1, STATION2, 10));
+            /*
+            전체 호선 : 1호선
+            1호선 구간 정보 : 1번역 - 2번역
+            */
+            List<Section> sections = List.of(
+                    new Section(1L, STATION1, STATION2, 10));
             PathManager pathManager = PathManager.of(GraphGenerator.toAdjacentPath(sections, COSTS));
 
             assertThatThrownBy(() -> pathManager.calculateOptimalPath(STATION1, STATION1))
@@ -86,8 +110,14 @@ class PathManagerTest {
 
         @Test
         void 도달할_수_없는_경로를_조회하려는_경우_예외_발생() {
-            List<Section> sections = List.of(new Section(1L, STATION1, STATION2, 10),
-                    new Section(1L,STATION3, STATION4, 20));
+            /*
+            전체 호선 : 1호선, 2호선
+            1호선 구간 정보 : 1번역 - 2번역
+            2호선 구간 정보 : 3번역 - 4번역
+            */
+            List<Section> sections = List.of(
+                    new Section(1L, STATION1, STATION2, 10),
+                    new Section(2L,STATION3, STATION4, 20));
             PathManager pathManager = PathManager.of(GraphGenerator.toAdjacentPath(sections, COSTS));
 
             assertThatThrownBy(() -> pathManager.calculateOptimalPath(STATION1, STATION3))
@@ -96,6 +126,15 @@ class PathManagerTest {
 
         @Test
         void 최단거리_경로가_2개이상이지만_유일한_최소비용경로가_존재하는_경우() {
+            /*
+            전체 호선 : 1호선, 2호선, 3호선, 4호선, 5호선, 6호선
+            1호선 구간 정보 : 2번역 - 4번역
+            2호선 구간 정보 : 4번역 - 5번역
+            3호선 구간 정보 : 3번역 - 5번역
+            4호선 구간 정보 : 1번역 - 2번역 - 5번역
+            5호선 구간 정보 : 2번역 - 3번역 - 6번역
+            6호선 구간 정보 : 2번역 - 4번역
+            */
             List<Section> sections = List.of(
                     new Section(4L, STATION1, STATION2, 1),
                     new Section(5L, STATION2, STATION3, 100),
@@ -103,7 +142,7 @@ class PathManagerTest {
                     new Section(2L, STATION4, STATION5, 3),
                     new Section(4L, STATION2, STATION5, 100),
                     new Section(3L, STATION5, STATION3, 5),
-                    new Section(5L, STATION2, STATION4, 2),
+                    new Section(6L, STATION2, STATION4, 2),
                     new Section(5L, STATION3, STATION6, 6));
             PathManager pathManager = PathManager.of(GraphGenerator.toAdjacentPath(sections, COSTS));
 
@@ -115,6 +154,11 @@ class PathManagerTest {
 
         @Test
         void 최소비용_경로가_2개이상이지만_유일한_최단경로가_존재하는_경우() {
+            /*
+            전체 호선 : 1호선, 3호선
+            1호선 구간 정보 : 1번역 - 2번역 - 4번역
+            3호선 구간 정보 : 1번역 - 3번역 - 4번역
+            */
             List<Section> sections = List.of(
                     new Section(1L, STATION1, STATION2, 100),
                     new Section(1L, STATION2, STATION4, 100),
