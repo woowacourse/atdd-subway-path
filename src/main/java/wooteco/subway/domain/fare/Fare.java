fdc77378f266234c65e8f5fec377d62bbae7d887
SeungCheol <ssc6839@gmail.com>
2022-05-24 22:27:11 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/wooteco/subway/domain/fare/Fare.java
+++ src/main/java/wooteco/subway/domain/fare/Fare.java
@@ -5,13 +5,6 @@ import wooteco.subway.domain.fare.distance.DistancePolicy;
 
 public class Fare {
 
-    private static final int OVER_FARE_PRICE = 100;
-    private static final int MINIMUM_FARE_PRICE = 1250;
-    private static final int MINIMUM_DISTANCE_BOUNDARY = 10;
-    private static final int MAXIMUM_DISTANCE_BOUNDARY = 50;
-    private static final int BELOW_FIFTY_KM_POLICY = 5;
-    private static final int ABOVE_FIFTY_KM_POLICY = 8;
-
     private final int price;
 
     public Fare(final int price) {
@@ -23,28 +16,8 @@ public class Fare {
     }
 
     private static int calculateFare(final double distance, final int extraFare, final int age) {
-        int fare = DistancePolicy.calculate(distance) + extraFare;
+        final int fare = DistancePolicy.calculate(distance) + extraFare;
         return AgePolicy.discount(age, fare);
-    }
-
-    private static int distancePolicy(final double distance, final int price) {
-        if (distance <= MINIMUM_DISTANCE_BOUNDARY) {
-            return price;
-        }
-
-        return price + calculateFarePrice(distance);
-    }
-
-    private static int calculateFarePrice(final double distance) {
-        if (distance <= MAXIMUM_DISTANCE_BOUNDARY) {
-            return calculateFareFeeByPolicy(distance - MINIMUM_DISTANCE_BOUNDARY, BELOW_FIFTY_KM_POLICY);
-        }
-
-        return 800 + calculateFareFeeByPolicy(distance - MAXIMUM_DISTANCE_BOUNDARY, ABOVE_FIFTY_KM_POLICY);
-    }
-
-    private static int calculateFareFeeByPolicy(final double overFaredDistance, final int policy) {
-        return (int) ((Math.ceil((overFaredDistance) / policy)) * OVER_FARE_PRICE);
     }
 
     public int getPrice() {
