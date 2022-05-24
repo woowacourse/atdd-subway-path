bcabda26e345af6e66a20a19e9eeee4855202840
SuyeonChoi <tndus970120@gmail.com>
2022-05-24 22:27:56 +0900

    refactor: fare 패키지 생성 및 코드 리포맷팅

--- src/main/java/wooteco/subway/service/PathService.java
+++ src/main/java/wooteco/subway/service/PathService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
-import wooteco.subway.domain.Fare;
+import wooteco.subway.domain.fare.Fare;
 import wooteco.subway.domain.line.Line;
 import wooteco.subway.domain.path.Path;
 import wooteco.subway.domain.station.Stations;
