0f00d29c204f10aea0513ae06f1f34bab23f432b
yoon <duddbs774@gmail.com>
2022-05-25 02:15:46 +0900

    refactor: 요청값에 검증에 대한 예외 발생시 에만 badRequest 상태코드를 반환하도록함

--- src/main/java/wooteco/subway/controller/SubwayAdvice.java
+++ src/main/java/wooteco/subway/controller/SubwayAdvice.java
@@ -1,5 +1,6 @@
 package wooteco.subway.controller;
 
+import java.util.NoSuchElementException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,7 +8,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class SubwayAdvice {
     @ExceptionHandler({Exception.class})
-    public ResponseEntity<String> handle(Exception e) {
+    public ResponseEntity<Void> handle(Exception e) {
+        return ResponseEntity.internalServerError().build();
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, NoSuchElementException.class})
+    public ResponseEntity<String> handle(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
--- src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -15,44 +15,64 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import wooteco.subway.domain.Station;
 import wooteco.subway.dto.line.LineResponse;
 
 public class LineAcceptanceTest extends AcceptanceTest {
 
+    private static final int EXTRA_FARE = 500;
+    private static final int DISTANCE = 2;
+    private static final String LINE_NAME = "테스트1호선";
+    private static final String LINE_COLOR = "테스트1색";
+    private static final String STATION_NAME1 = "테스트1역";
+    private static final String STATION_NAME2 = "테스트2역";
     private ExtractableResponse<Response> responseCreateLine;
+    private Long upStationId;
 
     @Override
     @BeforeEach
     public void setUp() {
         super.setUp();
-        responseCreateLine = insertLine("테스트호선", "테스트색", List.of("테스트1역", "테스트2역"));
+        responseCreateLine = insertLine(LINE_NAME, LINE_COLOR, List.of(STATION_NAME1, STATION_NAME2));
     }
 
     private ExtractableResponse<Response> insertLine(String name, String color, List<String> stationNames) {
         var response1 = create("/stations", Map.of("name", stationNames.get(0)));
         var response2 = create("/stations", Map.of("name", stationNames.get(1)));
 
+        upStationId = Long.parseLong(getId(response1));
+
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
         params.put("color", color);
         params.put("upStationId", getId(response1));
         params.put("downStationId", getId(response2));
-        params.put("distance", "4");
+        params.put("distance", String.valueOf(DISTANCE));
+        params.put("extraFare", String.valueOf(EXTRA_FARE));
 
         return create("/lines", params);
     }
 
     @Test
     void createLine() {
+        //given
+        var stations = responseCreateLine.jsonPath().getList("stations", Station.class);
+        var names = stations.stream()
+                .map(Station::getName)
+                .collect(Collectors.toList());
+
+        //when
         assertAll(
-                () -> assertThat(responseCreateLine.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
-                () -> assertThat(responseCreateLine.header("Location")).isNotBlank()
+                () -> assertThat(responseCreateLine.jsonPath().getString("name")).isEqualTo(LINE_NAME),
+                () -> assertThat(responseCreateLine.jsonPath().getString("color")).isEqualTo(LINE_COLOR),
+                () -> assertThat(responseCreateLine.jsonPath().getInt("extraFare")).isEqualTo(EXTRA_FARE),
+                () -> assertThat(names).containsExactly(STATION_NAME1, STATION_NAME2)
         );
     }
 
     @Test
     void createLineByDuplicateName() {
-        var response = insertLine("테스트호선", "테스트색", List.of("테스트3역", "테스트4역"));
+        var response = insertLine(LINE_NAME, "테스트색", List.of("테스트3역", "테스트4역"));
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
@@ -63,8 +83,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(response.body().jsonPath().get("name").toString()).isEqualTo("테스트호선"),
-                () -> assertThat(response.body().jsonPath().get("color").toString()).isEqualTo("테스트색")
+                () -> assertThat(response.body().jsonPath().get("name").toString()).isEqualTo(LINE_NAME),
+                () -> assertThat(response.body().jsonPath().get("color").toString()).isEqualTo(LINE_COLOR)
         );
     }
 
@@ -160,28 +180,31 @@ public class LineAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
-    void createSection() {
-        var response = requestCreateSection();
+    void createSectionByValidInformation() {
+        var newDownStationId = 100L;
+
+        var response = create(upStationId, newDownStationId);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
-    private ExtractableResponse<Response> requestCreateSection() {
+    private ExtractableResponse<Response> create(Long upStationId, Long downStationId) {
         var path = "/lines/" + getId(responseCreateLine) + "/sections";
 
         Map<String, String> params = new HashMap<>();
-        params.put("upStationId", "1");
-        params.put("downStationId", "100");
-        params.put("distance", "3");
+        params.put("upStationId", upStationId.toString());
+        params.put("downStationId", downStationId.toString());
+        params.put("distance", "1");
 
         return create(path, params);
     }
 
     @Test
     void deleteSection() {
-        requestCreateSection();
+        var newDownStationId = 100L;
+        create(upStationId, newDownStationId);
 
-        var path = "/lines/" + getId(responseCreateLine) + "/sections?stationId=1";
+        var path = "/lines/" + getId(responseCreateLine) + "/sections?stationId=" + upStationId;
         var response = delete(path);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
