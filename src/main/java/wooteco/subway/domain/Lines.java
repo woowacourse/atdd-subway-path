96391eb1b5cfa2e8d876ab2996222cf853da5818
jurlring <78091011+jurlring@users.noreply.github.com>
2022-05-24 20:38:47 +0900

    feat: 가장 높은 금액의 추가 요금 찾기

---
+++ src/main/java/wooteco/subway/domain/Lines.java
@@ -0,0 +1,27 @@
+package wooteco.subway.domain;
+
+import java.util.List;
+import wooteco.subway.exception.datanotfound.LineNotFoundException;
+
+public class Lines {
+
+    private final List<Line> lines;
+
+    public Lines(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public int findMaxExtraFare(final List<Long> lineIds) {
+        return lineIds.stream()
+                .mapToInt(id -> findExtraFare(id).getExtraFare())
+                .max()
+                .orElseThrow(() -> new LineNotFoundException("Line이 존재하지 않습니다."));
+    }
+
+    private Line findExtraFare(final Long id) {
+        return lines.stream()
+                .filter(line -> line.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new LineNotFoundException("Line이 존재하지 않습니다."));
+    }
+}
