1328fceaa054082533b2cb4327a20c1162df700d
kth990303 <kth990303@naver.com>
2022-05-24 15:37:55 +0900

    refactor: db 길이제한과 일치하도록 `@Valid` 조건설정 추가

--- src/main/java/wooteco/subway/dto/StationRequest.java
+++ src/main/java/wooteco/subway/dto/StationRequest.java
@@ -1,9 +1,12 @@
 package wooteco.subway.dto;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.validation.constraints.NotBlank;
 
 public class StationRequest {
     @NotBlank(message = "역 이름을 입력해야 합니다.")
+    @Length(max = 255)
     private String name;
 
     private StationRequest() {
