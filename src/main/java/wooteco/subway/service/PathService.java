5f7c5b13ef044d14cdf01d528ed81fedbb60eabb
nailseong <nnis9857@gmail.com>
2022-05-24 21:17:56 +0900

    refactor(PathService): 다른 service 에 의존하도록 변경

--- src/main/java/wooteco/subway/service/PathService.java
+++ src/main/java/wooteco/subway/service/PathService.java
@@ -3,33 +3,30 @@ package wooteco.subway.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import wooteco.subway.dao.LineDao;
-import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Distance;
 import wooteco.subway.domain.Line;
 import wooteco.subway.domain.Path;
 import wooteco.subway.domain.Station;
 import wooteco.subway.domain.fare.Fare;
 import wooteco.subway.domain.subwaymap.SubwayMap;
-import wooteco.subway.exception.station.NoSuchStationException;
 
 @Service
 @Transactional
 public class PathService {
 
-    private final LineDao lineDao;
-    private final StationDao stationDao;
+    private final LineService lineService;
+    private final StationService stationService;
 
-    public PathService(final LineDao lineDao, final StationDao stationDao) {
-        this.lineDao = lineDao;
-        this.stationDao = stationDao;
+    public PathService(final LineService lineService, final StationService stationService) {
+        this.lineService = lineService;
+        this.stationService = stationService;
     }
 
     @Transactional(readOnly = true)
     public Path find(final Long sourceStationId, final Long targetStationId, final int age) {
         final SubwayMap subwayMap = toSubwayMap();
-        final Station sourceStation = findStationById(sourceStationId);
-        final Station targetStation = findStationById(targetStationId);
+        final Station sourceStation = stationService.findById(sourceStationId);
+        final Station targetStation = stationService.findById(targetStationId);
 
         final List<Station> stations = subwayMap.searchPath(sourceStation, targetStation);
         final Distance distance = subwayMap.searchDistance(sourceStation, targetStation);
@@ -42,12 +39,7 @@ public class PathService {
     }
 
     private SubwayMap toSubwayMap() {
-        final List<Line> lines = lineDao.findAll();
+        final List<Line> lines = lineService.findAll();
         return new SubwayMap(lines);
-    }
-
-    private Station findStationById(final Long stationId) {
-        return stationDao.findById(stationId)
-                .orElseThrow(NoSuchStationException::new);
     }
 }
