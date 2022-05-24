7ce274531baa3e9c291c8d7fce49d184da6d00eb
her0807 <her0807@naver.com>
2022-05-24 09:32:02 +0900

    fix: 어린이 요금 정책 350원 공제하도록 변경

--- src/test/java/wooteco/subway/domain/strategy/discount/KidDiscountStrategyTest.java
+++ src/test/java/wooteco/subway/domain/strategy/discount/KidDiscountStrategyTest.java
@@ -1,20 +1,18 @@
-package wooteco.subway.domain.strategy;
+package wooteco.subway.domain.strategy.discount;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import wooteco.subway.domain.strategy.discount.KidDiscountStrategy;
 
 class KidDiscountStrategyTest {
 
     @DisplayName("50% 가 할인되는지 확인한다.")
     @ParameterizedTest
-    @CsvSource({"2000,1175", "2350,1350"})
+    @CsvSource({"1250,450", "2350,1000", "3650,1650"})
     void calculateAdolescentDiscountStrategy(int actual, int expected) {
         KidDiscountStrategy discountStrategy = new KidDiscountStrategy();
         assertThat(discountStrategy.calculate(actual)).isEqualTo(expected);
     }
-
 }
