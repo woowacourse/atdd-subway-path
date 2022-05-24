43d30864d6675749cc28ca285ffd74ec667a9a02
yeongunheo <gjduddns@gmail.com>
2022-05-24 15:27:43 +0900

    refactor: BasicFareStrategy에서 5, 8 상수를 int에서 double로 변경

--- src/main/java/wooteco/subway/domain/BasicFareStrategy.java
+++ src/main/java/wooteco/subway/domain/BasicFareStrategy.java
@@ -7,8 +7,8 @@ public class BasicFareStrategy implements FareStrategy {
     private static final int FIRST_OVER_DISTANCE = 10;
     private static final int SECOND_OVER_DISTANCE = 50;
     private static final int OVER_FARE_STANDARD = 100;
-    private static final int FIRST_OVER_DISTANCE_STANDARD = 5;
-    private static final int SECOND_OVER_DISTANCE_STANDARD = 8;
+    private static final double FIRST_OVER_DISTANCE_STANDARD = 5;
+    private static final double SECOND_OVER_DISTANCE_STANDARD = 8;
     private static final int MAX_FARE_FROM_TEN_TO_FIFTY = 800;
 
     @Override
@@ -26,10 +26,10 @@ public class BasicFareStrategy implements FareStrategy {
     }
 
     private int calculateOverFromTenToFifty(int distance) {
-        return (int) ((Math.ceil((distance - 1) / FIRST_OVER_DISTANCE_STANDARD) + 1) * OVER_FARE_STANDARD);
+        return (int) ((Math.ceil(distance / FIRST_OVER_DISTANCE_STANDARD) * OVER_FARE_STANDARD));
     }
 
     private int calculateOverFifty(int overDistance) {
-        return (int) ((Math.ceil((overDistance - 1) / SECOND_OVER_DISTANCE_STANDARD) + 1) * OVER_FARE_STANDARD);
+        return (int) ((Math.ceil(overDistance / SECOND_OVER_DISTANCE_STANDARD) * OVER_FARE_STANDARD));
     }
 }
