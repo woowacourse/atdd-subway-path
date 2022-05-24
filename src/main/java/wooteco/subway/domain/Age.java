474ea749dfa54e1aef5b5ec1c1dd043dd3ef924f
corinne <yoo77hyeon@gmail.com>
2022-05-24 14:56:13 +0900

    refactor: Enum Constant 적용

--- src/main/java/wooteco/subway/domain/Age.java
+++ src/main/java/wooteco/subway/domain/Age.java
@@ -7,8 +7,8 @@ import java.util.function.UnaryOperator;
 public enum Age {
 
     INFANTS(age -> age >= 0 && age < 6, fare -> 0),
-    CHILDREN(age -> age >= 6 && age < 13, fare -> (int) ((fare - 350) * (1 - 0.5))),
-    TEENAGERS(age -> age >= 13 && age < 19, fare -> (int) ((fare - 350) * (1 - 0.2))),
+    CHILDREN(age -> age >= 6 && age < 13, fare -> (int) ((fare - Discount.FARE) * (1 - Discount.CHILDREN_RATIO))),
+    TEENAGERS(age -> age >= 13 && age < 19, fare -> (int) ((fare - Discount.FARE) * (1 - Discount.TEENAGER_RATIO))),
     ADULTS(age -> age >= 19, fare -> fare),
     ;
 
@@ -29,5 +29,11 @@ public enum Age {
 
     public int discountFare(final int fare) {
         return this.fareCalculator.apply(fare);
+    }
+
+    private static class Discount {
+        private static final int FARE = 350;
+        private static final double CHILDREN_RATIO = 0.5;
+        private static final double TEENAGER_RATIO = 0.2;
     }
 }
