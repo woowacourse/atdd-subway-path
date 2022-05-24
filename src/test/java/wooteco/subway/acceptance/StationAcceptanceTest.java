72a1c14f54383f29b92470a23bfb8df46dc34d73
jaejae-yoo <wotj102@gmail.com>
2022-05-25 07:19:08 +0900

    refactor: 불필요한 import 제거

--- src/test/java/wooteco/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/StationAcceptanceTest.java
@@ -1,12 +1,11 @@
 package wooteco.subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import wooteco.subway.dto.StationRequest;
 import wooteco.subway.dto.StationResponse;
 
