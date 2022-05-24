2a65d52e7c3f1432ab894d767dcacc18772f7ed0
tonic523 <diddmlvkf@gmail.com>
2022-05-25 01:26:53 +0900

    feat(Age): 연령 구현

---
+++ src/main/java/wooteco/subway/domain/path/Age.java
@@ -0,0 +1,18 @@
+package wooteco.subway.domain.path;
+
+public class Age {
+
+    private static final int MINIMUM_AGE = 1;
+    private final int value;
+
+    public Age(int value) {
+        this.value = value;
+        validatePositiveNumber();
+    }
+
+    private void validatePositiveNumber() {
+        if (value < MINIMUM_AGE) {
+            throw new IllegalArgumentException("나이는 양수여야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/wooteco/subway/domain/path/AgeTest.java
@@ -0,0 +1,16 @@
+package wooteco.subway.domain.path;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AgeTest {
+
+    @Test
+    @DisplayName("나이가 양수가 아니면 예외를 발생시킨다.")
+    void validateNegativeNumber() {
+        Assertions.assertThatThrownBy(() -> new Age(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("나이는 양수여야 합니다.");
+    }
+}
