1fe15fbfa778b35e31f88d8d9ce666e8bc6f9f0b
YJGwon <yj970125@gmail.com>
2022-05-24 14:35:35 +0900

    refactor(ExtraFare): 조건에 따른 추가 요금을 각 Enum 객체에서 계산, 분기문 제거

--- src/main/java/wooteco/subway/domain/ExtraFare.java
+++ src/main/java/wooteco/subway/domain/ExtraFare.java
@@ -3,38 +3,37 @@ package wooteco.subway.domain;
 import java.util.function.Function;
 
 public enum ExtraFare {
-    OVER_TEN_KM(10, 5, 100),
-    OVER_FIFTY_KM(50, 8, 100)
+    OVER_TEN_KM {
+        @Override
+        int calculate(double distance) {
+            if (distance > 50) {
+                return calculate(50);
+            }
+            double extraDistance = distance - 10;
+            return calculate(extraDistance, 5, 100);
+        }
+    },
+    OVER_FIFTY_KM {
+        @Override
+        int calculate(double distance) {
+            double extraDistance = distance - 50;
+            return calculate(extraDistance, 8, 100);
+        }
+    }
     ;
 
     private static final int BASIC_FARE = 1250;
 
-    private final int criteria;
-    private final int unit;
-    private final int amount;
-
-    ExtraFare(int criteria, int unit, int amount) {
-        this.criteria = criteria;
-        this.unit = unit;
-        this.amount = amount;
-    }
-
     public static Function<Double, Integer> fareCalculator() {
-        return (distance) -> BASIC_FARE + calculateTotalExtra(distance);
+        return (distance) -> BASIC_FARE + OVER_TEN_KM.calculate(distance) + OVER_FIFTY_KM.calculate(distance);
     }
 
-    private static int calculateTotalExtra(double distance) {
-        if (distance > OVER_FIFTY_KM.criteria) {
-            return OVER_FIFTY_KM.calculate(distance - OVER_FIFTY_KM.criteria)
-                    + OVER_TEN_KM.calculate(OVER_FIFTY_KM.criteria - OVER_TEN_KM.criteria);
-        }
-        if (distance > OVER_TEN_KM.criteria) {
-            return OVER_TEN_KM.calculate(distance - OVER_TEN_KM.criteria);
-        }
-        return 0;
-    }
+    abstract int calculate(double distance);
 
-    private int calculate(double distance) {
+    int calculate(double distance, int unit, int amount) {
+        if (distance <= 0) {
+            return 0;
+        }
         return (int) ((Math.ceil(distance / unit)) * amount);
     }
 }
