39e67975522a51a509441b2a774914f806f572ff
leokim <kunga077@gmail.com>
2022-05-24 15:01:07 +0900

    refactor: 이넘 클래스 매직넘버 상수화

--- src/main/java/wooteco/subway/Age.java
+++ src/main/java/wooteco/subway/Age.java
@@ -1,16 +1,18 @@
 package wooteco.subway;
 
+import static wooteco.subway.Age.Constant.*;
+
 import java.util.Arrays;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 public enum Age {
 
-    INFANTS(age -> 1 <= age && age < 6, fare -> 0),
-    CHILDREN(age -> 6 <= age && age < 13, fare -> (int) ((fare - 350) * 0.5)),
-    TEENAGERS(age -> 13 <= age && age < 19, fare -> (int) ((fare - 350) * 0.8)),
-    ADULTS(age -> 19 <= age && age < 65, fare -> fare),
-    SENIORS(age -> 65 <= age, fare -> 0),
+    INFANTS(age -> INFANTS_START_AGE <= age && age < CHILDREN_START_AGE, fare -> FARE_FREE),
+    CHILDREN(age -> CHILDREN_START_AGE <= age && age < TEENAGERS_START_AGE, fare -> (int) ((fare - FIXED_DISCOUNT_FARE) * CHILDREN_DISCOUNT_RATE)),
+    TEENAGERS(age -> TEENAGERS_START_AGE <= age && age < ADULTS_START_AGE, fare -> (int) ((fare - FIXED_DISCOUNT_FARE) * TEENAGERS_DISCOUNT_RATE)),
+    ADULTS(age -> ADULTS_START_AGE <= age && age < SENIORS_START_AGE, fare -> fare),
+    SENIORS(age -> SENIORS_START_AGE <= age, fare -> FARE_FREE),
     ;
 
     private final Predicate<Integer> findAgeGroup;
@@ -30,5 +32,18 @@ public enum Age {
 
     public int calculateDiscount(int fare) {
         return this.fareCalculator.apply(fare);
+    }
+
+    static class Constant {
+
+        static final double CHILDREN_DISCOUNT_RATE = 0.5;
+        static final double TEENAGERS_DISCOUNT_RATE = 0.8;
+        static final int FARE_FREE = 0;
+        static final int FIXED_DISCOUNT_FARE = 350;
+        static final int INFANTS_START_AGE = 1;
+        static final int CHILDREN_START_AGE = 6;
+        static final int TEENAGERS_START_AGE = 13;
+        static final int ADULTS_START_AGE = 19;
+        static final int SENIORS_START_AGE = 65;
     }
 }
