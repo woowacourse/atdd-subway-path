847aff4e7f9f29d80c96e995957c621d21417fa1
Ohzzi <clover7even@naver.com>
2022-05-24 11:30:29 +0900

    feat: 노선 도메인에 대한 검증 기능 추가

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -14,10 +14,35 @@ public class Line {
     }
 
     public Line(Long id, String name, String color, int extraFare) {
+        validate(name, color, extraFare);
         this.id = id;
         this.name = name;
         this.color = color;
         this.extraFare = extraFare;
+    }
+
+    private void validate(String name, String color, int extraFare) {
+        validateName(name);
+        validateColor(color);
+        validateExtraFare(extraFare);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("노선 이름은 빈 값일 수 없습니다.");
+        }
+    }
+
+    private void validateColor(String color) {
+        if (color == null || color.isBlank()) {
+            throw new IllegalArgumentException("노선 색상은 빈 값일 수 없습니다.");
+        }
+    }
+
+    private void validateExtraFare(int extraFare) {
+        if (extraFare < 0) {
+            throw new IllegalArgumentException("추가 요금은 음수일 수 없습니다.");
+        }
     }
 
     public Long getId() {
@@ -53,5 +78,5 @@ public class Line {
     public int hashCode() {
         return Objects.hash(id, name, color, extraFare);
     }
-    
+
 }
