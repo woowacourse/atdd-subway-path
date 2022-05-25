e620a074cd5e1c050ee39a46bb93dab653bca3f2
yukong <kyb062666@gmail.com>
2022-05-25 14:10:20 +0900

    refactor(Line.java): 사용하지 않는 생성자 삭제

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -10,23 +10,10 @@ public class Line {
     public Line() {
     }
 
-    // TODO: 삭제
-    public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
-
     public Line(String name, String color, int extraFare) {
         this.name = name;
         this.color = color;
         this.extraFare = extraFare;
-    }
-
-    // TODO: 삭제
-    public Line(Long id, String name, String color) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
     }
 
     public Line(Long id, String name, String color, int extraFare) {
