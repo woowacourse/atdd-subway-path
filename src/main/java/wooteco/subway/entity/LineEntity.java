b8a76b8798b1cc3ee6c786154bc388bec3ab3b5e
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-25 19:40:33 +0900

    feat(entity): implement LineEntity to replace unnecessary Line domain

---
+++ src/main/java/wooteco/subway/entity/LineEntity.java
@@ -0,0 +1,58 @@
+package wooteco.subway.entity;
+
+import java.util.Objects;
+
+public class LineEntity {
+
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final int extraFare;
+
+    public LineEntity(Long id, String name, String color, int extraFare) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.extraFare = extraFare;
+    }
+
+    public LineEntity(String name, String color, int extraFare) {
+        this(null, name, color, extraFare);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getExtraFare() {
+        return extraFare;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LineEntity that = (LineEntity) o;
+        return extraFare == that.extraFare
+                && Objects.equals(id, that.id)
+                && Objects.equals(name, that.name)
+                && Objects.equals(color, that.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color, extraFare);
+    }
+}
