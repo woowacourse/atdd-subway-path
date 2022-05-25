9ead4d86417deceb16700cbdbd392ce02685e6b2
sun <syoun602@gmail.com>
2022-05-25 15:10:52 +0900

    refactor: 메서드 분리

--- src/main/java/wooteco/subway/domain/strategy/DijkstraStrategy.java
+++ src/main/java/wooteco/subway/domain/strategy/DijkstraStrategy.java
@@ -14,14 +14,7 @@ public class DijkstraStrategy implements ShortestPathStrategy {
 
     @Override
     public Optional<Path> findPath(final Station source, final Station target, final Sections sections) {
-        final WeightedMultigraph<Station, SubwayPathEdge> graph =
-                new WeightedMultigraph<>(SubwayPathEdge.class);
-        final DijkstraShortestPath<Station, SubwayPathEdge> dijkstraShortestPath =
-                new DijkstraShortestPath<>(initializeGraph(graph, sections));
-
-        final Optional<GraphPath<Station, SubwayPathEdge>> path =
-                Optional.ofNullable(dijkstraShortestPath.getPath(source, target));
-
+        final Optional<GraphPath<Station, SubwayPathEdge>> path = createPath(source, target, sections);
         if (path.isEmpty()) {
             return Optional.empty();
         }
@@ -29,9 +22,18 @@ public class DijkstraStrategy implements ShortestPathStrategy {
         final GraphPath<Station, SubwayPathEdge> graphPath = path.get();
         final int distance = (int) graphPath.getWeight();
 
-        return Optional.of(
-                new Path(graphPath.getVertexList(), getCrossedLines(graphPath), distance)
-        );
+        return Optional.of(new Path(graphPath.getVertexList(), getCrossedLines(graphPath), distance));
+    }
+
+    private Optional<GraphPath<Station, SubwayPathEdge>> createPath(final Station source,
+                                                                    final Station target,
+                                                                    final Sections sections) {
+        final WeightedMultigraph<Station, SubwayPathEdge> graph =
+                new WeightedMultigraph<>(SubwayPathEdge.class);
+        final DijkstraShortestPath<Station, SubwayPathEdge> dijkstraShortestPath =
+                new DijkstraShortestPath<>(initializeGraph(graph, sections));
+
+        return Optional.ofNullable(dijkstraShortestPath.getPath(source, target));
     }
 
     private WeightedMultigraph<Station, SubwayPathEdge> initializeGraph(
