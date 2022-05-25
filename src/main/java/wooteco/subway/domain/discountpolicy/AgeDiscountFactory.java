afba9b84e9a57a99d16c63a947ea290464703b64
skullKim <kyk990328@gmail.com>
2022-05-25 15:35:31 +0900

    refactor(Age.java): 클래스 이름 수정
    의도에 맞지 않는 클래스 이름을 명확성을 위해 수정
    
    BREAKING CHANGE: 클래스 이름을 Age에서 AgeRange로 수정

--- src/main/java/wooteco/subway/domain/discountpolicy/AgeDiscountFactory.java
+++ src/main/java/wooteco/subway/domain/discountpolicy/AgeDiscountFactory.java
@@ -1,16 +1,15 @@
 package wooteco.subway.domain.discountpolicy;
 
-import java.util.EnumMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
 
 public class AgeDiscountFactory {
 
-    private static final Map<Age, Supplier<AgeDiscountPolicy>> cache = Age.generateCache();
+    private static final Map<AgeRange, Supplier<AgeDiscountPolicy>> cache = AgeRange.generateCache();
 
     public static AgeDiscountPolicy from(final int age) {
-        final Supplier<AgeDiscountPolicy> supplier = Optional.ofNullable(cache.get(Age.from(age)))
+        final Supplier<AgeDiscountPolicy> supplier = Optional.ofNullable(cache.get(AgeRange.from(age)))
                 .orElseThrow(IllegalAccessError::new);
         return supplier.get();
     }
--- src/main/java/wooteco/subway/domain/discountpolicy/AgeRange.java
+++ src/main/java/wooteco/subway/domain/discountpolicy/AgeRange.java
@@ -1,13 +1,12 @@
 package wooteco.subway.domain.discountpolicy;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-public enum Age {
+public enum AgeRange {
 
     PRESCHOOLER(1, 5, PreschoolerPolicy::new),
     CHILD(6, 12, ChildDiscountPolicy::new),
@@ -19,20 +18,20 @@ public enum Age {
     private final int maxRage;
     private final Supplier<AgeDiscountPolicy> policySupplier;
 
-    Age(final int minRange, final int maxRage, final Supplier<AgeDiscountPolicy> policySupplier) {
+    AgeRange(final int minRange, final int maxRage, final Supplier<AgeDiscountPolicy> policySupplier) {
         this.minRange = minRange;
         this.maxRage = maxRage;
         this.policySupplier = policySupplier;
     }
 
-    public static Age from(final int targetAge) {
+    public static AgeRange from(final int targetAge) {
         return Arrays.stream(values())
                 .filter(age -> age.minRange <= targetAge && targetAge <= age.maxRage)
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
     }
 
-    public static Map<Age, Supplier<AgeDiscountPolicy>> generateCache() {
+    public static Map<AgeRange, Supplier<AgeDiscountPolicy>> generateCache() {
         return Arrays.stream(values())
                 .map(age -> Map.entry(age, age.policySupplier))
                 .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
