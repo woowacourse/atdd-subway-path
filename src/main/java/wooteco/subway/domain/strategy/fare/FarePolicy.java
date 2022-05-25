3b3c6611042846144f8445c83ff163d4a8bd46a8
Wishoon <heonga2@gmail.com>
2022-05-25 14:35:19 +0900

    fix: 변수명 오타 수정

--- src/main/java/wooteco/subway/domain/strategy/fare/FarePolicy.java
+++ src/main/java/wooteco/subway/domain/strategy/fare/FarePolicy.java
@@ -5,16 +5,16 @@ import wooteco.subway.domain.strategy.fare.distance.FareDistanceStrategyManager;
 
 public class FarePolicy {
 
-    private final FareDistanceStrategyManager fareDistanceStrategyManger;
+    private final FareDistanceStrategyManager fareDistanceStrategyManager;
     private final FareAgeStrategyManager fareAgeStrategyManager;
 
-    public FarePolicy(FareDistanceStrategyManager fareDistanceStrategyManger, FareAgeStrategyManager fareAgeStrategyManager) {
-        this.fareDistanceStrategyManger = fareDistanceStrategyManger;
+    public FarePolicy(FareDistanceStrategyManager fareDistanceStrategyManager, FareAgeStrategyManager fareAgeStrategyManager) {
+        this.fareDistanceStrategyManager = fareDistanceStrategyManager;
         this.fareAgeStrategyManager = fareAgeStrategyManager;
     }
 
     public int getFare(int age, int distance, int extraFare) {
-        int distanceFare = fareDistanceStrategyManger.calculateDistanceFare(distance) + extraFare;
+        int distanceFare = fareDistanceStrategyManager.calculateDistanceFare(distance) + extraFare;
         return distanceFare - fareAgeStrategyManager.calculateDiscountAgeFare(age, distanceFare);
     }
 }
