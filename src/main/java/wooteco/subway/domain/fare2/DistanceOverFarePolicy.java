dc143f9787093e995bf0e77923715f0894a6e352
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-25 22:25:42 +0900

    feat(fare): implement new Fare and Policy domains without decorators
    - add test cases

---
+++ src/main/java/wooteco/subway/domain/fare2/DistanceOverFarePolicy.java
@@ -0,0 +1,37 @@
+package wooteco.subway.domain.fare2;
+
+public enum DistanceOverFarePolicy {
+
+    OVER_TEN(10, 50, 5),
+    OVER_FIFTY(50, Integer.MAX_VALUE, 8),
+    ;
+
+    private static final int OVER_FARE_AMOUNT = 100;
+
+    private final int startExclusive;
+    private final int endInclusive;
+    private final int limit;
+
+    DistanceOverFarePolicy(int startExclusive, int endInclusive, int limit) {
+        this.startExclusive = startExclusive;
+        this.endInclusive = endInclusive;
+        this.limit = limit;
+    }
+
+    public boolean isApplicableTo(int value) {
+        return value > startExclusive;
+    }
+
+    public int toOverFare(int distance) {
+        if (distance <= startExclusive) {
+            return 0;
+        }
+        int overDistance = Math.min(distance, endInclusive) - startExclusive;
+        return calculateOverFare(overDistance, limit);
+    }
+
+    private int calculateOverFare(int overDistance, int limit) {
+        double overFareDigit = Math.ceil((overDistance - 1) / limit) + 1;
+        return (int) (overFareDigit * OVER_FARE_AMOUNT);
+    }
+}
---
+++ src/test/java/wooteco/subway/domain/fare2/DistanceOverFarePolicyTest.java
@@ -0,0 +1,51 @@
+package wooteco.subway.domain.fare2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@SuppressWarnings("NonAsciiCharacters")
+class DistanceOverFarePolicyTest {
+
+    @Test
+    void 이동거리가_10_초과인_경우_5km_단위마다_최대_100원씩_추가비용() {
+        DistanceOverFarePolicy distancePolicy = DistanceOverFarePolicy.OVER_TEN;
+
+        int actual = distancePolicy.toOverFare(18);
+        int expected = 200;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10,0", "11,100", "15,100", "50,800"})
+    void 경계값_검증_10초과_50이하(int input, int expected) {
+        DistanceOverFarePolicy distancePolicy = DistanceOverFarePolicy.OVER_TEN;
+
+        int actual = distancePolicy.toOverFare(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 이동거리가_50_초과인_경우_50을_초과한_거리에_대해_8km_단위마다_최대_100원씩_추가비용() {
+        DistanceOverFarePolicy distancePolicy = DistanceOverFarePolicy.OVER_FIFTY;
+
+        int actual = distancePolicy.toOverFare(60);
+        int expected = 200;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"50,0", "51,100", "58,100", "59,200"})
+    void 경계값_검증_50초과(int input, int expected) {
+        DistanceOverFarePolicy distancePolicy = DistanceOverFarePolicy.OVER_FIFTY;
+
+        int actual = distancePolicy.toOverFare(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
