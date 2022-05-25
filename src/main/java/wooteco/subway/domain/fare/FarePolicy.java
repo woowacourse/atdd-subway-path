6ede2e654e75ab54bbdebe63c5e6f0c2b47455c6
Richard JEON <ztzy1907@gmail.com>
2022-05-25 11:40:34 +0900

    refactor(fare-policy): 요금 관련 매직넘버 상수 처리 및 메서드 가독성 개선

--- src/main/java/wooteco/subway/domain/fare/FarePolicy.java
+++ src/main/java/wooteco/subway/domain/fare/FarePolicy.java
@@ -1,5 +1,17 @@
 package wooteco.subway.domain.fare;
 
+
+import static wooteco.subway.domain.fare.FarePolicy.Constants.FIFTY_KILOMETER;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.FIRST_SECTION_BASE_FARE;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.NO_FARE;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.SECOND_SECTION_ADDITIONAL;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.SECOND_SECTION_ADDITIONAL_STANDARD;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.TEN_KILOMETER;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.THIRD_SECTION_ADDITIONAL;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.THIRD_SECTION_ADDITIONAL_STANDARD;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.THIRD_SECTION_BASE_FARE;
+import static wooteco.subway.domain.fare.FarePolicy.Constants.ZERO;
+
 import java.util.Arrays;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -7,8 +19,8 @@ import wooteco.subway.exception.NotFoundFareSectionException;
 
 public enum FarePolicy {
 
-    ZERO_SECTION(isEvenOrLessThanTen(), distance -> 0L),
-    FIRST_SECTION(isLessThanTen(), distance -> 1250L),
+    ZERO_SECTION(isZeroOrNegative(), distance -> NO_FARE),
+    FIRST_SECTION(isTenOrLessThanTen(), distance -> FIRST_SECTION_BASE_FARE),
     SECOND_SECTION(isGreaterThanTenAndEvenOrLessThanFifty(), secondSectionFareCalculator()),
     THIRD_SECTION(isGreaterThanFifty(), thirdSectionFareCalculator());
 
@@ -31,31 +43,50 @@ public enum FarePolicy {
 
     private static Function<Long, Long> secondSectionFareCalculator() {
         return distance -> {
-            distance -= 10;
-            return 1250L + Double.valueOf(Math.ceil(distance / 5.0) * 100L).longValue();
+            distance -= TEN_KILOMETER;
+            return FIRST_SECTION_BASE_FARE +
+                    Double.valueOf(
+                            Math.ceil(distance / SECOND_SECTION_ADDITIONAL_STANDARD) * SECOND_SECTION_ADDITIONAL
+                    ).longValue();
         };
     }
 
     private static Function<Long, Long> thirdSectionFareCalculator() {
         return distance -> {
-            distance -= 50;
-            return 2050L + Double.valueOf(Math.ceil(distance / 8.0) * 100L).longValue();
+            distance -= FIFTY_KILOMETER;
+            return THIRD_SECTION_BASE_FARE +
+                    Double.valueOf(
+                            Math.ceil(distance / THIRD_SECTION_ADDITIONAL_STANDARD) * THIRD_SECTION_ADDITIONAL
+                    ).longValue();
         };
     }
 
-    private static Predicate<Long> isEvenOrLessThanTen() {
-        return distance -> distance <= 0;
+    private static Predicate<Long> isZeroOrNegative() {
+        return distance -> distance <= ZERO;
     }
 
-    private static Predicate<Long> isLessThanTen() {
-        return distance -> 0 < distance && distance <= 10;
+    private static Predicate<Long> isTenOrLessThanTen() {
+        return distance -> ZERO < distance && distance <= TEN_KILOMETER;
     }
 
     private static Predicate<Long> isGreaterThanTenAndEvenOrLessThanFifty() {
-        return distance -> 10 < distance && distance <= 50;
+        return distance -> TEN_KILOMETER < distance && distance <= FIFTY_KILOMETER;
     }
 
     private static Predicate<Long> isGreaterThanFifty() {
-        return distance -> 50 < distance;
+        return distance -> FIFTY_KILOMETER < distance;
+    }
+
+    static class Constants {
+        static final long NO_FARE = 0L;
+        static final long FIRST_SECTION_BASE_FARE = 1250L;
+        static final double SECOND_SECTION_ADDITIONAL_STANDARD = 5.0;
+        static final long SECOND_SECTION_ADDITIONAL = 100L;
+        static final long THIRD_SECTION_BASE_FARE = 2050L;
+        static final double THIRD_SECTION_ADDITIONAL_STANDARD = 8.0;
+        static final long THIRD_SECTION_ADDITIONAL = 100L;
+        static final int ZERO = 0;
+        static final int TEN_KILOMETER = 10;
+        static final int FIFTY_KILOMETER = 50;
     }
 }
