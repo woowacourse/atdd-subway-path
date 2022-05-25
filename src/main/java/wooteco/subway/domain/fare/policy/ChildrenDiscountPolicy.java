f0b71001061c4130b491549e074b56a4ea990895
yhh1056 <yhh1056@naver.com>
2022-05-25 12:58:17 +0900

    refactor: 할인 비율 상수명 수정

--- src/main/java/wooteco/subway/domain/fare/policy/ChildrenDiscountPolicy.java
+++ src/main/java/wooteco/subway/domain/fare/policy/ChildrenDiscountPolicy.java
@@ -3,10 +3,10 @@ package wooteco.subway.domain.fare.policy;
 public class ChildrenDiscountPolicy implements DiscountPolicy {
 
     private static final int DEDUCTIBLE_AMOUNT = 350;
-    private static final double DISCOUNT = 0.5;
+    private static final double CHILDREN_RATE = 0.5;
 
     @Override
     public int calculate(int fare) {
-        return (int) ((fare - DEDUCTIBLE_AMOUNT) * DISCOUNT);
+        return (int) ((fare - DEDUCTIBLE_AMOUNT) * CHILDREN_RATE);
     }
 }
--- src/main/java/wooteco/subway/domain/fare/policy/TeenagerDiscountPolicy.java
+++ src/main/java/wooteco/subway/domain/fare/policy/TeenagerDiscountPolicy.java
@@ -3,10 +3,10 @@ package wooteco.subway.domain.fare.policy;
 public class TeenagerDiscountPolicy implements DiscountPolicy {
 
     private static final int DEDUCTIBLE_AMOUNT = 350;
-    private static final double DISCOUNT = 0.8;
+    private static final double TEENAGER_RATE = 0.8;
 
     @Override
     public int calculate(int fare) {
-        return (int) ((fare - DEDUCTIBLE_AMOUNT) * DISCOUNT);
+        return (int) ((fare - DEDUCTIBLE_AMOUNT) * TEENAGER_RATE);
     }
 }
