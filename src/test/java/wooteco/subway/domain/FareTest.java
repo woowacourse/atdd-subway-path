e74f818f909beb8effb37dfd7e07b3002833869f
meatsby <meatsby@gmail.com>
2022-05-25 13:21:02 +0900

    test: 변수명 변경

--- src/test/java/wooteco/subway/domain/FareTest.java
+++ src/test/java/wooteco/subway/domain/FareTest.java
@@ -14,79 +14,79 @@ class FareTest {
     @ParameterizedTest
     @CsvSource({"10, 1250", "11, 1350", "16, 1450", "51, 2150"})
     void calculateBasicFare(int distance, int expected) {
-        Fare fareCalculator = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(22));
-        int fare = fareCalculator.calculate(distance, 0);
+        Fare fare = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(22));
+        int actual = fare.calculate(distance, 0);
 
-        assertThat(fare).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("추가 요금이 존재하는 경로의 거리에 따른 기본 운임 비용을 반환한다.")
     @ParameterizedTest
     @CsvSource({"10, 2150", "11, 2250", "16, 2350", "51, 3050"})
     void calculateBasicFareWithExtraFare(int distance, int expected) {
-        Fare fareCalculator = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(22));
-        int fare = fareCalculator.calculate(distance, 900);
+        Fare fare = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(22));
+        int actual = fare.calculate(distance, 900);
 
-        assertThat(fare).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("경로의 거리에 따른 어린이 운임 비용을 반환한다.")
     @ParameterizedTest
     @CsvSource({"10, 450", "11, 500", "16, 550", "51, 900"})
     void calculateChildrenFare(int distance, int expected) {
-        Fare fareCalculator = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(10));
-        int fare = fareCalculator.calculate(distance, 0);
+        Fare fare = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(10));
+        int actual = fare.calculate(distance, 0);
 
-        assertThat(fare).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("추가 요금이 존재하는 경로의 거리에 따른 어린이 운임 비용을 반환한다.")
     @ParameterizedTest
     @CsvSource({"10, 900", "11, 950", "16, 1000", "51, 1350"})
     void calculateChildrenFareWithExtraFare(int distance, int expected) {
-        Fare fareCalculator = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(10));
-        int fare = fareCalculator.calculate(distance, 900);
+        Fare fare = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(10));
+        int actual = fare.calculate(distance, 900);
 
-        assertThat(fare).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("경로의 거리에 따른 청소년 운임 비용을 반환한다.")
     @ParameterizedTest
     @CsvSource({"10, 720", "11, 800", "16, 880", "51, 1440"})
     void calculateTeenagerFare(int distance, int expected) {
-        Fare fareCalculator = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(15));
-        int fare = fareCalculator.calculate(distance, 0);
+        Fare fare = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(15));
+        int actual = fare.calculate(distance, 0);
 
-        assertThat(fare).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("추가 요금이 존재하는 경로의 거리에 따른 청소년 운임 비용을 반환한다.")
     @ParameterizedTest
     @CsvSource({"10, 1440", "11, 1520", "16, 1600", "51, 2160"})
     void calculateTeenagerFareWithExtraFare(int distance, int expected) {
-        Fare fareCalculator = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(15));
-        int fare = fareCalculator.calculate(distance, 900);
+        Fare fare = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(15));
+        int actual = fare.calculate(distance, 900);
 
-        assertThat(fare).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("경로의 거리에 따른 우대 운임 비용을 반환한다.")
     @ParameterizedTest
     @CsvSource({"10, 0", "11, 0", "16, 0", "51, 0"})
     void calculatePreferentialFare(int distance, int expected) {
-        Fare fareCalculator = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(65));
-        int fare = fareCalculator.calculate(distance, 0);
+        Fare fare = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(65));
+        int actual = fare.calculate(distance, 0);
 
-        assertThat(fare).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("추가 요금이 존재하는 경로의 거리에 따른 우대 운임 비용을 반환한다.")
     @ParameterizedTest
     @CsvSource({"10, 0", "11, 0", "16, 0", "51, 0"})
     void calculatePreferentialFareWithExtraFare(int distance, int expected) {
-        Fare fareCalculator = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(65));
-        int fare = fareCalculator.calculate(distance, 900);
+        Fare fare = new Fare(FarePolicyFactory.from(distance), DiscountPolicyFactory.from(65));
+        int actual = fare.calculate(distance, 900);
 
-        assertThat(fare).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 }
