be2d8b000c9b15deb5fe6ee810e058f198a9968d
bomi <bonita9931@gmail.com>
2022-05-26 02:58:18 +0900

    refactor: AgePolicy에서 Predicate를 갖도록 수정

--- src/main/java/wooteco/subway/domain/AgePolicy.java
+++ src/main/java/wooteco/subway/domain/AgePolicy.java
@@ -4,27 +4,25 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum AgePolicy {
-    INFANT(0, 6, 0, 0),
-    CHILD(6, 13, 350, 0.5),
-    TEEN(13, 19, 350, 0.8),
-    ADULT(19, 65, 0, 1),
-    OLD(65, Integer.MAX_VALUE, 0, 0);
+    INFANT(age -> 0 <= age && age < 6, 0, 0),
+    CHILD(age -> 6 <= age && age < 13, 350, 0.5),
+    TEEN(age -> 13 <= age && age < 19, 350, 0.8),
+    ADULT(age -> 19 <= age && age < 65, 0, 1),
+    OLD(age -> 65 <= age, 0, 0);
 
-    private int minAge;
-    private int maxAge;
+    private Predicate<Integer> predicate;
     private int deductibleFare;
     private double fareRatio;
 
-    AgePolicy(int minAge, int maxAge, int deductibleFare, double fareRatio) {
-        this.minAge = minAge;
-        this.maxAge = maxAge;
+    AgePolicy(Predicate<Integer> predicate, int deductibleFare, double fareRatio) {
+        this.predicate = predicate;
         this.deductibleFare = deductibleFare;
         this.fareRatio = fareRatio;
     }
 
     public static int calculateFareByAge(int age, int fare) {
         AgePolicy agePolicy = Arrays.stream(values())
-                .filter(policy -> age >= policy.minAge && age < policy.maxAge)
+                .filter(policy -> policy.predicate.test(age))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 나이입니다."));
 
