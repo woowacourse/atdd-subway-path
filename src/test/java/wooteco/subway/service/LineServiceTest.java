48d48a587d6ed1cbe5dd7f694bb46b6b3dfd7fd9
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-25 13:20:56 +0900

    test(LineServiceTest): 의존성을 Mock을 사용하도록 변경

--- src/test/java/wooteco/subway/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/service/LineServiceTest.java
@@ -1,44 +1,38 @@
 package wooteco.subway.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import wooteco.subway.domain.Line;
-import wooteco.subway.repository.dao.StationDao;
-import wooteco.subway.repository.entity.StationEntity;
+import wooteco.subway.repository.LineRepository;
 import wooteco.subway.service.dto.LineRequest;
-import wooteco.subway.service.dto.LineUpdateRequest;
 
-@SpringBootTest
-@Transactional
+@ExtendWith({MockitoExtension.class})
 class LineServiceTest {
 
-    @Autowired
+    @InjectMocks
     private LineService lineService;
 
-    @Autowired
-    private StationDao stationDao;
+    @Mock
+    private LineRepository lineRepository;
 
-    private StationEntity gangnam;
-    private StationEntity nowon;
-
-    @BeforeEach
-    void setUp() {
-        gangnam = stationDao.save(new StationEntity(null, "강남역"));
-        nowon = stationDao.save(new StationEntity(null, "노원역"));
-    }
+    @Mock
+    private SectionService sectionService;
 
     @Test
     @DisplayName("노선을 생성한다.")
     void createLine() {
         // given
-        final LineRequest request = new LineRequest("7호선", "bg-red-600", gangnam.getId(), nowon.getId(), 10);
+        final LineRequest request = new LineRequest("7호선", "bg-red-600", 1L, 2L, 10);
+        Line line = Line.withoutIdOf(request.getName(), request.getColor(), request.getExtraFare());
+        when(lineRepository.save(line)).thenReturn(new Line(1L, line.getName(), line.getColor(), line.getExtraFare()));
 
         // when
         Line save = lineService.save(request);
@@ -52,21 +46,24 @@ class LineServiceTest {
     void createLine_extraFare() {
         // given
         long extraFare = 100L;
-        final LineRequest request = new LineRequest("7호선", "bg-red-600", gangnam.getId(), nowon.getId(), 10, extraFare);
+        final LineRequest request = new LineRequest("7호선", "bg-red-600", 1L, 2L, 10, extraFare);
+        Line line = Line.withoutIdOf(request.getName(), request.getColor(), request.getExtraFare());
+        when(lineRepository.save(line)).thenReturn(new Line(1L, line.getName(), line.getColor(), line.getExtraFare()));
 
         // when
-        Line line = lineService.save(request);
+        Line saved = lineService.save(request);
 
         // then
-        assertThat(line.getExtraFare()).isEqualTo(extraFare);
+        assertThat(saved.getExtraFare()).isEqualTo(extraFare);
     }
 
     @Test
     @DisplayName("모든 노선을 조회한다.")
     void showLines() {
         // given
-        lineService.save(new LineRequest("1호선", "bg-red-600", gangnam.getId(), nowon.getId(), 10));
-        lineService.save(new LineRequest("2호선", "bg-blue-600", nowon.getId(), gangnam.getId(), 10));
+        Line 일호선 = new Line(1L, "1호선", "red", 100L);
+        Line 이호선 = new Line(2L, "2호선", "blue", 200L);
+        when(lineRepository.findAll()).thenReturn(List.of(일호선, 이호선));
 
         // when
         List<Line> lines = lineService.findAll();
@@ -81,47 +78,15 @@ class LineServiceTest {
         // given
         String color = "bg-red-600";
         String name = "7호선";
-
-        Line savedLine = lineService.save(new LineRequest(name, color, gangnam.getId(), nowon.getId(), 10));
+        long id = 1L;
+        Line 칠호선 = new Line(id, name, color, 100L);
+        when(lineRepository.findById(id)).thenReturn(칠호선);
 
         // when
-        Line response = lineService.findById(savedLine.getId());
+        Line response = lineService.findById(id);
 
         // then
         assertThat(response.getName()).isEqualTo(name);
         assertThat(response.getColor()).isEqualTo(color);
-    }
-
-    @Test
-    @DisplayName("id에 해당하는 노선 정보를 수정한다.")
-    void updateById() {
-        // given
-        Line saved = lineService.save(new LineRequest("1호선", "red", gangnam.getId(), nowon.getId(), 10));
-
-        final String name = "7호선";
-        final String color = "bg-blue-600";
-        final LineUpdateRequest request = new LineUpdateRequest(name, color, 200L);
-
-        // when
-        lineService.updateById(saved.getId(), request);
-
-        // then
-        Line updated = lineService.findById(saved.getId());
-        assertThat(updated.getName()).isEqualTo(name);
-        assertThat(updated.getColor()).isEqualTo(color);
-    }
-
-    @Test
-    @DisplayName("id에 해당하는 노선을 삭제한다.")
-    void deleteById() {
-        // given
-        Line saved = lineService.save(new LineRequest("1호선", "red", gangnam.getId(), nowon.getId(), 10));
-
-        // when
-        lineService.deleteById(saved.getId());
-
-        // then
-        List<Line> all = lineService.findAll();
-        assertThat(all).hasSize(0);
     }
 }
