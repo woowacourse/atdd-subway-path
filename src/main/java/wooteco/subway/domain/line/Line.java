8eb97b0715a6d0f99099aede7765c35149484c87
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-25 17:48:14 +0900

    feat(line): implement LineExtraFare as value object
    - move validation from Line to LineExtraFare
    - replace int type to LineExtraFare for extraFare field at Line

--- src/main/java/wooteco/subway/domain/line/Line.java
+++ src/main/java/wooteco/subway/domain/line/Line.java
@@ -4,35 +4,28 @@ import java.util.Objects;
 
 public class Line {
 
-    private static final int MIN_EXTRA_FARE = 0;
-    private static final String INVALID_EXTRA_FARE_VALUE_EXCEPTION = "노선 추가비용은 0원 이상이어야 합니다.";
-
     private final Long id;
     private final String name;
     private final String color;
-    private final int extraFare;
+    private final LineExtraFare extraFare;
 
-    public Line(Long id, String name, String color, int extraFare) {
-        validateExtraFare(extraFare);
+    public Line(Long id, String name, String color, LineExtraFare extraFare) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.extraFare = extraFare;
     }
 
+    public Line(Long id, String name, String color, int extraFare) {
+        this(id, name, color, new LineExtraFare(extraFare));
+    }
+
     public Line(String name, String color, int extraFare) {
         this(null, name, color, extraFare);
     }
 
-
     public Line(Long id, Line line) {
         this(id, line.name, line.color, line.extraFare);
-    }
-
-    private void validateExtraFare(int extraFare) {
-        if (extraFare < MIN_EXTRA_FARE) {
-            throw new IllegalArgumentException(INVALID_EXTRA_FARE_VALUE_EXCEPTION);
-        }
     }
 
     public Long getId() {
@@ -48,7 +41,7 @@ public class Line {
     }
 
     public int getExtraFare() {
-        return extraFare;
+        return extraFare.getValue();
     }
 
     @Override
@@ -60,10 +53,10 @@ public class Line {
             return false;
         }
         Line line = (Line) o;
-        return extraFare == line.extraFare
-                && Objects.equals(id, line.id)
+        return Objects.equals(id, line.id)
                 && Objects.equals(name, line.name)
-                && Objects.equals(color, line.color);
+                && Objects.equals(color, line.color)
+                && Objects.equals(extraFare, line.extraFare);
     }
 
     @Override
