31f65fbaa305fc98f215f757be68a2f7c8a73b6a
yoon <duddbs774@gmail.com>
2022-05-25 16:49:13 +0900

    refactor: enum 을 사용해서 분기문 제거

--- src/main/java/wooteco/subway/domain/discountstrategy/AgeDiscountStrategyFactory.java
+++ src/main/java/wooteco/subway/domain/discountstrategy/AgeDiscountStrategyFactory.java
@@ -1,16 +1,29 @@
 package wooteco.subway.domain.discountstrategy;
 
-public class AgeDiscountStrategyFactory {
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public enum AgeDiscountStrategyFactory {
+
+    CHILDREN_STRATEGY((age) -> age >= 6 && age < 13, ChildrenDiscountStrategy::new),
+    TEENAGER_STRATEGY((age) -> age >= 13 && age < 19, TeenagerDiscountStrategy::new),
+    DEFAULT_STRATEGY((age) -> false, DefaultDiscountStrategy::new);
+
+    private final Predicate<Integer> predicate;
+    private final Supplier<AgeDiscountStrategy> supplier;
+
+    AgeDiscountStrategyFactory(Predicate<Integer> predicate, Supplier<AgeDiscountStrategy> supplier) {
+        this.predicate = predicate;
+        this.supplier = supplier;
+    }
 
     public static AgeDiscountStrategy from(int age) {
-        if (age >= 6 && age < 13) {
-            return new ChildrenDiscountStrategy();
-        }
+        var strategy = Arrays.stream(values())
+                .filter(it -> it.predicate.test(age))
+                .findFirst()
+                .orElse(DEFAULT_STRATEGY);
 
-        if (age >= 13 && age < 19) {
-            return new TeenagerDiscountStrategy();
-        }
-
-        return new DefaultDiscountStrategy();
+        return strategy.supplier.get();
     }
 }
