eda9f0685d90643596625029a70715ae41fa5c02
wilgur513 <wilgur513@naver.com>
2022-05-25 17:05:43 +0900

    fix: 예외 핸들러에 예외 케이스 추가

--- src/main/java/wooteco/subway/ui/GlobalExceptionHandler.java
+++ src/main/java/wooteco/subway/ui/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import wooteco.subway.application.exception.DuplicateException;
+import wooteco.subway.application.exception.InvalidAgeException;
 import wooteco.subway.application.exception.NotFoundException;
 import wooteco.subway.application.exception.UnaddableSectionException;
 import wooteco.subway.application.exception.UndeletableSectionException;
@@ -29,7 +30,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({DuplicateException.class, BlankArgumentException.class,
         UnaddableSectionException.class, UndeletableSectionException.class,
-        UnreachablePathException.class,
+        UnreachablePathException.class, InvalidAgeException.class,
         UnsplittableException.class, UnmergeableException.class})
     private ResponseEntity<ErrorResponse> handleExceptionToBadRequest(Exception e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
