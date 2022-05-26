53d3be067bfa44d5d671ac90aafd2c6ba6ee36d2
Ohzzi <clover7even@naver.com>
2022-05-26 20:30:10 +0900

    test: 기존 픽스처를 이용하여 Section 인수 테스트의 가독성 개선

--- src/test/java/wooteco/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/SectionAcceptanceTest.java
@@ -1,39 +1,34 @@
 package wooteco.subway.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static wooteco.subway.acceptance.fixture.HttpRequestUtil.delete;
+import static wooteco.subway.acceptance.fixture.HttpRequestUtil.post;
+import static wooteco.subway.acceptance.fixture.TestLine.LINE_2;
+import static wooteco.subway.acceptance.fixture.TestStation.GANGNAM;
+import static wooteco.subway.acceptance.fixture.TestStation.SEOLLEUNG;
+import static wooteco.subway.acceptance.fixture.TestStation.YEOKSAM;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import wooteco.subway.dto.request.LineCreateRequest;
+import wooteco.subway.domain.Line;
+import wooteco.subway.domain.Station;
 import wooteco.subway.dto.request.SectionRequest;
-import wooteco.subway.dto.request.StationRequest;
 
 public class SectionAcceptanceTest extends AcceptanceTest {
-
-    private Long gangnamId;
-    private Long yeoksamId;
-    private Long seolleungId;
-
-    @BeforeEach
-    void setStations() {
-        gangnamId = requestPostStationAndReturnId(new StationRequest("강남역"));
-        yeoksamId = requestPostStationAndReturnId(new StationRequest("역삼역"));
-        seolleungId = requestPostStationAndReturnId(new StationRequest("선릉역"));
-    }
 
     @DisplayName("등록된 노선에 추가 구간 생성 요청을 하면 200 OK를 반환한다.")
     @Test
     void createSection() {
-        LineCreateRequest lineCreateRequest = new LineCreateRequest("2호선", "초록색", gangnamId, yeoksamId, 1, 0);
-        ExtractableResponse<Response> lineCreateResponse = requestPostLine(lineCreateRequest);
-        Long lineId = Long.parseLong(lineCreateResponse.header("Location").split("/")[2]);
-        SectionRequest sectionRequest = new SectionRequest(yeoksamId, seolleungId, 1);
+        Station gangnam = GANGNAM.save();
+        Station yeoksam = YEOKSAM.save();
+        Station seolleung = SEOLLEUNG.save();
+        Line line2 = LINE_2.save(gangnam, yeoksam, 1);
+        SectionRequest sectionRequest = new SectionRequest(yeoksam.getId(), seolleung.getId(), 1);
 
-        ExtractableResponse<Response> response = requestPostSection(lineId, sectionRequest);
+        ExtractableResponse<Response> response = post(sectionRequest, "/lines/" + line2.getId() + "/sections");
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
@@ -41,11 +36,12 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     @DisplayName("등록된 구간을 포함하고 있는 노선과 역의 id를 URI에 담아서 구간 삭제 요청을 하면 204 NO CONTENT를 반환한다.")
     @Test
     void deleteSection() {
-        LineCreateRequest lineCreateRequest = new LineCreateRequest("2호선", "초록색", gangnamId, yeoksamId, 1, 0);
-        ExtractableResponse<Response> lineCreateResponse = requestPostLine(lineCreateRequest);
-        Long lineId = Long.parseLong(lineCreateResponse.header("Location").split("/")[2]);
+        Station gangnam = GANGNAM.save();
+        Station yeoksam = YEOKSAM.save();
+        Line line2 = LINE_2.save(gangnam, yeoksam, 1);
 
-        ExtractableResponse<Response> response = requestDeleteSection(lineId, yeoksamId);
+        ExtractableResponse<Response> response = delete(
+                "/lines/" + line2.getId() + "/sections?stationId=" + gangnam.getId());
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
