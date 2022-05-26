a7565f1c094db5a808a9083843918a76379c01a2
Ohzzi <clover7even@naver.com>
2022-05-26 17:49:37 +0900

    test: Station 인수 테스트의 픽스처를 분리하고 가독성 개선

--- src/test/java/wooteco/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/StationAcceptanceTest.java
@@ -1,19 +1,23 @@
 package wooteco.subway.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static wooteco.subway.acceptance.fixture.HttpRequestUtil.delete;
+import static wooteco.subway.acceptance.fixture.HttpRequestUtil.get;
+import static wooteco.subway.acceptance.fixture.HttpRequestUtil.post;
+import static wooteco.subway.acceptance.fixture.TestLine.LINE_2;
+import static wooteco.subway.acceptance.fixture.TestStation.GANGNAM;
+import static wooteco.subway.acceptance.fixture.TestStation.YEOKSAM;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.http.HttpStatus;
-import wooteco.subway.dto.request.LineCreateRequest;
+import wooteco.subway.domain.Station;
 import wooteco.subway.dto.request.StationRequest;
 import wooteco.subway.dto.response.StationResponse;
 
@@ -23,9 +27,7 @@ public class StationAcceptanceTest extends AcceptanceTest {
     @DisplayName("올바른 생성 요청으로 지하철 역을 생성하면 201 CREATED와 역 리소스 주소를 반환한다.")
     @Test
     void createStation() {
-        StationRequest stationRequest = new StationRequest("강남역");
-
-        ExtractableResponse<Response> response = requestPostStation(stationRequest);
+        ExtractableResponse<Response> response = GANGNAM.requestSave();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.header("Location")).isNotBlank();
@@ -34,10 +36,9 @@ public class StationAcceptanceTest extends AcceptanceTest {
     @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역 생성을 요청하면 400 BAD REQUEST를 반환한다.")
     @Test
     void createStationWithDuplicateName() {
-        StationRequest stationRequest = new StationRequest("강남역");
-        requestPostStation(stationRequest);
+        GANGNAM.requestSave();
 
-        ExtractableResponse<Response> response = requestPostStation(stationRequest);
+        ExtractableResponse<Response> response = GANGNAM.requestSave();
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         assertThat(response.body().asString()).isEqualTo("이미 존재하는 역 이름입니다.");
@@ -46,39 +47,26 @@ public class StationAcceptanceTest extends AcceptanceTest {
     @DisplayName("전체 역을 조회를 요청하면 등록된 강남역과 역삼역 정보를 조회하고 200 OK를 반환한다.")
     @Test
     void getStations() {
-        StationRequest stationRequest = new StationRequest("강남역");
-        ExtractableResponse<Response> createResponse1 = requestPostStation(stationRequest);
-        stationRequest = new StationRequest("역삼역");
-        ExtractableResponse<Response> createResponse2 = requestPostStation(stationRequest);
+        Station gangnam = GANGNAM.save();
+        Station yeoksam = YEOKSAM.save();
 
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .get("/stations")
-                .then().log().all()
-                .extract();
+        ExtractableResponse<Response> response = get("/stations");
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        List<Long> expectedLineIds = Stream.of(createResponse1, createResponse2)
-                .map(it -> Long.parseLong(it.header("Location").split("/")[2]))
-                .collect(Collectors.toList());
-        List<Long> resultLineIds = response.jsonPath().getList(".", StationResponse.class).stream()
+        List<Long> resultLineIds = response.jsonPath()
+                .getList(".", StationResponse.class).stream()
                 .map(StationResponse::getId)
                 .collect(Collectors.toList());
-        assertThat(resultLineIds).containsAll(expectedLineIds);
+        assertThat(resultLineIds).containsOnly(gangnam.getId(), yeoksam.getId());
     }
 
     @DisplayName("등록되어있는 역의 id를 URI에 담아서 삭제 요청을 하면 204 NO CONTENT를 반환한다.")
     @Test
     void deleteStation() {
-        StationRequest stationRequest = new StationRequest("강남역");
-        ExtractableResponse<Response> createResponse = requestPostStation(stationRequest);
+        String uri = GANGNAM.requestSave()
+                .header("Location");
 
-        String uri = createResponse.header("Location");
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .delete(uri)
-                .then().log().all()
-                .extract();
+        ExtractableResponse<Response> response = delete(uri);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
@@ -86,16 +74,11 @@ public class StationAcceptanceTest extends AcceptanceTest {
     @DisplayName("해당 역을 포함하고 있는 구간이 있을 경우, 삭제 요청을 하면 400 BAD REQUEST를 반환한다.")
     @Test
     void notAllowDeleteStation() {
-        Long gangnamId = requestPostStationAndReturnId(new StationRequest("강남역"));
-        Long yeoksamId = requestPostStationAndReturnId(new StationRequest("역삼역"));
-        LineCreateRequest lineCreateRequest = new LineCreateRequest("2호선", "초록색", gangnamId, yeoksamId, 1, 0);
-        requestPostLine(lineCreateRequest);
+        Station gangnam = GANGNAM.save();
+        Station yeoksam = YEOKSAM.save();
+        LINE_2.requestSave(gangnam, yeoksam, 1);
 
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .delete("/stations/" + gangnamId)
-                .then().log().all()
-                .extract();
+        ExtractableResponse<Response> response = delete("/stations/" + gangnam.getId());
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
@@ -106,7 +89,7 @@ public class StationAcceptanceTest extends AcceptanceTest {
     void notAllowNullOrBlankName(String name) {
         StationRequest stationRequest = new StationRequest(name);
 
-        ExtractableResponse<Response> response = requestPostStation(stationRequest);
+        ExtractableResponse<Response> response = post(stationRequest, "/stations");
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         assertThat(response.body().asString()).contains("빈 값일 수 없습니다.");
