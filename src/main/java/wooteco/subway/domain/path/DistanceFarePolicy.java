5f1bc058ef20d61448b0dafa40b33ed61a159767
SuyeonChoi <tndus970120@gmail.com>
2022-05-26 19:16:23 +0900

    refactor(Path): 거리에 대한 요금을 측정하는 로직을 Enum 클래스로 분리

---
+++ src/main/java/wooteco/subway/domain/path/DistanceFarePolicy.java
@@ -0,0 +1,79 @@
+package wooteco.subway.domain.path;
+
+import static wooteco.subway.domain.path.DistanceFarePolicy.Constants.BASIC_CHARGE;
+import static wooteco.subway.domain.path.DistanceFarePolicy.Constants.FIRST_ADDITIONAL_INTERVAL;
+import static wooteco.subway.domain.path.DistanceFarePolicy.Constants.FIRST_INTERVAL_UNIT;
+import static wooteco.subway.domain.path.DistanceFarePolicy.Constants.OVER_FARE;
+import static wooteco.subway.domain.path.DistanceFarePolicy.Constants.SECOND_ADDITIONAL_INTERVAL;
+import static wooteco.subway.domain.path.DistanceFarePolicy.Constants.SECOND_INTERVAL_UNIT;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+public enum DistanceFarePolicy {
+    BASIC(DistanceFarePolicy::isDefaultInterval, distance -> BASIC_CHARGE),
+    FIRST_INTERVAL(DistanceFarePolicy::isFirstInterval, DistanceFarePolicy::calculateFirstIntervalFare),
+    SECOND_INTERVAL(DistanceFarePolicy::isSecondInterval, DistanceFarePolicy::calculateSecondIntervalFare);
+
+    private final Predicate<Integer> distanceCondition;
+
+    private final UnaryOperator<Integer> distancePolicy;
+
+    DistanceFarePolicy(Predicate<Integer> distanceCondition, UnaryOperator<Integer> distancePolicy) {
+        this.distanceCondition = distanceCondition;
+        this.distancePolicy = distancePolicy;
+    }
+
+    private static boolean isDefaultInterval(Integer distance) {
+        return distance <= FIRST_ADDITIONAL_INTERVAL;
+    }
+
+    private static boolean isFirstInterval(Integer distance) {
+        return FIRST_ADDITIONAL_INTERVAL < distance && distance <= SECOND_ADDITIONAL_INTERVAL;
+    }
+
+    private static boolean isSecondInterval(Integer distance) {
+        return SECOND_ADDITIONAL_INTERVAL < distance;
+    }
+
+    private static int calculateFirstIntervalFare(int distance) {
+        return BASIC_CHARGE + calculateFareOverFirstDistance(distance);
+    }
+
+    private static int calculateSecondIntervalFare(Integer distance) {
+        return BASIC_CHARGE + calculateFareOverFirstDistance(SECOND_ADDITIONAL_INTERVAL)
+                + calculateFareOverSecondDistance(distance);
+    }
+
+    private static int calculateFareOverFirstDistance(int distance) {
+        return (int) (Math.ceil((distance - FIRST_ADDITIONAL_INTERVAL) / FIRST_INTERVAL_UNIT) * OVER_FARE);
+    }
+
+    private static int calculateFareOverSecondDistance(int distance) {
+        return (int) (Math.ceil((distance - SECOND_ADDITIONAL_INTERVAL) / SECOND_INTERVAL_UNIT) * OVER_FARE);
+    }
+
+    public static DistanceFarePolicy of(int distance) {
+        return Arrays.stream(values())
+                .filter(it -> it.distanceCondition.test(distance))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("거리를 판단할 수 없습니다."));
+    }
+
+    public int calculate(int distance) {
+        return this.distancePolicy.apply(distance);
+    }
+
+    static class Constants {
+        private Constants() {
+        }
+
+        static final int BASIC_CHARGE = 1250;
+        static final int OVER_FARE = 100;
+        static final int FIRST_ADDITIONAL_INTERVAL = 10;
+        static final int SECOND_ADDITIONAL_INTERVAL = 50;
+        static final double FIRST_INTERVAL_UNIT = 5.0;
+        static final double SECOND_INTERVAL_UNIT = 8.0;
+    }
+}
--- src/main/java/wooteco/subway/domain/path/Path.java
+++ src/main/java/wooteco/subway/domain/path/Path.java
@@ -4,13 +4,6 @@ import java.util.Collections;
 import java.util.Set;
 
 public class Path {
-    private static final int OVER_FARE = 100;
-    private static final int BASIC_FARE = 1250;
-    private static final int FIRST_ADDITIONAL_INTERVAL = 10;
-    private static final int SECOND_ADDITIONAL_INTERVAL = 50;
-    private static final double FIRST_INTERVAL_UNIT = 5.0;
-    private static final double SECOND_INTERVAL_UNIT = 8.0;
-
     private final int distance;
     private final Set<Long> lineIds;
     private final Set<Long> stationIds;
@@ -22,25 +15,11 @@ public class Path {
     }
 
     public int calculateFare(int age, int extraCharge) {
+        DistanceFarePolicy distanceFarePolicy = DistanceFarePolicy.of(distance);
+        int fare = distanceFarePolicy.calculate(distance) + extraCharge;
+
         AgeDiscountPolicy discount = AgeDiscountPolicy.of(age);
-        if (distance <= FIRST_ADDITIONAL_INTERVAL) {
-            return discount.apply(BASIC_FARE + extraCharge);
-        }
-        if (distance <= SECOND_ADDITIONAL_INTERVAL) {
-            return discount.apply(BASIC_FARE + calculateFareOverFirstDistance(distance) + extraCharge);
-        }
-        return discount.apply(BASIC_FARE
-                + calculateFareOverFirstDistance(SECOND_ADDITIONAL_INTERVAL)
-                + calculateFareOverSecondDistance()
-                + extraCharge);
-    }
-
-    private int calculateFareOverFirstDistance(int distance) {
-        return (int) (Math.ceil((distance - FIRST_ADDITIONAL_INTERVAL) / FIRST_INTERVAL_UNIT) * OVER_FARE);
-    }
-
-    private int calculateFareOverSecondDistance() {
-        return (int) (Math.ceil((distance - SECOND_ADDITIONAL_INTERVAL) / SECOND_INTERVAL_UNIT) * OVER_FARE);
+        return discount.apply(fare);
     }
 
     public int getDistance() {
