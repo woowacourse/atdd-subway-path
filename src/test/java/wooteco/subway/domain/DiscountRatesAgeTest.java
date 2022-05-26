7647e9c8f43d1fbd38077094dce1fff91bc19cb8
RunaNam <nmj5012@naver.com>
2022-05-26 23:59:50 +0900

    test: 예외상황 추가 및 테스트 메시지 수정

--- src/test/java/wooteco/subway/domain/DiscountRatesAgeTest.java
+++ src/test/java/wooteco/subway/domain/DiscountRatesAgeTest.java
@@ -1,39 +1,44 @@
 package wooteco.subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class DiscountRatesAgeTest {
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}을 입력하면 어린이 요금제도를 생성한다.")
     @ValueSource(ints = {6, 10, 12})
-    @DisplayName("6세 이상, 13세 미만이면 어린이 요금제도를 생성한다")
-    void DiscountRatesAgeChild(int age){
+    void DiscountRatesAgeChild(int age) {
         //when
         DiscountRatesAge discountRatesAge = DiscountRatesAge.valueOf(age);
         //then
         assertThat(discountRatesAge).isEqualTo(DiscountRatesAge.CHILD);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}을 입력하면 청소년 요금제도를 생성한다.")
     @ValueSource(ints = {13, 18, 15})
-    @DisplayName("13세 이상, 19세 미만이면 청소년 요금제도를 생성한다")
-    void DiscountRatesAgeTeenager(int age){
+    void DiscountRatesAgeTeenager(int age) {
         //when
         DiscountRatesAge discountRatesAge = DiscountRatesAge.valueOf(age);
         //then
         assertThat(discountRatesAge).isEqualTo(DiscountRatesAge.TEENAGER);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}을 입력하면 성인 요금제도를 생성한다.")
     @ValueSource(ints = {19, 24})
-    @DisplayName("19세 이상이면 성인 요금제도를 생성한다")
     void DiscountRatesAgeAdult(int age){
         //when
         DiscountRatesAge discountRatesAge = DiscountRatesAge.valueOf(age);
         //then
         assertThat(discountRatesAge).isEqualTo(DiscountRatesAge.ADULT);
+    }
+
+    @ParameterizedTest(name = "{0}을 입력하면 에러를 발생시킨다.")
+    @ValueSource(ints = {0, -1})
+    void DiscountRatesAgeError(int age) {
+        assertThatThrownBy(() -> DiscountRatesAge.valueOf(age))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("적절한 연령이 입력되지 않았습니다.");
     }
 }
--- src/test/java/wooteco/subway/domain/DistanceFareTest.java
+++ src/test/java/wooteco/subway/domain/DistanceFareTest.java
@@ -1,39 +1,44 @@
 package wooteco.subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class DistanceFareTest {
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}을 입력하면 기본 거리 요금을 측정한다.")
     @ValueSource(ints = {9, 10, 1})
-    @DisplayName("10km 이하면 기본 거리 요금을 측정한다.")
-    void DistanceFareBasic(int distance){
+    void DistanceFareBasic(int distance) {
         //when
         DistanceFare distanceFare = DistanceFare.valueOf(distance);
         //then
         assertThat(distanceFare).isEqualTo(DistanceFare.BASIC_DISTANCE);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}을 입력하면 중거리 요금을 측정한다.")
     @ValueSource(ints = {11, 25, 50})
-    @DisplayName("10km 초과, 50km 이하면 중거리 요금을 측정한다.")
-    void DistanceFareMiddle(int distance){
+    void DistanceFareMiddle(int distance) {
         //when
         DistanceFare distanceFare = DistanceFare.valueOf(distance);
         //then
         assertThat(distanceFare).isEqualTo(DistanceFare.MIDDLE_DISTANCE);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}을 입력하면 장거리 요금을 측정한다.")
     @ValueSource(ints = {51, 70})
-    @DisplayName("50km 초과면 장거리 요금을 측정한다.")
-    void DistanceFareLong(int distance){
+    void DistanceFareLong(int distance) {
         //when
         DistanceFare distanceFare = DistanceFare.valueOf(distance);
         //then
         assertThat(distanceFare).isEqualTo(DistanceFare.LONG_DISTANCE);
+    }
+
+    @ParameterizedTest(name = "{0}을 입력하면 에러를 발생시킨다.")
+    @ValueSource(ints = {0, -1})
+    void DistanceFareError(int distance) {
+        assertThatThrownBy(() -> DistanceFare.valueOf(distance))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력받은 거리값에 따른 요금 정책을 찾을 수 없습니다.");
     }
 }
