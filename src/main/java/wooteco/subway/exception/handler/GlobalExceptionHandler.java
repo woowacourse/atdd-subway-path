473de06aed207dfca6face2f9be484ed8b53a472
progress0407 <progress0407@gmail.com>
2022-05-30 16:11:04 +0900

    refactor(GlobalExceptionHandler): `DefaultHandlerExceptionResolver`를 수동 정의한 것에서 `ResponseEntityExceptionHandler` 상속으로 예외 처리 핸들링 추상화

--- src/main/java/wooteco/subway/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/wooteco/subway/exception/handler/GlobalExceptionHandler.java
@@ -3,9 +3,11 @@ package wooteco.subway.exception.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
-import org.springframework.validation.BindException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -14,9 +16,11 @@ import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import wooteco.subway.exception.constant.ClientException;
 import wooteco.subway.exception.constant.NotExistException;
 import wooteco.subway.exception.dto.ErrorResult;
@@ -25,7 +29,7 @@ import static org.springframework.http.HttpStatus.*;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NotExistException.class)
@@ -47,58 +51,14 @@ public class GlobalExceptionHandler {
         return new ErrorResult(INTERNAL_SERVER_ERROR, "오류가 발생했습니다.");
     }
 
-    /**
-     * Spring 의 DefaultHandlerExceptionResolver 정책
-     */
-
-    @ResponseStatus(BAD_REQUEST)
-    @ExceptionHandler({
-            TypeMismatchException.class,
-            HttpMessageNotReadableException.class,
-            HttpMessageNotWritableException.class,
-            MissingServletRequestPartException.class,
-            BindException.class,
-            ServletRequestBindingException.class,
-            MissingServletRequestParameterException.class
-    })
-    private ErrorResult handleSpringBadRequestException(Exception exception) {
-        logError(exception);
-        return new ErrorResult(BAD_REQUEST, "오류가 발생했습니다.");
-    }
-
-    @ResponseStatus(NOT_FOUND)
-    @ExceptionHandler(NoHandlerFoundException.class)
-    private ErrorResult handleSpringNotFoundException(Exception exception) {
-        logError(exception);
-        return new ErrorResult(NOT_FOUND, "오류가 발생했습니다.");
-    }
-
-    @ResponseStatus(METHOD_NOT_ALLOWED)
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    private ErrorResult handleSpringMethodNotAllowedException(Exception exception) {
-        logError(exception);
-        return new ErrorResult(METHOD_NOT_ALLOWED, "오류가 발생했습니다.");
-    }
-
-    @ResponseStatus(NOT_ACCEPTABLE)
-    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-    private ErrorResult handleSpringNotAcceptableException(Exception exception) {
-        logError(exception);
-        return new ErrorResult(NOT_ACCEPTABLE, "오류가 발생했습니다.");
-    }
-
-    @ResponseStatus(UNSUPPORTED_MEDIA_TYPE)
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    private ErrorResult handleSpringUnsupportedMediaTypeException(Exception exception) {
-        logError(exception);
-        return new ErrorResult(UNSUPPORTED_MEDIA_TYPE, "오류가 발생했습니다.");
-    }
-
-    @ResponseStatus(SERVICE_UNAVAILABLE)
-    @ExceptionHandler(AsyncRequestTimeoutException.class)
-    private ErrorResult handleSpringServiceUnavailableException(Exception exception) {
-        logError(exception);
-        return new ErrorResult(SERVICE_UNAVAILABLE, "오류가 발생했습니다.");
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex,
+                                                             Object body,
+                                                             HttpHeaders headers,
+                                                             HttpStatus status,
+                                                             WebRequest request) {
+        return ResponseEntity.status(status)
+                .body(new ErrorResult(status, "오류가 발생했습니다."));
     }
 
     private static void logError(Exception exception) {
