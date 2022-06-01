08e39fd6757408d956fb6ec9f17b2f8f8f2db968
YJGwon <yj970125@gmail.com>
2022-06-01 21:40:25 +0900

    test(AgeTest): 값 비교 method test 추가

--- src/test/java/wooteco/subway/domain/path/AgeTest.java
+++ src/test/java/wooteco/subway/domain/path/AgeTest.java
@@ -1,9 +1,12 @@
 package wooteco.subway.domain.path;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class AgeTest {
 
@@ -13,5 +16,19 @@ public class AgeTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Age(-1))
                 .withMessageContaining("음수");
+    }
+
+    @ParameterizedTest(name = "나이가 {0}살 이상이면 {1}를 반환한다")
+    @CsvSource({"12, true", "13, true", "14, false"})
+    void isSameOrBiggerThan(int comparing, boolean expected) {
+        Age age = new Age(13);
+        assertThat(age.isSameOrBiggerThan(comparing)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "나이가 {0}살 미만이면 {1}를 반환한다")
+    @CsvSource({"12, false", "13, false", "14, true"})
+    void isSmallerThan(int comparing, boolean expected) {
+        Age age = new Age(13);
+        assertThat(age.isSmallerThan(comparing)).isEqualTo(expected);
     }
 }
